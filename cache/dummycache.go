cc2ec18cfd2e7eb14b4e697b2b7e30b4afb12500
Andrew Sweeney <asweeney@appnexus.com>
2017-05-01 21:29:59 -0400

    Config driven datacache selection and configuration
    - Add support for loading dummy, postgres, and filecache via config
    - Add tests for dummycache and filecache
    - Minor cleanup of the datacache loading/config options

--- cache/dummycache.go
+++ cache/dummycache.go
@@ -1,20 +1,27 @@
 package cache
 
+import "fmt"
+
+// DummyCache dummy config that will echo back results
 type DummyCache struct {
 }
 
+// NewDummyCache create new config
 func NewDummyCache() *DummyCache {
 
 	return &DummyCache{}
 }
 
+// Close nop
 func (c *DummyCache) Close() {
 }
 
+// GetConfig not supported, always returns and error
 func (c *DummyCache) GetConfig(key string) (string, error) {
-	return "", nil
+	return "", fmt.Errorf("Not supported")
 }
 
+// GetDomain echos back the domain
 func (c *DummyCache) GetDomain(key string) (*Domain, error) {
 
 	d := &Domain{
@@ -23,6 +30,7 @@ func (c *DummyCache) GetDomain(key string) (*Domain, error) {
 	return d, nil
 }
 
+// GetAccount echos back the account
 func (c *DummyCache) GetAccount(key string) (*Account, error) {
 
 	d := &Account{
