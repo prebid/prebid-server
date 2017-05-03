d504f20854f6af45252499a36d3f2bc3da440393
Andrew Sweeney <asweeney@appnexus.com>
2017-05-03 09:35:07 -0400

    Add basic postgres cache tests

---
+++ cache/postgres_test.go
@@ -0,0 +1,30 @@
+package cache
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPostgresConfig(t *testing.T) {
+	conf := PostgresDataCacheConfig{
+		Host:     "host",
+		Port:     1234,
+		Dbname:   "dbname",
+		User:     "user",
+		Password: "password",
+		TTL:      3434,
+		Size:     100,
+	}
+
+	u := conf.uri()
+	fmt.Println("u")
+
+	assert.True(t, strings.Contains(u, "host=host"))
+	assert.True(t, strings.Contains(u, "port=1234"))
+	assert.True(t, strings.Contains(u, "dbname=dbname"))
+	assert.True(t, strings.Contains(u, "user=user"))
+	assert.True(t, strings.Contains(u, "password=password"))
+}
