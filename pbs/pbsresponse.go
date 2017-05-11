dec485c7aa486c66b116b0f85b40270d22b70912
Brian O'Kelley <bokelley@appnexus.com>
2017-05-11 18:01:10 -0400

    fix formatting

--- pbs/pbsresponse.go
+++ pbs/pbsresponse.go
@@ -13,7 +13,7 @@ type PBSBid struct {
 	Width       uint64  `json:"width,omitempty"`
 	Height      uint64  `json:"height,omitempty"`
 	DealId      string  `json:"deal_id,omitempty"`
-	CacheID     string  `json""cache_id,omitempty`
+	CacheID     string  `json:"cache_id,omitempty"`
 }
 
 type PBSBidSlice []*PBSBid
