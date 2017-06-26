dc5bae15a1852e977836e67f582aacb97d58624b
Dave Bemiller <dbemiller@appnexus.com>
2017-06-26 16:37:23 -0400

    Documented the adapter.go file, to the best of my knowledge.

--- adapters/adapter.go
+++ adapters/adapter.go
@@ -9,18 +9,26 @@ import (
 	"time"
 )
 
+// Adapters connect prebid-server to a demand partner. Their primary purpose is to produce bids
+// in response to Auction requests.
 type Adapter interface {
+	// A name which uniquely identifies this adapter.
+	// This must not overlap with any other adapters in prebid-server.
 	Name() string
 	FamilyName() string
 	GetUsersyncInfo() *pbs.UsersyncInfo
+	// Produce bids which should be considered, given the auction params.
 	Call(ctx context.Context, req *pbs.PBSRequest, bidder *pbs.PBSBidder) (pbs.PBSBidSlice, error)
 }
 
+// HTTPAdapterConfig groups options which control how HTTP requests are made by adapters.
 type HTTPAdapterConfig struct {
-	IdleConnTimeout     time.Duration
-	MaxConns            int
-	MaxIdleConnsPerHost int
-	MaxConnsPerHost     int
+	// See IdleConnTimeout on https://golang.org/pkg/net/http/#Transport
+	IdleConnTimeout time.Duration
+	// See MaxIdleConns on https://golang.org/pkg/net/http/#Transport
+	MaxConns int
+	// See MaxIdleConnsPerHost on https://golang.org/pkg/net/http/#Transport
+	MaxConnsPerHost int
 }
 
 type HTTPAdapter struct {
@@ -28,13 +36,15 @@ type HTTPAdapter struct {
 	Client    *http.Client
 }
 
+// DefaultHTTPAdapterConfig is an HTTPAdapterConfig that chooses sensible default values.
 var DefaultHTTPAdapterConfig = &HTTPAdapterConfig{
-	MaxConns:            50,
-	MaxConnsPerHost:     10,
-	MaxIdleConnsPerHost: 3,
-	IdleConnTimeout:     60 * time.Second,
+	MaxConns:        50,
+	MaxConnsPerHost: 10,
+	IdleConnTimeout: 60 * time.Second,
 }
 
+// NewHTTPAdapter creates an HTTPAdapter which obeys the rules given by the config, and
+// has all the available SSL certs available in the project.
 func NewHTTPAdapter(c *HTTPAdapterConfig) *HTTPAdapter {
 	ts := &http.Transport{
 		MaxIdleConns:        c.MaxConns,
