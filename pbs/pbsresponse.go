ea2a49ca24c876068742f78e26792bb286c9745f
Dave Bemiller <dbemiller@appnexus.com>
2017-06-26 17:13:17 -0400

    Documented the pbsresponse file with my best guesses for what things mean.

--- pbs/pbsresponse.go
+++ pbs/pbsresponse.go
@@ -1,26 +1,39 @@
 package pbs
 
+// PBSBid is a bid from the auction. These are produced by Adapters, and target a particular Ad Unit.
 type PBSBid struct {
-	BidID             string            `json:"bid_id"`
-	AdUnitCode        string            `json:"code"`
-	Creative_id       string            `json:"creative_id,omitempty"`
-	BidderCode        string            `json:"bidder"`
-	BidHash           string            `json:"-"` // this is the hash of the bidder's unique bid identifier for blockchain. Should not be sent to browser.
-	Price             float64           `json:"price"`
-	Currency          string            `json:"currency,omitempty"`
-	NURL              string            `json:"nurl,omitempty"`
-	Adm               string            `json:"adm,omitempty"`
-	Width             uint64            `json:"width,omitempty"`
-	Height            uint64            `json:"height,omitempty"`
-	DealId            string            `json:"deal_id,omitempty"`
-	CacheID           string            `json:"cache_id,omitempty"`
+	// BidID identifies the Bid Request within the Ad Unit which this Bid targets. It should match one of
+	// the values inside PBSRequest.AdUnits[i].Bids[j].BidID.
+	BidID string `json:"bid_id"`
+	// AdUnitCode identifies the AdUnit which this Bid targets.
+	// It should match one of PBSRequest.AdUnits[i].Code, where "i" matches the AdUnit used in
+	// as BidID.
+	AdUnitCode  string `json:"code"`
+	Creative_id string `json:"creative_id,omitempty"`
+	BidderCode  string `json:"bidder"`
+	// BidHash is the hash of the bidder's unique bid identifier for blockchain. It should not be sent to browser.
+	BidHash string `json:"-"`
+	// Price is the cpm, in US Dollars, which the bidder is willing to pay if this bid is chosen.
+	// TODO: Add support for other currencies someday.
+	Price float64 `json:"price"`
+	NURL  string  `json:"nurl,omitempty"`
+	// Adm is the payload which should be used to deliver the ad, if this bid is chosen.
+	Adm string `json:"adm,omitempty"`
+	// Width is the intended width which Adm should be shown, in pixels.
+	Width uint64 `json:"width,omitempty"`
+	// Height is the intended width which Adm should be shown, in pixels.
+	Height  uint64 `json:"height,omitempty"`
+	DealId  string `json:"deal_id,omitempty"`
+	CacheID string `json:"cache_id,omitempty"`
+	// ResponseTime shows the
 	ResponseTime      int               `json:"response_time_ms,omitempty"`
 	AdServerTargeting map[string]string `json:"ad_server_targeting,omitempty"`
 }
 
+// PBSBidSlice attaches the methods of sort.Interface to []PBSBid, ordering them by response times.
+// For more information, see https://golang.org/pkg/sort/#Interface
 type PBSBidSlice []*PBSBid
 
-// Implement sort.Interface
 func (bids PBSBidSlice) Len() int {
 	return len(bids)
 }
