a04f0ec0d3edbe20bf6a5e657417b541a4708033
Dave Bemiller <dbemiller@appnexus.com>
2017-08-10 14:25:39 -0400

    Removed some dead code.

--- pbs_light.go
+++ pbs_light.go
@@ -201,7 +201,6 @@ func cookieSync(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 	enc := json.NewEncoder(w)
 	enc.SetEscapeHTML(false)
-	//enc.SetIndent("", "  ")
 	enc.Encode(csResp)
 }
 
