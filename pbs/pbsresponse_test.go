70133037c3adc51c1f4e4d3f8caf4aff07198dd8
Jason Snellbaker <jsnellbaker@appnexus.com>
2017-08-22 12:39:12 -0400

    second test with file in sub-folder

--- pbs/pbsresponse_test.go
+++ pbs/pbsresponse_test.go
@@ -24,19 +24,19 @@ func TestSortBids(t *testing.T) {
 		BidderCode: "testBidderCode",
 		Price:      2.0,
 	}
-	bid4 := PBSBid{
+	bid4 :=PBSBid{
 		BidID:      "testBidId",
 		AdUnitCode: "testAdUnitCode",
 		BidderCode: "testBidderCode",
 		Price:      0.50,
 	}
 
-	bids := make(PBSBidSlice, 0)
+	bids :=  make(PBSBidSlice, 0)
 	bids = append(bids, &bid1, &bid2, &bid3, &bid4)
 
 	sort.Sort(bids)
 	if bids[0].Price != 4.0 {
-		t.Error("Expected 4.00 to be highest price")
+		t.Error( "Expected 4.00 to be highest price")
 	}
 	if bids[1].Price != 2.0 {
 		t.Error("Expected 2.00 to be second highest price")
