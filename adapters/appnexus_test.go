f5b380b9b5d97852ea27a7785100ef684ece1b74
Pieter de Zwart <pdezwart@gmail.com>
2017-08-28 11:38:44 -0700

    Rubicon Project: Pass server-defined targeting through

--- adapters/appnexus_test.go
+++ adapters/appnexus_test.go
@@ -45,6 +45,14 @@ type anBidInfo struct {
 	delay     time.Duration
 }
 
+type rpExtensionTargetingObject struct {
+	rp rpTargetingObject
+}
+
+type rpTargetingObject struct {
+	targeting []string
+}
+
 var andata anBidInfo
 
 func DummyAppNexusServer(w http.ResponseWriter, r *http.Request) {
