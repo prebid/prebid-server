d08ee89c0e767c0ba2a11445758e4397995a3f84
Matt Kendall <mkendall@appnexus.com>
2017-08-30 17:00:39 -0400

    update to accept dollars instead of cents per fb endpoint update

--- adapters/facebook.go
+++ adapters/facebook.go
@@ -97,7 +97,7 @@ func (a *FacebookAdapter) callOne(ctx context.Context, req *pbs.PBSRequest, reqJ
 
 	result.bid = &pbs.PBSBid{
 		AdUnitCode: bid.ImpID,
-		Price:      bid.Price / 100, // convert from cents to dollars
+		Price:      bid.Price,
 		Adm:        bid.AdM,
 		Width:      300, // hard code as it's all FB supports
 		Height:     250, // hard code as it's all FB supports
