72104fce5c011d27a847e428ee00dc2862ec124a
jaydeep.mohite <jaydeep.mohite@pubmatic.com>
2017-09-20 22:23:08 +0530

    Updated for PubMatic Adapter related changes

--- adapters/pubmatic.go
+++ adapters/pubmatic.go
@@ -9,11 +9,17 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"sort"
+	"strconv"
+	"strings"
 
+	"github.com/golang/glog"
 	"github.com/mxmCherry/openrtb"
 	"github.com/prebid/prebid-server/pbs"
 	"golang.org/x/net/context/ctxhttp"
 )
+
+const MAX_IMPRESSIONS_PUBMATIC = 30
 
 type PubmaticAdapter struct {
 	http         *HTTPAdapter
@@ -44,36 +50,98 @@ type pubmaticParams struct {
 	AdSlot      string `json:"adSlot"`
 }
 
+func PrepareLogMessage(tID, pubId, adUnitId, bidID, details string, args ...interface{}) string {
+	return fmt.Sprintf("[PUBMATIC] ReqID [%s] PubID [%s] AdUnit [%s] BidID [%s] %s \n",
+		tID, pubId, adUnitId, bidID, details)
+}
+
 func (a *PubmaticAdapter) Call(ctx context.Context, req *pbs.PBSRequest, bidder *pbs.PBSBidder) (pbs.PBSBidSlice, error) {
 	mediaTypes := []pbs.MediaType{pbs.MEDIA_TYPE_BANNER, pbs.MEDIA_TYPE_VIDEO}
 	pbReq, err := makeOpenRTBGeneric(req, bidder, a.FamilyName(), mediaTypes, true)
 
 	if err != nil {
+		glog.Warningf("[PUBMATIC] Failed to make ortb request for request id [%s] \n", pbReq.ID)
 		return nil, err
+	}
+
+	adSlotFlag := false
+	pubId := ""
+	if len(bidder.AdUnits) > MAX_IMPRESSIONS_PUBMATIC {
+		glog.Warningf("[PUBMATIC] First %d impressions will be considered from request tid %s\n",
+			MAX_IMPRESSIONS_PUBMATIC, pbReq.ID)
 	}
 
 	for i, unit := range bidder.AdUnits {
 		var params pubmaticParams
 		err := json.Unmarshal(unit.Params, &params)
 		if err != nil {
-			return nil, err
+			glog.Warningf(PrepareLogMessage(pbReq.ID, params.PublisherId, unit.Code, unit.BidID,
+				fmt.Sprintf("Ignored bid: invalid JSON  [%s] err [%s]", unit.Params, err.Error())))
+			continue
 		}
+
 		if params.PublisherId == "" {
-			return nil, errors.New("Missing publisherId param")
+			glog.Warningf(PrepareLogMessage(pbReq.ID, params.PublisherId, unit.Code, unit.BidID,
+				fmt.Sprintf("Ignored bid: Publisher Id missing")))
+			continue
 		}
+		pubId = params.PublisherId
 		if params.AdSlot == "" {
-			return nil, errors.New("Missing adSlot param")
+			glog.Warningf(PrepareLogMessage(pbReq.ID, params.PublisherId, unit.Code, unit.BidID,
+				fmt.Sprintf("Ignored bid: adSlot missing")))
+			continue
 		}
-		if pbReq.Imp[i].Banner != nil {
-			pbReq.Imp[i].Banner.Format = nil
-		} // pubmatic doesn't support
-		pbReq.Imp[i].TagID = params.AdSlot
+
+		adSlotStr := strings.TrimSpace(params.AdSlot)
+		adSlot := strings.Split(adSlotStr, "@")
+		if len(adSlot) == 2 && adSlot[0] != "" && adSlot[1] != "" {
+
+			if pbReq.Imp[i].Banner != nil {
+				pbReq.Imp[i].Banner.Format = nil // pubmatic doesn't support
+				adSize := strings.Split(strings.ToLower(strings.TrimSpace(adSlot[1])), "x")
+				if len(adSize) == 2 {
+					width, err := strconv.Atoi(strings.TrimSpace(adSize[0]))
+					if err != nil {
+						glog.Warningf(PrepareLogMessage(pbReq.ID, params.PublisherId, unit.Code, unit.BidID,
+							fmt.Sprintf("Ignored bid: invalid adSlot width [%s]", adSize[0])))
+						continue
+					}
+
+					heightStr := strings.Split(strings.TrimSpace(adSize[1]), ":")
+					height, err := strconv.Atoi(strings.TrimSpace(heightStr[0]))
+					if err != nil {
+						glog.Warningf(PrepareLogMessage(pbReq.ID, params.PublisherId, unit.Code, unit.BidID,
+							fmt.Sprintf("Ignored bid: invalid adSlot height [%s]", heightStr[0])))
+						continue
+					}
+
+					pbReq.Imp[i].TagID = strings.TrimSpace(adSlot[0])
+					pbReq.Imp[i].Banner.H = uint64(height)
+					pbReq.Imp[i].Banner.W = uint64(width)
+					adSlotFlag = true
+				} else {
+					glog.Warningf(PrepareLogMessage(pbReq.ID, params.PublisherId, unit.Code, unit.BidID,
+						fmt.Sprintf("Ignored bid: invalid adSize [%s]", adSize)))
+					continue
+				}
+			}
+		} else {
+			glog.Warningf(PrepareLogMessage(pbReq.ID, params.PublisherId, unit.Code, unit.BidID,
+				fmt.Sprintf("Ignored bid: invalid adSlot [%s]", params.AdSlot)))
+			continue
+		}
+
 		if pbReq.Site != nil {
-			pbReq.Site.Publisher = &openrtb.Publisher{ID: params.PublisherId}
+			pbReq.Site.Publisher = &openrtb.Publisher{ID: params.PublisherId, Domain: req.Domain}
 		}
+
 		if pbReq.App != nil {
-			pbReq.App.Publisher = &openrtb.Publisher{ID: params.PublisherId}
+			pbReq.App.Publisher = &openrtb.Publisher{ID: params.PublisherId, Domain: req.Domain}
 		}
+	}
+
+	if !(adSlotFlag) {
+		return nil, errors.New("Incorrect adSlot / Publisher param")
 	}
 
 	reqJSON, err := json.Marshal(pbReq)
@@ -127,7 +195,7 @@ func (a *PubmaticAdapter) Call(ctx context.Context, req *pbs.PBSRequest, bidder 
 		return nil, err
 	}
 
-	bids := make(pbs.PBSBidSlice, 0)
+	bidInfo := make(map[string]pbs.PBSBidSlice, 0)
 
 	numBids := 0
 	for _, sb := range bidResp.SeatBid {
@@ -150,8 +218,24 @@ func (a *PubmaticAdapter) Call(ctx context.Context, req *pbs.PBSRequest, bidder 
 				Height:      bid.H,
 				DealId:      bid.DealID,
 			}
-			bids = append(bids, &pbid)
+			pbid.CreativeMediaType = "banner"
+
+			bidInfo[pbid.AdUnitCode] = append(bidInfo[pbid.AdUnitCode], &pbid)
+
+			glog.Infof("[PUBMATIC] Received Bid for PubID [%s] AdUnit [%s] BidID [%s] Size [%dx%d] Price [%f] \n",
+				pubId, pbid.AdUnitCode, pbid.BidID, pbid.Width, pbid.Height, pbid.Price)
 		}
+	}
+
+	bids := make(pbs.PBSBidSlice, 0)
+	for _, bids_list := range bidInfo {
+
+		if len(bids_list) > 1 {
+			sort.Sort(bids_list)
+		}
+		bids = append(bids, bids_list[0])
+		glog.Infof("[PUBMATIC] Returned Bid for PubID [%s] AdUnit [%s] BidID [%s] Size [%dx%d] Price [%f] \n",
+			pubId, bids_list[0].AdUnitCode, bids_list[0].BidID, bids_list[0].Width, bids_list[0].Height, bids_list[0].Price)
 	}
 
 	return bids, nil
--- adapters/pubmatic_test.go
+++ adapters/pubmatic_test.go
@@ -1,19 +1,80 @@
 package adapters
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 	"time"
 
-	"github.com/prebid/prebid-server/pbs"
-
-	"fmt"
-
 	"github.com/mxmCherry/openrtb"
+	"github.com/prebid/prebid-server/cache/dummycache"
+	"github.com/prebid/prebid-server/pbs"
 )
+
+func CompareStringValue(val1 string, val2 string, t *testing.T) {
+	if val1 != val2 {
+		t.Fatalf(fmt.Sprintf("Expected = %s , Actual = %s", val2, val1))
+	}
+}
+
+func DummyPubMaticServer(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var breq openrtb.BidRequest
+	err = json.Unmarshal(body, &breq)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	resp := openrtb.BidResponse{
+		ID:    breq.ID,
+		BidID: "bidResponse_ID",
+		Cur:   "USD",
+		SeatBid: []openrtb.SeatBid{
+			{
+				Seat: "pubmatic",
+				Bid:  make([]openrtb.Bid, 0),
+			},
+		},
+	}
+	rand.Seed(int64(time.Now().UnixNano()))
+	var bids []openrtb.Bid
+
+	for i, imp := range breq.Imp {
+		bids = append(bids, openrtb.Bid{
+			ID:     fmt.Sprintf("SeatID_%d", i),
+			ImpID:  imp.ID,
+			Price:  float64(int(rand.Float64()*1000)) / 100,
+			AdID:   fmt.Sprintf("adID-%d", i),
+			AdM:    "AdContent",
+			CrID:   fmt.Sprintf("creative-%d", i),
+			W:      imp.Banner.W,
+			H:      imp.Banner.H,
+			DealID: fmt.Sprintf("DealID_%d", i),
+		})
+	}
+	resp.SeatBid[0].Bid = bids
+
+	js, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
 
 func TestPubmaticInvalidCall(t *testing.T) {
 
@@ -56,11 +117,11 @@ func TestPubmaticTimeout(t *testing.T) {
 				MediaTypes: []pbs.MediaType{pbs.MEDIA_TYPE_BANNER},
 				Sizes: []openrtb.Format{
 					{
-						W: 10,
-						H: 12,
+						W: 120,
+						H: 240,
 					},
 				},
-				Params: json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \"12\"}"),
+				Params: json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \"slot@120x240\"}"),
 			},
 		},
 	}
@@ -91,11 +152,11 @@ func TestPubmaticInvalidJson(t *testing.T) {
 				MediaTypes: []pbs.MediaType{pbs.MEDIA_TYPE_BANNER},
 				Sizes: []openrtb.Format{
 					{
-						W: 10,
-						H: 12,
+						W: 120,
+						H: 240,
 					},
 				},
-				Params: json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \"12\"}"),
+				Params: json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \"slot@120x240\"}"),
 			},
 		},
 	}
@@ -127,11 +188,11 @@ func TestPubmaticInvalidStatusCode(t *testing.T) {
 				MediaTypes: []pbs.MediaType{pbs.MEDIA_TYPE_BANNER},
 				Sizes: []openrtb.Format{
 					{
-						W: 10,
-						H: 12,
+						W: 120,
+						H: 240,
 					},
 				},
-				Params: json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \"12\"}"),
+				Params: json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \"slot@120x240\"}"),
 			},
 		},
 	}
@@ -141,18 +202,10 @@ func TestPubmaticInvalidStatusCode(t *testing.T) {
 	}
 }
 
-func TestPubmaticMissingPublisherId(t *testing.T) {
-
-	server := httptest.NewServer(
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// Send 404
-			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-		}),
-	)
-	defer server.Close()
+func TestPubmaticInvalidInputParameters(t *testing.T) {
 
 	conf := *DefaultHTTPAdapterConfig
-	an := NewPubmaticAdapter(&conf, server.URL, "localhost")
+	an := NewPubmaticAdapter(&conf, "http://localhost/test", "localhost")
 	ctx := context.TODO()
 	pbReq := pbs.PBSRequest{}
 	pbBidder := pbs.PBSBidder{
@@ -161,90 +214,96 @@ func TestPubmaticMissingPublisherId(t *testing.T) {
 			{
 				Code:       "unitCode",
 				MediaTypes: []pbs.MediaType{pbs.MEDIA_TYPE_BANNER},
+				BidID:      "bidid",
 				Sizes: []openrtb.Format{
 					{
-						W: 10,
-						H: 12,
+						W: 120,
+						H: 240,
 					},
 				},
-				Params: json.RawMessage("{\"adSlot\": \"12\"}"),
 			},
 		},
 	}
+
+	// Invalid Request JSON
+	pbBidder.AdUnits[0].Params = json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \"slot@120x240\"")
 	_, err := an.Call(ctx, &pbReq, &pbBidder)
-	if err == nil {
-		t.Fatalf("No error received for missing publisherId")
-	}
-}
+	CompareStringValue(err.Error(), "Incorrect adSlot / Publisher param", t)
 
-func TestPubmaticMissingAdSlot(t *testing.T) {
+	// Missing adSlot in AdUnits.Params
+	pbBidder.AdUnits[0].Params = json.RawMessage("{\"publisherId\": \"10\"}")
+	_, err = an.Call(ctx, &pbReq, &pbBidder)
+	CompareStringValue(err.Error(), "Incorrect adSlot / Publisher param", t)
 
-	server := httptest.NewServer(
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// Send 404
-			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-		}),
-	)
-	defer server.Close()
+	// Missing publisher ID
+	pbBidder.AdUnits[0].Params = json.RawMessage("{\"adSlot\": \"slot@120x240\"}")
+	_, err = an.Call(ctx, &pbReq, &pbBidder)
+	CompareStringValue(err.Error(), "Incorrect adSlot / Publisher param", t)
 
-	conf := *DefaultHTTPAdapterConfig
-	an := NewPubmaticAdapter(&conf, server.URL, "localhost")
-	ctx := context.TODO()
-	pbReq := pbs.PBSRequest{}
-	pbBidder := pbs.PBSBidder{
-		BidderCode: "bannerCode",
-		AdUnits: []pbs.PBSAdUnit{
-			{
-				Code:       "unitCode",
-				MediaTypes: []pbs.MediaType{pbs.MEDIA_TYPE_BANNER},
-				Sizes: []openrtb.Format{
-					{
-						W: 10,
-						H: 12,
-					},
-				},
-				Params: json.RawMessage("{\"PublisherId\": \"10\"}"),
-			},
-		},
-	}
-	_, err := an.Call(ctx, &pbReq, &pbBidder)
-	if err == nil {
-		t.Fatalf("No error received for missing adSlot")
-	}
+	// Missing slot name  in AdUnits.Params
+	pbBidder.AdUnits[0].Params = json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \"@120x240\"}")
+	_, err = an.Call(ctx, &pbReq, &pbBidder)
+	CompareStringValue(err.Error(), "Incorrect adSlot / Publisher param", t)
+
+	// Invalid adSize in AdUnits.Params
+	pbBidder.AdUnits[0].Params = json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \"slot@120-240\"}")
+	_, err = an.Call(ctx, &pbReq, &pbBidder)
+	CompareStringValue(err.Error(), "Incorrect adSlot / Publisher param", t)
+
+	// Missing impression width and height in AdUnits.Params
+	pbBidder.AdUnits[0].Params = json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \"slot@\"}")
+	_, err = an.Call(ctx, &pbReq, &pbBidder)
+	CompareStringValue(err.Error(), "Incorrect adSlot / Publisher param", t)
+
+	// Missing height  in AdUnits.Params
+	pbBidder.AdUnits[0].Params = json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \"slot@120\"}")
+	_, err = an.Call(ctx, &pbReq, &pbBidder)
+	CompareStringValue(err.Error(), "Incorrect adSlot / Publisher param", t)
+
+	pbBidder.AdUnits[0].Params = json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \"slot@120x\"}")
+	_, err = an.Call(ctx, &pbReq, &pbBidder)
+	CompareStringValue(err.Error(), "Incorrect adSlot / Publisher param", t)
+
+	// Missing width  in AdUnits.Params
+	pbBidder.AdUnits[0].Params = json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \"slot@x120\"}")
+	_, err = an.Call(ctx, &pbReq, &pbBidder)
+	CompareStringValue(err.Error(), "Incorrect adSlot / Publisher param", t)
+
+	// Incorrect width param  in AdUnits.Params
+	pbBidder.AdUnits[0].Params = json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \"slot@valx120\"}")
+	_, err = an.Call(ctx, &pbReq, &pbBidder)
+	CompareStringValue(err.Error(), "Incorrect adSlot / Publisher param", t)
+
+	// Incorrect height param  in AdUnits.Params
+	pbBidder.AdUnits[0].Params = json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \"slot@120xval\"}")
+	_, err = an.Call(ctx, &pbReq, &pbBidder)
+	CompareStringValue(err.Error(), "Incorrect adSlot / Publisher param", t)
+
+	// Empty slot name in AdUnits.Params
+	pbBidder.AdUnits[0].Params = json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \" @120x240\"}")
+	_, err = an.Call(ctx, &pbReq, &pbBidder)
+	CompareStringValue(err.Error(), "Incorrect adSlot / Publisher param", t)
+
+	// Empty width in AdUnits.Params
+	pbBidder.AdUnits[0].Params = json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \"slot@ x240\"}")
+	_, err = an.Call(ctx, &pbReq, &pbBidder)
+	CompareStringValue(err.Error(), "Incorrect adSlot / Publisher param", t)
+
+	// Empty height in AdUnits.Params
+	pbBidder.AdUnits[0].Params = json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \"slot@120x \"}")
+	_, err = an.Call(ctx, &pbReq, &pbBidder)
+	CompareStringValue(err.Error(), "Incorrect adSlot / Publisher param", t)
+
+	// Empty height in AdUnits.Params
+	pbBidder.AdUnits[0].Params = json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \" @120x \"}")
+	_, err = an.Call(ctx, &pbReq, &pbBidder)
+	CompareStringValue(err.Error(), "Incorrect adSlot / Publisher param", t)
+
 }
 
 func TestPubmaticBasicResponse(t *testing.T) {
 
-	server := httptest.NewServer(
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			resp := openrtb.BidResponse{
-				SeatBid: []openrtb.SeatBid{
-					{
-						Bid: []openrtb.Bid{
-							{
-								ID:     "1234",
-								ImpID:  "unitCode",
-								Price:  1.0,
-								AdM:    "Content",
-								CrID:   "567",
-								W:      10,
-								H:      12,
-								DealID: "5",
-							},
-						},
-					},
-				},
-			}
-
-			js, err := json.Marshal(resp)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			w.Header().Set("Content-Type", "application/json")
-			w.Write(js)
-		}),
-	)
+	server := httptest.NewServer(http.HandlerFunc(DummyPubMaticServer))
 	defer server.Close()
 
 	conf := *DefaultHTTPAdapterConfig
@@ -260,14 +319,154 @@ func TestPubmaticBasicResponse(t *testing.T) {
 				MediaTypes: []pbs.MediaType{pbs.MEDIA_TYPE_BANNER},
 				Sizes: []openrtb.Format{
 					{
-						W: 10,
-						H: 12,
+						W: 336,
+						H: 280,
 					},
 				},
-				Params: json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \"12\"}"),
+				Params: json.RawMessage("{\"publisherId\": \"640\", \"adSlot\": \"slot1@336x280\"}"),
 			},
 		},
 	}
+	pbReq.IsDebug = true
+	bids, err := an.Call(ctx, &pbReq, &pbBidder)
+	if err != nil {
+		t.Fatalf("Should not have gotten an error: %v", err)
+	}
+	if len(bids) != 1 {
+		t.Fatalf("Should have received one bid")
+	}
+}
+
+func TestPubmaticMultiImpressionResponse(t *testing.T) {
+
+	server := httptest.NewServer(http.HandlerFunc(DummyPubMaticServer))
+	defer server.Close()
+
+	conf := *DefaultHTTPAdapterConfig
+	an := NewPubmaticAdapter(&conf, server.URL, "localhost")
+
+	ctx := context.TODO()
+	pbReq := pbs.PBSRequest{}
+	pbBidder := pbs.PBSBidder{
+		BidderCode: "bannerCode",
+		AdUnits: []pbs.PBSAdUnit{
+			{
+				Code:       "unitCode1",
+				MediaTypes: []pbs.MediaType{pbs.MEDIA_TYPE_BANNER},
+				BidID:      "bidid",
+				Sizes: []openrtb.Format{
+					{
+						W: 336,
+						H: 280,
+					},
+				},
+				Params: json.RawMessage("{\"publisherId\": \"640\", \"adSlot\": \"slot1@336x280\"}"),
+			},
+			{
+				Code:       "unitCode1",
+				MediaTypes: []pbs.MediaType{pbs.MEDIA_TYPE_BANNER},
+				BidID:      "bidid",
+				Sizes: []openrtb.Format{
+					{
+						W: 800,
+						H: 200,
+					},
+				},
+				Params: json.RawMessage("{\"publisherId\": \"640\", \"adSlot\": \"slot1@800x200\"}"),
+			},
+		},
+	}
+	bids, err := an.Call(ctx, &pbReq, &pbBidder)
+	if err != nil {
+		t.Fatalf("Should not have gotten an error: %v", err)
+	}
+	if len(bids) != 1 {
+		t.Fatalf("Should have received one bid")
+	}
+}
+
+func TestPubmaticMultiAdUnitResponse(t *testing.T) {
+
+	server := httptest.NewServer(http.HandlerFunc(DummyPubMaticServer))
+	defer server.Close()
+
+	conf := *DefaultHTTPAdapterConfig
+	an := NewPubmaticAdapter(&conf, server.URL, "localhost")
+
+	ctx := context.TODO()
+	pbReq := pbs.PBSRequest{}
+	pbBidder := pbs.PBSBidder{
+		BidderCode: "bannerCode",
+		AdUnits: []pbs.PBSAdUnit{
+			{
+				Code:       "unitCode1",
+				MediaTypes: []pbs.MediaType{pbs.MEDIA_TYPE_BANNER},
+				BidID:      "bidid",
+				Sizes: []openrtb.Format{
+					{
+						W: 336,
+						H: 280,
+					},
+				},
+				Params: json.RawMessage("{\"publisherId\": \"640\", \"adSlot\": \"slot1@336x280\"}"),
+			},
+			{
+				Code:       "unitCode2",
+				MediaTypes: []pbs.MediaType{pbs.MEDIA_TYPE_BANNER},
+				BidID:      "bidid",
+				Sizes: []openrtb.Format{
+					{
+						W: 800,
+						H: 200,
+					},
+				},
+				Params: json.RawMessage("{\"publisherId\": \"640\", \"adSlot\": \"slot1@800x200\"}"),
+			},
+		},
+	}
+	bids, err := an.Call(ctx, &pbReq, &pbBidder)
+	if err != nil {
+		t.Fatalf("Should not have gotten an error: %v", err)
+	}
+	if len(bids) != 2 {
+		t.Fatalf("Should have received one bid")
+	}
+
+}
+
+func TestPubmaticMobileResponse(t *testing.T) {
+
+	server := httptest.NewServer(http.HandlerFunc(DummyPubMaticServer))
+	defer server.Close()
+
+	conf := *DefaultHTTPAdapterConfig
+	an := NewPubmaticAdapter(&conf, server.URL, "localhost")
+
+	ctx := context.TODO()
+	pbReq := pbs.PBSRequest{}
+	pbBidder := pbs.PBSBidder{
+		BidderCode: "bannerCode",
+		AdUnits: []pbs.PBSAdUnit{
+			{
+				Code:       "unitCode",
+				BidID:      "bidid",
+				MediaTypes: []pbs.MediaType{pbs.MEDIA_TYPE_BANNER},
+				Sizes: []openrtb.Format{
+					{
+						W: 336,
+						H: 280,
+					},
+				},
+				Params: json.RawMessage("{\"publisherId\": \"640\", \"adSlot\": \"slot1@336x280\"}"),
+			},
+		},
+	}
+
+	pbReq.App = &openrtb.App{
+		ID:   "com.test",
+		Name: "testApp",
+	}
+
 	bids, err := an.Call(ctx, &pbReq, &pbBidder)
 	if err != nil {
 		t.Fatalf("Should not have gotten an error: %v", err)
@@ -289,5 +488,171 @@ func TestPubmaticUserSyncInfo(t *testing.T) {
 	if an.usersyncInfo.SupportCORS != false {
 		t.Fatalf("should have been false")
 	}
+}
 
+func TestPubmaticInvalidLookupBidIDParameter(t *testing.T) {
+
+	server := httptest.NewServer(http.HandlerFunc(DummyPubMaticServer))
+	defer server.Close()
+
+	conf := *DefaultHTTPAdapterConfig
+	an := NewPubmaticAdapter(&conf, server.URL, "localhost")
+
+	ctx := context.TODO()
+	pbReq := pbs.PBSRequest{}
+	pbBidder := pbs.PBSBidder{
+		BidderCode: "bannerCode",
+		AdUnits: []pbs.PBSAdUnit{
+			{
+				Code:       "unitCode",
+				MediaTypes: []pbs.MediaType{pbs.MEDIA_TYPE_BANNER},
+				Sizes: []openrtb.Format{
+					{
+						W: 120,
+						H: 240,
+					},
+				},
+			},
+		},
+	}
+
+	pbBidder.AdUnits[0].Params = json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \"slot@120x240\"}")
+	_, err := an.Call(ctx, &pbReq, &pbBidder)
+
+	CompareStringValue(err.Error(), "Unknown ad unit code 'unitCode'", t)
+}
+
+func TestPubmaticAdSlotParams(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(DummyPubMaticServer))
+	defer server.Close()
+
+	conf := *DefaultHTTPAdapterConfig
+	an := NewPubmaticAdapter(&conf, server.URL, "localhost")
+
+	ctx := context.TODO()
+	pbReq := pbs.PBSRequest{}
+	pbBidder := pbs.PBSBidder{
+		BidderCode: "bannerCode",
+		AdUnits: []pbs.PBSAdUnit{
+			{
+				Code:       "unitCode",
+				BidID:      "bidid",
+				MediaTypes: []pbs.MediaType{pbs.MEDIA_TYPE_BANNER},
+				Sizes: []openrtb.Format{
+					{
+						W: 120,
+						H: 240,
+					},
+				},
+			},
+		},
+	}
+	pbBidder.AdUnits[0].Params = json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \" slot@120x240\"}")
+	bids, err := an.Call(ctx, &pbReq, &pbBidder)
+	if err != nil && len(bids) != 1 {
+		t.Fatalf("Should not return err")
+	}
+
+	pbBidder.AdUnits[0].Params = json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \"slot @120x240\"}")
+	bids, err = an.Call(ctx, &pbReq, &pbBidder)
+	if err != nil && len(bids) != 1 {
+		t.Fatalf("Should not return err")
+	}
+
+	pbBidder.AdUnits[0].Params = json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \"slot@120x240 \"}")
+	bids, err = an.Call(ctx, &pbReq, &pbBidder)
+	if err != nil && len(bids) != 1 {
+		t.Fatalf("Should not return err")
+	}
+
+	pbBidder.AdUnits[0].Params = json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \"slot@ 120x240\"}")
+	bids, err = an.Call(ctx, &pbReq, &pbBidder)
+	if err != nil && len(bids) != 1 {
+		t.Fatalf("Should not return err")
+	}
+
+	pbBidder.AdUnits[0].Params = json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \"slot@220 x240\"}")
+	bids, err = an.Call(ctx, &pbReq, &pbBidder)
+	if err != nil && len(bids) != 1 {
+		t.Fatalf("Should not return err")
+	}
+
+	pbBidder.AdUnits[0].Params = json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \"slot@120x 240\"}")
+	bids, err = an.Call(ctx, &pbReq, &pbBidder)
+	if err != nil && len(bids) != 1 {
+		t.Fatalf("Should not return err")
+	}
+
+	pbBidder.AdUnits[0].Params = json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \"slot@120x240:1\"}")
+	bids, err = an.Call(ctx, &pbReq, &pbBidder)
+	if err != nil && len(bids) != 1 {
+		t.Fatalf("Should not return err")
+	}
+
+	pbBidder.AdUnits[0].Params = json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \"slot@120x 240:1\"}")
+	bids, err = an.Call(ctx, &pbReq, &pbBidder)
+	if err != nil && len(bids) != 1 {
+		t.Fatalf("Should not return err")
+	}
+
+	pbBidder.AdUnits[0].Params = json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \"slot@120x240 :1\"}")
+	bids, err = an.Call(ctx, &pbReq, &pbBidder)
+	if err != nil && len(bids) != 1 {
+		t.Fatalf("Should not return err")
+	}
+
+	pbBidder.AdUnits[0].Params = json.RawMessage("{\"publisherId\": \"10\", \"adSlot\": \"slot@120x240: 1\"}")
+	bids, err = an.Call(ctx, &pbReq, &pbBidder)
+	if err != nil && len(bids) != 1 {
+		t.Fatalf("Should not return err")
+	}
+}
+
+func TestPubmaticSampleRequest(t *testing.T) {
+
+	server := httptest.NewServer(http.HandlerFunc(DummyPubMaticServer))
+	defer server.Close()
+
+	pbReq := pbs.PBSRequest{
+		AdUnits: make([]pbs.AdUnit, 1),
+	}
+	pbReq.AdUnits[0] = pbs.AdUnit{
+		Code: "adUnit_1",
+		Sizes: []openrtb.Format{
+			{
+				W: 100,
+				H: 120,
+			},
+		},
+		Bids: []pbs.Bids{
+			{
+				BidderCode: "pubmatic",
+				BidID:      "BidID",
+				Params:     json.RawMessage("{\"publisherId\": \"640\", \"adSlot\": \"slot1@100x120\"}"),
+			},
+		},
+	}
+
+	pbReq.IsDebug = true
+
+	body := new(bytes.Buffer)
+	err := json.NewEncoder(body).Encode(pbReq)
+	if err != nil {
+		t.Fatalf("Error when serializing request")
+	}
+
+	httpReq := httptest.NewRequest("POST", server.URL, body)
+	httpReq.Header.Add("Referer", "http://test.com/sports")
+	pc := pbs.ParsePBSCookieFromRequest(httpReq)
+	pc.TrySync("pubmatic", "12345")
+	fakewriter := httptest.NewRecorder()
+	pc.SetCookieOnResponse(fakewriter, "")
+	httpReq.Header.Add("Cookie", fakewriter.Header().Get("Set-Cookie"))
+
+	cacheClient, _ := dummycache.New()
+
+	_, err = pbs.ParsePBSRequest(httpReq, cacheClient)
+	if err != nil {
+		t.Fatalf("Error when parsing request: %v", err)
+	}
 }
