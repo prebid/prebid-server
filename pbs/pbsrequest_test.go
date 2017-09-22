a3a6edb39b973a3413ac8c2d3ec092c69975f9bc
Wei Zhang <wzhang@appnexus.com>
2017-09-22 11:44:21 -0400

    update test messages

--- pbs/pbsrequest_test.go
+++ pbs/pbsrequest_test.go
@@ -547,15 +547,15 @@ func TestCompareVersions(t *testing.T) {
 
 	res = CompareVersions("0.0.3", "0.0.2")
 	if res != 1 {
-		t.Errorf("Wrong comparison of versions, 0.0.1 should be smaller than 0.0.2")
+		t.Errorf("Wrong comparison of versions, 0.0.3 should be greater than 0.0.2")
 	}
 
 	res = CompareVersions("0.0.2", "0.0.2")
 	if res != 0 {
-		t.Errorf("Wrong comparison of versions, 0.0.1 should be smaller than 0.0.2")
+		t.Errorf("Wrong comparison of versions, 0.0.2 should equal 0.0.2")
 	}
 	res = CompareVersions("", "0.0.2")
 	if res != -1 {
-		t.Errorf("Wrong comparison of versions, 0.0.1 should be smaller than 0.0.2")
+		t.Errorf("Wrong comparison of versions, zero value should be smaller than 0.0.2")
 	}
 }
