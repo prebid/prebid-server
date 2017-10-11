5b6571f8099a19e90b6746c2b6230475ce9750e2
Jason Snellbaker <jsnellbaker@appenxus.com>
2017-10-11 10:06:44 -0400

    small fix to the logic

--- scripts/check_coverage.sh
+++ scripts/check_coverage.sh
@@ -18,13 +18,14 @@ trap finish EXIT ERR INT TERM
 OUTPUT=`./scripts/coverage.sh`
 
 while read -r LINE; do
+  echo -e "$LINE"
   if [[ $LINE =~ "%" ]]; then
     PERCENT=$(echo "$LINE"|cut -d: -f2-|cut -d% -f1|cut -d. -f1|tr -d ' ')
     if [[ $PERCENT -lt 20 ]]; then
       CHECKCOV=true
     fi
   fi
-done < "$OUTPUT"
+done <<< "$OUTPUT"
 
 if $CHECKCOV; then
   echo "Detected at least one package had less than 20% code coverage.  Please review results below or from your terminal run ./scripts/coverage.sh --html for more detailed results"
