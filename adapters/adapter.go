7eecef4eaeef28b9fa5066e4492f059d2fd13179
Dave Bemiller <27972385+dbemiller@users.noreply.github.com>
2017-10-31 11:09:49 -0400

    Bidder interface.

--- adapters/adapter.go
+++ adapters/adapter.go
@@ -7,10 +7,46 @@ import (
 	"github.com/prebid/prebid-server/ssl"
 	"net/http"
 	"time"
+	"github.com/mxmCherry/openrtb"
+	"github.com/prebid/prebid-server/openrtb_ext"
 )
 
-// Adapters connect prebid-server to a demand partner. Their primary purpose is to produce bids
-// in response to Auction requests.
+// Bidders participate in prebid-server auctions.
+type Bidder interface {
+	// Bid should return the SeatBid containing all bids used by this bidder.
+	//
+	// All `Ext` fields from the argument request are generated from contract classes in openrtb_ext.
+	// Each bidder may define their own `Ext` format there.
+	//
+	// All `Ext` fields inside the returned SeatBid must also be generated from the contract classes in openrtb_ext.
+	//
+	// Bid should still attempt to return a SeatBid, even if some errors occurred. If there are no bids, return nil.
+	// Errors will be processed by prebid-server core code and logged or reported to the user as appropriate.
+	Bid(ctx context.Context, request *openrtb.BidRequest) *BidderResponse
+}
+
+// BidderResponse carries all the data needed for a Bidder's response.
+type BidderResponse struct {
+	// Bids contains all the bids that the Bidder wants to enter.
+	// This can be nil (for no bids), but should not contain nil elements.
+	Bids []*openrtb.Bid
+	// ServerCalls stores some debugging info.
+	// This is only required if the input request.Test was 1.
+	ServerCalls []*openrtb_ext.ExtServerCall
+	// Errors should contain a list of errors which occurred internally. These should report
+	// any conditions which result in "no" or "subpar" bids. For example:
+	//
+	// 1. The openrtb request needs an ad type which this bidder doesn't support.
+	// 2. The auction timed out before all the bids were entered.
+	// 3. The remote server returned unexpected input.
+	Errors []error
+}
+
+// Adapter is a deprecated interface which connects prebid-server to a demand partner.
+// PBS is currently being rewritten to use Bidder, and this will be removed after.
+// Their primary purpose is to produce bids in response to Auction requests.
+//
+// For the future, see Bidder.
 type Adapter interface {
 	// Name uniquely identifies this adapter. This must be identical to the code in Prebid.js,
 	// but cannot overlap with any other adapters in prebid-server.
