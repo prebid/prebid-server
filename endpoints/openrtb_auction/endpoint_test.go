bde4d61374c0f3a0f6ffe30c7625bdbf0e9c5c15
Dave Bemiller <27972385+dbemiller@users.noreply.github.com>
2017-11-06 12:18:06 -0500

    Added a test for a successful bid.

--- endpoints/openrtb_auction/endpoint_test.go
+++ endpoints/openrtb_auction/endpoint_test.go
@@ -7,7 +7,68 @@ import (
 	"net/http/httptest"
 	"strings"
 	"net/http"
+	"encoding/json"
 )
+
+func TestGoodRequest(t *testing.T) {
+	endpoint := &EndpointDeps{
+		Exchange: &nobidExchange{},
+	}
+
+	reqData := `
+{
+  "id": "some-request-id",
+  "imp": [
+    {
+      "id": "my-imp-id",
+      "banner": {
+    	"format": [
+    	  {
+    	    "w": 300,
+    	    "h": 250
+    	  },
+    	  {
+    	    "w": 300,
+    	    "h": 600
+    	  }
+    	]
+      },
+      "ext": {
+        "appnexus": {
+          "placementId": "10433394"
+        }
+      }
+    }
+  ],
+  "test": 1,
+  "tmax": 500
+}
+	`
+
+	request := httptest.NewRequest("POST", "/openrtb2/auction", strings.NewReader(reqData))
+	recorder := httptest.NewRecorder()
+
+	endpoint.Auction(recorder, request, nil)
+
+	if recorder.Code != http.StatusOK {
+		t.Errorf("Expected status %d. Got %d", http.StatusOK, recorder.Code)
+	}
+
+	var response openrtb.BidResponse
+	if err := json.Unmarshal(recorder.Body.Bytes(), &response); err != nil {
+		t.Fatalf("Error unmarshalling response: %s", err.Error())
+	}
+
+	if response.ID != "some-request-id" {
+		t.Errorf("Bad response.id. Expected %s, got %s.", "some-request-id", response.ID)
+	}
+	if response.BidID != "test bid id" {
+		t.Errorf("Bad response.id. Expected %s, got %s.", "test bid id", response.BidID)
+	}
+	if *response.NBR != openrtb.NoBidReasonCodeUnknownError {
+		t.Errorf("Bad response.nbr. Expected %d, got %d.", openrtb.NoBidReasonCodeUnknownError, response.NBR)
+	}
+}
 
 func TestBadRequestBody(t *testing.T) {
 	endpoint := &EndpointDeps{
