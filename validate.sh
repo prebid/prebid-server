1bacb415943d3a9b2488863f985b0c1f1a84cbcd
Duc Chau <duc.chau@gmail.com>
2017-11-08 12:41:48 -0800

    add support for the vet command, on by default

--- validate.sh
+++ validate.sh
@@ -4,11 +4,13 @@ set -e
 
 AUTOFMT=true
 COVERAGE=false
+VET=true
 
 while true; do
   case "$1" in
      --nofmt ) AUTOFMT=false; shift ;;
      --cov ) COVERAGE=true; shift ;;
+     --novet ) VET=false; shift ;;
      * ) break ;;
   esac
 done
@@ -17,11 +19,12 @@ die() { echo -e "$@" 1>&2 ; exit 1;  }
 
 # check there are no formatting issues
 GOFMT_LINES=`gofmt -l *.go pbs adapters | wc -l | xargs`
+GOGLOB="*.go pbs adapters prebid config prebid_cache_client"
 
 if $AUTOFMT; then
   # if there are files with formatting issues, they will be automatically corrected using the gofmt -w <file> command
   if [[ $GOFMT_LINES -ne 0 ]]; then
-    FMT_FILES=`gofmt -l *.go pbs adapters prebid config | xargs`
+    FMT_FILES=`gofmt -l $GOGLOB | xargs`
     for FILE in $FMT_FILES; do
         echo "Running: gofmt -w $FILE"
         `gofmt -w $FILE`
@@ -35,4 +38,17 @@ if $COVERAGE; then
   ./scripts/check_coverage.sh
 else
   go test $(go list ./... | grep -v /vendor/)
+fi
+
+if $VET; then
+  for SOURCE in $GOGLOB ; do
+    # default call for wildcards and directories
+    COMMAND="go tool vet -source $SOURCE"
+    if [ -f $SOURCE ]; then
+      # file
+      COMMAND="go vet -source $SOURCE"
+    fi
+    echo "Running: $COMMAND"
+    `$COMMAND`
+  done
 fi
