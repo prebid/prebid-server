0ace1be860147c032edbbf61ff5169154662d9fc
Dave Bemiller <27972385+dbemiller@users.noreply.github.com>
2017-11-10 13:59:30 -0500

    Moved the appnexus imp.ext into its own file.

--- openrtb_ext/imp.go
+++ openrtb_ext/imp.go
@@ -11,23 +11,6 @@ type ExtImpPrebid struct {
 	Config *ExtConfig `json:"managedconfig"`
 }
 
-// ExtImpAppnexus defines the contract for bidrequest.imp[i].ext.appnexus
-type ExtImpAppnexus struct {
-	PlacementId       int                     `json:"placementId"`
-	InvCode           string                  `json:"invCode"`
-	Member            string                  `json:"member"`
-	Keywords          []*ExtImpAppnexusKeyVal `json:"keywords"`
-	TrafficSourceCode string                  `json:"trafficSourceCode"`
-	Reserve           float64                 `json:"reserve"`
-	Position          string                  `json:"position"`
-}
-
-// ExtImpAppnexusKeyVal defines the contract for bidrequest.imp[i].ext.appnexus.keywords[i]
-type ExtImpAppnexusKeyVal struct {
-	Key    string   `json:"key,omitempty"`
-	Values []string `json:"value,omitempty"`
-}
-
 // ExtConfig defines the contract for bidrequest.imp[i].ext.prebid.managedconfig
 type ExtConfig struct {
 	ID string `json:"id"`
