ce50eb9c263ca1fb4a0cf8202fa7c0ecf75c74a0
dbemiller <27972385+dbemiller@users.noreply.github.com>
2017-11-27 13:51:58 -0500

    Adapter contact info (#209)
    * Added contact info for adapter maintainers.
    
    * Renamed adapter to bidder.
    
    * Ran gofmts
    
    * Fixed the Rubicon contact info.

---
+++ adapters/bidder_test.go
@@ -0,0 +1,61 @@
+package adapters
+
+import (
+	"errors"
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+// TestInfoFile makes sure that every bidder contains a valid info.yaml file.
+func TestInfoFile(t *testing.T) {
+	fileInfos, err := ioutil.ReadDir(".")
+	if err != nil {
+		t.Fatalf("Failed to read local directory.")
+	}
+
+	for _, fileInfo := range fileInfos {
+		if fileInfo.IsDir() {
+			infoFile, err := os.Open(fmt.Sprintf("./%s/info.yaml", fileInfo.Name()))
+			if err != nil {
+				t.Errorf("Error: %v. Did you forget to add it?", err)
+				continue
+			}
+
+			content, err := ioutil.ReadAll(infoFile)
+			if err != nil {
+				t.Errorf("Failed to read adapters/%s/info.yaml: %s", fileInfo.Name(), err.Error())
+				continue
+			}
+			var fileInfoContent infoFileStructure
+			if err := yaml.Unmarshal(content, &fileInfoContent); err != nil {
+				t.Errorf("Error interpreting content from adapters/%s/info.yaml: %s", fileInfo.Name(), err.Error())
+				continue
+			}
+			if err := fileInfoContent.validate(); err != nil {
+				t.Errorf("Invalid content in adapters/%s/info.yaml: %s", fileInfo.Name(), err.Error())
+			}
+		}
+	}
+}
+
+type infoFileStructure struct {
+	Maintainer maintainerInfo `yaml:"maintainer"`
+}
+
+func (info *infoFileStructure) validate() error {
+	return info.Maintainer.validate()
+}
+
+type maintainerInfo struct {
+	Email string `yaml:"email"`
+}
+
+func (info *maintainerInfo) validate() (err error) {
+	if info.Email == "" {
+		err = errors.New("Missing required field: maintainer.email")
+	}
+	return
+}
