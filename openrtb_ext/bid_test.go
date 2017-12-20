d08c1f27ab55d111039ba91c8661eeec1c4a0aa0
Dave Bemiller <27972385+dbemiller@users.noreply.github.com>
2017-12-20 11:06:51 -0500

    Added another test.

--- openrtb_ext/bid_test.go
+++ openrtb_ext/bid_test.go
@@ -15,3 +15,29 @@ func TestTruncatedKey(t *testing.T) {
 		t.Errorf("Bad truncated targeting key. Expected hb_pb_ap, got %s", apnKey)
 	}
 }
+
+func TestBidParsing(t *testing.T) {
+	assertBidParse(t, "banner", BidTypeBanner)
+	assertBidParse(t, "video", BidTypeVideo)
+	assertBidParse(t, "audio", BidTypeAudio)
+	assertBidParse(t, "native", BidTypeNative)
+	parsed, err := ParseBidType("unknown")
+	if err == nil {
+		t.Errorf("ParseBidType did not return the expected error.")
+	}
+	if parsed != "" {
+		t.Errorf("ParseBidType should return an empty string on error. Instead got %s", parsed)
+	}
+}
+
+func assertBidParse(t *testing.T, s string, bidType BidType) {
+	t.Helper()
+
+	parsed, err := ParseBidType(s)
+	if err != nil {
+		t.Errorf("Bid parsing failed with error: %v", err)
+	}
+	if parsed != bidType {
+		t.Errorf("Bid types did not match. Expected %s, got %s", bidType, parsed)
+	}
+}
