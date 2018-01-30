65016634c933354da5f8411e07c75c42ab36067c
Dave Bemiller <27972385+dbemiller@users.noreply.github.com>
2018-01-30 10:56:34 -0500

    Re-added a function I didnt mean to delete.

--- pbs_light.go
+++ pbs_light.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"math/rand"
+	"net"
 	"net/http"
 	_ "net/http/pprof"
 	"sort"
@@ -16,6 +17,7 @@ import (
 	"time"
 
 	"github.com/prebid/prebid-server/pbsmetrics"
+	"github.com/prebid/prebid-server/prebid"
 
 	"github.com/cloudfoundry/gosigar"
 	"github.com/golang/glog"
@@ -595,6 +597,37 @@ func (m NoCache) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	m.handler.ServeHTTP(w, r)
 }
 
+// https://blog.golang.org/context/userip/userip.go
+func getIP(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	if ua := user_agent.New(req.Header.Get("User-Agent")); ua != nil {
+		fmt.Fprintf(w, "User Agent: %v\n", ua)
+	}
+	ip, port, err := net.SplitHostPort(req.RemoteAddr)
+	if err != nil {
+		fmt.Fprintf(w, "userip: %q is not IP:port\n", req.RemoteAddr)
+	}
+
+	userIP := net.ParseIP(ip)
+	if userIP == nil {
+		//return nil, fmt.Errorf("userip: %q is not IP:port", req.RemoteAddr)
+		fmt.Fprintf(w, "userip: %q is not IP:port\n", req.RemoteAddr)
+		return
+	}
+
+	forwardedIP := prebid.GetForwardedIP(req)
+	realIP := prebid.GetIP(req)
+
+	fmt.Fprintf(w, "IP: %s\n", ip)
+	fmt.Fprintf(w, "Port: %s\n", port)
+	fmt.Fprintf(w, "Forwarded IP: %s\n", forwardedIP)
+	fmt.Fprintf(w, "Real IP: %s\n", realIP)
+
+	for k, v := range req.Header {
+		fmt.Fprintf(w, "%s: %s\n", k, v)
+	}
+
+}
+
 func validate(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Add("Content-Type", "text/plain")
 	defer r.Body.Close()
@@ -834,6 +867,7 @@ func serve(cfg *config.Configuration) error {
 	router.POST("/validate", validate)
 	router.GET("/status", status)
 	router.GET("/", serveIndex)
+	router.GET("/ip", getIP)
 	router.ServeFiles("/static/*filepath", http.Dir("static"))
 
 	hostCookieSettings = pbs.HostCookieSettings{
