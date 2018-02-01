2346e42c52992739de280e72c0f563712acfbae1
Hans Hjort <hhjort@appnexus.com>
2018-02-01 17:05:39 -0500

    typo fix

--- endpoints/openrtb2/amp_auction.go
+++ endpoints/openrtb2/amp_auction.go
@@ -123,11 +123,11 @@ func (deps *endpointDeps) AmpAuction(w http.ResponseWriter, r *http.Request, _ h
 		// Just to be safe
 		origin = r.Header.Get("Origin")
 	}
-	w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
 	// These are handled in CORS middleware
+	// w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
 	// w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
 	// w.Header().Set("Access-Control-Allow-Credentials", "true")
-	// w.Header().Set("AMP-Access-Control-Allow-Source-Origin", origin)
+	w.Header().Set("AMP-Access-Control-Allow-Source-Origin", origin)
 	w.Header().Set("Access-Control-Expose-Headers", "AMP-Access-Control-Allow-Source-Origin")
 
 	// Fixes #231
