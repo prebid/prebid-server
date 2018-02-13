e0f79a2a6eb68f21582b87682eb3d41d762d57a8
Dave Bemiller <27972385+dbemiller@users.noreply.github.com>
2018-02-13 16:53:16 -0500

    Better docs.

--- openrtb_ext/user.go
+++ openrtb_ext/user.go
@@ -5,7 +5,7 @@ type ExtUser struct {
 	Prebid *ExtUserPrebid `json:"prebid"`
 
 	// DigiTrust breaks the typical Prebid Server convention of namespacing "global" options inside "ext.prebid.*"
-	// to match the recommendation by the broader digitrust recommendations.
+	// to match the recommendation from the broader digitrust community.
 	// For more info, see: https://github.com/digi-trust/dt-cdn/wiki/OpenRTB-extension#openrtb-2x
 	DigiTrust *ExtUserDigiTrust `json:"digitrust,omitempty"`
 }
