943092fb702f32ef3c8084989f345a0cb97f2cd7
Dave Bemiller <27972385+dbemiller@users.noreply.github.com>
2018-02-28 11:52:28 -0500

    Found more elegant fixes to get nice test output.

--- scripts/check_coverage.sh
+++ scripts/check_coverage.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+
+COV_MIN=30
+
 #cleanup
 finish() {
+
   if [ -d ".cover" ]; then
     rm -rf .cover
   fi
@@ -9,22 +13,16 @@ finish() {
 
 trap finish EXIT ERR INT TERM
 
-# If the coverage script runs without errors, then make sure that it meets the min code coverage
+#start script logic
 OUTPUT=`./scripts/coverage.sh`
-if [ "$?" = "0" ]; then
-  while read -r LINE; do
-    echo -e "$LINE"
-    if [[ $LINE =~ "%" ]]; then
-      PERCENT=$(echo "$LINE"|cut -d: -f2-|cut -d% -f1|cut -d. -f1|tr -d ' ')
-      if [[ $PERCENT -lt 30 ]]; then
-        echo "Package has less than 30% code coverage. Run ./scripts/coverage.sh --html to see a detailed coverage report, and add tests to improve your coverage"
-        exit 1
-      fi
-    fi
-  done <<< "$OUTPUT"
 
-# Fixes #315. If it has errors, print those and exit with an error code
-else
-  echo -e "$OUTPUT"
-  exit 1
-fi
+while IFS= read -r LINE; do
+  echo -e "$LINE"
+  if [[ $LINE =~ "%" ]]; then
+    PERCENT=$(echo "$LINE"|cut -d: -f2-|cut -d% -f1|cut -d. -f1|tr -d ' ')
+    if [[ $PERCENT -lt $COV_MIN ]]; then
+      echo "Package has less than $COV_MIN% code coverage. Run ./scripts/coverage.sh --html to see a detailed coverage report, and add tests to improve your coverage"
+      exit 1
+    fi
+  fi
+done <<< "$OUTPUT"
