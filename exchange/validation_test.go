f13223b5664c765095ea136fd333fb1e1140a98e
Dave Bemiller <27972385+dbemiller@users.noreply.github.com>
2018-04-02 09:46:48 -0400

    Reverted the crid requirement temporarily.

--- exchange/validation_test.go
+++ exchange/validation_test.go
@@ -1,8 +1,9 @@
 package exchange
 
 import (
-	"github.com/mxmCherry/openrtb"
 	"testing"
+
+	"github.com/mxmCherry/openrtb"
 )
 
 func TestAllValidBids(t *testing.T) {
@@ -40,7 +41,7 @@ func TestAllValidBids(t *testing.T) {
 }
 
 func TestAllBadBids(t *testing.T) {
-	bids := make([]*pbsOrtbBid, 5)
+	bids := make([]*pbsOrtbBid, 4)
 	bids[0] = &pbsOrtbBid{
 		bid: &openrtb.Bid{
 			ID:    "one-bid",
@@ -55,27 +56,28 @@ func TestAllBadBids(t *testing.T) {
 			CrID:  "thatCreative",
 		},
 	}
+	// TODO #427: Add this back in after a breaking change window
+	// bids[2] = &pbsOrtbBid{
+	// 	bid: &openrtb.Bid{
+	// 		ID:    "123",
+	// 		ImpID: "456",
+	// 		Price: 0.44,
+	// 	},
+	// }
 	bids[2] = &pbsOrtbBid{
-		bid: &openrtb.Bid{
-			ID:    "123",
-			ImpID: "456",
-			Price: 0.44,
-		},
-	}
-	bids[3] = &pbsOrtbBid{
 		bid: &openrtb.Bid{
 			ImpID: "456",
 			Price: 0.44,
 			CrID:  "blah",
 		},
 	}
-	bids[4] = &pbsOrtbBid{}
+	bids[3] = &pbsOrtbBid{}
 	brw := &bidResponseWrapper{
 		adapterBids: &pbsOrtbSeatBid{
 			bids: bids,
 		},
 	}
-	assertBids(t, brw, 0, 5)
+	assertBids(t, brw, 0, 4)
 }
 
 func TestMixeddBids(t *testing.T) {
@@ -122,7 +124,7 @@ func TestMixeddBids(t *testing.T) {
 func assertBids(t *testing.T, brw *bidResponseWrapper, ebids int, eerrs int) {
 	errs := brw.validateBids()
 	if len(errs) != eerrs {
-		t.Errorf("Expected %d Errors validating bids, found %d", len(errs), eerrs)
+		t.Errorf("Expected %d Errors validating bids, found %d", eerrs, len(errs))
 	}
 	if len(brw.adapterBids.bids) != ebids {
 		t.Errorf("Expected %d bids, found %d bids", ebids, len(brw.adapterBids.bids))
