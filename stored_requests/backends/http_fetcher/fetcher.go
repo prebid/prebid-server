e7d4af6180c46f1f0fa1bda11d4e50376ef28b78
Dave Bemiller <27972385+dbemiller@users.noreply.github.com>
2018-04-05 16:48:54 -0400

    Added an HTTP fetcher to get Stored Request data from an endpoint.

--- stored_requests/backends/http_fetcher/fetcher.go
+++ stored_requests/backends/http_fetcher/fetcher.go
@@ -1,11 +1,14 @@
-package file_fetcher
+package http_fetcher
 
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strings"
 
+	"github.com/golang/glog"
 	"golang.org/x/net/context/ctxhttp"
 )
 
@@ -13,12 +16,12 @@ import (
 //
 // This file expects the endpoint to satisfy the following API:
 //
-// GET {endpoint}?req-id=req1,req2&imp-ids=imp1,imp2,imp3
+// GET {endpoint}?req-ids=req1,req2&imp-ids=imp1,imp2,imp3
 //
 // This endpoint should return a payload like:
 //
 // {
-//   "request": {
+//   "requests": {
 //     "req1": { ... stored data for req1 ... },
 //     "req2": { ... stored data for req2 ... },
 //   },
@@ -29,20 +32,37 @@ import (
 //   }
 // }
 //
-// If the request, or any of the imps are not found, then
+//
 func NewFetcher(client *http.Client, endpoint string) *httpFetcher {
+	// Do some work up-front to figure out if the (configurable) endpoint has a query string or not.
+	// When we build requests, we'll either want to add `?req-ids=...&imp-ids=...` _or_
+	// `&req-ids=...&imp-ids=...`, depending.
+	urlPrefix := endpoint
+	if strings.Contains(endpoint, "?") {
+		urlPrefix += "&"
+	} else {
+		urlPrefix += "?"
+	}
+
+	glog.Infof("http_fetcher will use: GET " + urlPrefix + "req-ids=%REQUEST_ID_LIST%&imp-ids=%IMP_ID_LIST%")
+
 	return &httpFetcher{
 		client:   client,
-		endpoint: endpoint,
+		endpoint: urlPrefix,
 	}
 }
 
 type httpFetcher struct {
 	client   *http.Client
 	endpoint string
+	hasQuery bool
 }
 
 func (fetcher *httpFetcher) FetchRequests(ctx context.Context, requestIDs []string, impIDs []string) (requestData map[string]json.RawMessage, impData map[string]json.RawMessage, errs []error) {
+	if len(requestIDs) == 0 && len(impIDs) == 0 {
+		return nil, nil, nil
+	}
+
 	httpReq, err := buildRequest(fetcher.endpoint, requestIDs, impIDs)
 	if err != nil {
 		return nil, nil, []error{err}
@@ -57,11 +77,36 @@ func (fetcher *httpFetcher) FetchRequests(ctx context.Context, requestIDs []stri
 }
 
 func buildRequest(endpoint string, requestIDs []string, impIDs []string) (*http.Request, error) {
-	// TODO: Build query params
-	return http.NewRequest("GET", endpoint, strings.NewReader(""))
+	if len(requestIDs) > 0 && len(impIDs) > 0 {
+		return http.NewRequest("GET", endpoint+"req-ids="+strings.Join(requestIDs, ",")+"&imp-ids="+strings.Join(impIDs, ","), nil)
+	} else if len(requestIDs) > 0 {
+		return http.NewRequest("GET", endpoint+"req-ids="+strings.Join(requestIDs, ","), nil)
+	} else {
+		return http.NewRequest("GET", endpoint+"imp-ids="+strings.Join(impIDs, ","), nil)
+	}
 }
 
 func unpackResponse(resp *http.Response) (requestData map[string]json.RawMessage, impData map[string]json.RawMessage, errs []error) {
-	// TODO: Implement
-	return nil, nil, nil
+	respBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, nil, []error{err}
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		var responseObj responseContract
+		if err := json.Unmarshal(respBytes, &responseObj); err != nil {
+			return nil, nil, []error{err}
+		}
+
+		return responseObj.Requests, responseObj.Imps, nil
+	}
+
+	return nil, nil, []error{fmt.Errorf("Error fetching Stored Requests via HTTP. Response code was %d", resp.StatusCode)}
+}
+
+// responseContract is used to unmarshal  for the endpoint
+type responseContract struct {
+	Requests map[string]json.RawMessage `json:"requests"`
+	Imps     map[string]json.RawMessage `json:"imps"`
 }
---
+++ stored_requests/backends/http_fetcher/fetcher_test.go
@@ -0,0 +1,111 @@
+package http_fetcher
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"strings"
+	"testing"
+)
+
+// Tests for buildRequest
+func TestSingleReq(t *testing.T) {
+	doBuildURLTest(t, "http://prebid.com/stored_requests", []string{"req-1"}, nil, "http://prebid.com/stored_requests?req-ids=req-1")
+}
+
+func TestSeveralReqs(t *testing.T) {
+	doBuildURLTest(t, "http://prebid.com/stored_requests", []string{"req-1", "req-2"}, nil, "http://prebid.com/stored_requests?req-ids=req-1,req-2")
+}
+
+func TestSingleImp(t *testing.T) {
+	doBuildURLTest(t, "http://prebid.com/stored_requests", nil, []string{"imp-1"}, "http://prebid.com/stored_requests?imp-ids=imp-1")
+}
+
+func TestSeveralImps(t *testing.T) {
+	doBuildURLTest(t, "http://prebid.com/stored_requests", nil, []string{"imp-1", "imp-2"}, "http://prebid.com/stored_requests?imp-ids=imp-1,imp-2")
+}
+
+func TestReqsAndImps(t *testing.T) {
+	doBuildURLTest(t, "http://prebid.com/stored_requests", []string{"req-1"}, []string{"imp-1"}, "http://prebid.com/stored_requests?req-ids=req-1&imp-ids=imp-1")
+}
+
+// Tests for unpackResponse
+func TestReqResp(t *testing.T) {
+	doResponseUnpackTest(t, `{"requests":{"req-1":{"isRequest":true}}}`, map[string]json.RawMessage{"req-1": json.RawMessage(`{"isRequest":true}`)}, nil, nil)
+}
+
+func TestImpResp(t *testing.T) {
+	doResponseUnpackTest(t, `{"imps":{"imp-1":{"isRequest":false}}}`, nil, map[string]json.RawMessage{"imp-1": json.RawMessage(`{"isRequest":false}`)}, nil)
+}
+
+func TestImpReqResp(t *testing.T) {
+	mockResponse := `{"requests":{"req-1":{"isRequest":true}},"imps":{"imp-1":{"isRequest":false}}}`
+	expectedRequestData := map[string]json.RawMessage{"req-1": json.RawMessage(`{"isRequest":true}`)}
+	expectedImpData := map[string]json.RawMessage{"imp-1": json.RawMessage(`{"isRequest":false}`)}
+	doResponseUnpackTest(t, mockResponse, expectedRequestData, expectedImpData, nil)
+}
+
+func TestMalformedResponse(t *testing.T) {
+	doResponseUnpackTest(t, `{`, nil, nil, []string{"unexpected end of JSON input"})
+}
+
+func doBuildURLTest(t *testing.T, endpoint string, requests []string, imps []string, expected string) {
+	httpFetcher := NewFetcher(nil, endpoint)
+	req, err := buildRequest(httpFetcher.endpoint, requests, imps)
+	if err != nil {
+		t.Fatalf("Unexpected error building URL: %v", err)
+	}
+
+	if req.URL.String() != expected {
+		t.Errorf("Bad URL. Expected %s, got %s", expected, req.URL.String())
+	}
+}
+
+func doResponseUnpackTest(t *testing.T, resp string, expectedReqs map[string]json.RawMessage, expectedImps map[string]json.RawMessage, expectedErrs []string) {
+	mockResponse := &http.Response{
+		StatusCode: 200,
+		Body:       closeWrapper{strings.NewReader(resp)},
+	}
+
+	requestData, impData, errs := unpackResponse(mockResponse)
+	assertSameContents(t, requestData, expectedReqs)
+	assertSameContents(t, impData, expectedImps)
+	assertSameErrMsgs(t, expectedErrs, errs)
+}
+
+func assertSameContents(t *testing.T, expected map[string]json.RawMessage, actual map[string]json.RawMessage) {
+	if len(expected) != len(actual) {
+		t.Errorf("Wrong counts. Expected %d, actual %d", len(expected), len(actual))
+		return
+	}
+	for expectedKey, expectedVal := range expected {
+		if actualVal, ok := actual[expectedKey]; ok {
+			if !bytes.Equal(expectedVal, actualVal) {
+				t.Errorf("actual[%s] value %s does not match expected: %s", expectedKey, string(actualVal), string(actualVal))
+			}
+		} else {
+			t.Errorf("actual map missing expected key %s", expectedKey)
+		}
+	}
+}
+
+func assertSameErrMsgs(t *testing.T, expected []string, actual []error) {
+	if len(expected) != len(actual) {
+		t.Errorf("Wrong error counts. Expected %d, actual %d", len(expected), len(actual))
+		return
+	}
+	for i, expectedErr := range expected {
+		if actual[i].Error() != expectedErr {
+			t.Errorf("Wrong error[%d]. Expected %s, got %s", i, expectedErr, actual[i].Error())
+		}
+	}
+}
+
+type closeWrapper struct {
+	io.Reader
+}
+
+func (w closeWrapper) Close() error {
+	return nil
+}
