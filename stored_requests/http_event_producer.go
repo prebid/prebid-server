f9f02fcbfb7fff01bee5a6e5b332e9f698ddf636
Dave Bemiller <27972385+dbemiller@users.noreply.github.com>
2018-04-09 11:36:24 -0400

    Added an HTTP EventProducer. No tests yet.

--- stored_requests/http_event_producer.go
+++ stored_requests/http_event_producer.go
@@ -1,9 +1,13 @@
 package stored_requests
 
 import (
+	"bytes"
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"time"
+
+	"github.com/buger/jsonparser"
 
 	"github.com/golang/glog"
 )
@@ -41,48 +45,116 @@ func NewHTTPEvents(client *http.Client, endpoint string, refreshRate time.Durati
 	e := &httpEvents{
 		client:        client,
 		endpoint:      endpoint,
-		updates:       make(chan map[string]json.RawMessage, 50),
-		invalidations: make(chan []string),
+		lastUpdate:    time.Now().UTC(),
+		updates:       make(chan Update, 1),
+		invalidations: make(chan Invalidation, 1),
 	}
-	timestamp := time.Now()
 	e.fetchAll()
 
-	go e.refresh(timestamp, time.Tick(refreshRate))
+	go e.refresh(time.Tick(refreshRate))
 }
 
 type httpEvents struct {
 	client        *http.Client
+	lastUpdate    time.Time
 	endpoint      string
-	updates       chan map[string]json.RawMessage
-	invalidations chan []string
+	updates       chan Update
+	invalidations chan Invalidation
 }
 
-func (e *httpEvents) Updates() <-chan map[string]json.RawMessage {
+func (e *httpEvents) Updates() <-chan Update {
 	return e.updates
 }
 
-func (e *httpEvents) Invalidations() <-chan []string {
+func (e *httpEvents) Invalidations() <-chan Invalidation {
 	return e.invalidations
 }
 
 func (e *httpEvents) fetchAll() {
 	resp, err := e.client.Get(e.endpoint)
-	if err != nil {
-		glog.Errorf("Failed call: GET %s for Stored Requests: %v", e.endpoint, err)
-		return
+	if respObj, ok := e.parse(e.endpoint, resp, err); ok {
+		if len(respObj.StoredRequests) > 0 || len(respObj.StoredImps) > 0 {
+			e.updates <- Update{
+				Requests: respObj.StoredRequests,
+				Imps:     respObj.StoredImps,
+			}
+		}
 	}
 }
 
-func (e *httpEvents) refresh(lastRefresh time.Time, ticker <-chan time.Time) {
+func (e *httpEvents) refresh(ticker <-chan time.Time) {
 	for {
 		select {
-		case _ = <-ticker:
-
+		case thisTime := <-ticker:
+			thisTimeInUTC := thisTime.UTC()
+			thisEndpoint := e.endpoint + "?last-modified=" + e.lastUpdate.Format(time.RFC3339)
+			resp, err := e.client.Get(thisEndpoint)
+			if respObj, ok := e.parse(thisEndpoint, resp, err); ok {
+				invalidations := Invalidation{
+					Requests: extractInvalidations(respObj.StoredRequests),
+					Imps:     extractInvalidations(respObj.StoredImps),
+				}
+				if len(respObj.StoredRequests) > 0 || len(respObj.StoredImps) > 0 {
+					e.updates <- Update{
+						Requests: respObj.StoredRequests,
+						Imps:     respObj.StoredImps,
+					}
+				}
+				if len(invalidations.Requests) > 0 || len(invalidations.Imps) > 0 {
+					e.invalidations <- invalidations
+				}
+				e.lastUpdate = thisTimeInUTC
+			}
 		}
 	}
+}
+
+// proceess unpacks the HTTP response and sends the relevant events to the channels.
+// It returns true if everything was successful, and false if any errors occurred.
+func (e *httpEvents) parse(endpoint string, resp *http.Response, err error) (*responseContract, bool) {
+	if err != nil {
+		glog.Errorf("Failed call: GET %s for Stored Requests: %v", endpoint, err)
+		return nil, false
+	}
+	defer resp.Body.Close()
+
+	respBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		glog.Errorf("Failed to read body of GET %s for Stored Requests: %v", endpoint, err)
+		return nil, false
+	}
+
+	var respObj responseContract
+	if err := json.Unmarshal(respBytes, &respObj); err != nil {
+		glog.Errorf("Failed to unmarshal body of GET %s for Stored Requests: %v", endpoint, err)
+		return nil, false
+	}
+
+	return &respObj, true
+}
+
+func extractInvalidations(changes map[string]json.RawMessage) []string {
+	deletedIDs := make([]string, 0, len(changes))
+	for id, msg := range changes {
+		if value, _, _, err := jsonparser.Get(msg, "deleted"); err == nil && bytes.Equal(value, []byte("true")) {
+			delete(changes, id)
+			deletedIDs = append(deletedIDs, id)
+		}
+	}
+	return deletedIDs
 }
 
 type responseContract struct {
 	StoredRequests map[string]json.RawMessage `json:"requests"`
 	StoredImps     map[string]json.RawMessage `json:"imps"`
+}
+
+type Update struct {
+	Requests map[string]json.RawMessage `json:"requests"`
+	Imps     map[string]json.RawMessage `json:"imps"`
+}
+
+type Invalidation struct {
+	Requests []string `json:"requests"`
+	Imps     []string `json:"imps"`
 }
