fe5975b94c130a8ce033ad234cdb459a0ed6543c
Dave Bemiller <27972385+dbemiller@users.noreply.github.com>
2018-04-13 16:20:35 -0400

    Some refactoring, and _just barely_ enough tests to clear the code coverage minimums.

--- server/server.go
+++ server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"fmt"
 	"net"
 	"net/http"
 	"os"
@@ -18,64 +19,80 @@ import (
 
 // Listen blocks forever, serving PBS requests on the given port. This will block forever, until the process is shut down.
 func Listen(cfg *config.Configuration, handler http.Handler, metrics pbsmetrics.MetricsEngine) {
+	stopSignals := make(chan os.Signal)
+	signal.Notify(stopSignals, syscall.SIGTERM, syscall.SIGINT)
 
 	// Run the servers. Fan any process-stopper signals out to each server for graceful shutdowns.
 	stopAdmin := make(chan os.Signal)
 	stopMain := make(chan os.Signal)
 	done := make(chan struct{})
 
-	go runAdmin(cfg, stopAdmin, done)
-	go runMain(cfg, handler, metrics, stopMain, done)
+	adminServer := newAdminServer(cfg)
+	go shutdownAfterSignals(adminServer, stopAdmin, done)
 
-	wait(done, stopMain, stopAdmin)
+	mainServer := newMainServer(cfg, handler)
+	go shutdownAfterSignals(mainServer, stopMain, done)
+
+	mainListener, err := newListener(mainServer.Addr, metrics)
+	if err != nil {
+		glog.Errorf("Error listening for TCP connections on %s: %v", mainServer.Addr, err)
+		return
+	}
+	adminListener, err := newListener(adminServer.Addr, nil)
+	if err != nil {
+		glog.Errorf("Error listening for TCP connections on %s: %v", adminServer.Addr, err)
+		return
+	}
+	go runServer(mainServer, "Main", mainListener)
+	go runServer(adminServer, "Admin", adminListener)
+
+	wait(stopSignals, done, stopMain, stopAdmin)
 	return
 }
 
-// runAdmin runs the admin server. This should block forever.
-// If an error occurs and it needs to return, a SIGTERM will be sent to stopSignals.
-func runAdmin(cfg *config.Configuration, stopper <-chan os.Signal, done chan<- struct{}) {
-	uri := cfg.Host + ":" + strconv.Itoa(cfg.AdminPort)
-	server := &http.Server{Addr: uri}
-	glog.Infof("Admin server starting on: %s", uri)
-	go shutdownAfterSignals(server, stopper, done)
-	err := server.ListenAndServe()
-	glog.Errorf("Admin server quit with error: %v", err)
-	return
+func newAdminServer(cfg *config.Configuration) *http.Server {
+	return &http.Server{
+		Addr: cfg.Host + ":" + strconv.Itoa(cfg.AdminPort),
+	}
 }
 
-// runMain runs the "main" server. This will block until a signal gets sent to stopper.
-// When it's complete,
-func runMain(cfg *config.Configuration, handler http.Handler, metrics pbsmetrics.MetricsEngine, stopper <-chan os.Signal, done chan<- struct{}) {
-	server := &http.Server{
+func newMainServer(cfg *config.Configuration, handler http.Handler) *http.Server {
+	return &http.Server{
 		Addr:         cfg.Host + ":" + strconv.Itoa(cfg.Port),
 		Handler:      handler,
 		ReadTimeout:  15 * time.Second,
 		WriteTimeout: 15 * time.Second,
 	}
-	listener, err := net.Listen("tcp", server.Addr)
-	if err != nil {
-		glog.Errorf("Error listening for TCP connections on %s: %v", server.Addr, err)
-		return
-	}
-
-	// This cast is in Go's core libs as Server.ListenAndServe(), so it _should_ be safe, but just in case...
-	if casted, ok := listener.(*net.TCPListener); ok {
-		listener = &monitorableListener{casted, metrics}
-	} else {
-		glog.Errorf("Golang's core lib didn't return a TCPListener as expected. Connection metrics will not be sent.")
-	}
-
-	glog.Infof("Main server starting on: %s", server.Addr)
-	go shutdownAfterSignals(server, stopper, done)
-	err = server.Serve(listener)
-	glog.Errorf("Main server quit with error: %v", err)
-	return
 }
 
-func wait(done <-chan struct{}, outbound ...chan<- os.Signal) {
-	stopSignals := make(chan os.Signal)
-	signal.Notify(stopSignals, syscall.SIGTERM, syscall.SIGINT)
-	sig := <-stopSignals
+func runServer(server *http.Server, name string, listener net.Listener) {
+	glog.Infof("%s server starting on: %s", name, server.Addr)
+	err := server.Serve(listener)
+	glog.Errorf("%s server quit with error: %v", name, err)
+}
+
+func newListener(address string, metrics pbsmetrics.MetricsEngine) (net.Listener, error) {
+	ln, err := net.Listen("tcp", address)
+	if err != nil {
+		return nil, fmt.Errorf("Error listening for TCP connections on %s: %v", address, err)
+	}
+
+	// This cast is in Go's core libs as Server.ListenAndServe(), so it _should_ be safe, but just in case it changes in a future version...
+	if casted, ok := ln.(*net.TCPListener); ok {
+		ln = &tcpKeepAliveListener{casted}
+	} else {
+		glog.Warning("net.Listen(\"tcp\", \"addr\") didn't return a TCPListener as it did in Go 1.9. Things will probably work fine... but this should be investigated.")
+	}
+
+	if metrics != nil {
+		ln = &monitorableListener{ln, metrics}
+	}
+
+	return ln, nil
+}
+
+func wait(inbound <-chan os.Signal, done <-chan struct{}, outbound ...chan<- os.Signal) {
+	sig := <-inbound
 
 	for i := 0; i < len(outbound); i++ {
 		go sendSignal(outbound[i], sig)
