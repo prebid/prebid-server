7a392793ed7b0ee9ab0f7707ef00a0ab7c260794
Dave Bemiller <27972385+dbemiller@users.noreply.github.com>
2018-04-13 13:50:51 -0400

    Fixed some bugs and tested most things.

--- server/listener.go
+++ server/listener.go
@@ -1,1 +1,32 @@
 package server
+
+import (
+	"net"
+	"time"
+)
+
+type monitorableConnection struct {
+	net.Conn
+	onClose func()
+}
+
+type monitorableListener struct {
+	*net.TCPListener
+	onNewConnection func()
+}
+
+func (l *monitorableConnection) Close() error {
+	l.onClose()
+	return l.Conn.Close()
+}
+
+func (ln *monitorableListener) Accept() (c net.Conn, err error) {
+	tc, err := ln.AcceptTCP()
+	if err != nil {
+		return
+	}
+	tc.SetKeepAlive(true)
+	tc.SetKeepAlivePeriod(3 * time.Minute)
+	ln.onNewConnection()
+	return tc, nil
+}
--- server/server.go
+++ server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
@@ -14,72 +15,88 @@ import (
 )
 
 // Listen blocks forever, serving PBS requests on the given port. This will block forever, until the process is shut down.
-func Listen(cfg *config.Configuration, handler http.Handler) error {
-	stopSignals := make(chan os.Signal)
-	signal.Notify(stopSignals, syscall.SIGTERM, syscall.SIGINT)
+func Listen(cfg *config.Configuration, handler http.Handler) {
 
 	// Run the servers. Fan any process-stopper signals out to each server for graceful shutdowns.
-	stopAdmin := make(chan struct{})
-	stopMain := make(chan struct{})
-	go dispatch(stopSignals, stopMain, stopAdmin)
-	go runAdmin(cfg, stopAdmin)
-	go runMain(cfg, handler, stopMain)
+	stopAdmin := make(chan os.Signal)
+	stopMain := make(chan os.Signal)
+	done := make(chan struct{})
 
-	<-stopSignals
-	return nil
+	go runAdmin(cfg, stopAdmin, done)
+	go runMain(cfg, handler, stopMain, done)
+
+	wait(done, stopMain, stopAdmin)
+
+	return
 }
 
-func dispatch(inbound <-chan os.Signal, outbound ...chan<- struct{}) {
-	var s struct{}
-	for {
-		<-inbound
-		done := make(chan bool)
-		for i := 0; i < len(outbound); i++ {
-			go func() {
-				outbound[i] <- s
-				done <- true
-			}()
-		}
-		for i := 0; i < len(outbound); i++ {
-			<-done
-		}
+func wait(done <-chan struct{}, outbound ...chan<- os.Signal) {
+	stopSignals := make(chan os.Signal)
+	signal.Notify(stopSignals, syscall.SIGTERM, syscall.SIGINT)
+	sig := <-stopSignals
+
+	for i := 0; i < len(outbound); i++ {
+		go sendSignal(outbound[i], sig)
 	}
+
+	for i := 0; i < len(outbound); i++ {
+		<-done
+	}
+}
+
+func sendSignal(to chan<- os.Signal, sig os.Signal) {
+	to <- sig
 }
 
 // runAdmin runs the admin server. This should block forever.
 // If an error occurs and it needs to return, a SIGTERM will be sent to stopSignals.
-func runAdmin(cfg *config.Configuration, stopper <-chan struct{}) {
+func runAdmin(cfg *config.Configuration, stopper <-chan os.Signal, done chan<- struct{}) {
 	uri := cfg.Host + ":" + strconv.Itoa(cfg.AdminPort)
 	server := &http.Server{Addr: uri}
 	glog.Infof("Admin server starting on: %s", uri)
-	go watchForStops(server, stopper)
+	go watchForStops(server, stopper, done)
 	err := server.ListenAndServe()
 	glog.Errorf("Admin server quit with error: %v", err)
 	return
 }
 
-// runMain runs the "main" server. This should block forever.
-// If an error occurs and it returns, a SIGTERM will be sent to stopSignals.
-func runMain(cfg *config.Configuration, handler http.Handler, stopper <-chan struct{}) {
+// runMain runs the "main" server. This should block forever, unless something goes wrong.
+func runMain(cfg *config.Configuration, handler http.Handler, stopper <-chan os.Signal, done chan<- struct{}) {
 	server := &http.Server{
 		Addr:         cfg.Host + ":" + strconv.Itoa(cfg.Port),
 		Handler:      handler,
 		ReadTimeout:  15 * time.Second,
 		WriteTimeout: 15 * time.Second,
 	}
+	listener, err := net.Listen("tcp", server.Addr)
+	if err != nil {
+		glog.Errorf("Error listening for TCP connections on %s: %v", server.Addr, err)
+		return
+	}
+
+	// This cast is in Go's core libs as Server.ListenAndServe(), so it _should_ be safe, but just in case...
+	if casted, ok := listener.(*net.TCPListener); ok {
+		listener = &monitorableListener{casted, func() {}}
+	} else {
+		glog.Errorf("Golang's core lib didn't return a TCPListener as expected. Connection metrics will not be sent.")
+	}
+
 	glog.Infof("Main server starting on: %s", server.Addr)
-	go watchForStops(server, stopper)
-	err := server.ListenAndServe()
+	go watchForStops(server, stopper, done)
+	err = server.Serve(listener)
 	glog.Errorf("Main server quit with error: %v", err)
 	return
 }
 
-func watchForStops(server *http.Server, stopper <-chan struct{}) {
-	<-stopper
+func watchForStops(server *http.Server, stopper <-chan os.Signal, done chan<- struct{}) {
+	sig := <-stopper
+	var s struct{}
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
+	glog.Infof("Stopping %s because of signal: %s", server.Addr, sig.String())
 	if err := server.Shutdown(ctx); err != nil {
-		glog.Errorf("Failed to stop server %s : %v", server.Addr, err)
+		glog.Errorf("Failed to shutdown %s: %v", server.Addr, err)
 	}
+	done <- s
 }
