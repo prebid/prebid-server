7309f1387b043138cc3ea1b0899681f15b249131
Jonathan Go <jgo@sonobi.com>
2018-04-16 16:39:22 -0400

    made usersync like the other bidders. but idk how this is supposed to work

--- usersync/sonobi.go
+++ usersync/sonobi.go
@@ -8,10 +8,11 @@ import (
 const USER_SYNC_URL = "http://sync.go.sonobi.com/us.gif?loc=%s"
 
 func NewSonobiSyncer(externalURL string) Usersyncer {
+	redirectURI := fmt.Sprintf("%s/setuid?bidder=sonobi&uid=$UID}", externalURL)
 	return &syncer{
 		familyName: "sonobi",
 		syncInfo: &UsersyncInfo{
-			URL:         fmt.Sprintf(USER_SYNC_URL, url.QueryEscape(externalURL)),
+			URL:         fmt.Sprintf(USER_SYNC_URL, url.QueryEscape(redirectURI)),
 			Type:        "redirect",
 			SupportCORS: false,
 		},
