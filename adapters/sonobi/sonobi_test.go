8b5bbe5eb84ca7224ae49e7462901fc8f146f495
Jonathan Go <jgo@sonobi.com>
2018-04-17 15:50:42 -0400

    added tests

--- adapters/sonobi/sonobi_test.go
+++ adapters/sonobi/sonobi_test.go
@@ -1,1 +1,11 @@
 package sonobi
+
+import (
+	"github.com/prebid/prebid-server/adapters/adapterstest"
+	"net/http"
+	"testing"
+)
+
+func TestJsonSamples(t *testing.T) {
+	adapterstest.RunJSONBidderTest(t, "sonobitest", NewSonobiBidder(http.DefaultClient, "endpoint"))
+}
---
+++ usersync/sonobi_test.go
@@ -0,0 +1,28 @@
+package usersync
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSonobiSyncer(t *testing.T) {
+	url := "https://some.prebid-server.com"
+	expectedUrl := "http://sync.go.sonobi.com/us.gif?loc=https%3A%2F%2Fsome.prebid-server.com%2Fsetuid%3Fbidder%3Dsonobi%26uid%3D%24UID"
+	syncer := NewSonobiSyncer(url)
+	info := syncer.GetUsersyncInfo()
+	fmt.Println(info.URL)
+	fmt.Println(expectedUrl)
+	if info.URL != expectedUrl {
+		t.Fatalf("should have matched")
+	}
+	if info.Type != "redirect" {
+		t.Fatalf("should be redirect")
+	}
+	if info.SupportCORS != false {
+		t.Fatalf("should have been false")
+	}
+
+	if syncer.FamilyName() != "sonobi" {
+		t.Errorf("FamilyName '%s' != 'rubicon'", syncer.FamilyName())
+	}
+}
