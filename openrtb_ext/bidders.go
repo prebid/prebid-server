217120183f2c1e8ad8f7a87c401647e5f4ec67a7
abemille <abemille@users.noreply.github.com>
2018-04-20 06:58:18 -0700

    Windows - Resolved load json schema error (#421)
    * Generalized the file paths for windows.
    
    * added visual studio folder to the gitignore.
    
    * Updated call to filepath.Abs() so that it returns error rather than ignoring it.

--- openrtb_ext/bidders.go
+++ openrtb_ext/bidders.go
@@ -5,7 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
-	"net/http"
+	"path/filepath"
 	"strings"
 
 	"github.com/mxmCherry/openrtb"
@@ -85,7 +85,6 @@ type BidderParamValidator interface {
 // NewBidderParamsValidator makes a BidderParamValidator, assuming all the necessary files exist in the filesystem.
 // This will error if, for example, a Bidder gets added but no JSON schema is written for them.
 func NewBidderParamsValidator(schemaDirectory string) (BidderParamValidator, error) {
-	filesystem := http.Dir(schemaDirectory)
 	fileInfos, err := ioutil.ReadDir(schemaDirectory)
 	if err != nil {
 		return nil, fmt.Errorf("Failed to read JSON schemas from directory %s. %v", schemaDirectory, err)
@@ -98,11 +97,14 @@ func NewBidderParamsValidator(schemaDirectory string) (BidderParamValidator, err
 		if _, isValid := BidderMap[bidderName]; !isValid {
 			return nil, fmt.Errorf("File %s/%s does not match a valid BidderName.", schemaDirectory, fileInfo.Name())
 		}
-
-		schemaLoader := gojsonschema.NewReferenceLoaderFileSystem(fmt.Sprintf("file:///%s", fileInfo.Name()), filesystem)
+		toOpen, err := filepath.Abs(filepath.Join(schemaDirectory, fileInfo.Name()))
+		if err != nil {
+			return nil, fmt.Errorf("Failed to get an absolute representation of the path: %s, %v", toOpen, err)
+		}
+		schemaLoader := gojsonschema.NewReferenceLoader("file:///" + toOpen)
 		loadedSchema, err := gojsonschema.NewSchema(schemaLoader)
 		if err != nil {
-			return nil, fmt.Errorf("Failed to load json schema at %s/%s: %v", schemaDirectory, fileInfo.Name(), err)
+			return nil, fmt.Errorf("Failed to load json schema at %s: %v", toOpen, err)
 		}
 
 		fileBytes, err := ioutil.ReadFile(fmt.Sprintf("%s/%s", schemaDirectory, fileInfo.Name()))
