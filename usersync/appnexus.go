cf67feaf1c0dcbe6f89d89d78f5e8ff753f3503f
jim <naumdev@gmail.com>
2018-05-02 05:13:18 -0400

    back to qa

--- usersync/appnexus.go
+++ usersync/appnexus.go
@@ -2,12 +2,17 @@ package usersync
 
 import (
 	"fmt"
+	"github.com/golang/glog"
 	"net/url"
 )
 
 func NewAppnexusSyncer(externalURL string) Usersyncer {
 	redirect_uri := fmt.Sprintf("%s/setuid?bidder=adnxs&uid=$UID", externalURL)
 	usersyncURL := "//ib.adnxs.com/getuid?"
+
+	glog.Info("\nredirect_uri    	: ", redirect_uri)
+	glog.Info("\nusersyncURL    	: ", usersyncURL)
+	glog.Info("\xurl    		: ", externalURL)
 
 	return &syncer{
 		familyName: "adnxs",
