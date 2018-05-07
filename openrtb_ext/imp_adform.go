70f4de6d1f6cf7e5e83cc1477ba0a65bfabd2a14
Olga Linkevich <o.linkevich@adform.com>
2018-05-07 16:27:47 +0300

    Formatting

--- openrtb_ext/imp_adform.go
+++ openrtb_ext/imp_adform.go
@@ -2,5 +2,5 @@ package openrtb_ext
 
 type ExtImpAdform struct {
 	MasterTagId string `json:"mid"`
-	PriceType string `json:"priceType,omitempty"`
+	PriceType   string `json:"priceType,omitempty"`
 }
