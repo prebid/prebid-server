58f9de0513809bd0762c19c091a9ce9c063143d4
Dave Bemiller <27972385+dbemiller@users.noreply.github.com>
2018-05-16 15:13:34 -0400

    Added lots of tests.

--- gdpr/impl_test.go
+++ gdpr/impl_test.go
@@ -1,45 +1,185 @@
 package gdpr
 
 import (
-	"net/http"
-	"net/http/httptest"
-	"strconv"
+	"context"
+	"errors"
+	"fmt"
 	"testing"
+
+	"github.com/prebid/prebid-server/config"
+	"github.com/prebid/prebid-server/openrtb_ext"
+
+	"github.com/prebid/go-gdpr/vendorlist"
 )
 
-func TestVendorFetch(t *testing.T) {
-	server := httptest.NewServer(http.HandlerFunc(mockServer(map[int]string{1: "{}"})))
-	defer server.Close()
+func TestNoConsentButAllowByDefault(t *testing.T) {
+	perms := permissionsImpl{
+		cfg: config.GDPR{
+			HostVendorID:        3,
+			UsersyncIfAmbiguous: true,
+		},
+		vendorIDs:       nil,
+		fetchVendorList: failedListFetcher,
+	}
+	allowSync, err := perms.BidderSyncAllowed(context.Background(), openrtb_ext.BidderAppnexus, "")
+	assertBoolsEqual(t, true, allowSync)
+	assertNilErr(t, err)
+	allowSync, err = perms.HostCookiesAllowed(context.Background(), "")
+	assertBoolsEqual(t, true, allowSync)
+	assertNilErr(t, err)
 }
 
-func testURLMaker(client *httptest.Server) func(uint16) string {
-	url := client.URL
-	return func(version uint16) string {
-		return url + "?version=" + strconv.Itoa(int(version))
+func TestNoConsentAndRejectByDefault(t *testing.T) {
+	perms := permissionsImpl{
+		cfg: config.GDPR{
+			HostVendorID:        3,
+			UsersyncIfAmbiguous: false,
+		},
+		vendorIDs:       nil,
+		fetchVendorList: failedListFetcher,
+	}
+	allowSync, err := perms.BidderSyncAllowed(context.Background(), openrtb_ext.BidderAppnexus, "")
+	assertBoolsEqual(t, false, allowSync)
+	assertNilErr(t, err)
+	allowSync, err = perms.HostCookiesAllowed(context.Background(), "")
+	assertBoolsEqual(t, false, allowSync)
+	assertNilErr(t, err)
+}
+
+func TestAllowedSyncs(t *testing.T) {
+	vendorListData := mockVendorListData(t, 1, map[uint16]*purposes{
+		2: &purposes{
+			purposes: []uint8{1}, // cookie reads/writes
+		},
+		3: &purposes{
+			purposes: []uint8{3}, // ad personalization
+		},
+	})
+	perms := permissionsImpl{
+		cfg: config.GDPR{
+			HostVendorID: 2,
+		},
+		vendorIDs: map[openrtb_ext.BidderName]uint16{
+			openrtb_ext.BidderAppnexus: 2,
+			openrtb_ext.BidderPubmatic: 3,
+		},
+		fetchVendorList: listFetcher(map[uint16]vendorlist.VendorList{
+			1: parseVendorListData(t, vendorListData),
+		}),
+	}
+
+	allowSync, err := perms.HostCookiesAllowed(context.Background(), "BON3PCUON3PCUABABBAAABoAAAAAMw")
+	assertNilErr(t, err)
+	assertBoolsEqual(t, true, allowSync)
+
+	allowSync, err = perms.BidderSyncAllowed(context.Background(), openrtb_ext.BidderPubmatic, "BON3PCUON3PCUABABBAAABoAAAAAMw")
+	assertNilErr(t, err)
+	assertBoolsEqual(t, true, allowSync)
+}
+
+func TestProhibitedPurposes(t *testing.T) {
+	vendorListData := mockVendorListData(t, 1, map[uint16]*purposes{
+		2: &purposes{
+			purposes: []uint8{1}, // cookie reads/writes
+		},
+		3: &purposes{
+			purposes: []uint8{3}, // ad personalization
+		},
+	})
+	perms := permissionsImpl{
+		cfg: config.GDPR{
+			HostVendorID: 2,
+		},
+		vendorIDs: map[openrtb_ext.BidderName]uint16{
+			openrtb_ext.BidderAppnexus: 2,
+			openrtb_ext.BidderPubmatic: 3,
+		},
+		fetchVendorList: listFetcher(map[uint16]vendorlist.VendorList{
+			1: parseVendorListData(t, vendorListData),
+		}),
+	}
+
+	allowSync, err := perms.HostCookiesAllowed(context.Background(), "BON3PCUON3PCUABABBAAABAAAAAAMw")
+	assertNilErr(t, err)
+	assertBoolsEqual(t, false, allowSync)
+
+	allowSync, err = perms.BidderSyncAllowed(context.Background(), openrtb_ext.BidderPubmatic, "BON3PCUON3PCUABABBAAABAAAAAAMw")
+	assertNilErr(t, err)
+	assertBoolsEqual(t, false, allowSync)
+}
+
+func TestProhibitedVendors(t *testing.T) {
+	vendorListData := mockVendorListData(t, 1, map[uint16]*purposes{
+		2: &purposes{
+			purposes: []uint8{1}, // cookie reads/writes
+		},
+		3: &purposes{
+			purposes: []uint8{3}, // ad personalization
+		},
+	})
+	perms := permissionsImpl{
+		cfg: config.GDPR{
+			HostVendorID: 2,
+		},
+		vendorIDs: map[openrtb_ext.BidderName]uint16{
+			openrtb_ext.BidderAppnexus: 2,
+			openrtb_ext.BidderPubmatic: 3,
+		},
+		fetchVendorList: listFetcher(map[uint16]vendorlist.VendorList{
+			1: parseVendorListData(t, vendorListData),
+		}),
+	}
+
+	allowSync, err := perms.HostCookiesAllowed(context.Background(), "BON3PCUON3PCUABABBAAABoAAAAANA")
+	assertNilErr(t, err)
+	assertBoolsEqual(t, false, allowSync)
+
+	allowSync, err = perms.BidderSyncAllowed(context.Background(), openrtb_ext.BidderPubmatic, "BON3PCUON3PCUABABBAAABoAAAAANA")
+	assertNilErr(t, err)
+	assertBoolsEqual(t, false, allowSync)
+}
+
+func parseVendorListData(t *testing.T, data string) vendorlist.VendorList {
+	t.Helper()
+	parsed, err := vendorlist.ParseEagerly([]byte(data))
+	if err != nil {
+		t.Fatalf("Failed to parse vendor list data. %v", err)
+	}
+	return parsed
+}
+
+func listFetcher(lists map[uint16]vendorlist.VendorList) func(context.Context, uint16) (vendorlist.VendorList, error) {
+	return func(ctx context.Context, id uint16) (vendorlist.VendorList, error) {
+		data, ok := lists[id]
+		if ok {
+			return data, nil
+		} else {
+			return nil, fmt.Errorf("vendorlist id=%d not found", id)
+		}
 	}
 }
 
-// mockServer returns a handler which returns the given response for each global vendor list version.
-//
-// If the "version" query param doesn't exist, it returns a 400.
-//
-// If the "version" query param points to a version which doesn't exist, it returns a 403.
-// Don't ask why... that's just what the official page is doing. See https://vendorlist.consensu.org/v-9999/vendorlist.json
-func mockServer(responses map[int]string) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, req *http.Request) {
-		version := req.URL.Query().Get("version")
-		versionInt, err := strconv.Atoi(version)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("Request had invalid version: " + version))
-			return
-		}
-		response, ok := responses[versionInt]
-		if !ok {
-			w.WriteHeader(http.StatusForbidden)
-			w.Write([]byte("Version not found: " + version))
-			return
-		}
-		w.Write([]byte(response))
+func failedListFetcher(ctx context.Context, id uint16) (vendorlist.VendorList, error) {
+	return nil, errors.New("vendor list can't be fetched")
+}
+
+func assertNilErr(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+}
+
+func assertErr(t *testing.T, err error) {
+	t.Helper()
+	if err == nil {
+		t.Errorf("Expected error did not occur.")
+	}
+}
+
+func assertBoolsEqual(t *testing.T, expected bool, actual bool) {
+	t.Helper()
+	if expected != actual {
+		t.Errorf("Expected %t, got %t", expected, actual)
 	}
 }
---
+++ gdpr/vendorlist-fetching.go
@@ -0,0 +1,129 @@
+package gdpr
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/golang/glog"
+	"github.com/prebid/go-gdpr/vendorlist"
+	"golang.org/x/net/context/ctxhttp"
+)
+
+// This file provides the vendorlist-fetching function for Prebid Server.
+//
+// For more info, see https://github.com/prebid/prebid-server/issues/504
+//
+// Nothing in this file is exported. Public APIs can be found in gdpr.go
+
+func newVendorListFetcher(initContext context.Context, client *http.Client, urlMaker func(uint16) string) func(ctx context.Context, id uint16) (vendorlist.VendorList, error) {
+	// These save and load functions can be used to store & retrieve lists from our cache.
+	save, load := newVendorListCache()
+	populateCache(initContext, client, urlMaker, save)
+
+	saveOneSometimes := newOccasionalSaver()
+
+	return func(ctx context.Context, id uint16) (vendorlist.VendorList, error) {
+		list := load(id)
+		if list != nil {
+			return list, nil
+		}
+		saveOneSometimes(ctx, client, urlMaker(id), save)
+		list = load(id)
+		if list != nil {
+			return list, nil
+		}
+		return nil, fmt.Errorf("gdpr vendor list version %d does not exist, or has not been loaded yet. Try again in a few minutes", id)
+	}
+}
+
+// populateCache saves all the known versions of the vendor list for future use.
+func populateCache(ctx context.Context, client *http.Client, urlMaker func(uint16) string, saver func(id uint16, list vendorlist.VendorList)) {
+	latestVersion := saveOne(ctx, client, urlMaker(0), saver)
+
+	for i := uint16(1); i < latestVersion; i++ {
+		saveOne(ctx, client, urlMaker(i), saver)
+	}
+}
+
+// Make a URL which can be used to fetch a given version of the Global Vendor List. If the version is 0,
+// this will fetch the latest version.
+func vendorListURLMaker(version uint16) string {
+	if version == 0 {
+		return "https://vendorlist.consensu.org/vendorlist.json"
+	}
+	return "https://vendorlist.consensu.org/v-" + strconv.Itoa(int(version)) + "/vendorlist.json"
+}
+
+// newOccasionalSaver returns a wrapped version of saveOne() which only activates every few minutes.
+//
+// The goal here is to update quickly when new versions of the VendorList are released, but not wreck
+// server performance if a bad CMP starts sending us malformed consent strings that advertize a version
+// that doesn't exist yet.
+func newOccasionalSaver() func(ctx context.Context, client *http.Client, url string, saver func(id uint16, list vendorlist.VendorList)) {
+	lastSaved := &atomic.Value{}
+	lastSaved.Store(time.Time{})
+
+	return func(ctx context.Context, client *http.Client, url string, saver func(id uint16, list vendorlist.VendorList)) {
+		now := time.Now()
+		if now.Sub(lastSaved.Load().(time.Time)).Minutes() > 10 {
+			saveOne(ctx, client, url, saver)
+			lastSaved.Store(now)
+		}
+	}
+}
+
+func saveOne(ctx context.Context, client *http.Client, url string, saver func(id uint16, list vendorlist.VendorList)) uint16 {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		glog.Errorf("Failed to build GET %s request. Cookie syncs may be affected: %v", url, err)
+		return 0
+	}
+
+	resp, err := ctxhttp.Do(ctx, client, req)
+	if err != nil {
+		glog.Errorf("Error calling GET %s. Cookie syncs may be affected: %v", url, err)
+		return 0
+	}
+	defer resp.Body.Close()
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		glog.Errorf("Error reading response body from GET %s. Cookie syncs may be affected: %v", url, err)
+		return 0
+	}
+	if resp.StatusCode != http.StatusOK {
+		glog.Errorf("GET %s returned %d. Cookie syncs may be affected.", url, resp.StatusCode)
+		return 0
+	}
+
+	newList, err := vendorlist.ParseEagerly(respBody)
+	if err != nil {
+		glog.Errorf("GET %s returned malformed JSON. Cookie syncs may be affected. Error was %v. Body was %s", url, err, string(respBody))
+		return 0
+	}
+
+	saver(newList.Version(), newList)
+	return newList.Version()
+}
+
+func newVendorListCache() (save func(id uint16, list vendorlist.VendorList), load func(id uint16) vendorlist.VendorList) {
+	cache := &sync.Map{}
+
+	save = func(id uint16, list vendorlist.VendorList) {
+		cache.Store(id, list)
+	}
+	load = func(id uint16) vendorlist.VendorList {
+		list, ok := cache.Load(id)
+		if ok {
+			return list.(vendorlist.VendorList)
+		}
+		return nil
+	}
+	return
+}
