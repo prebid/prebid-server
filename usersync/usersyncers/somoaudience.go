9d280fe10925a84b38fb93a296f5b1b8f82afb10
Bryan DeLong <ananon@gmail.com>
2018-05-21 11:19:08 -0400

    Added Tempoary Query String for gdpr detection

--- usersync/usersyncers/somoaudience.go
+++ usersync/usersyncers/somoaudience.go
@@ -9,7 +9,7 @@ func NewSomoaudienceSyncer(externalURL string) *syncer {
 	externalURL = strings.TrimRight(externalURL, "/")
 	redirectURL := url.QueryEscape(externalURL) + "%2Fsetuid%3Fbidder%3Dsomoaudience%26gdpr%3D{{gdpr}}%26gdpr_consent%3D{{gdpr_consent}}%26uid%3D%24UID"
 
-	usersyncURL := "//publisher-east.mobileadtrading.com/usersync?ru="
+	usersyncURL := "//publisher-east.mobileadtrading.com/usersync?gdprg=1&ru="
 
 	return &syncer{
 		familyName:          "somoaudience",
