1ebb2fb0ffa81d704f4b954fba954bd79de71109
Smitha Ammassamveettil <smithaa@yahoo-inc.com>
2018-05-22 11:53:15 -0700

    [Issue-518]Oath's Prebid Server adapter - Merged with latest master and updates accordingly

--- usersync/usersyncers/oath.go
+++ usersync/usersyncers/oath.go
@@ -1,24 +1,18 @@
 package usersyncers
 
 import (
-	"fmt"
-	"github.com/prebid/prebid-server/usersync"
 	"net/url"
 	"strings"
 )
 
-func NewOathSyncer(usersyncURL string, externalURL string) *syncer {
+func NewOathSyncer(userSyncURL string, externalURL string) *syncer {
 	externalURL = strings.TrimRight(externalURL, "/")
-	redirectURL := fmt.Sprintf("%s/setuid?bidder=oath&uid=${UID}", externalURL)
+	redirectURL := url.QueryEscape(externalURL) + "%2Fsetuid%3Fbidder%3Doath%26gdpr%3D{{gdpr}}%26gdpr_consent%3D{{gdpr_consent}}%26uid%3D%24%7BUID%7D"
 
 	return &syncer{
-		familyName:   "oath",
-		gdprVendorID: 25,
-		syncInfo: &usersync.UsersyncInfo{
-			URL:         fmt.Sprintf(usersyncURL, url.QueryEscape(redirectURL)),
-			Type:        "redirect",
-			SupportCORS: false,
-		},
+		familyName:          "oath",
+		gdprVendorID:        25,
+		syncEndpointBuilder: resolveMacros("http://east-bid.ybp.yahoo.com/sync/appnexuspbs?url=" + redirectURL),
+		syncType:            SyncTypeRedirect,
 	}
-
 }
--- usersync/usersyncers/oath_test.go
+++ usersync/usersyncers/oath_test.go
@@ -5,17 +5,17 @@ import (
 )
 
 func TestOathSyncer(t *testing.T) {
-	url := "http://east-bid.ybp.yahoo.com/sync/appnexuspbs?url=localhost%2Fsetuid%3Fbidder%3Doath%26uid%3D%24%7BUID%7D"
 
-	syncInfo := NewOathSyncer("http://east-bid.ybp.yahoo.com/sync/appnexuspbs?url=%s", "localhost").GetUsersyncInfo()
+	oath := NewOathSyncer("http://east-bid.ybp.yahoo.com/sync/appnexuspbs?url=", "localhost")
+	syncInfo := oath.GetUsersyncInfo("", "")
+	assertStringsMatch(t, "http://east-bid.ybp.yahoo.com/sync/appnexuspbs?url=localhost%2Fsetuid%3Fbidder%3Doath%26gdpr%3D%26gdpr_consent%3D%26uid%3D%24%7BUID%7D", syncInfo.URL)
+	assertStringsMatch(t, "redirect", syncInfo.Type)
 
-	if syncInfo.URL != url {
-		t.Fatalf("should have matched")
-	}
-	if syncInfo.Type != "redirect" {
-		t.Fatalf("should be redirect")
-	}
 	if syncInfo.SupportCORS != false {
 		t.Fatalf("should have been false")
+	}
+
+	if oath.GDPRVendorID() != 25 {
+		t.Errorf("Wrong Oath GDPR VendorID. Got %d", oath.GDPRVendorID())
 	}
 }
