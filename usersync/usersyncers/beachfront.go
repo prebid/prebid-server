85622124ac2acddee6210684383ef4021c7dc71b
jim <naumdev@gmail.com>
2018-05-23 09:47:31 -0400

    testing a host cookie

--- usersync/usersyncers/beachfront.go
+++ usersync/usersyncers/beachfront.go
@@ -3,10 +3,11 @@ package usersyncers
 import (
 	"fmt"
 	"github.com/prebid/prebid-server/usersync"
+	"github.com/prebid/prebid-server/config"
 )
 
-func NewBeachfrontSyncer(usersyncURL string, platformId string) usersync.Usersyncer {
-	url := fmt.Sprintf("%s%s", usersyncURL, platformId)
+func NewBeachfrontSyncer(usersyncURL string, platformId string, hostCookie config.Cookie) usersync.Usersyncer {
+	url := fmt.Sprintf("%s%s&yourmom=", usersyncURL, platformId, hostCookie.Value)
 
 	return &syncer{
 		familyName:          "beachfront",
