219f78feabf3798a8806dd0103bf746ecfff6177
Bryan DeLong <ananon@gmail.com>
2018-06-01 11:28:38 -0400

    Rebase fixes

--- usersync/usersyncers/somoaudience.go
+++ usersync/usersyncers/somoaudience.go
@@ -7,16 +7,12 @@ import (
 
 func NewSomoaudienceSyncer(externalURL string) *syncer {
 	externalURL = strings.TrimRight(externalURL, "/")
-<<<<<<< HEAD
-	redirectURL := url.QueryEscape(externalURL) + "%2Fsetuid%3Fbidder%3Dsomoaudience%26gdpr%3D{{gdpr}}%26gdpr_consent%3D{{gdpr_consent}}%26uid%3D%24UID"
-
-	usersyncURL := "//publisher-east.mobileadtrading.com/usersync?gdprg=1&ru="
-=======
 	redirectURL := url.QueryEscape(externalURL) + "%2Fsetuid%3Fbidder%3Dmobileadtrading%26gdpr%3D{{gdpr}}%26gdpr_consent%3D{{gdpr_consent}}%26uid%3D%24%7BUID%7D"
->>>>>>> 8615830... Further Tweaks
+
+	usersyncURL := "//publisher-east.mobileadtrading.com/usersync?ru="
 
 	return &syncer{
-		familyName:          "somoaudience",
+		familyName:          "mobileadtrading",
 		syncEndpointBuilder: resolveMacros(usersyncURL + redirectURL),
 		syncType:            SyncTypeRedirect,
 	}
