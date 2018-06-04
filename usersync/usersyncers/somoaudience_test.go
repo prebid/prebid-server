fc2b30e762ee189dcb422b2066e6b712687631af
Dave Bemiller <27972385+dbemiller@users.noreply.github.com>
2018-06-04 11:32:44 -0400

    Fixed broken test.

--- usersync/usersyncers/somoaudience_test.go
+++ usersync/usersyncers/somoaudience_test.go
@@ -7,7 +7,7 @@ import (
 func TestSomoaudienceSyncer(t *testing.T) {
 	somo := NewSomoaudienceSyncer("localhost")
 	syncInfo := somo.GetUsersyncInfo("", "")
-	if syncInfo.URL != "//publisher-east.mobileadtrading.com/usersync?ru=localhost%2Fsetuid%3Fbidder%3Dmobileadtrading%26gdpr%3D%26gdpr_consent%3D%26uid%3D%24%7BUID%7D" {
+	if syncInfo.URL != "//publisher-east.mobileadtrading.com/usersync?ru=localhost%2Fsetuid%3Fbidder%3Dsomoaudience%26gdpr%3D%26gdpr_consent%3D%26uid%3D%24%7BUID%7D" {
 		t.Fatalf("should have matched")
 	}
 	if syncInfo.Type != "redirect" {
