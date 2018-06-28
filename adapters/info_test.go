b2b8687dde957c5c9a5a1a062f09209b44157b4e
Dave Bemiller <27972385+dbemiller@users.noreply.github.com>
2018-06-28 09:22:11 -0400

    Used testify in these tests.

--- adapters/info_test.go
+++ adapters/info_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/mxmCherry/openrtb"
 	"github.com/prebid/prebid-server/adapters"
 	"github.com/prebid/prebid-server/openrtb_ext"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAppNotSupported(t *testing.T) {
@@ -22,11 +23,11 @@ func TestAppNotSupported(t *testing.T) {
 	bids, errs := constrained.MakeRequests(&openrtb.BidRequest{
 		App: &openrtb.App{},
 	})
-	if len(errs) != 1 || errs[0].Error() != "this bidder does not support app requests" {
-		t.Errorf("Unexpected error: %s", errs[0].Error())
+	if !assert.Len(t, errs, 1) || !assert.EqualError(t, errs[0], "this bidder does not support app requests") {
+		return
 	}
-	if len(bids) != 0 {
-		t.Errorf("Got %d unexpected bids", len(bids))
+	if !assert.Len(t, bids, 0) {
+		return
 	}
 }
 
@@ -43,12 +44,33 @@ func TestSiteNotSupported(t *testing.T) {
 	bids, errs := constrained.MakeRequests(&openrtb.BidRequest{
 		Site: &openrtb.Site{},
 	})
-	if len(errs) != 1 || errs[0].Error() != "this bidder does not support site requests" {
-		t.Errorf("Unexpected error: %s", errs[0].Error())
+	if !assert.Len(t, errs, 1) || !assert.EqualError(t, errs[0], "this bidder does not support site requests") {
+		return
 	}
-	if len(bids) != 0 {
-		t.Errorf("Got %d unexpected bids", len(bids))
+	if !assert.Len(t, bids, 0) {
+		return
 	}
+}
+
+func TestImpFiltering(t *testing.T) {
+	bidder := &mockBidder{}
+	info := adapters.BidderInfo{
+		Capabilities: &adapters.CapabilitiesInfo{
+			Site: &adapters.PlatformInfo{
+				MediaTypes: []openrtb_ext.BidType{openrtb_ext.BidTypeVideo},
+			},
+			App: &adapters.PlatformInfo{
+				MediaTypes: []openrtb_ext.BidType{openrtb_ext.BidTypeBanner},
+			},
+		},
+	}
+
+	constrained := adapters.EnforceBidderInfo(bidder, info)
+	_, _ = constrained.MakeRequests(&openrtb.BidRequest{
+		Imp:  []openrtb.Imp{},
+		Site: &openrtb.Site{},
+	})
+
 }
 
 type mockBidder struct {
@@ -70,23 +92,16 @@ func TestParsing(t *testing.T) {
 	if infos[string(mockBidderName)].Maintainer.Email != "some-email@domain.com" {
 		t.Errorf("Bad maintainer email. Got %s", infos[string(mockBidderName)].Maintainer.Email)
 	}
-	assertBoolsEqual(t, true, infos.HasAppSupport(mockBidderName))
-	assertBoolsEqual(t, true, infos.HasSiteSupport(mockBidderName))
+	assert.Equal(t, true, infos.HasAppSupport(mockBidderName))
+	assert.Equal(t, true, infos.HasSiteSupport(mockBidderName))
 
-	assertBoolsEqual(t, true, infos.SupportsAppMediaType(mockBidderName, openrtb_ext.BidTypeBanner))
-	assertBoolsEqual(t, false, infos.SupportsAppMediaType(mockBidderName, openrtb_ext.BidTypeVideo))
-	assertBoolsEqual(t, false, infos.SupportsAppMediaType(mockBidderName, openrtb_ext.BidTypeAudio))
-	assertBoolsEqual(t, true, infos.SupportsAppMediaType(mockBidderName, openrtb_ext.BidTypeNative))
+	assert.Equal(t, true, infos.SupportsAppMediaType(mockBidderName, openrtb_ext.BidTypeBanner))
+	assert.Equal(t, false, infos.SupportsAppMediaType(mockBidderName, openrtb_ext.BidTypeVideo))
+	assert.Equal(t, false, infos.SupportsAppMediaType(mockBidderName, openrtb_ext.BidTypeAudio))
+	assert.Equal(t, true, infos.SupportsAppMediaType(mockBidderName, openrtb_ext.BidTypeNative))
 
-	assertBoolsEqual(t, true, infos.SupportsWebMediaType(mockBidderName, openrtb_ext.BidTypeBanner))
-	assertBoolsEqual(t, true, infos.SupportsWebMediaType(mockBidderName, openrtb_ext.BidTypeVideo))
-	assertBoolsEqual(t, false, infos.SupportsWebMediaType(mockBidderName, openrtb_ext.BidTypeAudio))
-	assertBoolsEqual(t, true, infos.SupportsWebMediaType(mockBidderName, openrtb_ext.BidTypeNative))
-}
-
-func assertBoolsEqual(t *testing.T, expected bool, actual bool) {
-	t.Helper()
-	if actual != expected {
-		t.Errorf("expected %t, got %t", expected, actual)
-	}
+	assert.Equal(t, true, infos.SupportsWebMediaType(mockBidderName, openrtb_ext.BidTypeBanner))
+	assert.Equal(t, true, infos.SupportsWebMediaType(mockBidderName, openrtb_ext.BidTypeVideo))
+	assert.Equal(t, false, infos.SupportsWebMediaType(mockBidderName, openrtb_ext.BidTypeAudio))
+	assert.Equal(t, true, infos.SupportsWebMediaType(mockBidderName, openrtb_ext.BidTypeNative))
 }
