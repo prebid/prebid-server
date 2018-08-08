96e70ca0229f4afc92a4f832280d16e665d41a24
Dave Bemiller <27972385+dbemiller@users.noreply.github.com>
2018-08-08 10:39:58 -0400

    Added a regression test for the cors bug.

--- pbs_light_test.go
+++ pbs_light_test.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/mxmCherry/openrtb"
 	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
 
 	"github.com/prebid/prebid-server/cache/dummycache"
 	"github.com/prebid/prebid-server/config"
@@ -686,6 +687,19 @@ func TestPanicRecovery(t *testing.T) {
 	}
 	recovered := recoverSafely(panicker)
 	recovered(nil, pbsmetrics.AdapterLabels{})
+}
+
+// Prevents #648
+func TestCORSSupport(t *testing.T) {
+	handler := func(w http.ResponseWriter, r *http.Request) {}
+	cors := supportCORS(http.HandlerFunc(handler))
+	rr := httptest.NewRecorder()
+	req, err := http.NewRequest("POST", "http://some-domain.com/openrtb2/auction", nil)
+	if !assert.NoError(t, err) {
+		return
+	}
+	cors.ServeHTTP(rr, req)
+	assert.NotEqual(t, "*", rr.Header().Get("Access-Control-Allow-Origin"))
 }
 
 func compareStrings(t *testing.T, message string, expect string, actual string) {
