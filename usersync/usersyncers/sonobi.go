8a348f6f0145d0b44e6acc2d7b97d699e297a413
Jonathan Go <jgo@sonobi.com>
2018-08-21 13:51:22 -0400

    user syncer working

--- usersync/usersyncers/sonobi.go
+++ usersync/usersyncers/sonobi.go
@@ -1,17 +1,17 @@
 package usersyncers
 
 import (
-	"net/url"
 	"github.com/prebid/prebid-server/usersync"
+	"net/url"
 )
 
 func NewSonobiSyncer(externalURL string) usersync.Usersyncer {
 	redirectURI := url.QueryEscape(externalURL) + "%2Fsetuid%3Fbidder%3Dsonobi%26consent_string%3D{{gdpr}}%26gdpr%3D{{gdpr_consent}}%26uid%3D%24UID"
-	usersyncURL := "http://sync.go.sonobi.com/us.gif?loc="
+	usersyncURL := "//sync.go.sonobi.com/us.gif?loc="
 
 	return &syncer{
 		familyName:          "sonobi",
-		gdprVendorID:        32,
+		gdprVendorID:        104,
 		syncEndpointBuilder: resolveMacros(usersyncURL + redirectURI),
 		syncType:            SyncTypeRedirect,
 	}
