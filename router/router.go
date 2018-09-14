e7f02215a1b5fa47b3cc46bd1048383f6e55fb09
Zach Badgett <zach.badgett@gmail.com>
2018-09-14 08:41:30 -0600

    Move router and legacy auction to router directory (#673)
    * Move router and legacy auction to router directory
    
    * Move legacy auction to endpoints package, update tests
    
    * Omit main package from converage requirement
    
    Generally, we should withhold from adding new functions to the main
    file. All calls made should be covered in each individual package.
    
    * Add more tests to router package

---
+++ router/router.go
@@ -0,0 +1,263 @@
+package router
+
+import (
+	"context"
+	"crypto/tls"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/prebid/prebid-server/adapters"
+	"github.com/prebid/prebid-server/adapters/adform"
+	"github.com/prebid/prebid-server/adapters/appnexus"
+	"github.com/prebid/prebid-server/adapters/audienceNetwork"
+	"github.com/prebid/prebid-server/adapters/conversant"
+	"github.com/prebid/prebid-server/adapters/indexExchange"
+	"github.com/prebid/prebid-server/adapters/lifestreet"
+	"github.com/prebid/prebid-server/adapters/pubmatic"
+	"github.com/prebid/prebid-server/adapters/pulsepoint"
+	"github.com/prebid/prebid-server/adapters/rubicon"
+	"github.com/prebid/prebid-server/adapters/sovrn"
+	analyticsConf "github.com/prebid/prebid-server/analytics/config"
+	"github.com/prebid/prebid-server/cache"
+	"github.com/prebid/prebid-server/cache/dummycache"
+	"github.com/prebid/prebid-server/cache/filecache"
+	"github.com/prebid/prebid-server/cache/postgrescache"
+	"github.com/prebid/prebid-server/config"
+	"github.com/prebid/prebid-server/endpoints"
+	infoEndpoints "github.com/prebid/prebid-server/endpoints/info"
+	"github.com/prebid/prebid-server/endpoints/openrtb2"
+	"github.com/prebid/prebid-server/exchange"
+	"github.com/prebid/prebid-server/gdpr"
+	"github.com/prebid/prebid-server/openrtb_ext"
+	"github.com/prebid/prebid-server/pbs"
+	metricsConf "github.com/prebid/prebid-server/pbsmetrics/config"
+	pbc "github.com/prebid/prebid-server/prebid_cache_client"
+	"github.com/prebid/prebid-server/ssl"
+	storedRequestsConf "github.com/prebid/prebid-server/stored_requests/config"
+	"github.com/prebid/prebid-server/usersync/usersyncers"
+
+	"github.com/golang/glog"
+	"github.com/julienschmidt/httprouter"
+	_ "github.com/lib/pq"
+	"github.com/rs/cors"
+)
+
+var dataCache cache.Cache
+var exchanges map[string]adapters.Adapter
+
+const schemaDirectory = "../static/bidder-params"
+
+// NewJsonDirectoryServer is used to serve .json files from a directory as a single blob. For example,
+// given a directory containing the files "a.json" and "b.json", this returns a Handle which serves JSON like:
+//
+// {
+//   "a": { ... content from the file a.json ... },
+//   "b": { ... content from the file b.json ... }
+// }
+//
+// This function stores the file contents in memory, and should not be used on large directories.
+// If the root directory, or any of the files in it, cannot be read, then the program will exit.
+func NewJsonDirectoryServer(validator openrtb_ext.BidderParamValidator) httprouter.Handle {
+	// Slurp the files into memory first, since they're small and it minimizes request latency.
+	files, err := ioutil.ReadDir(schemaDirectory)
+	if err != nil {
+		glog.Fatalf("Failed to read directory %s: %v", schemaDirectory, err)
+	}
+
+	data := make(map[string]json.RawMessage, len(files))
+	for _, file := range files {
+		bidder := strings.TrimSuffix(file.Name(), ".json")
+		bidderName, isValid := openrtb_ext.BidderMap[bidder]
+		if !isValid {
+			glog.Fatalf("Schema exists for an unknown bidder: %s", bidder)
+		}
+		data[bidder] = json.RawMessage(validator.Schema(bidderName))
+	}
+	response, err := json.Marshal(data)
+	if err != nil {
+		glog.Fatalf("Failed to marshal bidder param JSON-schema: %v", err)
+	}
+
+	return func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		w.Header().Add("Content-Type", "application/json")
+		w.Write(response)
+	}
+}
+
+func serveIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	http.ServeFile(w, r, "static/index.html")
+}
+
+type NoCache struct {
+	Handler http.Handler
+}
+
+func (m NoCache) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Cache-Control", "no-cache, no-store, must-revalidate")
+	w.Header().Add("Pragma", "no-cache")
+	w.Header().Add("Expires", "0")
+	m.Handler.ServeHTTP(w, r)
+}
+
+func loadDataCache(cfg *config.Configuration, db *sql.DB) (err error) {
+	switch cfg.DataCache.Type {
+	case "dummy":
+		dataCache, err = dummycache.New()
+		if err != nil {
+			glog.Fatalf("Dummy cache not configured: %s", err.Error())
+		}
+
+	case "postgres":
+		if db == nil {
+			return fmt.Errorf("Nil db cannot connect to postgres. Did you forget to set the config.stored_requests.postgres values?")
+		}
+		dataCache = postgrescache.New(db, postgrescache.CacheConfig{
+			Size: cfg.DataCache.CacheSize,
+			TTL:  cfg.DataCache.TTLSeconds,
+		})
+		return nil
+	case "filecache":
+		dataCache, err = filecache.New(cfg.DataCache.Filename)
+		if err != nil {
+			return fmt.Errorf("FileCache Error: %s", err.Error())
+		}
+
+	default:
+		return fmt.Errorf("Unknown datacache.type: %s", cfg.DataCache.Type)
+	}
+	return nil
+}
+
+func newExchangeMap(cfg *config.Configuration) map[string]adapters.Adapter {
+	// These keys _must_ coincide with the bidder code in Prebid.js, if the adapter exists in both projects
+	return map[string]adapters.Adapter{
+		"appnexus":      appnexus.NewAppNexusAdapter(adapters.DefaultHTTPAdapterConfig, cfg.Adapters[string(openrtb_ext.BidderAppnexus)].Endpoint),
+		"districtm":     appnexus.NewAppNexusAdapter(adapters.DefaultHTTPAdapterConfig, cfg.Adapters[string(openrtb_ext.BidderAppnexus)].Endpoint),
+		"indexExchange": indexExchange.NewIndexAdapter(adapters.DefaultHTTPAdapterConfig, cfg.Adapters[strings.ToLower(string(openrtb_ext.BidderIndex))].Endpoint),
+		"pubmatic":      pubmatic.NewPubmaticAdapter(adapters.DefaultHTTPAdapterConfig, cfg.Adapters[string(openrtb_ext.BidderPubmatic)].Endpoint),
+		"pulsepoint":    pulsepoint.NewPulsePointAdapter(adapters.DefaultHTTPAdapterConfig, cfg.Adapters[string(openrtb_ext.BidderPulsepoint)].Endpoint),
+		"rubicon": rubicon.NewRubiconAdapter(adapters.DefaultHTTPAdapterConfig, cfg.Adapters[string(openrtb_ext.BidderRubicon)].Endpoint,
+			cfg.Adapters[string(openrtb_ext.BidderRubicon)].XAPI.Username, cfg.Adapters[string(openrtb_ext.BidderRubicon)].XAPI.Password, cfg.Adapters[string(openrtb_ext.BidderRubicon)].XAPI.Tracker),
+		"audienceNetwork": audienceNetwork.NewAdapterFromFacebook(adapters.DefaultHTTPAdapterConfig, cfg.Adapters[strings.ToLower(string(openrtb_ext.BidderFacebook))].PlatformID),
+		"lifestreet":      lifestreet.NewLifestreetAdapter(adapters.DefaultHTTPAdapterConfig, cfg.Adapters[string(openrtb_ext.BidderLifestreet)].Endpoint),
+		"conversant":      conversant.NewConversantAdapter(adapters.DefaultHTTPAdapterConfig, cfg.Adapters[string(openrtb_ext.BidderConversant)].Endpoint),
+		"adform":          adform.NewAdformAdapter(adapters.DefaultHTTPAdapterConfig, cfg.Adapters[string(openrtb_ext.BidderAdform)].Endpoint),
+		"sovrn":           sovrn.NewSovrnAdapter(adapters.DefaultHTTPAdapterConfig, cfg.Adapters[string(openrtb_ext.BidderSovrn)].Endpoint),
+	}
+}
+
+type Router struct {
+	*httprouter.Router
+	MetricsEngine   *metricsConf.DetailedMetricsEngine
+	ParamsValidator openrtb_ext.BidderParamValidator
+	Shutdown        func()
+}
+
+func New(cfg *config.Configuration) (r *Router, err error) {
+	r = &Router{
+		Router: httprouter.New(),
+	}
+	theClient := &http.Client{
+		Transport: &http.Transport{
+			MaxIdleConns:        400,
+			MaxIdleConnsPerHost: 10,
+			IdleConnTimeout:     60 * time.Second,
+			TLSClientConfig:     &tls.Config{RootCAs: ssl.GetRootCAPool()},
+		},
+	}
+	fetcher, ampFetcher, db, shutdown := storedRequestsConf.NewStoredRequests(&cfg.StoredRequests, theClient, r.Router)
+	// todo(zachbadgett): better shutdown
+	r.Shutdown = shutdown
+	if err := loadDataCache(cfg, db); err != nil {
+		return nil, fmt.Errorf("Prebid Server could not load data cache: %v", err)
+	}
+
+	pbsAnalytics := analyticsConf.NewPBSAnalytics(&cfg.Analytics)
+
+	// Hack because of how legacy handles districtm
+	bidderList := openrtb_ext.BidderList()
+	bidderList = append(bidderList, openrtb_ext.BidderName("districtm"))
+
+	// Metrics engine
+	r.MetricsEngine = metricsConf.NewMetricsEngine(cfg, bidderList)
+
+	paramsValidator, err := openrtb_ext.NewBidderParamsValidator(schemaDirectory)
+	if err != nil {
+		glog.Fatalf("Failed to create the bidder params validator. %v", err)
+	}
+
+	p, _ := filepath.Abs("../static/bidder-info")
+	bidderInfos := adapters.ParseBidderInfos(p, openrtb_ext.BidderList())
+
+	syncers := usersyncers.NewSyncerMap(cfg)
+	gdprPerms := gdpr.NewPermissions(context.Background(), cfg.GDPR, usersyncers.GDPRAwareSyncerIDs(syncers), theClient)
+
+	exchanges = newExchangeMap(cfg)
+	theExchange := exchange.NewExchange(theClient, pbc.NewClient(&cfg.CacheURL), cfg, r.MetricsEngine, bidderInfos, gdprPerms)
+
+	openrtbEndpoint, err := openrtb2.NewEndpoint(theExchange, paramsValidator, fetcher, cfg, r.MetricsEngine, pbsAnalytics)
+	if err != nil {
+		glog.Fatalf("Failed to create the openrtb endpoint handler. %v", err)
+	}
+
+	ampEndpoint, err := openrtb2.NewAmpEndpoint(theExchange, paramsValidator, ampFetcher, cfg, r.MetricsEngine, pbsAnalytics)
+	if err != nil {
+		glog.Fatalf("Failed to create the amp endpoint handler. %v", err)
+	}
+
+	r.POST("/auction", endpoints.Auction(cfg, syncers, gdprPerms, r.MetricsEngine, dataCache, exchanges))
+	r.POST("/openrtb2/auction", openrtbEndpoint)
+	r.GET("/openrtb2/amp", ampEndpoint)
+	r.GET("/info/bidders", infoEndpoints.NewBiddersEndpoint())
+	r.GET("/info/bidders/:bidderName", infoEndpoints.NewBidderDetailsEndpoint(bidderInfos))
+	r.GET("/bidders/params", NewJsonDirectoryServer(paramsValidator))
+	r.POST("/cookie_sync", endpoints.NewCookieSyncEndpoint(syncers, cfg, gdprPerms, r.MetricsEngine, pbsAnalytics))
+	r.GET("/status", endpoints.NewStatusEndpoint(cfg.StatusResponse))
+	r.GET("/", serveIndex)
+	r.ServeFiles("/static/*filepath", http.Dir("static"))
+
+	userSyncDeps := &pbs.UserSyncDeps{
+		HostCookieConfig: &(cfg.HostCookie),
+		ExternalUrl:      cfg.ExternalURL,
+		RecaptchaSecret:  cfg.RecaptchaSecret,
+		MetricsEngine:    r.MetricsEngine,
+		PBSAnalytics:     pbsAnalytics,
+	}
+
+	r.GET("/setuid", endpoints.NewSetUIDEndpoint(cfg.HostCookie, gdprPerms, pbsAnalytics, r.MetricsEngine))
+	r.POST("/optout", userSyncDeps.OptOut)
+	r.GET("/optout", userSyncDeps.OptOut)
+
+	return r, nil
+}
+
+// Fixes #648
+//
+// These CORS options pose a security risk... but it's a calculated one.
+// People _must_ call us with "withCredentials" set to "true" because that's how we use the cookie sync info.
+// We also must allow all origins because every site on the internet _could_ call us.
+//
+// This is an inherent security risk. However, PBS doesn't use cookies for authorization--just identification.
+// We only store the User's ID for each Bidder, and each Bidder has already exposed a public cookie sync endpoint
+// which returns that data anyway.
+//
+// For more info, see:
+//
+// - https://github.com/rs/cors/issues/55
+// - https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS/Errors/CORSNotSupportingCredentials
+// - https://portswigger.net/blog/exploiting-cors-misconfigurations-for-bitcoins-and-bounties
+func SupportCORS(handler http.Handler) http.Handler {
+	c := cors.New(cors.Options{
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return true
+		},
+		AllowedHeaders: []string{"Origin", "X-Requested-With", "Content-Type", "Accept"}})
+	return c.Handler(handler)
+}
