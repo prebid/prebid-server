9d597ec59874da54380546ec8edab53b8870404b
Hans Hjort <hhjort@appnexus.com>
2018-09-17 15:47:20 -0400

    adds a test

--- exchange/auction_test.go
+++ exchange/auction_test.go
@@ -3,6 +3,8 @@ package exchange
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/prebid/prebid-server/openrtb_ext"
@@ -38,51 +40,57 @@ func TestMakeVASTNurl(t *testing.T) {
 func TestDoCache(t *testing.T) {
 	bidRequest := &openrtb.BidRequest{
 		Imp: []openrtb.Imp{
-			openrtb.Imp{
+			{
 				ID:  "oneImp",
 				Exp: 300,
 			},
-			openrtb.Imp{
+			{
 				ID: "twoImp",
 			},
 		},
 	}
+	bid := make([]pbsOrtbBid, 5)
 	winningBidsByBidder := make(map[string]map[openrtb_ext.BidderName]*pbsOrtbBid)
 	roundedPrices := make(map[*pbsOrtbBid]string)
 	winningBidsByBidder["oneImp"] = make(map[openrtb_ext.BidderName]*pbsOrtbBid)
-	winningBidsByBidder["oneImp"][openrtb_ext.BidderAppnexus] = &pbsOrtbBid{
+	bid[0] = pbsOrtbBid{
 		bid: &openrtb.Bid{
 			Price: 7.64,
 			Exp:   600,
 		},
 	}
+	winningBidsByBidder["oneImp"][openrtb_ext.BidderAppnexus] = &bid[0]
 	roundedPrices[winningBidsByBidder["oneImp"][openrtb_ext.BidderAppnexus]] = "7.64"
-	winningBidsByBidder["oneImp"][openrtb_ext.BidderPubmatic] = &pbsOrtbBid{
+	bid[1] = pbsOrtbBid{
 		bid: &openrtb.Bid{
 			Price: 5.64,
 			Exp:   200,
 		},
 	}
+	winningBidsByBidder["oneImp"][openrtb_ext.BidderPubmatic] = &bid[1]
 	roundedPrices[winningBidsByBidder["oneImp"][openrtb_ext.BidderPubmatic]] = "5.64"
-	winningBidsByBidder["oneImp"][openrtb_ext.BidderOpenx] = &pbsOrtbBid{
+	bid[2] = pbsOrtbBid{
 		bid: &openrtb.Bid{
 			Price: 2.3,
 		},
 	}
+	winningBidsByBidder["oneImp"][openrtb_ext.BidderOpenx] = &bid[2]
 	roundedPrices[winningBidsByBidder["oneImp"][openrtb_ext.BidderOpenx]] = "2.3"
 	winningBidsByBidder["twoImp"] = make(map[openrtb_ext.BidderName]*pbsOrtbBid)
-	winningBidsByBidder["twoImp"][openrtb_ext.BidderAppnexus] = &pbsOrtbBid{
+	bid[3] = pbsOrtbBid{
 		bid: &openrtb.Bid{
 			Price: 1.64,
 		},
 	}
+	winningBidsByBidder["twoImp"][openrtb_ext.BidderAppnexus] = &bid[3]
 	roundedPrices[winningBidsByBidder["twoImp"][openrtb_ext.BidderAppnexus]] = "1.64"
-	winningBidsByBidder["twoImp"][openrtb_ext.BidderRubicon] = &pbsOrtbBid{
+	bid[4] = pbsOrtbBid{
 		bid: &openrtb.Bid{
 			Price: 7.64,
 			Exp:   900,
 		},
 	}
+	winningBidsByBidder["twoImp"][openrtb_ext.BidderRubicon] = &bid[4]
 	roundedPrices[winningBidsByBidder["twoImp"][openrtb_ext.BidderRubicon]] = "7.64"
 	testAuction := &auction{
 		winningBidsByBidder: winningBidsByBidder,
@@ -91,13 +99,54 @@ func TestDoCache(t *testing.T) {
 	cache := &mockCache{}
 
 	testAuction.doCache(ctx, cache, true, false, bidRequest, 60)
-	json1, _ := json.Marshal(winningBidsByBidder["oneImp"][openrtb_ext.BidderAppnexus])
-	cacheable1 := prebid_cache_client.Cacheable{
+	json0, _ := json.Marshal(bid[0].bid)
+	json1, _ := json.Marshal(bid[1].bid)
+	json2, _ := json.Marshal(bid[2].bid)
+	json3, _ := json.Marshal(bid[3].bid)
+	json4, _ := json.Marshal(bid[4].bid)
+	cacheables := make([]prebid_cache_client.Cacheable, 5)
+	cacheables[0] = prebid_cache_client.Cacheable{
 		Type:       prebid_cache_client.TypeJSON,
-		TTLSeconds: 660,
+		TTLSeconds: 360,
+		Data:       json0,
+	}
+	cacheables[1] = prebid_cache_client.Cacheable{
+		Type:       prebid_cache_client.TypeJSON,
+		TTLSeconds: 260,
 		Data:       json1,
 	}
-	assert.Contains(t, "AppNexus1 cacheable not found", cache.items, cacheable1)
+	cacheables[2] = prebid_cache_client.Cacheable{
+		Type:       prebid_cache_client.TypeJSON,
+		TTLSeconds: 360,
+		Data:       json2,
+	}
+	cacheables[3] = prebid_cache_client.Cacheable{
+		Type:       prebid_cache_client.TypeJSON,
+		TTLSeconds: 0,
+		Data:       json3,
+	}
+	cacheables[4] = prebid_cache_client.Cacheable{
+		Type:       prebid_cache_client.TypeJSON,
+		TTLSeconds: 960,
+		Data:       json4,
+	}
+	found := 0
+
+	for _, cExpected := range cacheables {
+		for _, cFound := range cache.items {
+			eq, _ := jSONBytesEqual(cExpected.Data, cFound.Data)
+			if cExpected.TTLSeconds == cFound.TTLSeconds && eq {
+				found++
+			}
+		}
+	}
+
+	if found != 5 {
+		fmt.Printf("Expected:\n%v\n\n", cacheables)
+		fmt.Printf("Found:\n%v\n\n", cache.items)
+		t.Errorf("All expected cacheables not found. Expected 5, found %d.", found)
+	}
+
 }
 
 type mockCache struct {
@@ -107,4 +156,17 @@ type mockCache struct {
 func (c *mockCache) PutJson(ctx context.Context, values []prebid_cache_client.Cacheable) []string {
 	c.items = values
 	return []string{"", "", "", "", ""}
+}
+
+// JSONBytesEqual compares the JSON in two byte slices.
+// By twotwotwo via Stack Overflow (https://stackoverflow.com/questions/32408890/how-to-compare-two-json-requests)
+func jSONBytesEqual(a, b []byte) (bool, error) {
+	var j, j2 interface{}
+	if err := json.Unmarshal(a, &j); err != nil {
+		return false, err
+	}
+	if err := json.Unmarshal(b, &j2); err != nil {
+		return false, err
+	}
+	return reflect.DeepEqual(j2, j), nil
 }
