704e2f434107af0d884e5ee731c8caf0c51d02ed
Dave Bemiller <27972385+dbemiller@users.noreply.github.com>
2018-10-02 15:33:48 -0400

    Removed a bunch of tests which arent valid anymore.

--- adapters/bidder.go
+++ adapters/bidder.go
@@ -19,13 +19,12 @@ type Bidder interface {
 	//   1. Only {Imp.Type, Platform} combinations which are valid, as defined by the static/bidder-info.{bidder}.yaml file.
 	//   2. Imp.Ext of the form {"bidder": params}, where "params" has been validated against the static/bidder-params/{bidder}.json JSON Schema.
 	//
-	//
 	// nil return values are acceptable, but nil elements *inside* those slices are not.
 	//
 	// The errors should contain a list of errors which explain why this bidder's bids will be
 	// "subpar" in some way. For example: the request contained ad types which this bidder doesn't support.
 	//
-	// If the error is caused by bad user input, return a BadInputError.
+	// If the error is caused by bad user input, return an errortypes.BadInput.
 	MakeRequests(request *openrtb.BidRequest) ([]*RequestData, []error)
 
 	// MakeBids unpacks the server's response into Bids.
@@ -35,8 +34,8 @@ type Bidder interface {
 	// The errors should contain a list of errors which explain why this bidder's bids will be
 	// "subpar" in some way. For example: the server response didn't have the expected format.
 	//
-	// If the error was caused by bad user input, return a BadInputError.
-	// If the error was caused by a bad server response, return a BadServerResponseError
+	// If the error was caused by bad user input, return a errortypes.BadInput.
+	// If the error was caused by a bad server response, return a errortypes.BadServerResponse
 	MakeBids(internalRequest *openrtb.BidRequest, externalRequest *RequestData, response *ResponseData) (*BidderResponse, []error)
 }
 
