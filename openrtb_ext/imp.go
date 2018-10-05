c8c33d54552c9da078b9aa205c2275c7aad2e4af
Dinesh Kumar Jayaram <djayaram@yume.com>
2018-10-05 17:10:33 +0530

    Fixed gofmt issue

--- openrtb_ext/imp.go
+++ openrtb_ext/imp.go
@@ -2,10 +2,10 @@ package openrtb_ext
 
 // ExtImp defines the contract for bidrequest.imp[i].ext
 type ExtImp struct {
-	Prebid   *ExtImpPrebid   `json:"prebid"`
-	Appnexus *ExtImpAppnexus `json:"appnexus"`
-	Rubicon  *ExtImpRubicon  `json:"rubicon"`
-	Adform   *ExtImpAdform   `json:"adform"`
+	Prebid    *ExtImpPrebid    `json:"prebid"`
+	Appnexus  *ExtImpAppnexus  `json:"appnexus"`
+	Rubicon   *ExtImpRubicon   `json:"rubicon"`
+	Adform    *ExtImpAdform    `json:"adform"`
 	Rhythmone *ExtImpRhythmone `json:"rhythmone"`
 }
 
