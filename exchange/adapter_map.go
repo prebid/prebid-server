552f9beae4759d3e9ce33fd71b5d6a59e9033fc8
dbemiller <27972385+dbemiller@users.noreply.github.com>
2018-10-15 10:35:00 -0400

    Remove "capability violation" logic (#702)
    * Removed all logic which would have resulted in a CAPABILITY VIOLATION log.
    
    * Simplified the adapter_map to guarantee that the right middleware gets applied to each Bidder. Updated the docs on the Bidder interface.
    
    * Removed a bunch of tests which arent valid anymore.
    
    * Deleted brightroll tests which only exist to test invalid Imp logic.
    
    * Fixed adform test.

--- exchange/adapter_map.go
+++ exchange/adapter_map.go
@@ -30,36 +30,46 @@ import (
 // to register itself. No wading through Exchange code to find it.
 
 func newAdapterMap(client *http.Client, cfg *config.Configuration, infos adapters.BidderInfos) map[openrtb_ext.BidderName]adaptedBidder {
-	return map[openrtb_ext.BidderName]adaptedBidder{
-		openrtb_ext.BidderAdform:      adaptBidder(adapters.EnforceBidderInfo(adform.NewAdformBidder(client, cfg.Adapters[string(openrtb_ext.BidderAdform)].Endpoint), infos[string(openrtb_ext.BidderAdform)]), client),
-		openrtb_ext.BidderAdkernelAdn: adaptBidder(adapters.EnforceBidderInfo(adkernelAdn.NewAdkernelAdnAdapter(cfg.Adapters[strings.ToLower(string(openrtb_ext.BidderAdkernelAdn))].Endpoint), infos[string(openrtb_ext.BidderAdkernelAdn)]), client),
-		openrtb_ext.BidderAdtelligent: adaptBidder(adapters.EnforceBidderInfo(adtelligent.NewAdtelligentBidder(cfg.Adapters[string(openrtb_ext.BidderAdtelligent)].Endpoint), infos[string(openrtb_ext.BidderAdtelligent)]), client),
-		openrtb_ext.BidderAppnexus:    adaptBidder(adapters.EnforceBidderInfo(appnexus.NewAppNexusBidder(client, cfg.Adapters[string(openrtb_ext.BidderAppnexus)].Endpoint), infos[string(openrtb_ext.BidderAppnexus)]), client),
+	ortbBidders := map[openrtb_ext.BidderName]adapters.Bidder{
+		openrtb_ext.BidderAdform:      adform.NewAdformBidder(client, cfg.Adapters[string(openrtb_ext.BidderAdform)].Endpoint),
+		openrtb_ext.BidderAdkernelAdn: adkernelAdn.NewAdkernelAdnAdapter(cfg.Adapters[strings.ToLower(string(openrtb_ext.BidderAdkernelAdn))].Endpoint),
+		openrtb_ext.BidderAdtelligent: adtelligent.NewAdtelligentBidder(cfg.Adapters[string(openrtb_ext.BidderAdtelligent)].Endpoint),
+		openrtb_ext.BidderAppnexus:    appnexus.NewAppNexusBidder(client, cfg.Adapters[string(openrtb_ext.BidderAppnexus)].Endpoint),
 		// TODO #615: Update the config setup so that the Beachfront URLs can be configured, and use those in TestRaceIntegration in exchange_test.go
-		openrtb_ext.BidderBeachfront: adaptBidder(adapters.EnforceBidderInfo(beachfront.NewBeachfrontBidder(), infos[string(openrtb_ext.BidderBeachfront)]), client),
-		openrtb_ext.BidderBrightroll: adaptBidder(adapters.EnforceBidderInfo(brightroll.NewBrightrollBidder(cfg.Adapters[string(openrtb_ext.BidderBrightroll)].Endpoint), infos[string(openrtb_ext.BidderBrightroll)]), client),
-		// TODO #267: Upgrade the Conversant adapter
-		openrtb_ext.BidderConversant: adaptLegacyAdapter(conversant.NewConversantAdapter(adapters.DefaultHTTPAdapterConfig, cfg.Adapters[string(openrtb_ext.BidderConversant)].Endpoint)),
-		openrtb_ext.BidderEPlanning:  adaptBidder(adapters.EnforceBidderInfo(eplanning.NewEPlanningBidder(client, cfg.Adapters[string(openrtb_ext.BidderEPlanning)].Endpoint), infos[string(openrtb_ext.BidderEPlanning)]), client),
-		// TODO #211: Upgrade the Facebook adapter
-		openrtb_ext.BidderFacebook: adaptLegacyAdapter(audienceNetwork.NewAdapterFromFacebook(adapters.DefaultHTTPAdapterConfig, cfg.Adapters[strings.ToLower(string(openrtb_ext.BidderFacebook))].PlatformID)),
-		// TODO #212: Upgrade the Index adapter
-		openrtb_ext.BidderIndex: adaptLegacyAdapter(indexExchange.NewIndexAdapter(adapters.DefaultHTTPAdapterConfig, cfg.Adapters[strings.ToLower(string(openrtb_ext.BidderIndex))].Endpoint)),
-		// TODO #213: Upgrade the Lifestreet adapter
-		openrtb_ext.BidderLifestreet: adaptLegacyAdapter(lifestreet.NewLifestreetAdapter(adapters.DefaultHTTPAdapterConfig, cfg.Adapters[string(openrtb_ext.BidderLifestreet)].Endpoint)),
-		openrtb_ext.BidderOpenx:      adaptBidder(adapters.EnforceBidderInfo(openx.NewOpenxBidder(cfg.Adapters[string(openrtb_ext.BidderOpenx)].Endpoint), infos[string(openrtb_ext.BidderOpenx)]), client),
-		openrtb_ext.BidderPubmatic:   adaptBidder(adapters.EnforceBidderInfo(pubmatic.NewPubmaticBidder(client, cfg.Adapters[string(openrtb_ext.BidderPubmatic)].Endpoint), infos[string(openrtb_ext.BidderPubmatic)]), client),
-		// TODO #215: Upgrade the Pulsepoint adapter
-		openrtb_ext.BidderPulsepoint: adaptLegacyAdapter(pulsepoint.NewPulsePointAdapter(adapters.DefaultHTTPAdapterConfig, cfg.Adapters[string(openrtb_ext.BidderPulsepoint)].Endpoint)),
-		openrtb_ext.BidderRubicon: adaptBidder(adapters.EnforceBidderInfo(
-			rubicon.NewRubiconBidder(
-				client,
-				cfg.Adapters[string(openrtb_ext.BidderRubicon)].Endpoint,
-				cfg.Adapters[string(openrtb_ext.BidderRubicon)].XAPI.Username,
-				cfg.Adapters[string(openrtb_ext.BidderRubicon)].XAPI.Password,
-				cfg.Adapters[string(openrtb_ext.BidderRubicon)].XAPI.Tracker),
-			infos[string(openrtb_ext.BidderRubicon)]), client),
-		openrtb_ext.BidderSomoaudience: adaptBidder(adapters.EnforceBidderInfo(somoaudience.NewSomoaudienceBidder(cfg.Adapters[string(openrtb_ext.BidderSomoaudience)].Endpoint), infos[string(openrtb_ext.BidderSomoaudience)]), client),
-		openrtb_ext.BidderSovrn:        adaptBidder(adapters.EnforceBidderInfo(sovrn.NewSovrnBidder(client, cfg.Adapters[string(openrtb_ext.BidderSovrn)].Endpoint), infos[string(openrtb_ext.BidderSovrn)]), client),
+		openrtb_ext.BidderBeachfront: beachfront.NewBeachfrontBidder(),
+		openrtb_ext.BidderBrightroll: brightroll.NewBrightrollBidder(cfg.Adapters[string(openrtb_ext.BidderBrightroll)].Endpoint),
+		openrtb_ext.BidderEPlanning:  eplanning.NewEPlanningBidder(client, cfg.Adapters[string(openrtb_ext.BidderEPlanning)].Endpoint),
+		openrtb_ext.BidderOpenx:      openx.NewOpenxBidder(cfg.Adapters[string(openrtb_ext.BidderOpenx)].Endpoint),
+		openrtb_ext.BidderPubmatic:   pubmatic.NewPubmaticBidder(client, cfg.Adapters[string(openrtb_ext.BidderPubmatic)].Endpoint),
+		openrtb_ext.BidderRubicon: rubicon.NewRubiconBidder(
+			client,
+			cfg.Adapters[string(openrtb_ext.BidderRubicon)].Endpoint,
+			cfg.Adapters[string(openrtb_ext.BidderRubicon)].XAPI.Username,
+			cfg.Adapters[string(openrtb_ext.BidderRubicon)].XAPI.Password,
+			cfg.Adapters[string(openrtb_ext.BidderRubicon)].XAPI.Tracker),
+		openrtb_ext.BidderSomoaudience: somoaudience.NewSomoaudienceBidder(cfg.Adapters[string(openrtb_ext.BidderSomoaudience)].Endpoint),
+		openrtb_ext.BidderSovrn:        sovrn.NewSovrnBidder(client, cfg.Adapters[string(openrtb_ext.BidderSovrn)].Endpoint),
 	}
+
+	legacyBidders := map[openrtb_ext.BidderName]adapters.Adapter{
+		// TODO #267: Upgrade the Conversant adapter
+		openrtb_ext.BidderConversant: conversant.NewConversantAdapter(adapters.DefaultHTTPAdapterConfig, cfg.Adapters[string(openrtb_ext.BidderConversant)].Endpoint),
+		// TODO #211: Upgrade the Facebook adapter
+		openrtb_ext.BidderFacebook: audienceNetwork.NewAdapterFromFacebook(adapters.DefaultHTTPAdapterConfig, cfg.Adapters[strings.ToLower(string(openrtb_ext.BidderFacebook))].PlatformID),
+		// TODO #212: Upgrade the Index adapter
+		openrtb_ext.BidderIndex: indexExchange.NewIndexAdapter(adapters.DefaultHTTPAdapterConfig, cfg.Adapters[strings.ToLower(string(openrtb_ext.BidderIndex))].Endpoint),
+		// TODO #213: Upgrade the Lifestreet adapter
+		openrtb_ext.BidderLifestreet: lifestreet.NewLifestreetAdapter(adapters.DefaultHTTPAdapterConfig, cfg.Adapters[string(openrtb_ext.BidderLifestreet)].Endpoint),
+		// TODO #215: Upgrade the Pulsepoint adapter
+		openrtb_ext.BidderPulsepoint: pulsepoint.NewPulsePointAdapter(adapters.DefaultHTTPAdapterConfig, cfg.Adapters[string(openrtb_ext.BidderPulsepoint)].Endpoint),
+	}
+
+	allBidders := make(map[openrtb_ext.BidderName]adaptedBidder, len(ortbBidders)+len(legacyBidders))
+	for name, bidder := range legacyBidders {
+		allBidders[name] = adaptLegacyAdapter(bidder)
+	}
+	for name, bidder := range ortbBidders {
+		allBidders[name] = adaptBidder(adapters.EnforceBidderInfo(bidder, infos[string(name)]), client)
+	}
+	return allBidders
 }
