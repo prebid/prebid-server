08dc9341ccf7bdb35c4823450487b77e4c0c764f
Björn <bjorn.e.a.andersson@gmail.com>
2018-12-03 11:32:53 +0100

    Handle file paths in windows

--- openrtb_ext/bidders.go
+++ openrtb_ext/bidders.go
@@ -113,7 +113,7 @@ func NewBidderParamsValidator(schemaDirectory string) (BidderParamValidator, err
 		if err != nil {
 			return nil, fmt.Errorf("Failed to get an absolute representation of the path: %s, %v", toOpen, err)
 		}
-		schemaLoader := gojsonschema.NewReferenceLoader("file:///" + toOpen)
+		schemaLoader := gojsonschema.NewReferenceLoader("file:///" + filepath.ToSlash(toOpen))
 		loadedSchema, err := gojsonschema.NewSchema(schemaLoader)
 		if err != nil {
 			return nil, fmt.Errorf("Failed to load json schema at %s: %v", toOpen, err)
