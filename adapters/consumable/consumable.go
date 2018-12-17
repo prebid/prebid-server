a49ea6989d73cb9fa7efa388240f03da1fc66c6e
Daniel Cassidy <daniel@velope.tv>
2018-12-17 20:44:10 +0000

    Create skeleton implementation of Bidder for ConsumableAdapter.

--- adapters/consumable/consumable.go
+++ adapters/consumable/consumable.go
@@ -1,3 +1,16 @@
 package consumable
 
+import (
+	"github.com/mxmCherry/openrtb"
+	"github.com/prebid/prebid-server/adapters"
+)
+
 type ConsumableAdapter struct{}
+
+func (a *ConsumableAdapter) MakeRequests(request *openrtb.BidRequest) ([]*adapters.RequestData, []error) {
+	return nil, nil
+}
+
+func (a *ConsumableAdapter) MakeBids(internalRequest *openrtb.BidRequest, externalRequest *adapters.RequestData, response *adapters.ResponseData) (*adapters.BidderResponse, []error) {
+	return nil, nil
+}
