12ca33cddab057023c2fd2aca51db2d22841b6f5
Daniel Cassidy <daniel@velope.tv>
2018-12-20 19:21:27 +0000

    Generate skeleton bid request.

--- adapters/consumable/consumable.go
+++ adapters/consumable/consumable.go
@@ -3,12 +3,27 @@ package consumable
 import (
 	"github.com/mxmCherry/openrtb"
 	"github.com/prebid/prebid-server/adapters"
+	"net/http"
 )
 
 type ConsumableAdapter struct{}
 
 func (a *ConsumableAdapter) MakeRequests(request *openrtb.BidRequest) ([]*adapters.RequestData, []error) {
-	return nil, nil
+	headers := http.Header{
+		"Content-Type": {"application/json"},
+		"Accept":       {"application/json"},
+	}
+
+	requests := []*adapters.RequestData{
+		{
+			Method:  "POST",
+			Uri:     "https://e.serverbid.com/api/v2",
+			Body:    nil, // TODO
+			Headers: headers,
+		},
+	}
+
+	return requests, nil
 }
 
 func (a *ConsumableAdapter) MakeBids(internalRequest *openrtb.BidRequest, externalRequest *adapters.RequestData, response *adapters.ResponseData) (*adapters.BidderResponse, []error) {
