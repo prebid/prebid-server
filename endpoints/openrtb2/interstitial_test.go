eb8983cd82037febb7a73f30da168bb13eb50782
Hans Hjort <hhjort@appnexus.com>
2019-01-22 14:20:00 -0500

    Improves interstitial support for banner Imps

---
+++ endpoints/openrtb2/interstitial_test.go
@@ -0,0 +1,84 @@
+package openrtb2
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/mxmCherry/openrtb"
+	"github.com/stretchr/testify/assert"
+)
+
+var request = &openrtb.BidRequest{
+	ID: "some-id",
+	Imp: []openrtb.Imp{
+		{
+			ID: "my-imp-id",
+			Banner: &openrtb.Banner{
+				Format: []openrtb.Format{
+					{
+						W: 300,
+						H: 600,
+					},
+				},
+			},
+			Instl: 1,
+			Ext:   json.RawMessage(`{"appnexus": {"placementId": 10433394}}`),
+		},
+	},
+	Device: &openrtb.Device{
+		H:   640,
+		W:   320,
+		Ext: json.RawMessage(`{"prebid": {"interstitial": {"minwidthperc": 60, "minheightperc": 60}}}`),
+	},
+}
+
+func TestInterstitial(t *testing.T) {
+	myRequest := request
+	if err := processInterstitials(myRequest); err != nil {
+		t.Fatalf("Error processing interstitials: %v", err)
+	}
+	targetFormat := []openrtb.Format{
+		{
+			W: 300,
+			H: 600,
+		},
+		{
+			W: 250,
+			H: 600,
+		},
+		{
+			W: 300,
+			H: 480,
+		},
+		{
+			W: 180,
+			H: 500,
+		},
+		{
+			W: 300,
+			H: 500,
+		},
+		{
+			W: 300,
+			H: 431,
+		},
+		{
+			W: 300,
+			H: 430,
+		},
+		{
+			W: 200,
+			H: 600,
+		},
+		{
+			W: 202,
+			H: 600,
+		},
+		{
+			W: 300,
+			H: 360,
+		},
+	}
+	assert.Equal(t, targetFormat, myRequest.Imp[0].Banner.Format)
+
+}
--- openrtb_ext/device.go
+++ openrtb_ext/device.go
@@ -1,10 +1,10 @@
 package openrtb_ext
 
 import (
-	"errors"
 	"strconv"
 
 	"github.com/buger/jsonparser"
+	"github.com/prebid/prebid-server/errortypes"
 )
 
 // ExtDevice defines the contract for bidrequest.device.ext
@@ -18,31 +18,31 @@ type ExtDevicePrebid struct {
 }
 
 type ExtDeviceInt struct {
-	MinWidthPerc  int64 `json:"minwidtheperc"`
-	MinHeightPerc int64 `json:"minheightperc"`
+	MinWidthPerc  uint64 `json:"minwidtheperc"`
+	MinHeightPerc uint64 `json:"minheightperc"`
 }
 
 func (edi *ExtDeviceInt) UnmarshalJSON(b []byte) error {
 	if len(b) == 0 {
-		return errors.New("request.device.ext.prebid.interstitial must have some data in it")
+		return &errortypes.BadInput{Message: "request.device.ext.prebid.interstitial must have some data in it"}
 	}
 	if value, dataType, _, _ := jsonparser.Get(b, "minwidthperc"); dataType != jsonparser.Number {
-		return errors.New(`request.device.ext.prebid.interstitial.minwidthperc must be a number between 0 and 100`)
+		return &errortypes.BadInput{Message: "request.device.ext.prebid.interstitial.minwidthperc must be a number between 0 and 100"}
 	} else {
 		perc, err := strconv.Atoi(string(value))
 		if err != nil || perc < 0 || perc > 100 {
-			return errors.New(`request.device.ext.prebid.interstitial.minwidthperc must be a number between 0 and 100`)
+			return &errortypes.BadInput{Message: "request.device.ext.prebid.interstitial.minwidthperc must be a number between 0 and 100"}
 		}
-		edi.MinWidthPerc = int64(perc)
+		edi.MinWidthPerc = uint64(perc)
 	}
 	if value, dataType, _, _ := jsonparser.Get(b, "minheightperc"); dataType != jsonparser.Number {
-		return errors.New(`request.device.ext.prebid.interstitial.minheightperc must be a number between 0 and 100`)
+		return &errortypes.BadInput{Message: "request.device.ext.prebid.interstitial.minheightperc must be a number between 0 and 100"}
 	} else {
 		perc, err := strconv.Atoi(string(value))
 		if err != nil || perc < 0 || perc > 100 {
-			return errors.New(`request.device.ext.prebid.interstitial.minheightperc must be a number between 0 and 100`)
+			return &errortypes.BadInput{Message: "request.device.ext.prebid.interstitial.minheightperc must be a number between 0 and 100"}
 		}
-		edi.MinHeightPerc = int64(perc)
+		edi.MinHeightPerc = uint64(perc)
 	}
 	return nil
 }
