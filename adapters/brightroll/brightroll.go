e35098830549bbdc042a648a78fb25cc8041c043
smithaa <smithaa@oath.com>
2019-02-07 12:47:25 -0800

    updates on bid request values

--- adapters/brightroll/brightroll.go
+++ adapters/brightroll/brightroll.go
@@ -90,7 +90,7 @@ func (a *BrightrollAdapter) MakeRequests(requestIn *openrtb.BidRequest) ([]*adap
 
 	request.AT = 1 //Defaulting to first price auction for all prebid requests
 	if brightrollExt.Publisher == "adthrive" {
-		//request.BCat = getBlockedCategoriesForAdthrive()
+		request.BCat = getBlockedCategoriesForAdthrive()
 	}
 	reqJSON, err := json.Marshal(request)
 	if err != nil {
@@ -158,24 +158,11 @@ func (a *BrightrollAdapter) MakeBids(internalRequest *openrtb.BidRequest, extern
 
 //customized request, need following blocked categories
 func getBlockedCategoriesForAdthrive() []string {
-	//request.BCat = []string{"IAB8-5", "IAB8-18", "IAB15-1", "IAB7-30", "IAB14-1", "IAB22-1", "IAB3-7", "IAB7-3", "IAB14-3", "IAB11", "IAB11-1", "IAB11-2", "IAB11-3", "IAB11-4", "IAB11-5", "IAB23", "IAB23-1", "IAB23-2", "IAB23-3", "IAB23-4", "IAB23-5", "IAB23-6", "IAB23-7", "IAB23-8", "IAB23-9", "IAB23-10", "IAB7-39", "IAB9-30", "IAB7-44", "IAB25", "IAB25-1", "IAB25-2", "IAB25-3", "IAB25-4", "IAB25-5", "IAB25-6", "IAB25-7", "IAB26", "IAB26-1", "IAB26-2", "IAB26-3", "IAB26-4"}
 	return []string{"IAB8-5", "IAB8-18", "IAB15-1", "IAB7-30", "IAB14-1", "IAB22-1", "IAB3-7", "IAB7-3", "IAB14-3", "IAB11", "IAB11-1", "IAB11-2", "IAB11-3", "IAB11-4", "IAB11-5", "IAB23", "IAB23-1", "IAB23-2", "IAB23-3", "IAB23-4", "IAB23-5", "IAB23-6", "IAB23-7", "IAB23-8", "IAB23-9", "IAB23-10", "IAB7-39", "IAB9-30", "IAB7-44", "IAB25", "IAB25-1", "IAB25-2", "IAB25-3", "IAB25-4", "IAB25-5", "IAB25-6", "IAB25-7", "IAB26", "IAB26-1", "IAB26-2", "IAB26-3", "IAB26-4"}
 }
 
 func getBlockedCreativetypesForAdThrive() []openrtb.CreativeAttribute {
-	return []openrtb.CreativeAttribute{openrtb.CreativeAttribute(1), openrtb.CreativeAttribute(2), openrtb.CreativeAttribute(3), openrtb.CreativeAttribute(6), openrtb.CreativeAttribute(9), openrtb.CreativeAttribute(17)}
-	//for i := 0; i < len(request.Imp); i++ {
-	//	if request.Imp[i].Banner != nil {
-	//		bannerCopy := *request.Imp[i].Banner
-	//		bannerCopy.BAttr = battr //[]int{1,3,5,6,9}
-	//		request.Imp[i].Banner = &bannerCopy
-	//	}
-	//	if request.Imp[i].Video != nil {
-	//		videoCopy := *request.Imp[i].Video
-	//		videoCopy.BAttr = battr
-	//		request.Imp[i].Video = &videoCopy
-	//	}
-	//}
+	return []openrtb.CreativeAttribute{openrtb.CreativeAttribute(1), openrtb.CreativeAttribute(2), openrtb.CreativeAttribute(3), openrtb.CreativeAttribute(6), openrtb.CreativeAttribute(9), openrtb.CreativeAttribute(10)}
 }
 
 //Adding header fields to request header
