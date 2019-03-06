974111a24ed3fb4bdb4e937cc02a19b6e7991be2
Veronika Solovei <vsolovei@appnexus.com>
2019-03-06 09:57:29 -0500

    Categories lazy unmarshalling

--- stored_requests/backends/file_fetcher/fetcher.go
+++ stored_requests/backends/file_fetcher/fetcher.go
@@ -17,11 +17,12 @@ import (
 // For example, when asked to fetch the request with ID == "23", it will return the data from "directory/23.json".
 func NewFileFetcher(directory string) (stored_requests.Fetcher, error) {
 	storedData, err := collectStoredData(directory, FileSystem{make(map[string]FileSystem), make(map[string]json.RawMessage)}, nil)
-	return &eagerFetcher{storedData}, err
+	return &eagerFetcher{storedData, nil}, err
 }
 
 type eagerFetcher struct {
 	FileSystem FileSystem
+	Categories map[string]map[string]string
 }
 
 func (fetcher *eagerFetcher) FetchRequests(ctx context.Context, requestIDs []string, impIDs []string) (map[string]json.RawMessage, map[string]json.RawMessage, []error) {
@@ -40,6 +41,14 @@ func (fetcher *eagerFetcher) FetchCategories(primaryAdServer, publisherId, iabCa
 			fileName = primaryAdServer + "_" + publisherId
 		}
 
+		if fetcher.Categories == nil {
+			fetcher.Categories = make(map[string]map[string]string)
+		}
+
+		if data, ok := fetcher.Categories[fileName]; ok {
+			return data[iabCategory], nil
+		}
+
 		if file, ok := primaryAdServerDir.Files[fileName]; ok {
 
 			tmp := make(map[string]string)
@@ -47,6 +56,7 @@ func (fetcher *eagerFetcher) FetchCategories(primaryAdServer, publisherId, iabCa
 			if err := json.Unmarshal(file, &tmp); err != nil {
 				return "", fmt.Errorf("Unable to unmarshal categories for adserver: '%s', publisherId: '%s'", primaryAdServer, publisherId)
 			}
+			fetcher.Categories[fileName] = tmp
 			resultCategory := tmp[iabCategory]
 			if len(resultCategory) == 0 {
 				return "", fmt.Errorf("Unable to find category for adserver '%s', publisherId: '%s', iab category: '%s'", primaryAdServer, publisherId, iabCategory)
