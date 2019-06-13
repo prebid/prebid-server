d21014534b1f154e2186d4d23843de7d761c6c7f
Kevin Kerr <kkerr@triplelift.com>
2019-06-13 16:31:09 -0400

    start small

---
+++ adapters/triplelift/triplelift.go
@@ -0,0 +1,36 @@
+package adapters
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"net/http"
+
+	"github.com/mxmCherry/openrtb"
+	"github.com/prebid/prebid-server/errortypes"
+	"github.com/prebid/prebid-server/openrtb_ext"
+)
+
+	func MakeRequests(request *openrtb.BidRequest) ([]*adapters.RequestData, []error) {
+	    errs := make([]error, 0, len(request.Imp))
+        reqs = make([]*RequestData, 0, 1) 
+        ad = "http://localhost:8076/s2s/auction"
+        reqJSON, err = json.Marshal(request)
+        if err != nil {
+            errs = append(errs,err)
+            return nil, errs
+        }
+        headers := http.Header{}
+        headers.add("Content-Type","application/json;charset=utf-8")
+        headers.add("Accept", "application/json")
+        reqs = append(reqs, &adapters.RequestData{
+            Method: "POST",
+            Uri: ad,
+            Body: reqJSON,
+            Headers: headers
+        })
+        return reqs, errs
+    }
+	
+    func MakeBids(internalRequest *openrtb.BidRequest, externalRequest *RequestData, response *ResponseData) (*BidderResponse, []error) {
+    }
+
