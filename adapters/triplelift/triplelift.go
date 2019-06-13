44f921b73076984bc807b6e5cc83790f52f7ceac
Kevin Kerr <kkerr@triplelift.com>
2019-06-13 18:30:06 -0400

    fix syntax

--- adapters/triplelift/triplelift.go
+++ adapters/triplelift/triplelift.go
@@ -1,49 +1,49 @@
-package adapters
+package triplelift 
 
 import (
-	"encoding/base64"
+	//"encoding/base64"
 	"encoding/json"
 	"net/http"
 
 	"github.com/mxmCherry/openrtb"
-	"github.com/prebid/prebid-server/errortypes"
-	"github.com/prebid/prebid-server/openrtb_ext"
+	//"github.com/prebid/prebid-server/errortypes"
+	//"github.com/prebid/prebid-server/openrtb_ext"
+	"github.com/prebid/prebid-server/adapters"
 )
 
 type TripleliftAdapter struct {
-    endpoint String
+    endpoint string
 }
 
 func MakeRequests(request *openrtb.BidRequest) ([]*adapters.RequestData, []error) {
     errs := make([]error, 0, len(request.Imp))
-    reqs = make([]*RequestData, 0, 1) 
-    ad = "http://localhost:8076/s2s/auction"
-    reqJSON, err = json.Marshal(request)
+    reqs := make([]*adapters.RequestData, 0, 1) 
+    ad := "http://localhost:8076/s2s/auction"
+    reqJSON, err := json.Marshal(request)
     if err != nil {
         errs = append(errs,err)
         return nil, errs
     }
     headers := http.Header{}
-    headers.add("Content-Type","application/json;charset=utf-8")
-    headers.add("Accept", "application/json")
+    headers.Add("Content-Type","application/json;charset=utf-8")
+    headers.Add("Accept", "application/json")
     reqs = append(reqs, &adapters.RequestData{
         Method: "POST",
         Uri: ad,
         Body: reqJSON,
-        Headers: headers
-    })
+        Headers: headers})
     return reqs, errs
 }
 
-func MakeBids(internalRequest *openrtb.BidRequest, externalRequest *RequestData, response *ResponseData) (*BidderResponse, []error) {
+func MakeBids(internalRequest *openrtb.BidRequest, externalRequest *adapters.RequestData, response *adapters.ResponseData) (*adapters.BidderResponse, []error) {
+    errs := make([]error,2)
     bidResponse := adapters.NewBidderResponseWithBidsCapacity(5)
-    return bidResponse
+    return bidResponse,errs
 }
 
-func NewTripleliftBidder(client *adapters.HTTPAdapterConfig, endpoint string) *TripleiftAdapter {
+func NewTripleliftBidder(client *adapters.HTTPAdapterConfig, endpoint string) *TripleliftAdapter {
     return &TripleliftAdapter{
-        endpoint: endpoint
-    }
+        endpoint: endpoint}
 }
 
 
