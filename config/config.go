d2d8a8af2b4ee69fdc7856f4cd164037956744b4
Kevin Kerr <kkerr@triplelift.com>
2019-06-17 16:09:43 -0400

    revert this

--- config/config.go
+++ config/config.go
@@ -469,9 +469,9 @@ func SetupViper(v *viper.Viper, filename string) {
 	}
 	// Fixes #475: Some defaults will be set just so they are accessible via environment variables
 	// (basically so viper knows they exist)
-	v.SetDefault("external_url", "http://localhost:8002")
+	v.SetDefault("external_url", "http://localhost:8000")
 	v.SetDefault("host", "")
-	v.SetDefault("port", 8002)
+	v.SetDefault("port", 8000)
 	v.SetDefault("admin_port", 6060)
 	v.SetDefault("enable_gzip", false)
 	v.SetDefault("status_response", "")
