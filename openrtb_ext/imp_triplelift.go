7bd8bb0614efd2fe496013467e90b8d653d82b22
Kevin Kerr <kkerr@triplelift.com>
2019-06-17 13:37:12 -0400

    fix compilation error

--- openrtb_ext/imp_triplelift.go
+++ openrtb_ext/imp_triplelift.go
@@ -1,7 +1,5 @@
 package openrtb_ext
 
-import "encoding/json"
-
 // ExtImpTriplelift defines the contract for bidrequest.imp[i].ext.triplelift
 type ExtImpTriplelift struct {
     InvCode                 string                  `json:"inventoryCode"`
