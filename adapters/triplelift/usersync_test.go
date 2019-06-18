951dcf454f8f7a807e1ce142dd12e8d2e39e0759
Kevin Kerr <kkerr@triplelift.com>
2019-06-18 11:36:54 -0400

    fix test

--- adapters/triplelift/usersync_test.go
+++ adapters/triplelift/usersync_test.go
@@ -12,7 +12,7 @@ func TestTripleliftSyncer(t *testing.T) {
 	syncer := NewTripleliftSyncer(temp)
 	syncInfo, err := syncer.GetUsersyncInfo("", "")
 	assert.NoError(t, err)
-	assert.Equal(t, "//eb2.3lift.com/sync?gdpr={{.GDPR}}&cmp_cs={{.GDPRConsent}}", syncInfo.URL)
+	assert.Equal(t, "//eb2.3lift.com/sync?gdpr=&cmp_cs=", syncInfo.URL)
 	assert.Equal(t, "redirect", syncInfo.Type)
 	assert.EqualValues(t, 28, syncer.GDPRVendorID())
 	assert.Equal(t, false, syncInfo.SupportCORS)
