68abbf988e0c02f5fd00f7c817912cd6e84fbaf2
Kevin Kerr <kkerr@triplelift.com>
2019-06-18 10:59:28 -0400

    add the floor here, too

--- openrtb_ext/imp_triplelift.go
+++ openrtb_ext/imp_triplelift.go
@@ -3,4 +3,5 @@ package openrtb_ext
 // ExtImpTriplelift defines the contract for bidrequest.imp[i].ext.triplelift
 type ExtImpTriplelift struct {
     InvCode                 string                  `json:"inventoryCode"`
+    Floor                   string                  `json:"floor"`
 }
