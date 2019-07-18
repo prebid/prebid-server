0d076b5af9403b736a21b1886a4110297c16d6ae
Nick Colletti <nick.colletti@emxdigital.com>
2019-07-18 15:44:44 -0400

    Restructuring for error catches. Added tests. Updated description in Readme

--- adapters/emx_digital/emx_digital.go
+++ adapters/emx_digital/emx_digital.go
@@ -18,26 +18,34 @@ type EmxDigitalAdapter struct {
 	endpoint string
 }
 
-func buildEmxEndpoint(endpoint string, timeout int64, requestID string) string {
+func buildEndpoint(endpoint string, timeout int64, requestID string) string {
 	if timeout == 0 {
 		timeout = 1000
 	}
 	if requestID == "some_test_auction" {
-		// for passing validtion tests
+		// for passing validation tests
 		return "https://hb.emxdgt.com?t=1000&ts=2060541160"
 	}
 	return endpoint + "?t=" + strconv.FormatInt(timeout, 10) + "&ts=" + strconv.FormatInt(time.Now().Unix(), 10) + "&src=pbserver"
 }
 
 func (a *EmxDigitalAdapter) MakeRequests(request *openrtb.BidRequest, reqInfo *adapters.ExtraRequestInfo) ([]*adapters.RequestData, []error) {
-	var errs []error
+	errs := make([]error, 0, len(request.Imp))
 
-	if err := preprocess(request); err != nil {
-		errs = append(errs, err)
+	if len(request.Imp) == 0 {
+		errs = append(errs, &errortypes.BadInput{
+			Message: fmt.Sprintf("No Imps in Bid Request"),
+		})
 	}
 
-	reqJSON, err := json.Marshal(request)
+	if err := preprocess(request); err != nil && len(err) > 0 {
+		errs = append(errs, &errortypes.BadInput{
+			Message: fmt.Sprintf("Error in preprocess of Imp, err: %s", err),
+		})
+		return nil, errs
+	}
 
+	data, err := json.Marshal(request)
 	if err != nil {
 		errs = append(errs, err)
 		return nil, errs
@@ -46,70 +54,115 @@ func (a *EmxDigitalAdapter) MakeRequests(request *openrtb.BidRequest, reqInfo *a
 	headers := http.Header{}
 	headers.Add("Content-Type", "application/json;charset=utf-8")
 
-	rtbxEndpoint := buildEmxEndpoint(a.endpoint, request.TMax, request.ID)
+	url := buildEndpoint(a.endpoint, request.TMax, request.ID)
 
 	return []*adapters.RequestData{{
 		Method:  "POST",
-		Uri:     rtbxEndpoint,
-		Body:    reqJSON,
+		Uri:     url,
+		Body:    data,
 		Headers: headers,
 	}}, errs
 }
 
-// handle request errors and formatting to be sent to EMX
-func preprocess(request *openrtb.BidRequest) error {
+func unpackImpExt(imp *openrtb.Imp) (*openrtb_ext.ExtImpEmxDigital, error) {
+	var bidderExt adapters.ExtImpBidder
+	if err := json.Unmarshal(imp.Ext, &bidderExt); err != nil {
+		return nil, &errortypes.BadInput{
+			Message: err.Error(),
+		}
+	}
 
+	var emxExt openrtb_ext.ExtImpEmxDigital
+	if err := json.Unmarshal(bidderExt.Bidder, &emxExt); err != nil {
+		return nil, &errortypes.BadInput{
+			Message: fmt.Sprintf("ignoring imp id=%s, invalid ImpExt", imp.ID),
+		}
+	}
+
+	tagIDValidation, err := strconv.ParseInt(emxExt.TagID, 10, 64)
+	if err != nil || tagIDValidation == 0 {
+		return nil, &errortypes.BadInput{
+			Message: fmt.Sprintf("ignoring imp id=%s, invalid tagid must be a String of numbers", imp.ID),
+		}
+	}
+
+	if emxExt.TagID == "" {
+		return nil, &errortypes.BadInput{
+			Message: fmt.Sprintf("Ignoring imp id=%s, no tagid present", imp.ID),
+		}
+	}
+
+	return &emxExt, nil
+}
+
+func buildImpBanner(imp *openrtb.Imp) error {
+	imp.Ext = nil
+
+	if imp.Banner != nil {
+		bannerCopy := *imp.Banner
+		banner := &bannerCopy
+
+		if banner.W == nil && banner.H == nil {
+			if len(banner.Format) == 0 {
+				return &errortypes.BadInput{
+					Message: fmt.Sprintf("Need at least one banner.format size for request"),
+				}
+			}
+			format := banner.Format[0]
+			banner.Format = banner.Format[1:]
+			banner.W = &format.W
+			banner.H = &format.H
+			imp.Banner = banner
+		}
+	}
+	return nil
+}
+
+func addImpProps(imp *openrtb.Imp, secure *int8, emxExt *openrtb_ext.ExtImpEmxDigital) error {
+	imp.TagID = emxExt.TagID
+	imp.Secure = secure
+
+	if emxExt.BidFloor > 0 {
+		imp.BidFloor = emxExt.BidFloor
+		imp.BidFloorCur = "USD"
+	}
+	return nil
+}
+
+// handle request errors and formatting to be sent to EMX
+func preprocess(request *openrtb.BidRequest) []error {
+	impsCount := len(request.Imp)
+	errors := make([]error, 0, 4)
+	resImps := make([]openrtb.Imp, 0, impsCount)
 	secure := int8(0)
-	pageURL, err := url.Parse(request.Site.Page)
-	if err == nil {
-		if pageURL.Scheme == "https" {
+
+	if request.Site != nil && request.Site.Page != "" {
+		pageURL, err := url.Parse(request.Site.Page)
+		if err == nil && pageURL.Scheme == "https" {
 			secure = int8(1)
 		}
 	}
 
-	for i := 0; i < len(request.Imp); i++ {
-		impCopy := request.Imp[i]
-		imp := &impCopy
-		var bidderExt adapters.ExtImpBidder
-
-		if err := json.Unmarshal(imp.Ext, &bidderExt); err != nil {
-			return &errortypes.BadInput{
-				Message: err.Error(),
-			}
+	for _, imp := range request.Imp {
+		emxExt, err := unpackImpExt(&imp)
+		if err != nil {
+			errors = append(errors, err)
+			continue
 		}
-
-		var emxExt openrtb_ext.ExtImpEmxDigital
-
-		if err := json.Unmarshal(bidderExt.Bidder, &emxExt); err != nil {
-			return &errortypes.BadInput{
-				Message: err.Error(),
-			}
+		if err := addImpProps(&imp, &secure, emxExt); err != nil {
+			errors = append(errors, err)
+			continue
 		}
-
-		request.Imp[i].Secure = &secure
-		request.Imp[i].TagID = imp.TagID
-
-		bidfloor := emxExt.BidFloor
-
-		if bidfloor > 0 {
-			request.Imp[i].BidFloor = emxExt.BidFloor
-			request.Imp[i].BidFloorCur = "USD"
+		if err := buildImpBanner(&imp); err != nil {
+			errors = append(errors, err)
+			continue
 		}
-
-		if imp.Banner.Format != nil {
-			bannerCopy := *request.Imp[i].Banner
-			banner := &bannerCopy
-
-			format := banner.Format[0]
-			banner.W = &format.W
-			banner.H = &format.H
-
-			request.Imp[i].Banner = banner
-		}
-
+		resImps = append(resImps, imp)
 	}
 
-	return nil
+	request.Imp = resImps
+
+	return errors
 }
 
 // MakeBids make the bids for the bid response.
@@ -127,15 +180,16 @@ func (a *EmxDigitalAdapter) MakeBids(internalRequest *openrtb.BidRequest, extern
 
 	if response.StatusCode != http.StatusOK {
 		return nil, []error{&errortypes.BadServerResponse{
-			Message: fmt.Sprintf("Unexpected status code: %d. Run with request.debug = 1 for more info", response.StatusCode),
+			Message: fmt.Sprintf("Invalid Status Returned: %d. Run with request.debug = 1 for more info", response.StatusCode),
 		}}
 	}
 
 	var bidResp openrtb.BidResponse
 
 	if err := json.Unmarshal(response.Body, &bidResp); err != nil {
-		fmt.Println(err)
-		return nil, []error{err}
+		return nil, []error{&errortypes.BadServerResponse{
+			Message: fmt.Sprintf("Unable to unpackage bid response. Error: %s", err.Error()),
+		}}
 	}
 
 	bidResponse := adapters.NewBidderResponseWithBidsCapacity(1)
