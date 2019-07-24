0b7abc7f9b91ee793c1682341de2e8180c5d59c6
Yan Naing Kyaw <naing@naing.bnmla.com>
2019-07-24 15:43:07 -0700

    update engagebdr adapter usersync

--- adapters/engagebdr/usersync_test.go
+++ adapters/engagebdr/usersync_test.go
@@ -1,1 +1,17 @@
 package engagebdr
+import (
+    "testing"
+    "text/template"
+    "github.com/stretchr/testify/assert"
+)
+
+func TestEngageBDRSyncer(t *testing.T) {
+	temp := template.Must(template.New("sync-template").Parse("https://match.bnmla.com/usersync?sspid=99999&gdpr={{.GDPR}}&gdpr_consent={{.GDPRConsent}}&redir=localhost%2Fsetuid%3Fbidder%3Dgumgum%26gdpr%3D{{.GDPR}}%26gdpr_consent%3D{{.GDPRConsent}}%26uid%3D"))
+	syncer := NewEngageBDRSyncer(temp)
+	syncInfo, err := syncer.GetUsersyncInfo("1", "BOPVK28OVJoTBABABAENBs-AAAAhuAKAANAAoACwAGgAPAAxAB0AHgAQAAiABOADkA")
+	assert.NoError(t, err)
+	assert.Equal(t, "https://match.bnmla.com/usersync?sspid=99999&gdpr=1&gdpr_consent=BOPVK28OVJoTBABABAENBs-AAAAhuAKAANAAoACwAGgAPAAxAB0AHgAQAAiABOADkA&redir=localhost%2Fsetuid%3Fbidder%3Dgumgum%26gdpr%3D1%26gdpr_consent%3DBOPVK28OVJoTBABABAENBs-AAAAhuAKAANAAoACwAGgAPAAxAB0AHgAQAAiABOADkA%26uid%3D", syncInfo.URL)
+	assert.Equal(t, "iframe", syncInfo.Type)
+	assert.EqualValues(t, 62, syncer.GDPRVendorID())
+	assert.Equal(t, false, syncInfo.SupportCORS)
+}
