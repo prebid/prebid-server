df4ed5b7db23481dd323121cd26628841bfee8cb
Nick Colletti <git@emxdigital.com>
2019-08-21 11:46:19 -0400

    fix usync assert url

--- adapters/emx_digital/usersync_test.go
+++ adapters/emx_digital/usersync_test.go
@@ -12,7 +12,7 @@ func TestEMXDigitalSyncer(t *testing.T) {
 	syncer := NewEMXDigitalSyncer(temp)
 	syncInfo, err := syncer.GetUsersyncInfo("1", "BOPVK28OVJoTBABABAENBs-AAAAhuAKAANAAoACwAGgAPAAxAB0AHgAQAAiABOADkA")
 	assert.NoError(t, err)
-	assert.Equal(t, "https%3A%2F%2Fcs.emxdgt.com%2Fum%3Fssp%3Dpbs%26gdpr%3D1%26gdpr_consent%3DBOPVK28OVJoTBABABAENBs-AAAAhuAKAANAAoACwAGgAPAAxAB0AHgAQAAiABOADkA%26redirect%3Dlocalhost%2Fsetuid%3Fbidder%3Demx_digital%26uid%3D%24UID", syncInfo.URL)
+	assert.Equal(t, "https://cs.emxdgt.com/um?ssp=pbs&gdpr=1&gdpr_consent=BOPVK28OVJoTBABABAENBs-AAAAhuAKAANAAoACwAGgAPAAxAB0AHgAQAAiABOADkA&redirect=localhost%2Fsetuid%3Fbidder%3Demx_digital%26uid%3D%24UID", syncInfo.URL)
 	assert.Equal(t, "iframe", syncInfo.Type)
 	assert.EqualValues(t, 183, syncer.GDPRVendorID())
 	assert.Equal(t, false, syncInfo.SupportCORS)
