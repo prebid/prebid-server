37b905a4eedbf8ec4c8eaba678f3a196e19b5ff6
Sergio <sergio.anru@gmail.com>
2019-09-18 13:26:49 +0200

    fix usersync_test

--- adapters/adpone/usersync_test.go
+++ adapters/adpone/usersync_test.go
@@ -8,11 +8,11 @@ import (
 )
 
 func TestAdponeSyncer(t *testing.T) {
-	temp := template.Must(template.New("sync-template").Parse("https://usersync.adpone.com/csync?redir=https%3A%2F%2Flocalhost%3A8888%2Fsetuid%3Fbidder%3Dadpone%26gdpr={{.GDPR&gdpr_consent={{.GDPRConsent}}%26uid%3D%7Buid%7D"))
+	temp := template.Must(template.New("sync-template").Parse("https://usersync.adpone.com/csync?t=p&ep=0&redir=localhost%2Fsetuid%3Fbidder%3Dadtelligent%26gdpr%3D{{.GDPR}}%26gdpr_consent%3D{{.GDPRConsent}}%26uid%3D%7Buid%7D"))
 	syncer := NewadponeSyncer(temp)
-	syncInfo, err := syncer.GetUsersyncInfo("1", "BONciguONcjGKADACHENAOLS1rAHDAFAAEAASABQAMwAeACEAFw")
+	syncInfo, err := syncer.GetUsersyncInfo("", "")
 	assert.NoError(t, err)
-	assert.Equal(t, "https://usersync.adpone.com/csync?redir=https%3A%2F%2Flocalhost%3A8888%2Fsetuid%3Fbidder%3Dadpone%26gdpr=1&gdpr_consent=BONciguONcjGKADACHENAOLS1rAHDAFAAEAASABQAMwAeACEAFw%26uid%3D%7Buid%7D", syncInfo.URL)
+	assert.Equal(t, "https://usersync.adpone.com/csync?t=p&ep=0&redir=localhost%2Fsetuid%3Fbidder%3Dadtelligent%26gdpr%3D%26gdpr_consent%3D%26uid%3D%7Buid%7D", syncInfo.URL)
 	assert.Equal(t, "redirect", syncInfo.Type)
 	assert.EqualValues(t, adponeGDPRVendorID, syncer.GDPRVendorID())
 }
