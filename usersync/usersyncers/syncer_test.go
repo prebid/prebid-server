2630c8aebe81b31449a20401dbfb4bcaa52c8433
Sergio <sergio.anru@gmail.com>
2019-09-18 13:55:45 +0200

    add adpone to syncer_test.go

--- usersync/usersyncers/syncer_test.go
+++ usersync/usersyncers/syncer_test.go
@@ -17,6 +17,7 @@ func TestNewSyncerMap(t *testing.T) {
 			string(openrtb_ext.BidderAdform):         syncConfig,
 			string(openrtb_ext.BidderAdkernel):       syncConfig,
 			string(openrtb_ext.BidderAdkernelAdn):    syncConfig,
+			string(openrtb_ext.BidderAdpone):         syncConfig,
 			string(openrtb_ext.BidderAdtelligent):    syncConfig,
 			string(openrtb_ext.BidderAdvangelists):   syncConfig,
 			string(openrtb_ext.BidderAppnexus):       syncConfig,
