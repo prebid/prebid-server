95584d6f3d9bade4d5ec1404e86e23a269b88868
Sachin Survase <sachin.survase@pubmatic.com>
2019-10-04 15:48:03 +0530

    Fixed syncInfo.URL query param

--- endpoints/cookie_sync.go
+++ endpoints/cookie_sync.go
@@ -161,8 +161,9 @@ func (deps *cookieSyncDeps) Endpoint(w http.ResponseWriter, r *http.Request, _ h
 		}
 		syncInfo, err := deps.syncers[openrtb_ext.BidderName(newBidder)].GetUsersyncInfo(gdprToString(parsedReq.GDPR), parsedReq.Consent)
 		if err == nil {
+			//For secure = true
 			if secParam == "1" && newBidder == "pubmatic" {
-				syncInfo.URL += "%26sec%3D1"
+				syncInfo.URL += "%26sec%3D1%26"
 			}
 
 			newSync := &usersync.CookieSyncBidders{
