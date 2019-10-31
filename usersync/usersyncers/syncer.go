59d2aeb0dec555c7031dcc24ddb9f07d20d4f27b
Henry Tang <htang555@gmail.com>
2019-10-31 13:52:31 -0400

    redo ttx

--- usersync/usersyncers/syncer.go
+++ usersync/usersyncers/syncer.go
@@ -6,7 +6,7 @@ import (
 	"text/template"
 
 	"github.com/golang/glog"
-	"github.com/prebid/prebid-server/adapters/33across"
+	ttx "github.com/prebid/prebid-server/adapters/33across"
 	"github.com/prebid/prebid-server/adapters/adform"
 	"github.com/prebid/prebid-server/adapters/adkernel"
 	"github.com/prebid/prebid-server/adapters/adkernelAdn"
