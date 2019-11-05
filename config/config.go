719bcec0cb568b3506f03aa9660505d09f8fb9de
Kevin Kerr <kkerr@triplelift.com>
2019-11-05 11:03:23 -0500

    add the extra info

--- config/config.go
+++ config/config.go
@@ -642,6 +642,7 @@ func SetupViper(v *viper.Viper, filename string) {
 	v.SetDefault("adapters.somoaudience.endpoint", "http://publisher-east.mobileadtrading.com/rtb/bid")
 	v.SetDefault("adapters.sovrn.endpoint", "http://ap.lijit.com/rtb/bid?src=prebid_server")
 	v.SetDefault("adapters.triplelift_native.endpoint", "https://tlx.3lift.com/s2sn/auction?supplier_id=20")
+	v.SetDefault("adapters.triplelift_native.extrainfo", "{}")
 	v.SetDefault("adapters.adkernel.endpoint", "http://{{.Host}}/hb?zone={{.ZoneID}}")
 	v.SetDefault("adapters.adkerneladn.endpoint", "http://{{.Host}}/rtbpub?account={{.PublisherID}}")
 	v.SetDefault("adapters.33across.partner_id", "")
