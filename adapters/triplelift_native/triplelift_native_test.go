418183ab8453be7d59181a75d8b6f8a9e3125afd
Kevin Kerr <kkerr@triplelift.com>
2019-11-18 14:00:58 -0500

    fixing tests

--- adapters/triplelift_native/triplelift_native_test.go
+++ adapters/triplelift_native/triplelift_native_test.go
@@ -6,5 +6,5 @@ import (
 )
 
 func TestJsonSamples(t *testing.T) {
-	adapterstest.RunJSONBidderTest(t, "triplelifttest", NewTripleliftNativeBidder(nil, "http://tlx.3lift.net/s2s/auction?supplier_id=19", "{\"publisher_whitelist\":[]}"))
+	adapterstest.RunJSONBidderTest(t, "triplelift_nativetest", NewTripleliftNativeBidder(nil, "http://tlx.3lift.net/s2s/auction?supplier_id=19", "{\"publisher_whitelist\":[], \"endpoint\":\"http://tlx.3lift.net/s2sn/auction?supplier_id=20\"}"))
 }
