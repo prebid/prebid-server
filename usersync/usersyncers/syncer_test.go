280bba53e6efa6a13fd162c7f31c1fdf37b281a4
Kevin Kerr <kkerr@triplelift.com>
2019-11-19 11:50:50 -0500

    ws

--- usersync/usersyncers/syncer_test.go
+++ usersync/usersyncers/syncer_test.go
@@ -35,7 +35,6 @@ func TestNewSyncerMap(t *testing.T) {
 			string(openrtb_ext.BidderIx):             syncConfig,
 			string(openrtb_ext.BidderLifestreet):     syncConfig,
 			string(openrtb_ext.BidderLockerDome):     syncConfig,
-
 			string(openrtb_ext.BidderMgid):             syncConfig,
 			string(openrtb_ext.BidderOpenx):            syncConfig,
 			string(openrtb_ext.BidderPubmatic):         syncConfig,
