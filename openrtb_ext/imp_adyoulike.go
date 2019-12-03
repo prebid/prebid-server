b3911656a1b7e611aa75940ef0b9a2fb270e1387
Guillaume <guillaume.andouard@adyoulike.com>
2019-12-03 11:19:25 +0100

    register possible parameters for adyoulike bidder

--- openrtb_ext/imp_adyoulike.go
+++ openrtb_ext/imp_adyoulike.go
@@ -2,5 +2,18 @@ package openrtb_ext
 
 // ExtImpAdkernel defines the contract for bidrequest.imp[i].ext.adkernel
 type ExtImpAdYouLike struct {
-	PlacementId int    `json:"Placement"`
+	// placementId, only mandatory field
+	PlacementId string `json:"placementId"`
+	
+	// Id of the forced campaign	
+	Campaign string    `json:"Campaign"`
+	// Id of the forced track
+	Track string       `json:"Track"`
+	// Id of the forced creative
+	Creative string    `json:"Creative"`
+	// Context of the campaign values [SSP|AdServer]
+	Source string	  `json:"Source"`
+	// Abitrary Id used for debug purpose
+	Debug string	  `json:"Debug"`
+
 }
