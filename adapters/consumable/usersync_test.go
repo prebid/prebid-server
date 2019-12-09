4856058f8afb8b868d8133307f0b981b51f2c038
Scott Kay <noreply@syntaxnode.com>
2019-12-08 22:52:13 -0500

    PR Feedback

--- adapters/consumable/usersync_test.go
+++ adapters/consumable/usersync_test.go
@@ -16,12 +16,13 @@ func TestConsumableSyncer(t *testing.T) {
 	)
 
 	syncer := NewConsumableSyncer(syncURLTemplate)
-	u, _ := syncer.GetUsersyncInfo(privacy.Policies{
+	u, err := syncer.GetUsersyncInfo(privacy.Policies{
 		GDPR: gdpr.Policy{
 			Signal: "0",
 		},
 	})
 
+	assert.NoError(t, err)
 	assert.Equal(t, "//e.serverbid.com/udb/9969/match?redir=http%3A%2F%2Flocalhost%3A8000%2Fsetuid%3Fbidder%3Dconsumable%26gdpr%3D0%26gdpr_consent%3D%26uid%3D", u.URL)
 	assert.Equal(t, "redirect", u.Type)
 	assert.Equal(t, uint16(65535), syncer.GDPRVendorID())
