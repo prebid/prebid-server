1a27659a89ca677590817911a423858f979f6eaf
shalmali-patil <shalmali.patil@pubmatic.com>
2020-01-24 14:34:15 +0530

    adding cert file name

--- config/config.go
+++ config/config.go
@@ -727,7 +727,7 @@ func SetupViper(v *viper.Viper, filename string) {
 	v.SetDefault("blacklisted_apps", []string{""})
 	v.SetDefault("blacklisted_accts", []string{""})
 	v.SetDefault("account_required", false)
-	v.SetDefault("certificates_file", "")
+	v.SetDefault("certificates_file", "/etc/ssl/cert.pem")
 
 	// Set environment variable support:
 	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
