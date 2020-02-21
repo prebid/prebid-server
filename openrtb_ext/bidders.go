8e382e7b8c12b66e4ce6392078b00c2151ce6f32
Viacheslav Chimishuk <vchimishuk@yandex.ru>
2020-02-21 20:14:53 +0200

    Add Adoppler bidder support. (#1186)
    * Add Adoppler bidder support.
    
    * Address code review comments. Use JSON-templates for testing.
    
    * Fix misprint; Add url.PathEscape call for adunit URL parameter.

--- openrtb_ext/bidders.go
+++ openrtb_ext/bidders.go
@@ -29,6 +29,7 @@ const (
 	BidderAdvangelists     BidderName = "advangelists"
 	BidderApplogy          BidderName = "applogy"
 	BidderAppnexus         BidderName = "appnexus"
+	BidderAdoppler         BidderName = "adoppler"
 	BidderBeachfront       BidderName = "beachfront"
 	BidderBrightroll       BidderName = "brightroll"
 	BidderConsumable       BidderName = "consumable"
@@ -84,6 +85,7 @@ var BidderMap = map[string]BidderName{
 	"advangelists":      BidderAdvangelists,
 	"applogy":           BidderApplogy,
 	"appnexus":          BidderAppnexus,
+	"adoppler":          BidderAdoppler,
 	"beachfront":        BidderBeachfront,
 	"brightroll":        BidderBrightroll,
 	"consumable":        BidderConsumable,
