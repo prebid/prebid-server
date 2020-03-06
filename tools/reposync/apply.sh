2aa6d6c6ca1b0c7be01dd824579ef2c3e1b60bf1
sandeep <garidepalli.sandeepkumar@ntsindia.co.in>
2020-03-06 14:41:42 +0530

    removed tools from this commit

--- tools/reposync/apply.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-#kubectl create secret generic reposync -n reposync --dry-run -oyaml --from-file=kubernetes/secrets/reposync/ | kubectl apply -f -
-
-kubectl apply -f kubernetes/deployment.yaml
