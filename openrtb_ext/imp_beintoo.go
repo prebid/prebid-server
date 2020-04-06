5b028bbe876a7fe368f3d6cf99fcd1454e5f91c3
ddantuonobeintoo <58686785+ddantuonobeintoo@users.noreply.github.com>
2020-04-06 16:09:58 +0200

    Create imp_beintoo.go

---
+++ openrtb_ext/imp_beintoo.go
@@ -0,0 +1,7 @@
+package openrtb_ext
+
+// ExtImpBeintoo defines the contract for bidrequest.imp[i].ext.Beintoo
+type ExtImpBeintoo struct {
+	SupplyPartnerId  string `json:"supplyPartnerId"`
+	FavoredMediaType string `json:"favoredMediaType"`
+}
