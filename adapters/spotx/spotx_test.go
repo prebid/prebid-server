8639d70ea08b3cb03d7953f0bfb95793f8e943a1
mohammad-murad <mohammad.murad@pubmatic.com>
2020-04-22 12:17:56 +0530

    update test
    Signed-off-by: mohammad-murad <mohammad.murad@pubmatic.com>

--- adapters/spotx/spotx_test.go
+++ adapters/spotx/spotx_test.go
@@ -1,6 +1,7 @@
 package spotx
 
 import (
+	"encoding/json"
 	"github.com/PubMatic-OpenWrap/openrtb"
 	"github.com/PubMatic-OpenWrap/prebid-server/adapters"
 	"github.com/magiconair/properties/assert"
@@ -47,4 +48,18 @@ func TestSpotxMakeBid(t *testing.T) {
 	assert.Equal(t, reqData[0].Method, "POST")
 	assert.Equal(t, reqData[0].Uri, "https://search.spotxchange.com/openrtb/2.3/dados/85394")
 	assert.Equal(t, reqData[0].Headers.Get("Content-Type"), "application/json;charset=utf-8")
+
+	var bodyMap map[string]interface{}
+	_ = json.Unmarshal(reqData[0].Body, &bodyMap)
+	assert.Equal(t, bodyMap["id"].(string), "85394")
+
+	impMap := bodyMap["imp"].(map[string]interface{})
+	assert.Equal(t, impMap["bidfloor"].(float64), float64(9))
+	assert.Equal(t, impMap["secure"].(float64), float64(1))
+
+	extMap := impMap["video"].(map[string]interface{})["ext"].(map[string]interface{})
+	assert.Equal(t, extMap["ad_unit"], "instream")
+	assert.Equal(t, extMap["ad_volume"], 0.8)
+	assert.Equal(t, extMap["hide_skin"].(float64), float64(0))
+
 }
