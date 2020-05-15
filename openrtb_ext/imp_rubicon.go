268321f439483c6b884bd414156ffdd931e1f7c2
Umut Demirel <umutdemirell@gmail.com>
2020-05-14 23:48:15 -0700

    [NO-JIRA] Viewability vendors support

--- openrtb_ext/imp_rubicon.go
+++ openrtb_ext/imp_rubicon.go
@@ -6,13 +6,14 @@ import (
 
 // ExtImpRubicon defines the contract for bidrequest.imp[i].ext.rubicon
 type ExtImpRubicon struct {
-	AccountId int                `json:"accountId"`
-	SiteId    int                `json:"siteId"`
-	ZoneId    int                `json:"zoneId"`
-	Inventory json.RawMessage    `json:"inventory,omitempty"`
-	Visitor   json.RawMessage    `json:"visitor,omitempty"`
-	Video     rubiconVideoParams `json:"video"`
-	Region    string             `json:"region"`
+	AccountId          int                `json:"accountId"`
+	SiteId             int                `json:"siteId"`
+	ZoneId             int                `json:"zoneId"`
+	Inventory          json.RawMessage    `json:"inventory,omitempty"`
+	Visitor            json.RawMessage    `json:"visitor,omitempty"`
+	Video              rubiconVideoParams `json:"video"`
+	Region             string             `json:"region"`
+	ViewabilityVendors []string           `json:"viewabilityvendors"`
 }
 
 // rubiconVideoParams defines the contract for bidrequest.imp[i].ext.rubicon.video
