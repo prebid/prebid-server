60f77985c390b7b810dd148bb64e60c9c3435e59
ShriprasadM <shriprasad.marathe@gmail.com>
2020-05-25 15:21:13 +0530

    UOE-5196 Duration based Bid Response Combination Generator (#38)

--- endpoints/openrtb2/ctv/combination.go
+++ endpoints/openrtb2/ctv/combination.go
@@ -1,24 +1,39 @@
 package ctv
 
-import "github.com/PubMatic-OpenWrap/prebid-server/openrtb_ext"
+import (
+	"github.com/PubMatic-OpenWrap/prebid-server/openrtb_ext"
+)
 
+// ICombination ...
 type ICombination interface {
 	Get() []int
 }
 
+// Combination ...
 type Combination struct {
 	ICombination
-	data   []int
-	config *openrtb_ext.VideoAdPod
+	generator PodDurationCombination
+	config    *openrtb_ext.VideoAdPod
 }
 
+// NewCombination ...
 func NewCombination(data []int, config *openrtb_ext.VideoAdPod) *Combination {
+	generator := new(PodDurationCombination)
+	generator.Init(config, nil)
 	return &Combination{
-		data:   data[:],
-		config: config,
+		generator: *generator,
+		config:    config,
 	}
 }
 
+// Get next valid combination
 func (c *Combination) Get() []int {
-	return c.data[:]
+	nextComb := c.generator.Next()
+	nextCombInt := make([]int, len(nextComb))
+	cnt := 0
+	for _, duration := range nextComb {
+		nextCombInt[cnt] = int(duration)
+		cnt++
+	}
+	return nextCombInt
 }
