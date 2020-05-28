c05c013185580dd8ea5d7bdf5a93286aedfb18d9
moroz <wertixvost@gmail.com>
2020-05-28 16:20:02 +0300

    Add Adtarget server adapter

--- usersync/usersyncers/syncer_test.go
+++ usersync/usersyncers/syncer_test.go
@@ -21,6 +21,7 @@ func TestNewSyncerMap(t *testing.T) {
 			string(openrtb_ext.BidderAdmixer):          syncConfig,
 			string(openrtb_ext.BidderAdOcean):          syncConfig,
 			string(openrtb_ext.BidderAdpone):           syncConfig,
+			string(openrtb_ext.BidderAdtarget):         syncConfig,
 			string(openrtb_ext.BidderAdtelligent):      syncConfig,
 			string(openrtb_ext.BidderAdvangelists):     syncConfig,
 			string(openrtb_ext.BidderAJA):              syncConfig,
