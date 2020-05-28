d8d5fca2ae7956d0ba2b4ac84b9af38959b4f83e
moroz <wertixvost@gmail.com>
2020-05-28 16:13:25 +0300

    Reformat imports

--- adapters/adtelligent/usersync_test.go
+++ adapters/adtelligent/usersync_test.go
@@ -2,18 +2,17 @@ package adtelligent
 
 import (
 	"fmt"
-	"github.com/prebid/prebid-server/privacy/ccpa"
 	"testing"
 	"text/template"
 
 	"github.com/prebid/prebid-server/privacy"
+	"github.com/prebid/prebid-server/privacy/ccpa"
 	"github.com/prebid/prebid-server/privacy/gdpr"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestAdtelligentSyncer(t *testing.T) {
 	syncURL := "//sync.adtelligent.com/csync?t=p&ep=0&gdpr={{.GDPR}}&gdpr_consent={{.GDPRConsent}}&us_privacy={{.USPrivacy}}&redir=localhost%2Fsetuid%3Fbidder%3Dadtelligent%26gdpr%3D{{.GDPR}}%26gdpr_consent%3D{{.GDPRConsent}}%26uid%3D%7Buid%7D"
-	fmt.Println("adtelligent sync")
 	syncURLTemplate := template.Must(
 		template.New("sync-template").Parse(syncURL),
 	)
