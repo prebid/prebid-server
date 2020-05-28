051ff3901219b4051fb2abc22b38dbe6c85ad146
Dmitriy <DimaGarbar49@gmail.com>
2020-05-29 00:03:34 +0300

    Change placement of cpmoverride for Rubicon (#1310)

--- adapters/rubicon/rubicon_test.go
+++ adapters/rubicon/rubicon_test.go
@@ -1442,10 +1442,12 @@ func TestOpenRTBResponseOverridePriceFromBidRequest(t *testing.T) {
 				"zoneId": 327642
 			}}`),
 		}},
-		Ext: json.RawMessage(`{"rubicon": {
-				"debug": {
-					"cpmOverride" : 10 
-				}}}`),
+		Ext: json.RawMessage(`{"prebid": {
+			"bidders": {
+				"rubicon": {
+					"debug": {
+						"cpmoverride": 10
+			}}}}}`),
 	}
 
 	requestJson, _ := json.Marshal(request)
@@ -1486,14 +1488,16 @@ func TestOpenRTBResponseOverridePriceFromCorrespondingImp(t *testing.T) {
 				"siteId": 68780,
 				"zoneId": 327642,
 				"debug": {
-					"cpmOverride" : 20 
+					"cpmoverride" : 20 
 				}
 			}}`),
 		}},
-		Ext: json.RawMessage(`{"rubicon": {
-				"debug": {
-					"cpmOverride" : 10 
-				}}}`),
+		Ext: json.RawMessage(`{"prebid": {
+			"bidders": {
+				"rubicon": {
+					"debug": {
+						"cpmoverride": 10
+			}}}}}`),
 	}
 
 	requestJson, _ := json.Marshal(request)
