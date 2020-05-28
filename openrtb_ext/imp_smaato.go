5da5b8f84dc6c3875993f5612b38314e1fb9ea17
vikram <vikram.chandel@smaato.com>
2020-05-28 13:32:28 +0200

    PREB-1: updated Smaato adapter params doc

--- openrtb_ext/imp_smaato.go
+++ openrtb_ext/imp_smaato.go
@@ -1,7 +1,7 @@
 package openrtb_ext
 
 // ExtImpSmaato defines the contract for bidrequest.imp[i].ext.smaato
-// PublisherId is mandatory parameters, others are optional parameters
+// PublisherId and AdSpaceId are mandatory parameters, others are optional parameters
 // AdSpaceId is identifier for specific ad placement or ad tag
 
 type ExtImpSmaato struct {
