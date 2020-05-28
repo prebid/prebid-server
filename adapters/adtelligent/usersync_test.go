71a2be891685ed1927058fe78cd19d1c39db7c59
moroz <wertixvost@gmail.com>
2020-05-28 12:29:36 +0300

    Update adtelligent ortb endpoint

--- adapters/adtelligent/usersync_test.go
+++ adapters/adtelligent/usersync_test.go
@@ -1,6 +1,8 @@
 package adtelligent
 
 import (
+	"fmt"
+	"github.com/prebid/prebid-server/privacy/ccpa"
 	"testing"
 	"text/template"
 
@@ -10,7 +12,8 @@ import (
 )
 
 func TestAdtelligentSyncer(t *testing.T) {
-	syncURL := "//sync.adtelligent.com/csync?t=p&ep=0&redir=localhost%2Fsetuid%3Fbidder%3Dadtelligent%26gdpr%3D{{.GDPR}}%26gdpr_consent%3D{{.GDPRConsent}}%26uid%3D%7Buid%7D"
+	syncURL := "//sync.adtelligent.com/csync?t=p&ep=0&gdpr={{.GDPR}}&gdpr_consent={{.GDPRConsent}}&us_privacy={{.USPrivacy}}&redir=localhost%2Fsetuid%3Fbidder%3Dadtelligent%26gdpr%3D{{.GDPR}}%26gdpr_consent%3D{{.GDPRConsent}}%26uid%3D%7Buid%7D"
+	fmt.Println("adtelligent sync")
 	syncURLTemplate := template.Must(
 		template.New("sync-template").Parse(syncURL),
 	)
@@ -18,12 +21,16 @@ func TestAdtelligentSyncer(t *testing.T) {
 	syncer := NewAdtelligentSyncer(syncURLTemplate)
 	syncInfo, err := syncer.GetUsersyncInfo(privacy.Policies{
 		GDPR: gdpr.Policy{
-			Signal: "0",
+			Signal:  "0",
+			Consent: "123",
+		},
+		CCPA: ccpa.Policy{
+			Value: "1-YY",
 		},
 	})
 
 	assert.NoError(t, err)
-	assert.Equal(t, "//sync.adtelligent.com/csync?t=p&ep=0&redir=localhost%2Fsetuid%3Fbidder%3Dadtelligent%26gdpr%3D0%26gdpr_consent%3D%26uid%3D%7Buid%7D", syncInfo.URL)
+	assert.Equal(t, "//sync.adtelligent.com/csync?t=p&ep=0&gdpr=0&gdpr_consent=123&us_privacy=1-YY&redir=localhost%2Fsetuid%3Fbidder%3Dadtelligent%26gdpr%3D0%26gdpr_consent%3D123%26uid%3D%7Buid%7D", syncInfo.URL)
 	assert.Equal(t, "redirect", syncInfo.Type)
 	assert.EqualValues(t, 0, syncer.GDPRVendorID())
 	assert.Equal(t, false, syncInfo.SupportCORS)
