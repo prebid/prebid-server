a4ac24fca5d93dbb6e6c2c1943c5c116adf88153
steve alliance <steve.alliance@gmail.com>
2020-06-03 11:28:47 -0400

    update latest change for GDPR CCPA

--- adapters/dmx/usersync_test.go
+++ adapters/dmx/usersync_test.go
@@ -1,6 +1,7 @@
 package dmx
 
 import (
+	"github.com/prebid/prebid-server/privacy"
 	"testing"
 	"text/template"
 
@@ -10,7 +11,7 @@ import (
 func TestDmxSyncer(t *testing.T) {
 	temp := template.Must(template.New("sync-template").Parse("https://dmx.districtm.io/s/v1/img/s/10007"))
 	syncer := NewDmxSyncer(temp)
-	syncInfo, err := syncer.GetUsersyncInfo("", "")
+	syncInfo, err := syncer.GetUsersyncInfo(privacy.Policies{})
 	assert.NoError(t, err)
 	assert.Equal(t, "https://dmx.districtm.io/s/v1/img/s/10007", syncInfo.URL)
 	assert.Equal(t, "redirect", syncInfo.Type)
