12674a89035e94f18bcd625d9533a8a2e3085fb8
Aiholkin <artem.iholkin@smartyads.com>
2020-06-16 13:25:26 +0300

    add adman line to syner test

--- usersync/usersyncers/syncer_test.go
+++ usersync/usersyncers/syncer_test.go
@@ -73,6 +73,7 @@ func TestNewSyncerMap(t *testing.T) {
 			string(openrtb_ext.BidderYieldmo):          syncConfig,
 			string(openrtb_ext.BidderYieldone):         syncConfig,
 			string(openrtb_ext.BidderZeroClickFraud):   syncConfig,
+			string(openrtb_ext.BidderAdman):            syncConfig,
 		},
 	}
 
