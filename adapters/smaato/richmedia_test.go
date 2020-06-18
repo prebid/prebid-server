fe5161acf7ef044a8b3386f357974cb535842463
vikram <vikram.chandel@smaato.com>
2020-06-18 16:32:58 +0200

    PREB-1: Added test case for bad Ext and rich media cases

--- adapters/smaato/richmedia_test.go
+++ adapters/smaato/richmedia_test.go
@@ -6,6 +6,7 @@ import (
 
 func TestExtractAdmRichMedia(t *testing.T) {
 	type args struct {
+		adType             adMarkupType
 		adapterResponseAdm string
 	}
 	tests := []struct {
@@ -13,13 +14,14 @@ func TestExtractAdmRichMedia(t *testing.T) {
 		args     args
 		result   string
 	}{
-		{"richmediaTest", args{"{\"richmedia\":{\"mediadata\":{\"content\":\"<div>hello</div>\"," + "\"w\":350," +
+		{"richmediaTest", args{"Richmedia", "{\"richmedia\":{\"mediadata\":{\"content\":\"<div>hello</div>\"," +
+			"" + "\"w\":350," +
 			"\"h\":50},\"impressiontrackers\":[\"//prebid-test.smaatolabs.net/track/imp/1\",\"//prebid-test.smaatolabs.net/track/imp/2\"],\"clicktrackers\":[\"//prebid-test.smaatolabs.net/track/click/1\",\"//prebid-test.smaatolabs.net/track/click/2\"]}}"}, `<div onclick="fetch(decodeURIComponent('%2F%2Fprebid-test.smaatolabs.net%2Ftrack%2Fclick%2F1'), {cache: 'no-cache'});fetch(decodeURIComponent('%2F%2Fprebid-test.smaatolabs.net%2Ftrack%2Fclick%2F2'), {cache: 'no-cache'});"><div>hello</div><img src="//prebid-test.smaatolabs.net/track/imp/1" alt="" width="0" height="0"/><img src="//prebid-test.smaatolabs.net/track/imp/2" alt="" width="0" height="0"/></div>`,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.testName, func(t *testing.T) {
-			got, err := extractAdmRichMedia(tt.args.adapterResponseAdm)
+			got, err := renderAdMarkup(tt.args.adType, tt.args.adapterResponseAdm)
 			if err != nil {
 				t.Errorf("error rendering ad markup: %v", err)
 			}
