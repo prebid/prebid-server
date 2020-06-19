84973a95015c86a08fb17043da2cb4fc90d8ee3c
tadam <tadam@smartadserver.com>
2020-06-19 11:01:51 +0200

    Testing smartadserver params.

--- openrtb_ext/imp_smartadserver.go
+++ openrtb_ext/imp_smartadserver.go
@@ -2,9 +2,8 @@ package openrtb_ext
 
 // ExtImpSmartadserver defines the contract for bidrequest.imp[i].ext.smartadserver
 type ExtImpSmartadserver struct {
-	Domain    string `json:"domain"`
-	SiteID    int    `json:"siteId"`
-	PageID    int    `json:"pageId"`
-	FormatID  int    `json:"formatId"`
-	NetworkID int    `json:"networkId"`
+	SiteID    int `json:"siteId"`
+	PageID    int `json:"pageId"`
+	FormatID  int `json:"formatId"`
+	NetworkID int `json:"networkId"`
 }
