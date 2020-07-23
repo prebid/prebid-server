2ff6955814f3a89da4d771e5ac1a2a1f85d84a04
vikram <vikram.chandel@smaato.com>
2020-07-23 11:16:59 +0200

    PREB-8: Updated prebid uri for test cases

--- adapters/smaato/smaato_test.go
+++ adapters/smaato/smaato_test.go
@@ -7,5 +7,5 @@ import (
 )
 
 func TestJsonSamples(t *testing.T) {
-	adapterstest.RunJSONBidderTest(t, "smaatotest", NewSmaatoBidder(nil, "https://prebid.ad.smaato.net/oapi/prebid"))
+	adapterstest.RunJSONBidderTest(t, "smaatotest", NewSmaatoBidder(nil, "https://prebid/bidder"))
 }
