d74cf9ac03ec6df7c3eae8c22eb5f6a260b9bb9a
bsardo <1168933+bsardo@users.noreply.github.com>
2020-08-11 15:47:30 -0400

    Simplify rate converter GetInfo endpoint to only call GetRates once

--- currencies/rate_converter.go
+++ currencies/rate_converter.go
@@ -139,9 +139,7 @@ func (rc *RateConverter) checkStaleRates() bool {
 // GetInfo returns setup information about the converter
 func (rc *RateConverter) GetInfo() ConverterInfo {
 	var rates *map[string]map[string]float64
-	if rc.Rates() != nil {
-		rates = rc.Rates().GetRates()
-	}
+	rates = rc.Rates().GetRates()
 	return converterInfo{
 		source:      rc.syncSourceURL,
 		lastUpdated: rc.LastUpdated(),
