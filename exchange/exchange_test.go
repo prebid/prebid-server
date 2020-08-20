80d557ece7ae79413755db8021e9fd0559b1954c
hhhjort <31041505+hhhjort@users.noreply.github.com>
2020-08-20 15:36:33 -0400

    Fixes bug (#1448)
    * Fixes bug
    
    * shortens list

--- exchange/exchange_test.go
+++ exchange/exchange_test.go
@@ -48,6 +48,9 @@ func TestNewExchange(t *testing.T) {
 			ExpectedTimeMillis: 20,
 		},
 		Adapters: blankAdapterConfig(openrtb_ext.BidderList()),
+		GDPR: config.GDPR{
+			EEACountries: []string{"FIN", "FRA", "GUF"},
+		},
 	}
 
 	currencyConverter := currencies.NewRateConverter(&http.Client{}, "", time.Duration(0))
