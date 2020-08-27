8cc2c9f1e261be649d90837515a53c848da8bbfa
Scott Kay <noreply@syntaxnode.com>
2020-08-27 14:19:25 -0400

    Add Scheme Option To External Cache URL (#1460)

--- config/config_test.go
+++ config/config_test.go
@@ -90,6 +90,21 @@ func TestExternalCacheURLValidate(t *testing.T) {
 			data:      ExternalCache{Host: "http://", Path: ""},
 			expErrors: 1,
 		},
+		{
+			desc:      "Scheme Invalid",
+			data:      ExternalCache{Scheme: "invalid", Host: "www.google.com", Path: "/path/v1"},
+			expErrors: 1,
+		},
+		{
+			desc:      "Scheme HTTP",
+			data:      ExternalCache{Scheme: "http", Host: "www.google.com", Path: "/path/v1"},
+			expErrors: 0,
+		},
+		{
+			desc:      "Scheme HTTPS",
+			data:      ExternalCache{Scheme: "https", Host: "www.google.com", Path: "/path/v1"},
+			expErrors: 0,
+		},
 	}
 	for _, test := range testCases {
 		var errs configErrors
@@ -150,6 +165,7 @@ cache:
   host: prebidcache.net
   query: uuid=%PBS_CACHE_UUID%
 external_cache:
+  scheme: https
   host: www.externalprebidcache.net
   path: /endpoints/cache
 http_client:
@@ -307,6 +323,7 @@ func TestFullConfig(t *testing.T) {
 	cmpStrings(t, "cache.scheme", cfg.CacheURL.Scheme, "http")
 	cmpStrings(t, "cache.host", cfg.CacheURL.Host, "prebidcache.net")
 	cmpStrings(t, "cache.query", cfg.CacheURL.Query, "uuid=%PBS_CACHE_UUID%")
+	cmpStrings(t, "external_cache.scheme", cfg.ExtCacheURL.Scheme, "https")
 	cmpStrings(t, "external_cache.host", cfg.ExtCacheURL.Host, "www.externalprebidcache.net")
 	cmpStrings(t, "external_cache.path", cfg.ExtCacheURL.Path, "/endpoints/cache")
 	cmpInts(t, "http_client.max_connections_per_host", cfg.Client.MaxConnsPerHost, 10)
