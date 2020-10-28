5f32d6d5040db8c655ef93d0a15cf8b5e8356af2
shalmali-patil <shalmali.patil@pubmatic.com>
2020-10-28 11:34:29 +0530

    UOE-5741: adding omitempty for ExtImpPrebid fields

--- openrtb_ext/imp.go
+++ openrtb_ext/imp.go
@@ -18,18 +18,18 @@ type ExtImp struct {
 
 // ExtImpPrebid defines the contract for bidrequest.imp[i].ext.prebid
 type ExtImpPrebid struct {
-	StoredRequest *ExtStoredRequest `json:"storedrequest"`
+	StoredRequest *ExtStoredRequest `json:"storedrequest,omitempty"`
 
 	// Rewarded inventory signal, can be 0 or 1
-	IsRewardedInventory int8 `json:"is_rewarded_inventory"`
+	IsRewardedInventory int8 `json:"is_rewarded_inventory,omitempty"`
 
 	// NOTE: This is not part of the official API, we are not expecting clients
 	// migrate from imp[...].ext.${BIDDER} to imp[...].ext.prebid.bidder.${BIDDER}
 	// at this time
 	// https://github.com/PubMatic-OpenWrap/prebid-server/pull/846#issuecomment-476352224
-	Bidder map[string]json.RawMessage `json:"bidder"`
+	Bidder map[string]json.RawMessage `json:"bidder,omitempty"`
 
-	SKAdnetwork json.RawMessage `json:"skadn"`
+	SKAdnetwork json.RawMessage `json:"skadn,omitempty"`
 }
 
 // ExtStoredRequest defines the contract for bidrequest.imp[i].ext.prebid.storedrequest
