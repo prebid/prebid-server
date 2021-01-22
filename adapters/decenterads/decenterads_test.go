d0462f61df1382cf5a9107ecb644cc63ea9d4872
vlad <vlaktionov@decenterads.com>
2021-01-22 16:14:24 +0200

    set DC adapter

--- adapters/decenterads/decenterads_test.go
+++ adapters/decenterads/decenterads_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestJsonSamples(t *testing.T) {
 	bidder, buildErr := Builder(openrtb_ext.BidderDecenterAds, config.Adapter{
-		Endpoint: "http://example.com/?c=o&m=rtb"})
+		Endpoint: "http://example.com/?c=o&m=ortb"})
 	if buildErr != nil {
 		t.Fatalf("Builder returned unexpected error %v", buildErr)
 	}
