1b0865b4453626d13a8464c5d18391400e542106
Guillaume <guillaume.andouard@adyoulike.com>
2021-02-12 11:47:36 +0100

    remove camel case from json

--- openrtb_ext/imp_adyoulike.go
+++ openrtb_ext/imp_adyoulike.go
@@ -3,17 +3,16 @@ package openrtb_ext
 // ExtImpAdkernel defines the contract for bidrequest.imp[i].ext.adkernel
 type ExtImpAdyoulike struct {
 	// placementId, only mandatory field
-	PlacementId string `json:"placementId"`
-	
-	// Id of the forced campaign	
-	Campaign string    `json:"Campaign"`
-	// Id of the forced track
-	Track string       `json:"Track"`
-	// Id of the forced creative
-	Creative string    `json:"Creative"`
-	// Context of the campaign values [SSP|AdServer]
-	Source string	  `json:"Source"`
-	// Abitrary Id used for debug purpose
-	Debug string	  `json:"Debug"`
+	PlacementId string `json:"placement"`
 
+	// Id of the forced campaign
+	Campaign string `json:"campaign"`
+	// Id of the forced track
+	Track string `json:"track"`
+	// Id of the forced creative
+	Creative string `json:"creative"`
+	// Context of the campaign values [SSP|AdServer]
+	Source string `json:"source"`
+	// Abitrary Id used for debug purpose
+	Debug string `json:"debug"`
 }
