340b70bfc97a7cd35dd9814947781424e0d2d6cb
Guillaume <guillaume.andouard@adyoulike.com>
2021-02-15 16:47:52 +0100

    update usync url

--- adapters/adyoulike/usersync_test.go
+++ adapters/adyoulike/usersync_test.go
@@ -7,11 +7,12 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/prebid/prebid-server/privacy"
+	"github.com/prebid/prebid-server/privacy/ccpa"
 	"github.com/prebid/prebid-server/privacy/gdpr"
 )
 
 func TestAdyoulikeSyncer(t *testing.T) {
-	syncURL := "//visitor.omnitagjs.com/broker/bsync?gdpr_consent_string={{.GDPRConsent}}&gdpr={{.GDPR}}"
+	syncURL := "//visitor.omnitagjs.com/visitor/bsync?uid=19340f4f097d16f41f34fc0274981ca4&name=PrebidServer&gdpr_consent_string={{.GDPRConsent}}&gdpr={{.GDPR}}&us_privacy={{.USPrivacy}}"
 	syncURLTemplate := template.Must(
 		template.New("sync-template").Parse(syncURL),
 	)
@@ -22,10 +23,13 @@ func TestAdyoulikeSyncer(t *testing.T) {
 			Signal:  "1",
 			Consent: "BONciguONcjGKADACHENAOLS1rAHDAFAAEAASABQAMwAeACEAFw",
 		},
+		CCPA: ccpa.Policy{
+			Consent: "1-YY",
+		},
 	})
 
 	assert.NoError(t, err)
-	assert.Equal(t, "//visitor.omnitagjs.com/broker/bsync?gdpr_consent_string=BONciguONcjGKADACHENAOLS1rAHDAFAAEAASABQAMwAeACEAFw&gdpr=1", syncInfo.URL)
+	assert.Equal(t, "//visitor.omnitagjs.com/visitor/bsync?uid=19340f4f097d16f41f34fc0274981ca4&name=PrebidServer&gdpr_consent_string=BONciguONcjGKADACHENAOLS1rAHDAFAAEAASABQAMwAeACEAFw&gdpr=1&us_privacy=1-YY", syncInfo.URL)
 	assert.Equal(t, "redirect", syncInfo.Type)
 	assert.EqualValues(t, 0, syncer.GDPRVendorID())
 	assert.Equal(t, false, syncInfo.SupportCORS)
