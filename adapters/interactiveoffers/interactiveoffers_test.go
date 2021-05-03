87949dbbcb4431ca5f6e136fc1fb8e8c2435f744
IOTiagoFaria <76956619+IOTiagoFaria@users.noreply.github.com>
2021-05-03 10:36:19 +0100

    New Adapter: InteractiveOffers check with master

--- adapters/interactiveoffers/interactiveoffers_test.go
+++ adapters/interactiveoffers/interactiveoffers_test.go
@@ -12,6 +12,7 @@ func TestJsonSamples(t *testing.T) {
 	bidder, buildErr := Builder(openrtb_ext.BidderInteractiveoffers, config.Adapter{
 		Endpoint: "https://rtb.ioadx.com/bidRequest/?partnerId=d9e56d418c4825d466ee96c7a31bf1da6b62fa04"})
 
+
 	if buildErr != nil {
 		t.Fatalf("Builder returned unexpected error %v", buildErr)
 	}
