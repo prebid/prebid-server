9a0bc46a6854e10e7ac3fba8c9d218b5d9527a9c
jim naumann <jim@beachfront.com>
2021-05-13 12:43:11 +0000

    removed superfluous conditional

--- adapters/beachfront/beachfront.go
+++ adapters/beachfront/beachfront.go
@@ -266,11 +266,7 @@ func getAppId(ext openrtb_ext.ExtImpBeachfront, media openrtb_ext.BidType) (stri
 
 func getSchain(request *openrtb2.BidRequest) (openrtb_ext.ExtRequestPrebidSChain, error) {
 	var schain openrtb_ext.ExtRequestPrebidSChain
-	var err error
-	if err = json.Unmarshal(request.Source.Ext, &schain); err != nil {
-		return schain, err
-	}
-
+	var err error = json.Unmarshal(request.Source.Ext, &schain)
 	return schain, err
 }
 
