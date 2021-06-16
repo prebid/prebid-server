b8a58b9d84984fb371a57a255ff00a657cf53708
Sachin Survase <sachin.survase@pubmatic.com>
2021-06-16 11:40:47 +0530

    Removed unnecessary space in definition of ExtImpBidder

--- adapters/bidder.go
+++ adapters/bidder.go
@@ -128,7 +128,7 @@ type ExtImpBidder struct {
 	//
 	// Bidder implementations may safely assume that this JSON has been validated by their
 	// static/bidder-params/{bidder}.json file.
-	Bidder json.RawMessage      `json:"bidder"`
+	Bidder json.RawMessage `json:"bidder"`
 }
 
 func (r *RequestData) SetBasicAuth(username string, password string) {
