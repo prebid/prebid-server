9489942cc0bc837e01384ca78caea307cfbcb81d
Scott Kay <noreply@syntaxnode.com>
2021-06-22 20:53:25 -0400

    Friendlier Startup Error Messages (#1894)

--- main.go
+++ main.go
@@ -34,12 +34,12 @@ func main() {
 
 	cfg, err := loadConfig()
 	if err != nil {
-		glog.Fatalf("Configuration could not be loaded or did not pass validation: %v", err)
+		glog.Exitf("Configuration could not be loaded or did not pass validation: %v", err)
 	}
 
 	err = serve(Rev, cfg)
 	if err != nil {
-		glog.Errorf("prebid-server failed: %v", err)
+		glog.Exitf("prebid-server failed: %v", err)
 	}
 }
 
