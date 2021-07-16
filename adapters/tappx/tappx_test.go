cb143be6bcf6a1c19312c5bd93f0a3876a403acc
Albert Grandes <agrandes@tappx.com>
2021-07-16 17:25:11 +0200

    Tappx - Try if this is the origin of problem

--- adapters/tappx/tappx_test.go
+++ adapters/tappx/tappx_test.go
@@ -44,6 +44,7 @@ func TestTsValue(t *testing.T) {
 	tappxExt.Host = "example.host.tappx.com"
 	tappxExt.Endpoint = "DUMMYENDPOINT"
 	tappxExt.TappxKey = "dummy-tappx-key"
+	tappxExt.Rewarded = 1
 
 	url, err := bidderTappx.buildEndpointURL(&tappxExt, test)
 
