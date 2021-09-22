2ef05d350ad9327ddbe9d06ce460cace7eb4529b
Mikael Lundin <mikael@coil.se>
2021-09-22 12:54:29 +0200

    Tests work but fail.

--- adapters/adnuntius/params_test.go
+++ adapters/adnuntius/params_test.go
@@ -7,9 +7,9 @@ import (
 	"github.com/prebid/prebid-server/openrtb_ext"
 )
 
-// This file actually intends to test static/bidder-params/adtelligent.json
-// These also validate the format of the external API: request.imp[i].ext.adtelligent
-// TestValidParams makes sure that the adtelligent schema accepts all imp.ext fields which we intend to support.
+// This file actually intends to test static/bidder-params/adnuntius.json
+// These also validate the format of the external API: request.imp[i].ext.adnuntius
+// TestValidParams makes sure that the adnuntius schema accepts all imp.ext fields which we intend to support.
 
 func TestValidParams(t *testing.T) {
 	validator, err := openrtb_ext.NewBidderParamsValidator("../../static/bidder-params")
@@ -18,13 +18,13 @@ func TestValidParams(t *testing.T) {
 	}
 
 	for _, validParam := range validParams {
-		if err := validator.Validate(openrtb_ext.BidderAdtelligent, json.RawMessage(validParam)); err != nil {
-			t.Errorf("Schema rejected adtelligent params: %s", validParam)
+		if err := validator.Validate(openrtb_ext.BidderAdnuntius, json.RawMessage(validParam)); err != nil {
+			t.Errorf("Schema rejected adnuntius params: %s", validParam)
 		}
 	}
 }
 
-// TestInvalidParams makes sure that the adtelligent schema rejects all the imp.ext fields we don't support.
+// TestInvalidParams makes sure that the adnuntius schema rejects all the imp.ext fields we don't support.
 func TestInvalidParams(t *testing.T) {
 	validator, err := openrtb_ext.NewBidderParamsValidator("../../static/bidder-params")
 	if err != nil {
@@ -32,17 +32,14 @@ func TestInvalidParams(t *testing.T) {
 	}
 
 	for _, invalidParam := range invalidParams {
-		if err := validator.Validate(openrtb_ext.BidderAdtelligent, json.RawMessage(invalidParam)); err == nil {
+		if err := validator.Validate(openrtb_ext.BidderAdnuntius, json.RawMessage(invalidParam)); err == nil {
 			t.Errorf("Schema allowed unexpected params: %s", invalidParam)
 		}
 	}
 }
 
 var validParams = []string{
-	`{"aid":123}`,
-	`{"aid":123,"placementId":1234}`,
-	`{"aid":123,"siteId":4321}`,
-	`{"aid":123,"siteId":0,"bidFloor":0}`,
+	`{"auId":123}`,
 }
 
 var invalidParams = []string{
