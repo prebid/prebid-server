da559f6b22563b44fec11448a8c0f6a19d934811
Scott Kay <noreply@syntaxnode.com>
2021-09-24 00:26:45 -0400

    Public Version Endpoint

--- endpoints/version.go
+++ endpoints/version.go
@@ -7,31 +7,33 @@ import (
 	"github.com/golang/glog"
 )
 
-type versionModel struct {
-	Version  string `json:"version"`
-	Revision string `json:"revision"`
-}
+const versionNotSet = "not-set"
 
 // NewVersionEndpoint returns the latest git tag as the version and commit hash as the revision from which the binary was built
-func NewVersionEndpoint(version string, revision string) http.HandlerFunc {
-	if version == "" {
-		version = "not-set"
-	}
-	if revision == "" {
-		revision = "not-set"
+func NewVersionEndpoint(version, revision string) http.HandlerFunc {
+	response, err := prepareVersionResponse(version, revision)
+	if err != nil {
+		glog.Fatalf("error creating /version endpoint response: %v", err)
 	}
 
 	return func(w http.ResponseWriter, _ *http.Request) {
-		jsonOutput, err := json.Marshal(versionModel{
-			Version:  version,
-			Revision: revision,
-		})
-		if err != nil {
-			glog.Errorf("/version Critical error when trying to marshal versionModel: %v", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		w.Write(jsonOutput)
+		w.Write(response)
 	}
+}
+
+func prepareVersionResponse(version, revision string) (json.RawMessage, error) {
+	if version == "" {
+		version = versionNotSet
+	}
+	if revision == "" {
+		revision = versionNotSet
+	}
+
+	return json.Marshal(struct {
+		Revision string `json:"revision"`
+		Version  string `json:"version"`
+	}{
+		Revision: revision,
+		Version:  version,
+	})
 }
