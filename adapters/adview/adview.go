ef9ab394d019e24554ee87137f51d8c877058cf8
wildcat0601 <wildcat.wang@qq.com>
2021-10-29 10:30:46 +0800

    (1)remove unused codes (2)add test codes for currency.

--- adapters/adview/adview.go
+++ adapters/adview/adview.go
@@ -100,7 +100,6 @@ func (a *adapter) MakeBids(request *openrtb2.BidRequest, requestData *adapters.R
 	}
 
 	bidResponse := adapters.NewBidderResponseWithBidsCapacity(len(request.Imp))
-	//bidResponse.Currency = response.Cur
 	bidResponse.Currency = "USD" //we just support USD for resp
 
 	var errors []error
