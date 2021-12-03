a9894964ef07547966158104182b3d101d9b034b
Michael Stevens <michael.stevens@dianomi.com>
2021-12-03 11:27:47 +0000

    very basic start of bidder

--- adapters/dianomi/dianomi.go
+++ adapters/dianomi/dianomi.go
@@ -4,38 +4,86 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"text/template"
 
 	"github.com/mxmCherry/openrtb/v15/openrtb2"
 	"github.com/prebid/prebid-server/adapters"
 	"github.com/prebid/prebid-server/config"
 	"github.com/prebid/prebid-server/errortypes"
+	"github.com/prebid/prebid-server/macros"
 	"github.com/prebid/prebid-server/openrtb_ext"
 )
 
 type adapter struct {
-	endpoint string
+	endpoint         string
+	endpointTemplate *template.Template
 }
 
 func Builder(bidderName openrtb_ext.BidderName, config config.Adapter) (adapters.Bidder, error) {
+	template, err := template.New("endpointTemplate").Parse(config.Endpoint)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse endpoint url template: %v", err)
+	}
+
 	bidder := &adapter{
-		endpoint: config.Endpoint,
+		endpoint:         config.Endpoint,
+		endpointTemplate: template,
 	}
 	return bidder, nil
 }
 
+type dianomiExtImpBidder struct {
+	Bidder json.RawMessage `json:"bidder"`
+}
+
 func (a *adapter) MakeRequests(request *openrtb2.BidRequest, requestInfo *adapters.ExtraRequestInfo) ([]*adapters.RequestData, []error) {
-	requestJSON, err := json.Marshal(request)
-	if err != nil {
-		return nil, []error{err}
+	numRequests := len(request.Imp)
+	requestData := make([]*adapters.RequestData, 0, numRequests)
+	errs := make([]error, 0, len(request.Imp))
+	var err error
+
+	requestImpCopy := request.Imp
+	for _, imp := range requestImpCopy {
+
+		var bidderExt dianomiExtImpBidder
+		if err = json.Unmarshal(imp.Ext, &bidderExt); err != nil {
+			errs = append(errs, &errortypes.BadInput{
+				Message: err.Error(),
+			})
+			continue
+		}
+
+		var dianomiExt openrtb_ext.ImpExtDianomi
+		if err = json.Unmarshal(bidderExt.Bidder, &dianomiExt); err != nil {
+			errs = append(errs, &errortypes.BadInput{
+				Message: err.Error(),
+			})
+			continue
+		}
+
+		endpoint, err := macros.ResolveMacros(a.endpointTemplate, dianomiExt)
+
+		if err != nil {
+			errs = append(errs, err)
+			continue
+		}
+
+		request := &adapters.RequestData{
+			Method: "GET",
+			Uri:    endpoint,
+		}
+
+		requestData = append(requestData, request)
 	}
 
-	requestData := &adapters.RequestData{
-		Method: "POST",
-		Uri:    a.endpoint,
-		Body:   requestJSON,
-	}
+	return requestData, errs
+}
 
-	return []*adapters.RequestData{requestData}, nil
+type dianomiResponse struct {
+	bidAmount  float64 `json:"bid_amount"`
+	bidCurency string  `json:"bid_currency"`
+	winURL     string  `json:"win_url"`
+	content    string  `json:"content"`
 }
 
 func (a *adapter) MakeBids(request *openrtb2.BidRequest, requestData *adapters.RequestData, responseData *adapters.ResponseData) (*adapters.BidderResponse, []error) {
@@ -57,23 +105,25 @@ func (a *adapter) MakeBids(request *openrtb2.BidRequest, requestData *adapters.R
 		return nil, []error{err}
 	}
 
-	var response openrtb2.BidResponse
+	var response dianomiResponse
 	if err := json.Unmarshal(responseData.Body, &response); err != nil {
 		return nil, []error{err}
 	}
 
 	bidResponse := adapters.NewBidderResponseWithBidsCapacity(len(request.Imp))
-	bidResponse.Currency = response.Cur
-	for _, seatBid := range response.SeatBid {
-		for i := range seatBid.Bid {
-			b := &adapters.TypedBid{
-				Bid: &seatBid.Bid[i],
-				//BidType: getMediaTypeForBid(bid),
-				BidType: openrtb_ext.BidTypeBanner,
-			}
-			bidResponse.Bids = append(bidResponse.Bids, b)
+	bidResponse.Currency = response.bidCurency
+	for _, imp := range request.Imp {
+		b := &adapters.TypedBid{
+			Bid: &openrtb2.Bid{
+				ID:    "1234", // bid id
+				CrID:  "1234", // creative id
+				ImpID: imp.ID,
+				Price: response.bidAmount,
+				AdM:   response.content,
+			},
+			BidType: openrtb_ext.BidTypeBanner,
 		}
-		return bidResponse, nil
+		bidResponse.Bids = append(bidResponse.Bids, b)
 	}
 
 	return bidResponse, nil
