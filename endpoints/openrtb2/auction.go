13dd53d54109b28b92a31a7c84925aa4ee0af97f
Doug Shore <dshore@jwplayer.com>
2022-02-02 13:24:59 -0500

    Add auction error logging

--- endpoints/openrtb2/auction.go
+++ endpoints/openrtb2/auction.go
@@ -1579,6 +1579,9 @@ func writeError(errs []error, w http.ResponseWriter, labels *metrics.Labels) boo
 		labels.RequestStatus = metricsStatus
 		for _, err := range errs {
 			w.Write([]byte(fmt.Sprintf("Invalid request: %s\n", err.Error())))
+			if glog.V(2) {
+				glog.Infof("Invalid request: %s", err.Error())
+			}
 		}
 		rc = true
 	}
