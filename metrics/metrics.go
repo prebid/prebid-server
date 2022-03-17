fd3bdeb63bbc7ca05bc236bfaaa90a5ec7fe9f83
Scott Kay <noreply@syntaxnode.com>
2022-03-17 14:38:40 -0400

    Fix Tpyos

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -404,7 +404,7 @@ type MetricsEngine interface {
 	RecordStoredDataError(labels StoredDataLabels)
 	RecordPrebidCacheRequestTime(success bool, length time.Duration)
 	RecordRequestQueueTime(success bool, requestType RequestType, length time.Duration)
-	RecordTimeoutNotice(sucess bool)
+	RecordTimeoutNotice(success bool)
 	RecordRequestPrivacy(privacy PrivacyLabels)
 	RecordAdapterGDPRRequestBlocked(adapterName openrtb_ext.BidderName)
 }
