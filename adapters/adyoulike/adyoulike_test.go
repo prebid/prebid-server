c32a10d4e64a33076cb3bf08befa68ae0fab942e
Chris <christophe@adyoulike.com>
2022-03-21 18:03:34 +0100

    Adyoulike: use USD currency

--- adapters/adyoulike/adyoulike_test.go
+++ adapters/adyoulike/adyoulike_test.go
@@ -12,7 +12,8 @@ const testsBidderEndpoint = "https://localhost/bid/4"
 
 func TestJsonSamples(t *testing.T) {
 	bidder, buildErr := Builder(openrtb_ext.BidderAdyoulike, config.Adapter{
-		Endpoint: testsBidderEndpoint})
+		Endpoint: testsBidderEndpoint,
+	})
 
 	if buildErr != nil {
 		t.Fatalf("Builder returned unexpected error %v", buildErr)
