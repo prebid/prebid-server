0a06ddd958282b0f00bfd9d8fbf2a1fabe2d8004
pm-nilesh-chate <nilesh.chate@pubmatic.com>
2022-03-23 14:25:10 +0000

    UOE-7451: PubMatic: add "acat" parameter

--- adapters/pubmatic/pubmatic_test.go
+++ adapters/pubmatic/pubmatic_test.go
@@ -150,3 +150,79 @@ func TestParseImpressionObject(t *testing.T) {
 		})
 	}
 }
+
+func TestExtractPubmaticExtFromRequest(t *testing.T) {
+	type args struct {
+		request *openrtb2.BidRequest
+	}
+	tests := []struct {
+		name               string
+		args               args
+		expectedWrapperExt *pubmaticWrapperExt
+		expectedAcat       []string
+		wantErr            bool
+	}{
+		{
+			name:    "Empty bidder param",
+			wantErr: true,
+		},
+		{
+			name: "Pubmatic wrapper ext missing/empty",
+			args: args{
+				request: &openrtb2.BidRequest{
+					Ext: json.RawMessage(`{"prebid":{"bidderparams":{}}}`),
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Only Pubmatic wrapper ext present",
+			args: args{
+				request: &openrtb2.BidRequest{
+					Ext: json.RawMessage(`{"prebid":{"bidderparams":{"wrapper":{"profile":123,"version":456}}}}`),
+				},
+			},
+			expectedWrapperExt: &pubmaticWrapperExt{ProfileID: 123, VersionID: 456},
+			wantErr:            false,
+		},
+		{
+			name: "Invalid Pubmatic wrapper ext",
+			args: args{
+				request: &openrtb2.BidRequest{
+					Ext: json.RawMessage(`{"prebid":{"bidderparams":{"wrapper":{"profile":"123","version":456}}}}`),
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "Valid Pubmatic acat ext",
+			args: args{
+				request: &openrtb2.BidRequest{
+					Ext: json.RawMessage(`{"prebid":{"bidderparams":{"acat":["drg”,”dlu”,”ssr"],"wrapper":{"profile":123,"version":456}}}}`),
+				},
+			},
+			expectedWrapperExt: &pubmaticWrapperExt{ProfileID: 123, VersionID: 456},
+			expectedAcat:       []string{"drg”,”dlu”,”ssr"},
+			wantErr:            false,
+		},
+		{
+			name: "Invalid Pubmatic acat ext. We are ok with acat being non nil in this case as we are returning unmarshal error",
+			args: args{
+				request: &openrtb2.BidRequest{
+					Ext: json.RawMessage(`{"prebid":{"bidderparams":{"acat":[1,3,4],"wrapper":{"profile":123,"version":456}}}}`),
+				},
+			},
+			expectedWrapperExt: &pubmaticWrapperExt{ProfileID: 123, VersionID: 456},
+			expectedAcat:       []string{"", "", ""},
+			wantErr:            true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotWrapperExt, gotAcat, err := extractPubmaticExtFromRequest(tt.args.request)
+			assert.Equal(t, tt.wantErr, err != nil)
+			assert.Equal(t, tt.expectedWrapperExt, gotWrapperExt)
+			assert.Equal(t, tt.expectedAcat, gotAcat)
+		})
+	}
+}
