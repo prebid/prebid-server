824b6bfe8b1a79fe875b0766c64250e42f44ff06
mediasquare-alexandre <alexandre.harribey@mediasquare.fr>
2022-04-15 11:44:07 +0200

    [T]: test & coverage.

--- server/server_test.go
+++ server/server_test.go
@@ -3,7 +3,9 @@ package server
 import (
 	"net/http"
 	"os"
+	"strconv"
 	"testing"
+	"time"
 
 	"github.com/prebid/prebid-server/config"
 )
@@ -80,4 +82,101 @@ func forwardSignal(t *testing.T, outbound chan<- struct{}, inbound <-chan os.Sig
 		t.Errorf("Unexpected signal: %s", sig.String())
 	}
 	outbound <- s
+}
+
+func Test_newSocketServer(t *testing.T) {
+	cfg := new(config.Configuration)
+	cfg.Socket = "mock_socket_addr"
+
+	mock_server := &http.Server{
+		Addr:         cfg.Socket,
+		Handler:      nil,
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+	}
+
+	ret := newSocketServer(cfg, nil)
+	switch {
+	case ret.Addr != mock_server.Addr:
+		t.Error("[Test_newSocketServer] Addr invalide: %v != %v",
+			ret.Addr, mock_server.Addr)
+	case ret.ReadTimeout != mock_server.ReadTimeout:
+		t.Error("[Test_newSocketServer] ReadTimeout invalide: %v != %v",
+			ret.ReadTimeout, mock_server.ReadTimeout)
+	case ret.WriteTimeout != mock_server.WriteTimeout:
+		t.Error("[Test_newSocketServer] WriteTimeout invalide: %v != %v",
+			ret.WriteTimeout, mock_server.WriteTimeout)
+	}
+	ret.Close()
+}
+
+func Test_newMainServer(t *testing.T) {
+	cfg := new(config.Configuration)
+	cfg.Socket = "mock_socket_addr"
+
+	mock_server := &http.Server{
+		Addr:         cfg.Socket,
+		Handler:      nil,
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+	}
+
+	ret := newMainServer(cfg, nil)
+	switch {
+	case ret.Addr != mock_server.Addr:
+		t.Error("[Test_newMainServer] Addr invalide: %v != %v",
+			ret.Addr, mock_server.Addr)
+	case ret.ReadTimeout != mock_server.ReadTimeout:
+		t.Error("[Test_newMainServer] ReadTimeout invalide: %v != %v",
+			ret.ReadTimeout, mock_server.ReadTimeout)
+	case ret.WriteTimeout != mock_server.WriteTimeout:
+		t.Error("[Test_newMainServer] WriteTimeout invalide: %v != %v",
+			ret.WriteTimeout, mock_server.WriteTimeout)
+	}
+	ret.Close()
+}
+
+func Test_newTCPListener(t *testing.T) {
+	const mock_address_value = "chose_your_value"
+
+	if ret, err := newTCPListener(mock_address_value, nil); err != nil {
+		t.Error("[Test_newTCPListener] err_ :", err)
+	} else {
+		ret.Close()
+	}
+}
+
+func Test_newUnixListener(t *testing.T) {
+	const mock_address_value = "chose_your_value"
+
+	if ret, err := newUnixListener(mock_address_value, nil); err != nil {
+		t.Error("[Test_newUnixListener] err_ :", err)
+	} else {
+		ret.Close()
+	}
+}
+
+func Test_newAdminServer(t *testing.T) {
+	const (
+		mock_host_value       = "chose_your_value"
+		mock_admin_port_value = 42
+	)
+	cfg := new(config.Configuration)
+	cfg.Host = mock_host_value
+	cfg.AdminPort = mock_admin_port_value
+
+	mock_server := &http.Server{
+		Addr:    cfg.Host + ":" + strconv.Itoa(cfg.AdminPort),
+		Handler: nil,
+	}
+
+	if ret := newAdminServer(cfg, nil); ret == nil {
+		t.Error("[Test_newAdminServer] ret : isNil()")
+	} else {
+		if ret.Addr != mock_server.Addr {
+			t.Error("[Test_newAdminServer] Addr invalide: %v != %v",
+				ret.Addr, mock_server.Addr)
+		}
+		ret.Close()
+	}
 }
