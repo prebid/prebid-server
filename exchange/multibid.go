1f22885df35269fc47e174af727eefa5e21c201a
pm-nilesh-chate <nilesh.chate@pubmatic.com>
2022-05-21 04:44:03 +0000

    pbs java logic

--- exchange/multibid.go
+++ exchange/multibid.go
@@ -93,3 +93,84 @@ func getBidsByImpId(pbsBids []*pbsOrtbBid) (impIdToBidMap map[string][]*pbsOrtbB
 	}
 	return
 }
+
+/*
+ JAVA-PBS
+
+ 		bidderToMultiBids
+				pubmatic -> maxBids: 2
+							prefixBiddercode: pm
+				appnexus -> maxBids: 2
+
+	create(auctionParticipations) - Creates an OpenRTB {@link BidResponse} from the bids supplied by the bidder, including processing of winning bids with cache IDs.
+		[]auctionParticipations.bidderResponses
+		...
+		bidderResponses
+			[
+				SeatBid{pubmatic, [bid1, bid2, bid3, bid7, bid8, bid9]}
+				SeatBid{appnexus, [bid4, bid5, bid6, bid10, bid11, bid12]}
+			]
+		cacheBidsAndCreateResponse(bidderResponses)
+			toBidderResponseWithTargetingBidInfos(bidderResponses)
+				bidderResponseToReducedBidInfos - uses toSortedMultiBidInfo()(returns list with sort and maxBid applied)
+						{pubmatic, [bid1, bid2, bid3, bid7, bid8, bid9]}		->	[bid1, bid2, bid7, bid8]
+						{appnexus, [bid4, bid5, bid6, bid10, bid11, bid12]}		->	[bid4, bid5, bid10, bid11]
+				impIdToBidderToBidInfos -> group by impId, bidder
+						imp1	-> pubmatic -> [bid1, bid2]   (bid3 removed by maxBid)
+								-> appnexus -> [bid4, bid5]   (bid6 removed by maxBid)
+						imp2    -> pubmatic -> [bid7, bid8]   (bid9 removed by maxBid)
+								-> appnexus -> [bid10, bid11] (bid12 removed by maxBid)
+				forEach impIdToBidderToBidInfo
+							0.
+								bidderToBidInfos
+									pubmatic -> [bid1, bid2]
+									appnexus -> [bid4, bid5]
+
+								winningBidsByBidder -> [bid1, bid2, bid4, bid5]
+								winningBids -> [bid1] -> (bid1 is max in [bid1, bid2, bid4, bid5])
+
+							1.
+								bidderToBidInfos
+									pubmatic -> [bid7, bid8]
+									appnexus -> [bid10, bid11]
+
+								winningBidsByBidder -> [bid1, bid2, bid3, bid4, bid5, bid7, bid8, bid10, bid11]
+								winningBids -> [bid1, bid7] -> (bid7 is max in [bid7, bid8, bid10, bid11])
+				forEach bidderResponseToReducedBidInfos
+					0.
+						bidderResponseInfo - bidderResponseToReducedBidInfos[0].Key - {pubmatic, [bid1, bid2, bid3, bid7, bid8, bid9]}
+						bidderBidInfos - bidderResponseToReducedBidInfos[0].Value - [bid1, bid2, bid7, bid8]
+
+						injectBidInfoWithTargeting()
+							bidder = pubmatic
+							toBidInfoWithTargeting()
+								bidderBidInfos -> impIdToBidInfos ->	imp1	-> 	bid1, bid2
+																		imp2	->	bid7, bid8
+								// forEach impIdToBidInfos.values() -> [bid1, bid2, bid7, bid8] -> CHECK THIS WITH AMOL
+									injectTargeting()
+										bidderImpIdBidInfos: [bid1, bid2, bid7, bid8]
+										forEach bidderImpIdBidInfos
+											0.
+												bidderCode = pubmatic
+											1.
+												bidderCode = pm2
+											2.
+												bidderCode = pm3
+											3.
+												bidderCode = pm4
+								bidInfosWithTargeting = [bid1, bid2, bid7, bid8]
+
+					1.
+						bidderResponseInfo - bidderResponseToReducedBidInfos[1].Key - {appnexus, [bid4, bid5, bid6, bid10, bid11, bid12]}
+						bidderBidInfos - bidderResponseToReducedBidInfos[1].Value - [bid4, bid5, bid10, bid11]
+
+						injectBidInfoWithTargeting()
+							bidder = appnexus
+							toBidInfoWithTargeting()
+								impIdToBidInfos ->	imp1	-> 	bid4, bid5
+													imp2	->	bid10, bid11
+								forEach impIdToBidInfos.values()
+									injectTargeting(impIdToBidInfos[i])
+
+
+*/
