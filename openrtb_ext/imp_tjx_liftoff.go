31cac7fd248b83717a01d03c93c68a5747b7d27e
Umut Demirel <umutdemirell@gmail.com>
2022-06-02 13:48:33 -0700

    [NGS-965] Liftoff HTML endcard 100%

--- openrtb_ext/imp_tjx_liftoff.go
+++ openrtb_ext/imp_tjx_liftoff.go
@@ -2,12 +2,11 @@ package openrtb_ext
 
 // ExtImpLiftoff defines the contract for bidrequest.imp[i].ext.liftoff
 type ExtImpTJXLiftoff struct {
-	Video                liftoffVideoParams `json:"video"`
-	Region               string             `json:"region"`
-	SKADNSupported       bool               `json:"skadn_supported"`
-	MRAIDSupported       bool               `json:"mraid_supported"`
-	EndcardHTMLSupported bool               `json:"endcard_html_supported"`
-	BidFloor             *float64           `json:"bid_floor,omitempty"`
+	Video          liftoffVideoParams `json:"video"`
+	Region         string             `json:"region"`
+	SKADNSupported bool               `json:"skadn_supported"`
+	MRAIDSupported bool               `json:"mraid_supported"`
+	BidFloor       *float64           `json:"bid_floor,omitempty"`
 }
 
 // liftoffVideoParams defines the contract for bidrequest.imp[i].ext.liftoff.video
