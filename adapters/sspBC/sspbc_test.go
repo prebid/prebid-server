grupawp/prebid-server-dev

[sspbc-adapter] add new adapter

adapters/sspbc/sspbc_test.go
@@ -1,0 +1,20 @@
+package sspbc
+
+import (
+	"testing"
+
+	"github.com/prebid/prebid-server/adapters/adapterstest"
+	"github.com/prebid/prebid-server/config"
+	"github.com/prebid/prebid-server/openrtb_ext"
+)
+
+func TestJsonSamples(t *testing.T) {
+	bidder, buildErr := Builder(openrtb_ext.BidderConnectAd, config.Adapter{
+		Endpoint: "http://ssp.wp.test/bidder/"})
+
+	if buildErr != nil {
+		t.Fatalf("Builder returned unexpected error %v", buildErr)
+	}
+
+	adapterstest.RunJSONBidderTest(t, "sspbctest", bidder)
+}

prebid/prebid-server

adapters/sspBC/sspbc_test.go
@@ -1,0 +1,20 @@
+package sspBC
+
+import (
+	"testing"
+
+	"github.com/prebid/prebid-server/adapters/adapterstest"
+	"github.com/prebid/prebid-server/config"
+	"github.com/prebid/prebid-server/openrtb_ext"
+)
+
+func TestJsonSamples(t *testing.T) {
+	bidder, buildErr := Builder(openrtb_ext.BidderSspBC, config.Adapter{
+		Endpoint: "http://ssp.wp.test/bidder/"})
+
+	if buildErr != nil {
+		t.Fatalf("Builder returned unexpected error %v", buildErr)
+	}
+
+	adapterstest.RunJSONBidderTest(t, "sspbctest", bidder)
+}