37caee8a3865fa5a2e8a6d03772b19c56259da09
VolodymyrPavlov <waldemar6pavlov@gmail.com>
2022-08-03 01:14:18 +0300

    adapters/adtrgtme: added supplemental tests
    * added tests for headers ipv6
    * added tests for handling another non-200 statuses
    * removed port number

--- adapters/adtrgtme/adtrgtme_test.go
+++ adapters/adtrgtme/adtrgtme_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestJsonSamples(t *testing.T) {
 	bidder, buildErr := Builder(openrtb_ext.BidderAdtrgtme, config.Adapter{
-		Endpoint: "http://localhost:8077/ssp"})
+		Endpoint: "http://localhost/ssp"})
 
 	if buildErr != nil {
 		t.Fatalf("Builder returned unexpected error %v", buildErr)
