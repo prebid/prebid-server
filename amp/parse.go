40c7cd5d6579017d6197abb8a43f6fc10d8d2091
Gus Carreon <gcarreongutierrez@vpn-10-249-97-58.nym2.appnexus.com>
2022-08-16 14:09:57 -0400

    All tests are passing

--- amp/parse.go
+++ amp/parse.go
@@ -63,7 +63,7 @@ func ReadPolicy(ampParams Params, req *openrtb2.BidRequest, pbsConfigGDPREnabled
 	case ConsentTCF2:
 		if pbsConfigGDPREnabled {
 			// Even if consent is invalid, we write the consent string to req.user.ext.consent
-			rv = gdpr.ConsentWriter{ampParams.Consent, ampParams.GdprApplies}
+			rv = gdpr.ConsentWriter{ampParams.Consent}
 
 			if tcf2.IsConsentV2(ampParams.Consent) {
 				if _, err := tcf2.ParseString(ampParams.Consent); err != nil {
