98e09397c61e54c404e41f867a027c0885d58d26
m.jaworski <m.jaworski@clickonometrics.pl>
2022-10-06 15:50:43 +0200

    Clickonometrics adapter

--- adapters/ccx/ccx.go
+++ adapters/ccx/ccx.go
@@ -66,27 +66,13 @@ func (a *adapter) MakeBids(request *openrtb2.BidRequest, requestData *adapters.R
 	bidResponse := adapters.NewBidderResponseWithBidsCapacity(len(request.Imp))
 	bidResponse.Currency = response.Cur
 	for _, seatBid := range response.SeatBid {
-		for i, bid := range seatBid.Bid {
+		for i := range seatBid.Bid {
 			b := &adapters.TypedBid{
 				Bid:     &seatBid.Bid[i],
-				BidType: getMediaTypeForBid(bid),
+				BidType: "banner",
 			}
 			bidResponse.Bids = append(bidResponse.Bids, b)
 		}
 	}
 	return bidResponse, nil
-}
-
-func getMediaTypeForImp(impID string, imps []openrtb2.Imp) openrtb_ext.BidType {
-	for _, imp := range imps {
-		if imp.ID == impID {
-			if imp.Video != nil {
-				return openrtb_ext.BidTypeVideo
-			} else if imp.Native != nil {
-				return openrtb_ext.BidTypeNative
-			}
-			return openrtb_ext.BidTypeBanner
-		}
-	}
-	return openrtb_ext.BidTypeBanner
 }
