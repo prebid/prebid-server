b5c10ce3e441e84bb9fa02d7ded035ced3add4eb
Takuya Tokuda <cs.toku.mail@gmail.com>
2022-10-18 18:48:53 +0900

    Remove only yieldone changes

--- adapters/yieldone/yieldone.go
+++ adapters/yieldone/yieldone.go
@@ -86,9 +86,6 @@ func (a *YieldoneAdapter) MakeBids(internalRequest *openrtb2.BidRequest, externa
 			})
 		}
 	}
-	if bidResp.Cur != "" {
-		bidResponse.Currency = bidResp.Cur
-	}
 	return bidResponse, nil
 
 }
