c52533e6c0c64e74eac2d5b31ade287ede7c5ee1
mwang-sticky <mwang@freewheel.tv>
2022-10-23 10:16:57 +0800

    Update adapter_builders.go
    add old bidder go name

--- exchange/adapter_builders.go
+++ exchange/adapter_builders.go
@@ -225,6 +225,7 @@ func newAdapterBuilders() map[openrtb_ext.BidderName]adapters.Builder {
 		openrtb_ext.BidderEpom:              epom.Builder,
 		openrtb_ext.BidderEVolution:         evolution.Builder,
 		openrtb_ext.BidderFreewheelSSP:      freewheelssp.Builder,
+		openrtb_ext.BidderFreewheelSSPOld:   freewheelssp.Builder,
 		openrtb_ext.BidderGamma:             gamma.Builder,
 		openrtb_ext.BidderGamoshi:           gamoshi.Builder,
 		openrtb_ext.BidderGrid:              grid.Builder,
