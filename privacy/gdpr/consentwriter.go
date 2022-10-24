718628ceba61526a5e64a72c7eebf1d4ae7a5b65
guscarreon <guscarreon@gmail.com>
2022-10-24 13:17:17 -0400

    AMP additionalConsent (#2378)

--- privacy/gdpr/consentwriter.go
+++ privacy/gdpr/consentwriter.go
@@ -1,8 +1,6 @@
 package gdpr
 
 import (
-	"encoding/json"
-
 	"github.com/prebid/openrtb/v17/openrtb2"
 	"github.com/prebid/prebid-server/openrtb_ext"
 )
@@ -15,42 +13,30 @@ type ConsentWriter struct {
 
 // Write mutates an OpenRTB bid request with the GDPR TCF consent.
 func (c ConsentWriter) Write(req *openrtb2.BidRequest) error {
+	if req == nil {
+		return nil
+	}
+	reqWrap := &openrtb_ext.RequestWrapper{BidRequest: req}
+
 	if c.RegExtGDPR != nil {
-		reqWrap := &openrtb_ext.RequestWrapper{BidRequest: req}
 		if regsExt, err := reqWrap.GetRegExt(); err == nil {
 			regsExt.SetGDPR(c.RegExtGDPR)
 		} else {
 			return err
 		}
-		if err := reqWrap.RebuildRequest(); err != nil {
+	}
+
+	if c.Consent != "" {
+		if userExt, err := reqWrap.GetUserExt(); err == nil {
+			userExt.SetConsent(&c.Consent)
+		} else {
 			return err
 		}
 	}
 
-	if c.Consent == "" {
-		return nil
-	}
-
-	if req.User == nil {
-		req.User = &openrtb2.User{}
-	}
-
-	if req.User.Ext == nil {
-		ext, err := json.Marshal(openrtb_ext.ExtUser{Consent: c.Consent})
-		if err == nil {
-			req.User.Ext = ext
-		}
+	if err := reqWrap.RebuildRequest(); err != nil {
 		return err
 	}
 
-	var extMap map[string]interface{}
-	err := json.Unmarshal(req.User.Ext, &extMap)
-	if err == nil {
-		extMap["consent"] = c.Consent
-		ext, err := json.Marshal(extMap)
-		if err == nil {
-			req.User.Ext = ext
-		}
-	}
-	return err
+	return nil
 }
