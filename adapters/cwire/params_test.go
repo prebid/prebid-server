1da85bdd7493620e7476abdd02dfbbed386f56d0
Mateja <mail@matejamaric.com>
2022-10-25 14:32:25 +0200

    Adapt params_test to our needs

--- adapters/cwire/params_test.go
+++ adapters/cwire/params_test.go
@@ -7,9 +7,9 @@ import (
 	"github.com/prebid/prebid-server/openrtb_ext"
 )
 
-// This file actually intends to test static/bidder-params/adtelligent.json
-// These also validate the format of the external API: request.imp[i].ext.adtelligent
-// TestValidParams makes sure that the adtelligent schema accepts all imp.ext fields which we intend to support.
+// This file actually intends to test static/bidder-params/cwire.json
+// These also validate the format of the external API: request.imp[i].ext.bidder
+// TestValidParams makes sure that the cwire schema accepts all imp.ext fields which we intend to support.
 
 func TestValidParams(t *testing.T) {
 	validator, err := openrtb_ext.NewBidderParamsValidator("../../static/bidder-params")
@@ -18,13 +18,13 @@ func TestValidParams(t *testing.T) {
 	}
 
 	for _, validParam := range validParams {
-		if err := validator.Validate(openrtb_ext.BidderAdtelligent, json.RawMessage(validParam)); err != nil {
-			t.Errorf("Schema rejected adtelligent params: %s", validParam)
+		if err := validator.Validate(openrtb_ext.BidderCWire, json.RawMessage(validParam)); err != nil {
+			t.Errorf("Schema rejected cwire params: %s", validParam)
 		}
 	}
 }
 
-// TestInvalidParams makes sure that the adtelligent schema rejects all the imp.ext fields we don't support.
+// TestInvalidParams makes sure that the cwire schema rejects all the imp.ext fields we don't support.
 func TestInvalidParams(t *testing.T) {
 	validator, err := openrtb_ext.NewBidderParamsValidator("../../static/bidder-params")
 	if err != nil {
@@ -32,17 +32,15 @@ func TestInvalidParams(t *testing.T) {
 	}
 
 	for _, invalidParam := range invalidParams {
-		if err := validator.Validate(openrtb_ext.BidderAdtelligent, json.RawMessage(invalidParam)); err == nil {
+		if err := validator.Validate(openrtb_ext.BidderCWire, json.RawMessage(invalidParam)); err == nil {
 			t.Errorf("Schema allowed unexpected params: %s", invalidParam)
 		}
 	}
 }
 
 var validParams = []string{
-	`{"aid":123}`,
-	`{"aid":123,"placementId":1234}`,
-	`{"aid":123,"siteId":4321}`,
-	`{"aid":123,"siteId":0,"bidFloor":0}`,
+	`{"placementId":123,"siteId":321}`,
+  `{"placementId":123,"siteId":321,"pageViewId":2}`,
 }
 
 var invalidParams = []string{
@@ -53,8 +51,8 @@ var invalidParams = []string{
 	`4.2`,
 	`[]`,
 	`{}`,
-	`{"aid":"123"}`,
-	`{"aid":"0"}`,
-	`{"aid":"123","placementId":"123"}`,
-	`{"aid":123, "placementId":"123", "siteId":"321"}`,
+	`{"pageViewId":"123"}`,
+	`{"pageViewId":"0"}`,
+	`{"pageViewId":"123","placementId":"123"}`,
+	`{"pageViewId":123, "placementId":"123", "siteId":"321"}`,
 }
