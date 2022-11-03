f08c0288d30969af1aeec5a7abc37678519c1d2f
bsardo <1168933+bsardo@users.noreply.github.com>
2022-11-03 12:05:54 -0400

    Remove previously deleted functions readded during merge with master

--- config/accounts.go
+++ config/accounts.go
@@ -81,17 +81,6 @@ type AccountGDPR struct {
 	SpecialFeature1     AccountGDPRSpecialFeature      `mapstructure:"special_feature1" json:"special_feature1"`
 }
 
-// BasicEnforcementVendor checks if the given bidder is considered a basic enforcement vendor which indicates whether
-// weak vendor enforcement applies to that bidder.
-func (a *AccountGDPR) BasicEnforcementVendor(bidder openrtb_ext.BidderName) (value, exists bool) {
-	if a.BasicEnforcementVendorsMap == nil {
-		return false, false
-	}
-	_, found := a.BasicEnforcementVendorsMap[string(bidder)]
-
-	return found, true
-}
-
 // EnabledForChannelType indicates whether GDPR is turned on at the account level for the specified channel type
 // by using the channel type setting if defined or the general GDPR setting if defined; otherwise it returns nil.
 func (a *AccountGDPR) EnabledForChannelType(channelType ChannelType) *bool {
