c884d8f3bb63ac6d92880ff67629d7ba4552a086
4lexvav <4lexvav@gmail.com>
2022-11-04 13:44:05 +0200

    Move invocation package

--- hooks/hookstage/allprocbidresponses.go
+++ hooks/hookstage/allprocbidresponses.go
@@ -2,19 +2,16 @@ package hookstage
 
 import (
 	"context"
-
-	"github.com/prebid/prebid-server/hooks/invocation"
 )
 
 type AllProcessedBidResponses interface {
 	HandleAllProcBidResponsesHook(
 		context.Context,
-		invocation.Context,
+		InvocationContext,
 		AllProcessedBidResponsesPayload,
-	) (invocation.HookResult[AllProcessedBidResponsesPayload], error)
+	) (HookResult[AllProcessedBidResponsesPayload], error)
 }
 
 type AllProcessedBidResponsesPayload struct {
-	// todo: decide what payload to use within hook invocation task
-	// initially, we planned to use map[openrtb_ext.BidderName]*exchange.pbsOrtbSeatBid, but the type is not exported
+	// todo: decide what payload to use within the hook invocation task
 }
--- hooks/hookstage/procauction.go
+++ hooks/hookstage/procauction.go
@@ -4,15 +4,14 @@ import (
 	"context"
 
 	"github.com/prebid/openrtb/v17/openrtb2"
-	"github.com/prebid/prebid-server/hooks/invocation"
 )
 
 type ProcessedAuction interface {
 	HandleProcessedAuctionHook(
 		context.Context,
-		invocation.Context,
+		InvocationContext,
 		ProcessedAuctionPayload,
-	) (invocation.HookResult[ProcessedAuctionPayload], error)
+	) (HookResult[ProcessedAuctionPayload], error)
 }
 
 type ProcessedAuctionPayload struct {
