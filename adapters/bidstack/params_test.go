324759369736c02922c0d9321e60473e108b1001
Arturs Lataks <arturs.lataks@bidstack.com>
2022-11-25 13:21:54 +0200

    Bidstack Adapter: Add optional consent field

--- adapters/bidstack/params_test.go
+++ adapters/bidstack/params_test.go
@@ -10,6 +10,8 @@ import (
 var validParams = []string{
 	`{"publisherId": "355cf800-8348-433a-9d95-70345fa70afc"}`,
 	`{"publisherId": "355cf800-8348-433a-9d95-70345fa70afc","placementId":"Some placement ID"}`,
+	`{"publisherId": "355cf800-8348-433a-9d95-70345fa70afc","consent":false}`,
+	`{"publisherId": "355cf800-8348-433a-9d95-70345fa70afc","placementId":"Some placement ID","consent":true}`,
 }
 
 var invalidParams = []string{
