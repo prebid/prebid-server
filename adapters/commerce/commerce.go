25559b11b8babc217bfa8f093ef2a38ef7271591
Gobi Sakthivel <gobi.sakthivel@pubmatic.com>
2022-12-21 00:27:29 +0530

    Test Adapter Commerce

---
+++ adapters/commerce/commerce.go
@@ -0,0 +1,33 @@
+package commerce
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+
+	"github.com/prebid/prebid-server/adapters"
+	"github.com/prebid/prebid-server/config"
+	"github.com/prebid/prebid-server/errortypes"
+	"github.com/prebid/prebid-server/openrtb_ext"
+)
+
+type CommerceAdapter struct {
+	URI string
+}
+
+func (a *CommerceAdapter) MakeRequests(request *openrtb2.BidRequest, reqInfo *adapters.ExtraRequestInfo) ([]*adapters.RequestData, []error) {
+		return nil, nil
+}
+func (a *CommerceAdapter) MakeBids(internalRequest *openrtb2.BidRequest, externalRequest *adapters.RequestData, response *adapters.ResponseData) (*adapters.BidderResponse, []error) {
+		return nil, nil
+}
+
+// Builder builds a new instance of the Pubnative adapter for the given bidder with the given config.
+func Builder(bidderName openrtb_ext.BidderName, config config.Adapter) (adapters.Bidder, error) {
+	bidder := &CommerceAdapter{
+		URI: config.Endpoint,
+	}
+	return bidder, nil
+}
