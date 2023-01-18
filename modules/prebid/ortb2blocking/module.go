63150100442015b27a9f06223ac29bc2b83cd9ef
Oleksii Varlamov <4lexvav@gmail.com>
2023-01-18 23:49:38 +0200

    Openrtb2blocking module part 2: BidderRequestHook implementation (#2531)
    Co-authored-by: legendko <legend.ko@hotmail.com>

--- modules/prebid/ortb2blocking/module.go
+++ modules/prebid/ortb2blocking/module.go
@@ -1,8 +1,11 @@
 package ortb2blocking
 
 import (
+	"context"
 	"encoding/json"
 
+	"github.com/prebid/openrtb/v17/adcom1"
+	"github.com/prebid/prebid-server/hooks/hookstage"
 	"github.com/prebid/prebid-server/modules/moduledeps"
 )
 
@@ -11,3 +14,32 @@ func Builder(_ json.RawMessage, _ moduledeps.ModuleDeps) (interface{}, error) {
 }
 
 type Module struct{}
+
+// HandleBidderRequestHook updates blocking fields on the openrtb2.BidRequest.
+// Fields are updated only if request satisfies conditions provided by the module config.
+func (m Module) HandleBidderRequestHook(
+	_ context.Context,
+	miCtx hookstage.ModuleInvocationContext,
+	payload hookstage.BidderRequestPayload,
+) (hookstage.HookResult[hookstage.BidderRequestPayload], error) {
+	result := hookstage.HookResult[hookstage.BidderRequestPayload]{}
+	if len(miCtx.AccountConfig) == 0 {
+		return result, nil
+	}
+
+	cfg, err := newConfig(miCtx.AccountConfig)
+	if err != nil {
+		return result, err
+	}
+
+	return handleBidderRequestHook(cfg, payload)
+}
+
+type blockingAttributes struct {
+	bAdv   []string
+	bApp   []string
+	bCat   []string
+	bType  map[string][]int
+	bAttr  map[string][]int
+	catTax adcom1.CategoryTaxonomy
+}
