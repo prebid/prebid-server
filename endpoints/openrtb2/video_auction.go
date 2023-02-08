c05fef8f74487f77d68f3c68ee556a1735a3d055
vsolovei <veronika.solovei@appnexus.com>
2023-02-07 16:03:03 -0800

    Trivy test without go mod changes

--- endpoints/openrtb2/video_auction.go
+++ endpoints/openrtb2/video_auction.go
@@ -113,13 +113,6 @@ func NewVideoEndpoint(
  11. Build proper response format.
 */
 func (deps *endpointDeps) VideoAuctionEndpoint(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	start := time.Now()
-
-	vo := analytics.VideoObject{
-		Status:    http.StatusOK,
-		Errors:    make([]error, 0),
-		StartTime: start,
-	}
 
 	labels := metrics.Labels{
 		Source:        metrics.DemandUnknown,
@@ -127,6 +120,13 @@ func (deps *endpointDeps) VideoAuctionEndpoint(w http.ResponseWriter, r *http.Re
 		PubID:         metrics.PublisherUnknown,
 		CookieFlag:    metrics.CookieFlagUnknown,
 		RequestStatus: metrics.RequestStatusOK,
+	}
+	start := time.Now()
+
+	vo := analytics.VideoObject{
+		Status:    http.StatusOK,
+		Errors:    make([]error, 0),
+		StartTime: start,
 	}
 
 	debugQuery := r.URL.Query().Get("debug")
