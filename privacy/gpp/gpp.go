10ec1571fa3bab926c2a74fbe9ae100b758d6365
Hans Hjort <hhjort@appnexus.com>
2023-02-08 18:45:08 -0500

    GPP passthrough for cookiesync

---
+++ privacy/gpp/gpp.go
@@ -0,0 +1,8 @@
+package gpp
+
+// Policy represents the GPP privacy string container.
+// Currently just a placeholder until more expansive support is made.
+type Policy struct {
+	String string
+	RawSID string
+}
