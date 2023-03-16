8b3aef4f77beb57231a7af694e4302b1cd0c912d
Sonali Shankar More <sonali.more@xandr.com>
2023-03-16 17:39:35 +0530

    Add data type KeywordsString and define custom unmarshaller

---
+++ util/jsonutil/keywordsutil.go
@@ -0,0 +1,59 @@
+package jsonutil
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+type KeywordsString string
+
+// extImpAppnexusKeyVal defines the contract for bidrequest.imp[i].ext.prebid.bidder.appnexus.keywords[i]
+type extImpAppnexusKeyVal struct {
+	Key    string   `json:"key,omitempty"`
+	Values []string `json:"value,omitempty"`
+}
+
+func (ks *KeywordsString) UnmarshalJSON(b []byte) error {
+	if len(b) == 0 {
+		return nil
+	}
+
+	switch b[0] {
+	case '{':
+		var results map[string][]string
+		err := json.Unmarshal(b, &results)
+		if err != nil {
+			return err
+		}
+
+		var keywords string
+		for key, values := range results {
+			for _, val := range values {
+				keywords = keywords + fmt.Sprintf("%s=%s", key, val)
+			}
+		}
+		*ks = KeywordsString(strings.TrimSuffix(keywords, ","))
+	case '[':
+		var results []extImpAppnexusKeyVal
+		err := json.Unmarshal(b, &results)
+		if err != nil {
+			return err
+		}
+		var kvs string
+		for _, kv := range results {
+			if len(kv.Values) == 0 {
+				kvs = kvs + fmt.Sprintf("%s,", kv.Key)
+			} else {
+				for _, val := range kv.Values {
+					kvs = kvs + fmt.Sprintf("%s=%s,", kv.Key, val)
+				}
+			}
+		}
+
+		*ks = KeywordsString(strings.TrimSuffix(kvs, ","))
+	case '"':
+		*ks = KeywordsString(string(b[1 : len(b)-1]))
+	}
+	return nil
+}
---
+++ util/jsonutil/keywordsutil_test.go
@@ -0,0 +1,34 @@
+package jsonutil
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestKeywordsUtilUnmarshalJSON(t *testing.T) {
+	type Keywords struct {
+		Keywords KeywordsString `json:"keywords"`
+	}
+
+	t.Run("dynamic json", func(t *testing.T) {
+		jsonData := []byte(`{"keywords" : { "genre": ["rock", "pop"], "pets": ["dog"] }}`)
+		var keywords Keywords
+		assert.NoError(t, json.Unmarshal(jsonData, &keywords))
+	})
+
+	t.Run("json array", func(t *testing.T) {
+		jsonData := []byte(`{"keywords" : [{"key": "foo", "value": ["bar","baz"]},{"key": "valueless"}]}`)
+		var keywords Keywords
+		assert.NoError(t, json.Unmarshal(jsonData, &keywords))
+		assert.Equal(t, "foo=bar,foo=baz,valueless", string(keywords.Keywords))
+	})
+
+	t.Run("string", func(t *testing.T) {
+		jsonData := []byte(`{"keywords" : "foo=bar,foo=baz,valueless"}`)
+		var keywords Keywords
+		assert.NoError(t, json.Unmarshal(jsonData, &keywords))
+		assert.Equal(t, "foo=bar,foo=baz,valueless", string(keywords.Keywords))
+	})
+}
