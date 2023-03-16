cad10a7246692f381549eaa2a194f221b9ea7f90
Ashish Garg <ashish@clypd.com>
2023-03-16 16:16:27 +0530

    remove TestVideoSinglePod
    This test case is covered in video folder

--- adapters/appnexus/appnexus_test.go
+++ adapters/appnexus/appnexus_test.go
@@ -1,17 +1,11 @@
 package appnexus
 
 import (
-	"encoding/json"
-	"regexp"
 	"testing"
 
-	"github.com/prebid/prebid-server/adapters"
 	"github.com/prebid/prebid-server/adapters/adapterstest"
 	"github.com/prebid/prebid-server/config"
 	"github.com/prebid/prebid-server/openrtb_ext"
-
-	"github.com/prebid/openrtb/v17/openrtb2"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestJsonSamples(t *testing.T) {
@@ -32,42 +26,3 @@ func TestMemberQueryParam(t *testing.T) {
 		t.Errorf("appendMemberId() failed on URI with query string. Expected %s, got %s", expected, uriWithMember)
 	}
 }
-
-func TestVideoSinglePod(t *testing.T) {
-	var a adapter
-	a.URI = "http://test.com/openrtb2"
-	a.hbSource = 5
-
-	var reqInfo adapters.ExtraRequestInfo
-	reqInfo.PbsEntryPoint = "video"
-
-	var req openrtb2.BidRequest
-	req.ID = "test_id"
-
-	reqExt := `{"prebid":{}}`
-	impExt := `{"bidder":{"placementId":123, "generate_ad_pod_id":true}}`
-	req.Ext = []byte(reqExt)
-
-	req.Imp = append(req.Imp, openrtb2.Imp{ID: "1_0", Ext: []byte(impExt)})
-	req.Imp = append(req.Imp, openrtb2.Imp{ID: "1_1", Ext: []byte(impExt)})
-	req.Imp = append(req.Imp, openrtb2.Imp{ID: "1_2", Ext: []byte(impExt)})
-
-	result, err := a.MakeRequests(&req, &reqInfo)
-
-	assert.Empty(t, err, "Errors array should be empty")
-	assert.Len(t, result, 1, "Only one request should be returned")
-
-	var error error
-	var reqData *openrtb2.BidRequest
-	error = json.Unmarshal(result[0].Body, &reqData)
-	assert.NoError(t, error, "Response body unmarshalling error should be nil")
-
-	var reqDataExt *appnexusReqExt
-	error = json.Unmarshal(reqData.Ext, &reqDataExt)
-	assert.NoError(t, error, "Response ext unmarshalling error should be nil")
-
-	regMatch, matchErr := regexp.Match(`^[0-9]+$`, []byte(reqDataExt.Appnexus.AdPodId))
-	assert.NoError(t, matchErr, "Regex match error should be nil")
-	assert.True(t, regMatch, "AdPod id doesn't present in Appnexus extension or has incorrect format")
-}
-
