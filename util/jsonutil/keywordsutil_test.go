b082b260d82302a856ff5b31ace3b3fbd2f85746
Sonali Shankar More <sonali.more@xandr.com>
2023-03-17 17:09:14 +0530

    add equality assertion

--- util/jsonutil/keywordsutil_test.go
+++ util/jsonutil/keywordsutil_test.go
@@ -11,11 +11,12 @@ func TestKeywordsUtilUnmarshalJSON(t *testing.T) {
 	type keywords struct {
 		Keywords Keywords `json:"keywords"`
 	}
-	
+
 	t.Run("dynamic-json", func(t *testing.T) {
-		jsonData := []byte(`{"keywords" : { "genre": ["rock", "pop"], "pets": ["dog"] }}`)
+		jsonData := []byte(`{"keywords" : { "pets": ["dog"] }}`)
 		var keywords keywords
 		assert.NoError(t, json.Unmarshal(jsonData, &keywords))
+		assert.Equal(t, "pets=dog", string(keywords.Keywords))
 	})
 
 	t.Run("json-array", func(t *testing.T) {
