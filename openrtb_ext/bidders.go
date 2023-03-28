c0e9aa88a37397bcca5465d05f61961bbb2c598d
Sonali Shankar More <sonali.more@xandr.com>
2023-03-28 16:23:31 +0530

    Remove oftmedia from CoreBidderNames in bidders.go

--- openrtb_ext/bidders.go
+++ openrtb_ext/bidders.go
@@ -203,7 +203,6 @@ const (
 	BidderNextMillennium    BidderName = "nextmillennium"
 	BidderNinthDecimal      BidderName = "ninthdecimal"
 	BidderNoBid             BidderName = "nobid"
-	BidderOFTMedia          BidderName = "oftmedia"
 	BidderOneTag            BidderName = "onetag"
 	BidderOpenWeb           BidderName = "openweb"
 	BidderOpenx             BidderName = "openx"
@@ -386,7 +385,6 @@ func CoreBidderNames() []BidderName {
 		BidderNextMillennium,
 		BidderNinthDecimal,
 		BidderNoBid,
-		BidderOFTMedia,
 		BidderOneTag,
 		BidderOpenWeb,
 		BidderOpenx,
