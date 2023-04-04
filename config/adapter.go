ce808990c68106d9ea78bf2a4e607d9014ef4fab
Ashish Garg <ashish@clypd.com>
2023-04-04 14:55:16 +0530

    introduce RandomGenerator in adapter struct

--- config/adapter.go
+++ config/adapter.go
@@ -1,8 +1,13 @@
 package config
 
+import "github.com/prebid/prebid-server/util/randomutil"
+
 type Adapter struct {
 	Endpoint         string
 	ExtraAdapterInfo string
+
+	// needed for AppNexus
+	RandomGenerator randomutil.RandomGenerator
 
 	// needed for Rubicon
 	XAPI AdapterXAPI
