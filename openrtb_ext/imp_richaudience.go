7c2d1721b005629aa91b6324604b4d0f7ccee592
richaudience <partnerintegrations@richaudience.com>
2023-04-04 16:12:04 +0200

    Remove keywords from 'ExtImpRichaudience'

--- openrtb_ext/imp_richaudience.go
+++ openrtb_ext/imp_richaudience.go
@@ -3,7 +3,6 @@ package openrtb_ext
 type ExtImpRichaudience struct {
 	Pid         string  `json:"pid"`
 	SupplyType  string  `json:"supplyType"`
-	Keywords    string  `json:"keywords"`
 	BidFloor    float64 `json:"bidfloor"`
 	BidFloorCur string  `json:"bidfloorcur"`
 	Test        bool    `json:"test"`
