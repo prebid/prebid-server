e0090cfdf3d911be7533fcd2f766da9aa6b1a73c
abermanov <abermanov@zetaglobal.com>
2023-04-17 18:48:07 +0200

    Zeta Global SSP adapter: fix user sync url, parse bid type from response.

--- adapters/zeta_global_ssp/zeta_global_ssp.go
+++ adapters/zeta_global_ssp/zeta_global_ssp.go
@@ -2,6 +2,8 @@ package zeta_global_ssp
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/prebid/prebid-server/errortypes"
 	"github.com/prebid/prebid-server/util/httputil"
 	"net/http"
 
@@ -58,27 +60,34 @@ func (a *adapter) MakeBids(request *openrtb2.BidRequest, requestData *adapters.R
 
 	bidResponse := adapters.NewBidderResponseWithBidsCapacity(len(request.Imp))
 	bidResponse.Currency = response.Cur
+	var errors []error
 	for _, seatBid := range response.SeatBid {
 		for i := range seatBid.Bid {
+			bidType, err := getMediaTypeForBid(seatBid.Bid[i])
+			if err != nil {
+				errors = append(errors, err)
+				continue
+			}
 			b := &adapters.TypedBid{
 				Bid:     &seatBid.Bid[i],
-				BidType: getMediaTypeForImp(seatBid.Bid[i].ImpID, request.Imp),
+				BidType: bidType,
 			}
 			bidResponse.Bids = append(bidResponse.Bids, b)
 		}
 	}
-	return bidResponse, nil
+	return bidResponse, errors
 }
 
-func getMediaTypeForImp(impID string, imps []openrtb2.Imp) openrtb_ext.BidType {
-	mediaType := openrtb_ext.BidTypeBanner
-	for _, imp := range imps {
-		if imp.ID == impID {
-			if imp.Banner == nil && imp.Video != nil {
-				mediaType = openrtb_ext.BidTypeVideo
-			}
-			return mediaType
+func getMediaTypeForBid(bid openrtb2.Bid) (openrtb_ext.BidType, error) {
+	if bid.Ext != nil {
+		var bidExt openrtb_ext.ExtBid
+		err := json.Unmarshal(bid.Ext, &bidExt)
+		if err == nil && bidExt.Prebid != nil {
+			return openrtb_ext.ParseBidType(string(bidExt.Prebid.Type))
 		}
 	}
-	return mediaType
+
+	return "", &errortypes.BadServerResponse{
+		Message: fmt.Sprintf("Failed to parse impression \"%s\" mediatype", bid.ImpID),
+	}
 }
