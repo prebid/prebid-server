05a132fad254aa904f36010ad9995461ce989b8c
AlexBVolcy <alexbvolcy@gmail.com>
2023-04-18 10:54:35 -0700

    Address module error

--- exchange/bidadjustments_test.go
+++ exchange/bidadjustments_test.go
@@ -3,7 +3,7 @@ package exchange
 import (
 	"testing"
 
-	"github.com/prebid/openrtb/v17/openrtb2"
+	"github.com/prebid/openrtb/v19/openrtb2"
 	"github.com/prebid/prebid-server/adapters"
 	"github.com/prebid/prebid-server/config"
 	"github.com/prebid/prebid-server/openrtb_ext"
