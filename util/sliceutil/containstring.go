cc78a2f57427ddce1ee8ba77817933ea170d8ac8
Hans Hjort <hhjort@appnexus.com>
2023-04-25 15:22:07 -0400

    Fix Merge cloning clone function

--- util/sliceutil/containstring.go
+++ util/sliceutil/containstring.go
@@ -12,13 +12,3 @@ func ContainsStringIgnoreCase(s []string, v string) bool {
 	}
 	return false
 }
-
-// CloneSlice creates an indepent copy of a slice,
-func Clone[T any](s []T) []T {
-	if s == nil {
-		return nil
-	}
-	clone := make([]T, len(s))
-	copy(clone, s)
-	return clone
-}
