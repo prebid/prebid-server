b445c71e067eabe41c5e2f2e8fb773326a810238
Sonali Shankar More <sonali.more@xandr.com>
2023-04-28 11:29:22 +0530

    add RecordBidderServerResponse to MetricsEngine

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -36,8 +36,6 @@ const (
 	MakeAuctionResponse OverheadType = "make-auction-response"
 	// MakeBidderRequests - measures the time needed to fetch a stored request (if needed), parse, unmarshal, and validate the OpenRTB request, interpret its privacy policies, and split it into multiple requests sanitized for each bidder
 	MakeBidderRequests OverheadType = "make-bidder-requests"
-	// BidderServerResponse - measures the time needed to receive response back from the bidder server
-	BidderServerResponse OverheadType = "bidder-server-response"
 )
 
 func (t OverheadType) String() string {
@@ -45,7 +43,7 @@ func (t OverheadType) String() string {
 }
 
 func OverheadTypes() []OverheadType {
-	return []OverheadType{PreBidder, MakeAuctionResponse, MakeBidderRequests, BidderServerResponse}
+	return []OverheadType{PreBidder, MakeAuctionResponse, MakeBidderRequests}
 }
 
 // ImpLabels defines metric labels describing the impression type.
@@ -432,6 +430,7 @@ type MetricsEngine interface {
 	RecordAdapterConnections(adapterName openrtb_ext.BidderName, connWasReused bool, connWaitTime time.Duration)
 	RecordDNSTime(dnsLookupTime time.Duration)
 	RecordTLSHandshakeTime(tlsHandshakeTime time.Duration)
+	RecordBidderServerResponseTime(bidderServerResponseTime time.Duration)
 	RecordAdapterPanic(labels AdapterLabels)
 	RecordAdapterBidReceived(labels AdapterLabels, bidType openrtb_ext.BidType, hasAdm bool)
 	RecordAdapterPrice(labels AdapterLabels, cpm float64)
