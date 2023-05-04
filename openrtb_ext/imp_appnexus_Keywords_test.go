034e9950d26f5223e94b5341f8333f77cb32dbe6
Sonali Shankar More <sonali.more@xandr.com>
2023-05-04 13:10:25 +0530

    Add table driven test cases

--- openrtb_ext/imp_appnexus_Keywords_test.go
+++ openrtb_ext/imp_appnexus_Keywords_test.go
@@ -7,29 +7,27 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestKeywordsUtilUnmarshalJSON(t *testing.T) {
+func TestKeywordsUnmarshalJSON(t *testing.T) {
 	type keywords struct {
 		Keywords Keywords `json:"keywords"`
 	}
 
-	t.Run("dynamic-json", func(t *testing.T) {
-		jsonData := []byte(`{"keywords" : { "pets": ["dog"] }}`)
-		var keywords keywords
-		assert.NoError(t, json.Unmarshal(jsonData, &keywords))
-		assert.Equal(t, "pets=dog", keywords.Keywords.String())
-	})
+	type validTest struct {
+		input    []byte
+		expected string
+	}
 
-	t.Run("json-array", func(t *testing.T) {
-		jsonData := []byte(`{"keywords" : [{"key": "foo", "value": ["bar","baz"]},{"key": "valueless"}]}`)
-		var keywords keywords
-		assert.NoError(t, json.Unmarshal(jsonData, &keywords))
-		assert.Equal(t, "foo=bar,foo=baz,valueless", keywords.Keywords.String())
-	})
+	validTests := []validTest{
+		{input: []byte(`{"keywords" : { "pets": ["dog"] }}`), expected: "pets=dog"},
+		{input: []byte(`{"keywords" : { "foo":[] }}`), expected: "foo"},
+		{input: []byte(`{"keywords" : [{"key": "foo", "value": ["bar","baz"]},{"key": "valueless"}]}`), expected: "foo=bar,foo=baz,valueless"},
+		{input: []byte(`{"keywords" : "foo=bar,foo=baz,valueless"}`), expected: "foo=bar,foo=baz,valueless"},
+		{input: []byte(`{"keywords" : ""}`), expected: ""},
+	}
 
-	t.Run("string", func(t *testing.T) {
-		jsonData := []byte(`{"keywords" : "foo=bar,foo=baz,valueless"}`)
+	for _, test := range validTests {
 		var keywords keywords
-		assert.NoError(t, json.Unmarshal(jsonData, &keywords))
-		assert.Equal(t, "foo=bar,foo=baz,valueless", keywords.Keywords.String())
-	})
+		assert.NoError(t, json.Unmarshal(test.input, &keywords))
+		assert.Equal(t, test.expected, keywords.Keywords.String())
+	}
 }
