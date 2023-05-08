825b1640408890f3c12a4196a6114b72fb15b1e6
Sonali Shankar More <sonali.more@xandr.com>
2023-05-08 12:30:20 +0530

    Add description to the test cases and add invalid JSONs

--- openrtb_ext/imp_appnexus_test.go
+++ openrtb_ext/imp_appnexus_test.go
@@ -12,22 +12,36 @@ func TestKeywordsUnmarshalJSON(t *testing.T) {
 		Keywords ExtImpAppnexusKeywords `json:"keywords"`
 	}
 
-	type validTest struct {
+	type testCase struct {
 		input    []byte
 		expected string
+		desc     string
 	}
 
-	validTests := []validTest{
-		{input: []byte(`{"keywords" : { "pets": ["dog"] }}`), expected: "pets=dog"},
-		{input: []byte(`{"keywords" : { "foo":[] }}`), expected: "foo"},
-		{input: []byte(`{"keywords" : [{"key": "foo", "value": ["bar","baz"]},{"key": "valueless"}]}`), expected: "foo=bar,foo=baz,valueless"},
-		{input: []byte(`{"keywords" : "foo=bar,foo=baz,valueless"}`), expected: "foo=bar,foo=baz,valueless"},
-		{input: []byte(`{"keywords" : ""}`), expected: ""},
+	validTestCases := []testCase{
+		{input: []byte(`{"keywords" : { "pets": ["dog"] }}`), expected: "pets=dog", desc: "dynamic json object"},
+		{input: []byte(`{"keywords" : { "foo":[] }}`), expected: "foo", desc: "dynamic json object with empty value array"},
+		{input: []byte(`{"keywords" : [{"key": "foo", "value": ["bar","baz"]},{"key": "valueless"}]}`), expected: "foo=bar,foo=baz,valueless", desc: "array of objects"},
+		{input: []byte(`{"keywords" : "foo=bar,foo=baz,valueless"}`), expected: "foo=bar,foo=baz,valueless", desc: "string keywords"},
+		{input: []byte(`{"keywords" : ""}`), expected: "", desc: "empty string"},
+		{input: []byte(`{"keywords" : {}}`), expected: "", desc: "empty keywords object"},
+		{input: []byte(`{"keywords" : [{}]}`), expected: "", desc: "empty keywords object array"},
 	}
 
-	for _, test := range validTests {
+	for _, test := range validTestCases {
 		var keywords keywords
-		assert.NoError(t, json.Unmarshal(test.input, &keywords))
+		assert.NoError(t, json.Unmarshal(test.input, &keywords), test.desc)
 		assert.Equal(t, test.expected, keywords.Keywords.String())
+	}
+
+	invalidTestCases := []testCase{
+		{input: []byte(`{keywords": []}`), desc: "empty keywords array"},
+		{input: []byte(`{keywords": [{]}`), desc: "invalid keywords array"},
+		{input: []byte(`{"keywords" : {"}}`), desc: "invalid keywords object"},
+	}
+
+	for _, test := range invalidTestCases {
+		var keywords keywords
+		assert.Error(t, json.Unmarshal(test.input, &keywords), test.desc)
 	}
 }
