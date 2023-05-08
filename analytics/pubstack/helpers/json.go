d8950ef0be8d7dea27faced762c1c5956ab8267a
Scott Kay <noreply@syntaxnode.com>
2023-05-08 14:49:06 -0400

    Separate Analytics Data Model (#2739)

--- analytics/pubstack/helpers/json.go
+++ analytics/pubstack/helpers/json.go
@@ -8,12 +8,25 @@ import (
 )
 
 func JsonifyAuctionObject(ao *analytics.AuctionObject, scope string) ([]byte, error) {
+	var logEntry *logAuction
+	if ao != nil {
+		logEntry = &logAuction{
+			Status:               ao.Status,
+			Errors:               ao.Errors,
+			Request:              ao.Request,
+			Response:             ao.Response,
+			Account:              ao.Account,
+			StartTime:            ao.StartTime,
+			HookExecutionOutcome: ao.HookExecutionOutcome,
+		}
+	}
+
 	b, err := json.Marshal(&struct {
 		Scope string `json:"scope"`
-		*analytics.AuctionObject
+		*logAuction
 	}{
-		Scope:         scope,
-		AuctionObject: ao,
+		Scope:      scope,
+		logAuction: logEntry,
 	})
 
 	if err == nil {
@@ -24,12 +37,25 @@ func JsonifyAuctionObject(ao *analytics.AuctionObject, scope string) ([]byte, er
 }
 
 func JsonifyVideoObject(vo *analytics.VideoObject, scope string) ([]byte, error) {
+	var logEntry *logVideo
+	if vo != nil {
+		logEntry = &logVideo{
+			Status:        vo.Status,
+			Errors:        vo.Errors,
+			Request:       vo.Request,
+			Response:      vo.Response,
+			VideoRequest:  vo.VideoRequest,
+			VideoResponse: vo.VideoResponse,
+			StartTime:     vo.StartTime,
+		}
+	}
+
 	b, err := json.Marshal(&struct {
 		Scope string `json:"scope"`
-		*analytics.VideoObject
+		*logVideo
 	}{
-		Scope:       scope,
-		VideoObject: vo,
+		Scope:    scope,
+		logVideo: logEntry,
 	})
 
 	if err == nil {
@@ -40,12 +66,21 @@ func JsonifyVideoObject(vo *analytics.VideoObject, scope string) ([]byte, error)
 }
 
 func JsonifyCookieSync(cso *analytics.CookieSyncObject, scope string) ([]byte, error) {
+	var logEntry *logUserSync
+	if cso != nil {
+		logEntry = &logUserSync{
+			Status:       cso.Status,
+			Errors:       cso.Errors,
+			BidderStatus: cso.BidderStatus,
+		}
+	}
+
 	b, err := json.Marshal(&struct {
 		Scope string `json:"scope"`
-		*analytics.CookieSyncObject
+		*logUserSync
 	}{
-		Scope:            scope,
-		CookieSyncObject: cso,
+		Scope:       scope,
+		logUserSync: logEntry,
 	})
 
 	if err == nil {
@@ -56,12 +91,23 @@ func JsonifyCookieSync(cso *analytics.CookieSyncObject, scope string) ([]byte, e
 }
 
 func JsonifySetUIDObject(so *analytics.SetUIDObject, scope string) ([]byte, error) {
+	var logEntry *logSetUID
+	if so != nil {
+		logEntry = &logSetUID{
+			Status:  so.Status,
+			Bidder:  so.Bidder,
+			UID:     so.UID,
+			Errors:  so.Errors,
+			Success: so.Success,
+		}
+	}
+
 	b, err := json.Marshal(&struct {
 		Scope string `json:"scope"`
-		*analytics.SetUIDObject
+		*logSetUID
 	}{
-		Scope:        scope,
-		SetUIDObject: so,
+		Scope:     scope,
+		logSetUID: logEntry,
 	})
 
 	if err == nil {
@@ -72,12 +118,26 @@ func JsonifySetUIDObject(so *analytics.SetUIDObject, scope string) ([]byte, erro
 }
 
 func JsonifyAmpObject(ao *analytics.AmpObject, scope string) ([]byte, error) {
+	var logEntry *logAMP
+	if ao != nil {
+		logEntry = &logAMP{
+			Status:               ao.Status,
+			Errors:               ao.Errors,
+			Request:              ao.Request,
+			AuctionResponse:      ao.AuctionResponse,
+			AmpTargetingValues:   ao.AmpTargetingValues,
+			Origin:               ao.Origin,
+			StartTime:            ao.StartTime,
+			HookExecutionOutcome: ao.HookExecutionOutcome,
+		}
+	}
+
 	b, err := json.Marshal(&struct {
 		Scope string `json:"scope"`
-		*analytics.AmpObject
+		*logAMP
 	}{
-		Scope:     scope,
-		AmpObject: ao,
+		Scope:  scope,
+		logAMP: logEntry,
 	})
 
 	if err == nil {
