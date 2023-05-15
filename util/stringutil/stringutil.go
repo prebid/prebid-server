b26ff9799931b5206cc6a8b0a3381cbb44fd9994
guscarreon <guscarreon@gmail.com>
2023-05-15 15:26:50 -0400

    StrToInt8Slice now throws error

--- util/stringutil/stringutil.go
+++ util/stringutil/stringutil.go
@@ -7,17 +7,19 @@ import (
 
 // StrToInt8Slice breaks a string into a series of tokens using a comma as a delimiter but only
 // appends the tokens into the return array if tokens can be interpreted as an 'int8'
-func StrToInt8Slice(str string) []int8 {
+func StrToInt8Slice(str string) ([]int8, error) {
 	var r []int8
 
 	if len(str) > 0 {
 		strSlice := strings.Split(str, ",")
 		for _, s := range strSlice {
-			if v, err := strconv.ParseInt(s, 10, 8); err == nil {
-				r = append(r, int8(v))
+			v, err := strconv.ParseInt(s, 10, 8)
+			if err != nil {
+				return nil, err
 			}
+			r = append(r, int8(v))
 		}
 	}
 
-	return r
+	return r, nil
 }
