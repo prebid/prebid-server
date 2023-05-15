bd8505f96df00d0bccc65de902a54ecdbb521afc
Gobi Sakthivel <gobi.sakthivel@pubmatic.com>
2023-05-15 17:48:19 +0530

    Request Response impl

---
+++ adapters/adbuttler/constant.go
@@ -0,0 +1,34 @@
+package adbuttler
+
+const (
+	FLOOR_PRICE                   = "floor_price"
+	ZONE_ID                       = "zone_id"
+	ACCOUNT_ID                    = "account_id"
+	SEARCHTYPE_DEFAULT            = "exact"
+	SEARCHTYPE_EXACT              = "exact"
+	SEARCHTYPE_BROAD              = "broad"
+	SEARCHTYPE                    = "search_type"
+	PAGE_SOURCE                   = "page_source"
+	USER_AGE					  = "user_age"
+	GENDER_MALE					  = "male"
+	GENDER_FEMALE                 = "female"
+	GENDER_OTHER                  = "other"
+	USER_GENDER                   = "user_gender"
+	COUNTRY                       = "country"
+	REGION                        = "region"
+	CITY                          = "city"
+	CATEGORY                      = "category"
+	BRAND                         = "brand"
+	RESPONSE_SUCCESS              = "success"
+	RESPONSE_NOADS                = "NO_ADS"
+	SEAT_ADBUTLER                 = "adbuttler"
+	BEACONTYPE_IMP                = "impression"
+	BEACONTYPE_CLICK              = "click"
+	RESPONSE_PRODUCTID            = "sku"
+    
+	DATATYE_INT                   = 1
+	DATATYE_FLOAT                 = 2
+	DATATYE_STRING                = 3
+	DATATYE_BOOL                  = 4
+
+)
