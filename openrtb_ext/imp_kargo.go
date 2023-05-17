fe2c490e24cf5fbc475c2cc1c4c1f9512514725a
Jeremy Sadwith <jeremy@kargo.com>
2023-05-17 10:49:35 -0400

    Deprecation note update

--- openrtb_ext/imp_kargo.go
+++ openrtb_ext/imp_kargo.go
@@ -2,5 +2,5 @@ package openrtb_ext
 
 type ImpExtKargo struct {
 	PlacementId string `json:"placementId"`
-	AdSlotID    string `json:"adSlotID"` // Deprecated
+	AdSlotID    string `json:"adSlotID"` // Deprecated - Use `placementId`
 }
