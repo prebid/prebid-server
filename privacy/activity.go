324d2936ea7fcbc7d6f37ca7e58c633f435e2298
Scott Kay <noreply@syntaxnode.com>
2023-05-18 13:17:57 -0400

    Privacy Activity Config (#2740)

---
+++ privacy/activity.go
@@ -0,0 +1,35 @@
+package privacy
+
+// Activity defines privileges which can be controlled directly by the publisher or via privacy policies.
+type Activity int
+
+const (
+	ActivitySyncUser Activity = iota + 1
+	ActivityFetchBids
+	ActivityEnrichUserFPD
+	ActivityReportAnalytics
+	ActivityTransmitUserFPD
+	ActivityTransmitPreciseGeo
+	ActivityTransmitUniqueRequestIds
+)
+
+func (a Activity) String() string {
+	switch a {
+	case ActivitySyncUser:
+		return "syncUser"
+	case ActivityFetchBids:
+		return "fetchBids"
+	case ActivityEnrichUserFPD:
+		return "enrichUfpd"
+	case ActivityReportAnalytics:
+		return "reportAnalytics"
+	case ActivityTransmitUserFPD:
+		return "transmitUfpd"
+	case ActivityTransmitPreciseGeo:
+		return "transmitPreciseGeo"
+	case ActivityTransmitUniqueRequestIds:
+		return "transmitUniqueRequestIds"
+	}
+
+	return ""
+}
