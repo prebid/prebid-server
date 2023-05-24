56d2290096d0647d34a1a9a24834abaf71952bf5
Dmytro Shyrokov <shirokoffdima@gmail.com>
2023-05-24 14:32:17 +0300

    requested changes

--- adapters/xeworks/xeworks.go
+++ adapters/xeworks/xeworks.go
@@ -15,8 +15,6 @@ import (
 	"github.com/prebid/prebid-server/util/httputil"
 )
 
-// { prebid: { type: 'banner' } }
-
 type bidType struct {
 	Type string `json:"type"`
 }
@@ -30,32 +28,30 @@ type adapter struct {
 }
 
 func Builder(bidderName openrtb_ext.BidderName, config config.Adapter, server config.Server) (adapters.Bidder, error) {
-	template, err := template.New("endpointTemplate").Parse(config.Endpoint)
+	tmpl, err := template.New("endpointTemplate").Parse(config.Endpoint)
 	if err != nil {
-		return nil, fmt.Errorf("unable to parse endpoint url template: %v", err)
+		return nil, fmt.Errorf("unable to parse endpoint URL template: %v", err)
 	}
 
 	bidder := &adapter{
-		endpoint: template,
+		endpoint: tmpl,
 	}
 
 	return bidder, nil
 }
 
 func (a *adapter) buildEndpointFromRequest(imp *openrtb2.Imp) (string, error) {
-	impExtRaw := imp.Ext
 	var impExt adapters.ExtImpBidder
-
-	if err := json.Unmarshal(impExtRaw, &impExt); err != nil {
+	if err := json.Unmarshal(imp.Ext, &impExt); err != nil {
 		return "", &errortypes.BadInput{
-			Message: "Bidder impression extension can't be deserialized",
+			Message: fmt.Sprintf("Failed to deserialize bidder impression extension: %v", err),
 		}
 	}
 
 	var xeworksExt openrtb_ext.ExtXeworks
 	if err := json.Unmarshal(impExt.Bidder, &xeworksExt); err != nil {
 		return "", &errortypes.BadInput{
-			Message: "Xeworks extenson can't be deserialized",
+			Message: fmt.Sprintf("Failed to deserialize Xeworks extension: %v", err),
 		}
 	}
 
@@ -67,29 +63,25 @@ func (a *adapter) buildEndpointFromRequest(imp *openrtb2.Imp) (string, error) {
 	return macros.ResolveMacros(a.endpoint, endpointParams)
 }
 
-func (a *adapter) MakeRequests(openRTBRequest *openrtb2.BidRequest, requestInfo *adapters.ExtraRequestInfo) ([]*adapters.RequestData, []error) {
-	if len(openRTBRequest.Imp) == 0 {
-		return nil, []error{
-			&errortypes.BadInput{
-				Message: "Imp array can't be empty",
-			},
-		}
-	}
-
-	requests := make([]*adapters.RequestData, 0, len(openRTBRequest.Imp))
-	errs := make([]error, 0)
-
-	body, err := json.Marshal(openRTBRequest)
-	if err != nil {
-		return nil, []error{err}
-	}
+func (a *adapter) MakeRequests(request *openrtb2.BidRequest, requestInfo *adapters.ExtraRequestInfo) ([]*adapters.RequestData, []error) {
+	var requests []*adapters.RequestData
+	var errs []error
 
 	headers := http.Header{}
 	headers.Add("Content-Type", "application/json;charset=utf-8")
 	headers.Add("Accept", "application/json")
 
-	for _, imp := range openRTBRequest.Imp {
+	requestCopy := *request
+	for _, imp := range request.Imp {
+		requestCopy.Imp = []openrtb2.Imp{imp}
+
 		endpoint, err := a.buildEndpointFromRequest(&imp)
+		if err != nil {
+			errs = append(errs, err)
+			continue
+		}
+
+		requestJSON, err := json.Marshal(requestCopy)
 		if err != nil {
 			errs = append(errs, err)
 			continue
@@ -97,7 +89,7 @@ func (a *adapter) MakeRequests(openRTBRequest *openrtb2.BidRequest, requestInfo 
 
 		request := &adapters.RequestData{
 			Method:  http.MethodPost,
-			Body:    body,
+			Body:    requestJSON,
 			Uri:     endpoint,
 			Headers: headers,
 		}
@@ -115,7 +107,7 @@ func (a *adapter) MakeBids(openRTBRequest *openrtb2.BidRequest, requestToBidder 
 
 	if bidderRawResponse.StatusCode == http.StatusServiceUnavailable {
 		return nil, []error{&errortypes.BadInput{
-			Message: "Bidder unavailable. Please contact the bidder support.",
+			Message: "Bidder Xeworks is unavailable. Please contact the bidder support.",
 		}}
 	}
 
@@ -123,9 +115,8 @@ func (a *adapter) MakeBids(openRTBRequest *openrtb2.BidRequest, requestToBidder 
 		return nil, []error{err}
 	}
 
-	responseBody := bidderRawResponse.Body
 	var bidResp openrtb2.BidResponse
-	if err := json.Unmarshal(responseBody, &bidResp); err != nil {
+	if err := json.Unmarshal(bidderRawResponse.Body, &bidResp); err != nil {
 		return nil, []error{err}
 	}
 
@@ -139,42 +130,37 @@ func (a *adapter) MakeBids(openRTBRequest *openrtb2.BidRequest, requestToBidder 
 }
 
 func prepareBidResponse(seats []openrtb2.SeatBid) (*adapters.BidderResponse, []error) {
-	errs := make([]error, 0)
+	errs := []error{}
 	bidResponse := adapters.NewBidderResponseWithBidsCapacity(len(seats))
 
 	for seatId, seatBid := range seats {
-		if len(seatBid.Bid) == 0 {
-			errs = append(errs, &errortypes.BadServerResponse{
-				Message: fmt.Sprintf("Array SeatBid[%d].Bid cannot be empty", seatId),
-			})
-		}
-
 		for bidId, bid := range seatBid.Bid {
 			var bidExt bidExt
 			if err := json.Unmarshal(bid.Ext, &bidExt); err != nil {
 				errs = append(errs, &errortypes.BadServerResponse{
-					Message: fmt.Sprintf("Couldn't parse SeatBid[%d].Bid[%d].Ext, err: %s", seatId, bidId, err.Error()),
+					Message: fmt.Sprintf("Failed to parse SeatBid[%d].Bid[%d].Ext: %s", seatId, bidId, err.Error()),
 				})
 				continue
 			}
 
 			bidType, err := openrtb_ext.ParseBidType(bidExt.Prebid.Type)
-
 			if err != nil {
 				errs = append(errs, &errortypes.BadServerResponse{
 					Message: fmt.Sprintf("SeatBid[%d].Bid[%d].Ext.Prebid.Type expects one of the following values: 'banner', 'native', 'video', 'audio', got '%s'", seatId, bidId, bidExt.Prebid.Type),
 				})
+				continue
 			}
 
-			// create copy if bid struct since without it bid address get's polluted with previous value
-			// because of range
-			bidCopy := bid
 			bidResponse.Bids = append(bidResponse.Bids, &adapters.TypedBid{
-				Bid:     &bidCopy,
+				Bid:     &bid,
 				BidType: bidType,
 			})
 		}
 	}
 
-	return bidResponse, errs
+	if len(errs) > 0 {
+		return nil, errs
+	}
+
+	return bidResponse, nil
 }
