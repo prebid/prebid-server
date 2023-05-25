3c84cb4ffee83ab28e653976fe209985cf301627
oh0387 <onkar.hanumante@xandr.com>
2023-05-26 00:26:13 +0530

    test

--- adapters/aax/aax_test.go
+++ adapters/aax/aax_test.go
@@ -18,6 +18,8 @@ import (
 // test
 // test
 // test
+// test
+// test
 func TestJsonSamples(t *testing.T) {
 	bidder, buildErr := Builder(openrtb_ext.BidderAax, config.Adapter{
 		Endpoint:         "https://example.aax.media/rtb/prebid",
