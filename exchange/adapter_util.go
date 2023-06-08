4bb8757eddb0e846e11f1dfafa66f7ea6e33c130
Sonali Shankar More <sonali.more@xandr.com>
2023-06-08 18:57:26 +0530

    add verizonmedia to disabled bidders

--- exchange/adapter_util.go
+++ exchange/adapter_util.go
@@ -92,6 +92,7 @@ func GetDisabledBiddersErrorMessages(infos config.BidderInfos) map[string]string
 		"andbeyondmedia": `Bidder "andbeyondmedia" is no longer available in Prebid Server. If you're looking to use the AndBeyond.Media SSP adapter, please rename it to "beyondmedia" in your configuration.`,
 		"oftmedia":       `Bidder "oftmedia" is no longer available in Prebid Server. Please update your configuration.`,
 		"groupm":         `Bidder "groupm" is no longer available in Prebid Server. Please update your configuration.`,
+		"verizonmedia":   `Bidder "verizonmedia" is no longer available in Prebid Server. Please update your configuration.`,
 	}
 
 	for name, info := range infos {
