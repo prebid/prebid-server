1e1334b13d89366be53421190cc1feea116d80f9
gotham-ads <devteam@gothamads.com>
2023-06-29 14:46:55 +0300

    init gothamads adapter

--- openrtb_ext/bidders.go
+++ openrtb_ext/bidders.go
@@ -174,6 +174,7 @@ const (
 	BidderGamma             BidderName = "gamma"
 	BidderGamoshi           BidderName = "gamoshi"
 	BidderGlobalsun         BidderName = "globalsun"
+	BidderGothamads         BidderName = "gothamads"
 	BidderGrid              BidderName = "grid"
 	BidderGumGum            BidderName = "gumgum"
 	BidderHuaweiAds         BidderName = "huaweiads"
@@ -370,6 +371,7 @@ func CoreBidderNames() []BidderName {
 		BidderGamma,
 		BidderGamoshi,
 		BidderGlobalsun,
+		BidderGothamads,
 		BidderGrid,
 		BidderGumGum,
 		BidderHuaweiAds,
