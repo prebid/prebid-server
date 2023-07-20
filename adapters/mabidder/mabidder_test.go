a48b8f5b825ee45f5cf7b66e005935f8d91db6bd
John Buco <john.buco@loblaw.ca>
2023-07-20 17:12:13 -0400

    update fake test endpoint

--- adapters/mabidder/mabidder_test.go
+++ adapters/mabidder/mabidder_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestJsonSamples(t *testing.T) {
 	bidder, buildErr := Builder(openrtb_ext.BidderMabidder, config.Adapter{
-		Endpoint: "http://whatever.url"},
+		Endpoint: "https://mabidder-test"},
 		config.Server{ExternalUrl: "http://hosturl.com", GvlID: 1, DataCenter: "2"})
 
 	if buildErr != nil {
