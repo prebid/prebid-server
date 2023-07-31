e20f2b61cfda119ef5fb1846bc695651a1299ed2
Ubuntu <ubuntu@ip-172-31-25-92.us-west-1.compute.internal>
2023-07-31 20:02:09 +0000

    Added Native Test Cases and 2.6 Response MType Utilization Support

--- adapters/vrtcal/vrtcal.go
+++ adapters/vrtcal/vrtcal.go
@@ -71,7 +71,7 @@ func (a *VrtcalAdapter) MakeBids(internalRequest *openrtb2.BidRequest, externalR
 	var errs []error
 	for _, sb := range bidResp.SeatBid {
 		for i := range sb.Bid {
-			bidType, err := getReturnTypeForImp(sb.Bid[i].ImpID, internalRequest.Imp)
+			bidType, err := getReturnTypeForImp(sb.Bid[i].MType, sb.Bid[i].ImpID, internalRequest.Imp)
 			if err == nil {
 				bidResponse.Bids = append(bidResponse.Bids, &adapters.TypedBid{
 					Bid:     &sb.Bid[i],
@@ -94,26 +94,37 @@ func Builder(bidderName openrtb_ext.BidderName, config config.Adapter, server co
 	return bidder, nil
 }
 
-func getReturnTypeForImp(impID string, imps []openrtb2.Imp) (openrtb_ext.BidType, error) {
+func getReturnTypeForImp(MType openrtb2.MarkupType, impID string, imps []openrtb2.Imp) (openrtb_ext.BidType, error) {
 	for _, imp := range imps {
 		if imp.ID == impID {
-			if imp.Banner != nil {
-				return openrtb_ext.BidTypeBanner, nil
-			}
-			if imp.Video != nil {
-				return openrtb_ext.BidTypeVideo, nil
-			}
-			if imp.Native != nil {
-				return openrtb_ext.BidTypeNative, nil
-			}
-			return "", &errortypes.BadServerResponse{
-				Message: fmt.Sprintf("Unsupported return type for ID: \"%s\"", impID),
+			if MType != 0 {
+				if MType == openrtb2.MarkupBanner {
+					return openrtb_ext.BidTypeBanner, nil
+				} else if MType == openrtb2.MarkupVideo {
+					return openrtb_ext.BidTypeVideo, nil
+				} else if MType == openrtb2.MarkupNative {
+					return openrtb_ext.BidTypeNative, nil
+				} else {
+					return "", &errortypes.BadServerResponse{
+						Message: fmt.Sprintf("Unsupported return type for ID (mtype present): \"%s\"", impID)}
+				}
+			} else { //Pre-2.6 support when mtype is not passed
+				if imp.Banner != nil {
+					return openrtb_ext.BidTypeBanner, nil
+				}
+				if imp.Video != nil {
+					return openrtb_ext.BidTypeVideo, nil
+				}
+				if imp.Native != nil {
+					return openrtb_ext.BidTypeNative, nil
+				} else {
+					return "", &errortypes.BadServerResponse{
+						Message: fmt.Sprintf("Unsupported return type for ID: (mtype not present)\"%s\"", impID)}
+				}
 			}
 		}
 	}
 
-	//Failsafe default in case impression ID is not found
 	return "", &errortypes.BadServerResponse{
-		Message: fmt.Sprintf("Failed to find impression for ID: \"%s\"", impID),
-	}
+		Message: fmt.Sprintf("Failed to find impression for ID: \"%s\"", impID)}
 }
