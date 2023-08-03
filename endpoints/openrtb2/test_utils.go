dda9230d03f6c737e34e0693b8e735a184dc3ec1
Scott Kay <noreply@syntaxnode.com>
2023-08-03 13:46:25 -0400

    Refactor Disabled Bidders List (#2993)
    * Refactor Disabled Bidders
    
    * Simplified

--- endpoints/openrtb2/test_utils.go
+++ endpoints/openrtb2/test_utils.go
@@ -1246,7 +1246,7 @@ func buildTestEndpoint(test testCase, cfg *config.Configuration) (httprouter.Han
 
 	bidderInfos := getBidderInfos(test.Config.DisabledAdapters, openrtb_ext.CoreBidderNames())
 	bidderMap := exchange.GetActiveBidders(bidderInfos)
-	disabledBidders := exchange.GetDisabledBiddersErrorMessages(bidderInfos)
+	disabledBidders := exchange.GetDisabledBidderWarningMessages(bidderInfos)
 	met := &metricsConfig.NilMetricsEngine{}
 	mockFetcher := empty_fetcher.EmptyFetcher{}
 
