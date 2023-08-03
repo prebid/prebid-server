b782df26e19399a31e40d2b5b74d5e183e6ea851
VeronikaSolovei9 <kalypsonika@gmail.com>
2023-08-03 16:20:28 -0700

    Unit test

--- endpoints/cookie_sync_test.go
+++ endpoints/cookie_sync_test.go
@@ -1909,27 +1909,32 @@ func TestCookieSyncActivityControlIntegration(t *testing.T) {
 	testCases := []struct {
 		name           string
 		bidderName     string
-		allow          bool
+		accPrivacy     *config.AccountPrivacy
 		expectedResult bool
 	}{
 		{
 			name:           "activity_is_allowed",
 			bidderName:     "bidderA",
-			allow:          true,
+			accPrivacy:     getDefaultActivityConfig("bidderA", true),
 			expectedResult: true,
 		},
 		{
 			name:           "activity_is_denied",
 			bidderName:     "bidderA",
-			allow:          false,
+			accPrivacy:     getDefaultActivityConfig("bidderA", false),
 			expectedResult: false,
+		},
+		{
+			name:           "activity_is_abstain",
+			bidderName:     "bidderA",
+			accPrivacy:     nil,
+			expectedResult: true,
 		},
 	}
 
 	for _, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
-			privacyConfig := getDefaultActivityConfig(test.bidderName, test.allow)
-			activities, err := privacy.NewActivityControl(privacyConfig)
+			activities, err := privacy.NewActivityControl(test.accPrivacy)
 			assert.NoError(t, err)
 			up := usersyncPrivacy{
 				activityControl: activities,
