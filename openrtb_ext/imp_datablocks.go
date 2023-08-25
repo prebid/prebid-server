94c827923d6c5082440a9de64f985d227bd8b71a
John <john@johnmayor.ca>
2023-08-25 15:17:44 -0400

    remove host param

--- openrtb_ext/imp_datablocks.go
+++ openrtb_ext/imp_datablocks.go
@@ -2,5 +2,5 @@ package openrtb_ext
 
 // ExtImpDatablocks defines the contract for bidrequest.imp[i].ext.prebid.bidder.datablocks
 type ExtImpDatablocks struct {
-	SourceId int    `json:"sourceId"`
+	SourceId int `json:"sourceId"`
 }
