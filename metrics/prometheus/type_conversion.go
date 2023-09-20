55302c6d9e4f8806f2044560d301f0043da08873
Scott Kay <noreply@syntaxnode.com>
2023-09-20 17:35:54 -0400

    Cleanup Prometheus Type Conversions (#3106)

--- metrics/prometheus/type_conversion.go
+++ metrics/prometheus/type_conversion.go
@@ -2,22 +2,9 @@ package prometheusmetrics
 
 import (
 	"strconv"
-
-	"github.com/prebid/prebid-server/metrics"
-	"github.com/prebid/prebid-server/openrtb_ext"
 )
 
-func adaptersAsString() []string {
-	values := openrtb_ext.CoreBidderNames()
-	valuesAsString := make([]string, len(values))
-	for i, v := range values {
-		valuesAsString[i] = string(v)
-	}
-	return valuesAsString
-}
-
-func adapterErrorsAsString() []string {
-	values := metrics.AdapterErrors()
+func enumAsString[T ~string](values []T) []string {
 	valuesAsString := make([]string, len(values))
 	for i, v := range values {
 		valuesAsString[i] = string(v)
@@ -30,112 +17,4 @@ func boolValuesAsString() []string {
 		strconv.FormatBool(true),
 		strconv.FormatBool(false),
 	}
-}
-
-func cacheResultsAsString() []string {
-	values := metrics.CacheResults()
-	valuesAsString := make([]string, len(values))
-	for i, v := range values {
-		valuesAsString[i] = string(v)
-	}
-	return valuesAsString
-}
-
-func cookieTypesAsString() []string {
-	values := metrics.CookieTypes()
-	valuesAsString := make([]string, len(values))
-	for i, v := range values {
-		valuesAsString[i] = string(v)
-	}
-	return valuesAsString
-}
-
-func cookieSyncStatusesAsString() []string {
-	values := metrics.CookieSyncStatuses()
-	valuesAsString := make([]string, len(values))
-	for i, v := range values {
-		valuesAsString[i] = string(v)
-	}
-	return valuesAsString
-}
-
-func requestStatusesAsString() []string {
-	values := metrics.RequestStatuses()
-	valuesAsString := make([]string, len(values))
-	for i, v := range values {
-		valuesAsString[i] = string(v)
-	}
-	return valuesAsString
-}
-
-func syncerRequestStatusesAsString() []string {
-	values := metrics.SyncerRequestStatuses()
-	valuesAsString := make([]string, len(values))
-	for i, v := range values {
-		valuesAsString[i] = string(v)
-	}
-	return valuesAsString
-}
-
-func overheadTypesAsString() []string {
-	overheadTypes := metrics.OverheadTypes()
-	overheadTypesAsString := make([]string, len(overheadTypes))
-	for i, ot := range overheadTypes {
-		overheadTypesAsString[i] = ot.String()
-	}
-	return overheadTypesAsString
-}
-
-func syncerSetStatusesAsString() []string {
-	values := metrics.SyncerSetUidStatuses()
-	valuesAsString := make([]string, len(values))
-	for i, v := range values {
-		valuesAsString[i] = string(v)
-	}
-	return valuesAsString
-}
-
-func requestTypesAsString() []string {
-	values := metrics.RequestTypes()
-	valuesAsString := make([]string, len(values))
-	for i, v := range values {
-		valuesAsString[i] = string(v)
-	}
-	return valuesAsString
-}
-
-func setUidStatusesAsString() []string {
-	values := metrics.SetUidStatuses()
-	valuesAsString := make([]string, len(values))
-	for i, v := range values {
-		valuesAsString[i] = string(v)
-	}
-	return valuesAsString
-}
-
-func storedDataFetchTypesAsString() []string {
-	values := metrics.StoredDataFetchTypes()
-	valuesAsString := make([]string, len(values))
-	for i, v := range values {
-		valuesAsString[i] = string(v)
-	}
-	return valuesAsString
-}
-
-func storedDataErrorsAsString() []string {
-	values := metrics.StoredDataErrors()
-	valuesAsString := make([]string, len(values))
-	for i, v := range values {
-		valuesAsString[i] = string(v)
-	}
-	return valuesAsString
-}
-
-func tcfVersionsAsString() []string {
-	values := metrics.TCFVersions()
-	valuesAsString := make([]string, len(values))
-	for i, v := range values {
-		valuesAsString[i] = string(v)
-	}
-	return valuesAsString
 }
