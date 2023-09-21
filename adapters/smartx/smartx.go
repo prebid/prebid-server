d15a8f6f7176dfb267df8fdd344e82511458b809
schubert-sc <144821265+schubert-sc@users.noreply.github.com>
2023-09-21 11:22:04 +0000

    review adjustments
    - removed comments
    - removed native as its not used currently

--- adapters/smartx/smartx.go
+++ adapters/smartx/smartx.go
@@ -12,30 +12,23 @@ import (
 	"github.com/prebid/prebid-server/openrtb_ext"
 )
 
-// adapter is a implementation of the adapters.Bidder interface.
 type adapter struct {
 	endpointURL string
 }
 
 func Builder(_ openrtb_ext.BidderName, config config.Adapter, _ config.Server) (adapters.Bidder, error) {
-	//println("Builder smartx", config.Endpoint)
-	// initialize the adapter and return it
 	return &adapter{
 		endpointURL: config.Endpoint,
 	}, nil
 }
 
-// MakeRequests prepares the HTTP requests which should be made to fetch bids.
 func (a *adapter) MakeRequests(openRTBRequest *openrtb2.BidRequest, requestInfo *adapters.ExtraRequestInfo) (requestsToBidder []*adapters.RequestData, errs []error) {
-	// parse the requests answer
-	openRTBRequestJSON, err := json.MarshalIndent(openRTBRequest, "", "   ")
+	openRTBRequestJSON, err := json.Marshal(openRTBRequest)
 	if err != nil {
-		// can't parse the request, this is an critical error
 		errs = append(errs, fmt.Errorf("marshal bidRequest: %w", err))
 		return nil, errs
 	}
 
-	// create the HEADER of the request
 	headers := http.Header{}
 	headers.Add("Content-Type", "application/json;charset=utf-8")
 	headers.Add("x-openrtb-version", "2.5")
@@ -51,7 +44,6 @@ func (a *adapter) MakeRequests(openRTBRequest *openrtb2.BidRequest, requestInfo 
 		}
 	}
 
-	// add the new request to the list
 	return append(requestsToBidder, &adapters.RequestData{
 		Method:  http.MethodPost,
 		Uri:     a.endpointURL,
@@ -60,15 +52,11 @@ func (a *adapter) MakeRequests(openRTBRequest *openrtb2.BidRequest, requestInfo 
 	}), nil
 }
 
-// MakeBids unpacks the server's response into Bids.
 func (a *adapter) MakeBids(request *openrtb2.BidRequest, _ *adapters.RequestData, responseData *adapters.ResponseData) (*adapters.BidderResponse, []error) {
-	// check HTTP status code 400 - Bad Request
-	// check HTTP status code NOT 200
 	if err := adapters.CheckResponseStatusCodeForErrors(responseData); err != nil {
 		return nil, []error{err}
 	}
 
-	// check HTTP status code 204 - No Content
 	if adapters.IsResponseStatusCodeNoContent(responseData) {
 		return nil, nil
 	}
@@ -87,20 +75,15 @@ func (a *adapter) MakeBids(request *openrtb2.BidRequest, _ *adapters.RequestData
 
 	var errs []error
 
-	// loop the SeatBids
 	for _, seatBid := range response.SeatBid {
-		// loop the Bids
 		for i, bid := range seatBid.Bid {
-			// get the MType of the bid
 			bidType, err := getMediaTypeForBid(bid)
 
 			if err != nil {
-				// if an error occures add it to the errors list and continue
 				errs = append(errs, err)
 				continue
 			}
 
-			// add the response to the list
 			bidResponse.Bids = append(bidResponse.Bids, &adapters.TypedBid{
 				Bid:     &seatBid.Bid[i],
 				BidType: bidType,
