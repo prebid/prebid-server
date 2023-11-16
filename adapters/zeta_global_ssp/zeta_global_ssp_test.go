79d2f27870c928c410c8159947a596a6bd2550e6
abermanov-zeta <95416296+abermanov-zeta@users.noreply.github.com>
2023-11-16 19:24:06 +0100

    Zeta Global SSP: Update endpoints (#3201)

--- adapters/zeta_global_ssp/zeta_global_ssp_test.go
+++ adapters/zeta_global_ssp/zeta_global_ssp_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestJsonSamples(t *testing.T) {
 	bidder, buildErr := Builder(openrtb_ext.BidderZetaGlobalSsp, config.Adapter{
-		Endpoint: "http://whatever.url"},
+		Endpoint: "https://ssp.disqus.com/bid/prebid-server?sid=11"},
 		config.Server{ExternalUrl: "http://hosturl.com", GvlID: 1, DataCenter: "2"})
 
 	if buildErr != nil {
