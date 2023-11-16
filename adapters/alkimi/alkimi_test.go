325861de4980b92477000428ec47a8c84b1251e8
Aleksandr Bogdanov <motors@live.ru>
2023-11-16 19:10:18 +0100

    tests fixed

--- adapters/alkimi/alkimi_test.go
+++ adapters/alkimi/alkimi_test.go
@@ -1,10 +1,8 @@
 package alkimi
 
 import (
-	"encoding/json"
     "testing"
 
-    "github.com/prebid/openrtb/v19/openrtb2"
     "github.com/prebid/prebid-server/v2/adapters"
     "github.com/prebid/prebid-server/v2/adapters/adapterstest"
     "github.com/prebid/prebid-server/v2/config"
