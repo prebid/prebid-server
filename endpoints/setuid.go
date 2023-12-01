e2da4460646849c02a89c76325b96ec284969bf5
ashishshinde-pubm <109787960+ashishshinde-pubm@users.noreply.github.com>
2023-12-01 23:27:38 +0530

    Fix: setuid endpoint sets cookie when both GDPR and GPP are set (#3165)

--- endpoints/setuid.go
+++ endpoints/setuid.go
@@ -126,7 +126,6 @@ func NewSetUIDEndpoint(cfg *config.Configuration, syncersByBidder map[string]use
 				handleBadStatus(w, http.StatusBadRequest, metrics.SetUidBadRequest, err, metricsEngine, &so)
 				return
 			}
-			w.Write([]byte("Warning: " + err.Error()))
 		}
 
 		tcf2Cfg := tcf2CfgBuilder(cfg.GDPR.TCF2, account.GDPR)
