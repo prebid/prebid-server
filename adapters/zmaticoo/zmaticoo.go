cbadd47bed9e5053c6fb743d7357dfb0f3ad2c2d
adam <L12354*com>
2023-12-29 17:49:42 +0800

    feat:change some code by reviewer commment

--- adapters/zmaticoo/zmaticoo.go
+++ adapters/zmaticoo/zmaticoo.go
@@ -3,77 +3,65 @@ package zmaticoo
 import (
 	"encoding/json"
 	"fmt"
-	"net/http"
-	"net/url"
-	"text/template"
-
 	"github.com/prebid/openrtb/v19/openrtb2"
 	"github.com/prebid/prebid-server/v2/adapters"
 	"github.com/prebid/prebid-server/v2/config"
 	"github.com/prebid/prebid-server/v2/errortypes"
-	"github.com/prebid/prebid-server/v2/macros"
 	"github.com/prebid/prebid-server/v2/openrtb_ext"
+	"net/http"
+	"net/url"
 )
 
 type adapter struct {
-	EndpointTemplate *template.Template
+	endpoint string
 }
 
 // Builder builds a new instance of the zmaticoo adapter for the given bidder with the given config.
 func Builder(bidderName openrtb_ext.BidderName, config config.Adapter, server config.Server) (adapters.Bidder, error) {
-	template, err := template.New("endpointTemplate").Parse(config.Endpoint)
+	endpointURL, err := url.Parse(config.Endpoint)
 	if err != nil {
-		return nil, fmt.Errorf("unable to parse endpoint url template: %v", err)
+		return nil, fmt.Errorf("invalid endpoint: %v", err)
 	}
-
 	bidder := &adapter{
-		EndpointTemplate: template,
+		endpoint: endpointURL.String(),
 	}
 	return bidder, nil
 }
 
 func (a *adapter) MakeRequests(request *openrtb2.BidRequest, reqInfo *adapters.ExtraRequestInfo) ([]*adapters.RequestData, []error) {
 	var adapterRequests []*adapters.RequestData
-
 	adapterRequest, errs := a.makeRequest(request)
 	if errs == nil {
 		adapterRequests = append(adapterRequests, adapterRequest)
 	}
-
 	return adapterRequests, errs
 }
 
 func (a *adapter) makeRequest(request *openrtb2.BidRequest) (*adapters.RequestData, []error) {
 	var errs []error
-
 	zmaticooExt, errs := getZmaticooExt(request)
-
 	if zmaticooExt == nil {
 		return nil, errs
 	}
-	endPoint, err := a.getEndpoint(zmaticooExt)
+	err := transform(request)
 	if err != nil {
 		return nil, append(errs, err)
 	}
-	transform(request)
 	reqBody, err := json.Marshal(request)
-
 	if err != nil {
 		return nil, append(errs, err)
 	}
-
 	headers := http.Header{}
 	headers.Add("Content-Type", "application/json;charset=utf-8")
-
 	return &adapters.RequestData{
 		Method:  "POST",
-		Uri:     endPoint,
+		Uri:     a.endpoint,
 		Body:    reqBody,
 		Headers: headers,
 	}, errs
 }
 
-func transform(request *openrtb2.BidRequest) {
+func transform(request *openrtb2.BidRequest) error {
 	for i, imp := range request.Imp {
 		if imp.Native != nil {
 			var nativeRequest map[string]interface{}
@@ -85,26 +73,26 @@ func transform(request *openrtb2.BidRequest) {
 				if exists {
 					continue
 				}
-
 				nativeCopyRequest["native"] = nativeRequest
 				nativeReqByte, err := json.Marshal(nativeCopyRequest)
 				//just ignore the bad native request
 				if err != nil {
-					continue
+					return err
 				}
-
 				nativeCopy := *request.Imp[i].Native
 				nativeCopy.Request = string(nativeReqByte)
 				request.Imp[i].Native = &nativeCopy
+			} else {
+				return err
 			}
 		}
 	}
+	return nil
 }
 
 func getZmaticooExt(request *openrtb2.BidRequest) (*openrtb_ext.ExtImpZmaticoo, []error) {
 	var extImpZmaticoo openrtb_ext.ExtImpZmaticoo
 	var errs []error
-
 	for _, imp := range request.Imp {
 		var extBidder adapters.ExtImpBidder
 		err := json.Unmarshal(imp.Ext, &extBidder)
@@ -119,71 +107,49 @@ func getZmaticooExt(request *openrtb2.BidRequest) (*openrtb_ext.ExtImpZmaticoo, 
 		}
 		break
 	}
-
 	return &extImpZmaticoo, errs
 
 }
 
-func (a *adapter) getEndpoint(ext *openrtb_ext.ExtImpZmaticoo) (string, error) {
-	return macros.ResolveMacros(a.EndpointTemplate, macros.EndpointTemplateParams{Host: "gw-" + url.QueryEscape(ext.ZoneId) + "-bid.zmaticoo.com"})
-}
-
 // MakeBids make the bids for the bid response.
 func (a *adapter) MakeBids(internalRequest *openrtb2.BidRequest, externalRequest *adapters.RequestData, response *adapters.ResponseData) (*adapters.BidderResponse, []error) {
-	if response.StatusCode == http.StatusNoContent {
+	if adapters.IsResponseStatusCodeNoContent(response) {
 		return nil, nil
 	}
-
-	if response.StatusCode == http.StatusBadRequest {
+	if err := adapters.CheckResponseStatusCodeForErrors(response); err != nil {
 		return nil, []error{&errortypes.BadInput{
 			Message: fmt.Sprintf("Unexpected status code: %d.", response.StatusCode),
 		}}
 	}
-
-	if response.StatusCode != http.StatusOK {
-		return nil, []error{&errortypes.BadServerResponse{
-			Message: fmt.Sprintf("Unexpected status code: %d.", response.StatusCode),
-		}}
-	}
-
 	var bidResp openrtb2.BidResponse
-
 	if err := json.Unmarshal(response.Body, &bidResp); err != nil {
 		return nil, []error{err}
 	}
-
+	var errs []error
 	bidResponse := adapters.NewBidderResponseWithBidsCapacity(1)
-
 	for _, sb := range bidResp.SeatBid {
 		for i := range sb.Bid {
-			var mediaType = getBidType(sb.Bid[i].ImpID, internalRequest.Imp)
+			mediaType, err := getMediaTypeForBid(sb.Bid[i])
+			if err != nil {
+				errs = append(errs, err)
+				continue
+			}
 			bidResponse.Bids = append(bidResponse.Bids, &adapters.TypedBid{
 				Bid:     &sb.Bid[i],
 				BidType: mediaType,
 			})
 		}
 	}
-	return bidResponse, nil
-
+	return bidResponse, errs
 }
 
-func getBidType(impId string, imps []openrtb2.Imp) openrtb_ext.BidType {
-	bidType := openrtb_ext.BidTypeBanner
-	for _, imp := range imps {
-		if imp.ID == impId {
-			if imp.Banner != nil {
-				break
-			}
-			if imp.Video != nil {
-				bidType = openrtb_ext.BidTypeVideo
-				break
-			}
-			if imp.Native != nil {
-				bidType = openrtb_ext.BidTypeNative
-				break
-			}
-
-		}
+func getMediaTypeForBid(bid openrtb2.Bid) (openrtb_ext.BidType, error) {
+	switch bid.MType {
+	case openrtb2.MarkupBanner:
+		return openrtb_ext.BidTypeBanner, nil
+	case openrtb2.MarkupNative:
+		return openrtb_ext.BidTypeNative, nil
+	default:
+		return "", fmt.Errorf("unrecognized bid type in response from rtbhouse for bid %s", bid.ImpID)
 	}
-	return bidType
 }
