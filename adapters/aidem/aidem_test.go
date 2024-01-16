ef2a2cc3340baae3faafdaababe03477ecace2a6
AndreaT <devel@dbshaper.com>
2024-01-16 15:31:53 +0100

    Added use to macros library

--- adapters/aidem/aidem_test.go
+++ adapters/aidem/aidem_test.go
@@ -5,9 +5,9 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/prebid/prebid-server/adapters/adapterstest"
-	"github.com/prebid/prebid-server/config"
-	"github.com/prebid/prebid-server/openrtb_ext"
+	"github.com/prebid/prebid-server/v2/adapters/adapterstest"
+	"github.com/prebid/prebid-server/v2/config"
+	"github.com/prebid/prebid-server/v2/openrtb_ext"
 )
 
 func TestJsonSamples(t *testing.T) {
