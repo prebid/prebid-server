b85c27c2a59b86d4debbbf3f71d12ebf5c733378
dengxinjing <xinjing.deng@cheezz.cc>
2024-02-23 10:08:07 +0800

    supplemental test cases and email

--- adapters/roulax/roulax.go
+++ adapters/roulax/roulax.go
@@ -2,6 +2,7 @@ package roulax
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"github.com/prebid/openrtb/v20/openrtb2"
 	"github.com/prebid/prebid-server/v2/adapters"
@@ -14,10 +15,6 @@ type adapter struct {
 	endpoint string
 }
 
-
-
-
-
 // Builder builds a new instance of the Roulax adapter for the given bidder with the given config.
 func Builder(bidderName openrtb_ext.BidderName, config config.Adapter, server config.Server) (adapters.Bidder, error) {
 	bidder := &adapter{
@@ -26,20 +23,16 @@ func Builder(bidderName openrtb_ext.BidderName, config config.Adapter, server co
 	return bidder, nil
 }
 
-
-
 // getImpAdotExt parses and return first imp ext or nil
 func getImpRoulaxExt(imp *openrtb2.Imp) (openrtb_ext.ExtImpRoulax, error) {
 	var extBidder adapters.ExtImpBidder
 	if err := json.Unmarshal(imp.Ext, &extBidder); err != nil {
 		return openrtb_ext.ExtImpRoulax{}, err
 	}
-
 	var extImpRoulax openrtb_ext.ExtImpRoulax
 	if err := json.Unmarshal(extBidder.Bidder, &extImpRoulax); err != nil {
 		return openrtb_ext.ExtImpRoulax{}, err
 	}
-
 	return extImpRoulax, nil
 }
 
@@ -47,24 +40,16 @@ func getImpRoulaxExt(imp *openrtb2.Imp) (openrtb_ext.ExtImpRoulax, error) {
 func (a *adapter) MakeRequests(request *openrtb2.BidRequest, reqInfo *adapters.ExtraRequestInfo) (res []*adapters.RequestData, errs []error) {
 	reqJson, err := json.Marshal(request)
 	if err != nil {
-		errs = append(errs, err)
-		return nil, errs
+		return nil, append(errs, err)
 	}
-
 	headers := http.Header{}
 	headers.Add("Content-Type", "application/json;charset=utf-8")
-
 	roulaxExt,err := getImpRoulaxExt(&request.Imp[0])
 	if err != nil {
-		errs = append(errs, err)
-		return nil, errs
+		return nil, append(errs, err)
 	}
-
 	a.endpoint = strings.Replace(a.endpoint, "{PUBLISHER_PATH}", roulaxExt.PublisherPath, -1)
 	a.endpoint = strings.Replace(a.endpoint, "{PID}", roulaxExt.Pid, -1)
-
-
-
 	return []*adapters.RequestData{{
 		Method:  "POST",
 		Uri:     a.endpoint,
@@ -74,28 +59,23 @@ func (a *adapter) MakeRequests(request *openrtb2.BidRequest, reqInfo *adapters.E
 }
 
 // MakeBids unpacks the server's response into Bids.
-// The bidder return a status code 204 when it cannot delivery an ad.
-// MakeBids unpacks the server's response into Bids.
 func (a *adapter) MakeBids(request *openrtb2.BidRequest, _ *adapters.RequestData, responseData *adapters.ResponseData) (*adapters.BidderResponse, []error) {
 	if adapters.IsResponseStatusCodeNoContent(responseData) {
 		return nil, nil
 	}
-
 	if err := adapters.CheckResponseStatusCodeForErrors(responseData); err != nil {
 		return nil, []error{err}
 	}
-
 	var response openrtb2.BidResponse
 	if err := json.Unmarshal(responseData.Body, &response); err != nil {
 		return nil, []error{err}
 	}
-
 	bidResponse := adapters.NewBidderResponseWithBidsCapacity(len(request.Imp))
 	bidResponse.Currency = response.Cur
 	var errs []error
 	for _, seatBid := range response.SeatBid {
 		for i, bid := range seatBid.Bid {
-			bidType, err := getMediaTypeForImp(bid,request.Imp)
+			bidType, err := getMediaTypeForBid(bid)
 			if err != nil {
 				errs = append(errs, err)
 				continue
@@ -107,32 +87,19 @@ func (a *adapter) MakeBids(request *openrtb2.BidRequest, _ *adapters.RequestData
 			})
 		}
 	}
-
 	return bidResponse, errs
 }
 
-func getMediaTypeForImp(bid openrtb2.Bid, imps []openrtb2.Imp) (openrtb_ext.BidType, error) {
-	var mediaType openrtb_ext.BidType
-	var typeCnt = 0
-	for _, imp := range imps {
-		if imp.ID == bid.ImpID {
-			if imp.Banner != nil {
-				typeCnt += 1
-				mediaType = openrtb_ext.BidTypeBanner
-			}
-			if imp.Native != nil {
-				typeCnt += 1
-				mediaType = openrtb_ext.BidTypeNative
-			}
-			if imp.Video != nil {
-				typeCnt += 1
-				mediaType = openrtb_ext.BidTypeVideo
-			}
-		}
+func getMediaTypeForBid(bid openrtb2.Bid) (openrtb_ext.BidType, error) {
+	switch bid.MType {
+	case openrtb2.MarkupBanner:
+		return openrtb_ext.BidTypeBanner, nil
+	case openrtb2.MarkupVideo:
+		return openrtb_ext.BidTypeVideo, nil
+	case openrtb2.MarkupNative:
+		return openrtb_ext.BidTypeNative, nil
+	default:
+		return "", fmt.Errorf("Unable to fetch mediaType in impID: %s, mType: %d", bid.ImpID, bid.MType)
 	}
-	if typeCnt == 1 {
-		return mediaType, nil
-	}
-	return openrtb_ext.BidTypeBanner,nil
 }
 
