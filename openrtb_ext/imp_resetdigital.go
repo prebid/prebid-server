eb067f7a7ceb5693937adb814602e70c1c3647a8
Bruno Jacinto <bruno@siira.world>
2024-03-26 21:40:07 +0000

    Change of data on export

--- openrtb_ext/imp_resetdigital.go
+++ openrtb_ext/imp_resetdigital.go
@@ -1,7 +1,6 @@
 package openrtb_ext
 
 type ExtResetDigital struct {
-	cid  string `json:"bid_id"`
-	crid string `json:"imp_id"`
-	adid string `json:"adid"`
+	BidID string `json:"bid_id"`
+	ImpID string `json:"imp_id"`
 }
