c22ee0332ea35662f7099be0149ac8539e13619e
bsardo <1168933+bsardo@users.noreply.github.com>
2024-04-12 10:19:41 -0400

    Refactor for readability

--- exchange/utils.go
+++ exchange/utils.go
@@ -189,20 +189,24 @@ func (rs *requestSplitter) cleanOpenRTBRequests(ctx context.Context,
 
 		passIDActivityAllowed := auctionReq.Activities.Allow(privacy.ActivityTransmitUserFPD, scopedName, privacy.NewRequestFromBidRequest(*req))
 		buyerUIDSet := reqWrapper.User != nil && reqWrapper.User.BuyerUID != ""
+		buyerUIDRemoved := false
 		if !passIDActivityAllowed {
 			privacy.ScrubUserFPD(reqWrapper)
+			buyerUIDRemoved = true
 		} else {
 			// run existing policies (GDPR, CCPA, COPPA, LMT)
 			// potentially block passing IDs based on GDPR
 			if gdprEnforced && (gdprErr != nil || !auctionPermissions.PassID) {
 				privacy.ScrubGdprID(reqWrapper)
+				buyerUIDRemoved = true
 			}
 			// potentially block passing IDs based on CCPA
 			if ccpaEnforcer.ShouldEnforce(bidderRequest.BidderName.String()) {
 				privacy.ScrubDeviceIDsIPsUserDemoExt(reqWrapper, ipConf, "eids", false)
+				buyerUIDRemoved = true
 			}
 		}
-		if buyerUIDSet && reqWrapper.User.BuyerUID == "" {
+		if buyerUIDSet && buyerUIDRemoved {
 			rs.me.RecordAdapterBuyerUIDScrubbed(bidderRequest.BidderCoreName)
 		}
 
