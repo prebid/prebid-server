c64e1a79cde2f8ec1b0cc6eb51fb9374d1fe6740
Tuomo Tilli <tuomo.tilli@readpeak.com>
2024-04-19 11:18:43 +0300

    Add ImpIDs to MakeRequests

--- adapters/readpeak/readpeak.go
+++ adapters/readpeak/readpeak.go
@@ -92,6 +92,7 @@ func (a *adapter) MakeRequests(request *openrtb2.BidRequest, requestInfo *adapte
 		Uri:     a.endpoint,
 		Body:    requestJSON,
 		Headers: headers,
+		ImpIDs:  openrtb_ext.GetImpIDs(request.Imp),
 	}
 
 	return []*adapters.RequestData{requestData}, errors
