56887ff152757df77793dcd2b280711404839790
Scott Kay <noreply@syntaxnode.com>
2024-04-22 11:22:32 -0400

    "complex" test for slice reflectutil

--- util/reflectutil/slice_test.go
+++ util/reflectutil/slice_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestUnsafeSliceClone(t *testing.T) {
+func TestUnsafeSliceCloneSimple(t *testing.T) {
 	testCases := []struct {
 		name  string
 		given []int
@@ -31,6 +31,43 @@ func TestUnsafeSliceClone(t *testing.T) {
 			original := test.given
 			clonePtr := UnsafeSliceClone(reflect2.PtrOf(test.given), reflect2.TypeOf([]int{}).(*reflect2.UnsafeSliceType))
 			clone := *(*[]int)(clonePtr)
+
+			assert.NotSame(t, original, clone, "reference")
+			assert.Equal(t, original, clone, "equality")
+			assert.Equal(t, len(original), len(clone), "len")
+			assert.Equal(t, cap(original), cap(clone), "cap")
+		})
+	}
+}
+
+func TestUnsafeSliceCloneComplex(t *testing.T) {
+	type foo struct {
+		value string
+	}
+
+	testCases := []struct {
+		name  string
+		given []foo
+	}{
+		{
+			name:  "empty",
+			given: []foo{},
+		},
+		{
+			name:  "one",
+			given: []foo{{value: "a"}},
+		},
+		{
+			name:  "many",
+			given: []foo{{value: "a"}, {value: "b"}},
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.name, func(t *testing.T) {
+			original := test.given
+			clonePtr := UnsafeSliceClone(reflect2.PtrOf(test.given), reflect2.TypeOf([]foo{}).(*reflect2.UnsafeSliceType))
+			clone := *(*[]foo)(clonePtr)
 
 			assert.NotSame(t, original, clone, "reference")
 			assert.Equal(t, original, clone, "equality")
