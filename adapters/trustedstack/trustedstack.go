542fc212970ad3688581c101d9c9084a1af47ba8
product-trustedstack <product@trustedstack.com>
2024-04-22 16:18:32 +0530

    remove mtype parsing for audio type as it is not yet supported

--- adapters/trustedstack/trustedstack.go
+++ adapters/trustedstack/trustedstack.go
@@ -96,8 +96,6 @@ func getBidMediaTypeFromMtype(bid *openrtb2.Bid) (openrtb_ext.BidType, error) {
 		return openrtb_ext.BidTypeBanner, nil
 	case openrtb2.MarkupVideo:
 		return openrtb_ext.BidTypeVideo, nil
-	case openrtb2.MarkupAudio:
-		return openrtb_ext.BidTypeAudio, nil
 	case openrtb2.MarkupNative:
 		return openrtb_ext.BidTypeNative, nil
 	default:
