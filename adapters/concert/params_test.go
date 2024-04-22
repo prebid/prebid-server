dffccd1284c1c1342abcf735b439f693f663cc71
Brett Bloxom <brett.bloxom@voxmedia.com>
2024-04-22 14:52:44 -0600

    scaffolds test files

---
+++ adapters/concert/params_test.go
@@ -0,0 +1,45 @@
+package concert
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/prebid/prebid-server/v2/openrtb_ext"
+)
+
+func TestValidParams(t *testing.T) {
+	validator, err := openrtb_ext.NewBidderParamsValidator("../../static/bidder-params")
+	if err != nil {
+		t.Fatalf("Failed to fetch the json schema. %v", err)
+	}
+
+	for _, p := range validParams {
+		if err := validator.Validate(openrtb_ext.BidderConcert, json.RawMessage(p)); err != nil {
+			t.Errorf("Schema rejected valid params: %s", p)
+		}
+	}
+}
+
+func TestInvalidParams(t *testing.T) {
+	validator, err := openrtb_ext.NewBidderParamsValidator("../../static/bidder-params")
+	if err != nil {
+		t.Fatalf("Failed to fetch the json schema. %v", err)
+	}
+
+	for _, p := range invalidParams {
+		if err := validator.Validate(openrtb_ext.BidderConcert, json.RawMessage(p)); err == nil {
+			t.Errorf("Schema allowed invalid params: %s", p)
+		}
+	}
+}
+
+var validParams = []string{
+	`{"publisherId": ""}`,
+	`{"publisherId": "concert_publisher_id"}`,
+}
+
+var invalidParams = []string{
+	`{"publisherId": 42}`,
+	`{"publisherId": ["concert_publisher_id"]}`,
+	`{"publisherId": {"id": "concert_publisher_id"}}`,
+}
