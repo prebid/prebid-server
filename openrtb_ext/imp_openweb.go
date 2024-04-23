858b0afae99ece7c90c879c8e69b40651877c4d0
Zdravko Kosanovic <zdravko.kosanovic@risecodes.com>
2024-04-23 14:08:26 +0200

    OpenWeb: Fix adapter

--- openrtb_ext/imp_openweb.go
+++ openrtb_ext/imp_openweb.go
@@ -2,8 +2,6 @@ package openrtb_ext
 
 // ExtImpOpenWeb defines the contract for bidrequest.imp[i].ext.prebid.bidder.openweb
 type ExtImpOpenWeb struct {
-	SourceID    int     `json:"aid"`
-	PlacementID int     `json:"placementId,omitempty"`
-	SiteID      int     `json:"siteId,omitempty"`
-	BidFloor    float64 `json:"bidFloor,omitempty"`
+	Org         string `json:"org"`
+	PlacementID string `json:"placementId,omitempty"`
 }
