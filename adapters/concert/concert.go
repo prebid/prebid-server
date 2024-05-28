2c7b6f90bcaee39066edf20918488ce648c43ad3
Brett Bloxom <brett.bloxom@voxmedia.com>
2024-05-28 09:01:02 -0600

    use response error message

--- adapters/concert/concert.go
+++ adapters/concert/concert.go
@@ -74,9 +74,7 @@ func (a *adapter) MakeBids(request *openrtb2.BidRequest, requestData *adapters.R
 	}
 
 	if err := adapters.CheckResponseStatusCodeForErrors(responseData); err != nil {
-		return nil, []error{&errortypes.BadInput{
-			Message: fmt.Sprintf("Unexpected status code: %d. Run with request.debug = 1 for more info", responseData.StatusCode),
-		}}
+		return nil, []error{err}
 	}
 
 	var response openrtb2.BidResponse
