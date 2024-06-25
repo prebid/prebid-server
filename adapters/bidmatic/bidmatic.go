5dcf0534a8f43a1bb9b01add0e6198052346225c
d.kornet <d.kornet@adtelligent.com>
2024-06-25 13:44:54 +0300

    fix

--- adapters/bidmatic/bidmatic.go
+++ adapters/bidmatic/bidmatic.go
@@ -198,8 +198,7 @@ func validateImpression(imp *openrtb2.Imp) (int, error) {
 
 // Builder builds a new instance of the bidmatic adapter for the given bidder with the given config.
 func Builder(bidderName openrtb_ext.BidderName, config config.Adapter, server config.Server) (adapters.Bidder, error) {
-	bidder := &Adapter{
+	return &Adapter{
 		endpoint: config.Endpoint,
-	}
-	return bidder, nil
+	}, nil
 }
