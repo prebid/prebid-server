573c59ed37f6072b3665426543f32b6c5df29c30
Богатов Герман <bogatovgrmn@gmail.com>
2024-07-02 12:42:18 +0300

    zentotem: add adapter add adapter for zentotem

--- openrtb_ext/bidders.go
+++ openrtb_ext/bidders.go
@@ -239,6 +239,7 @@ var coreBidderNames []BidderName = []BidderName{
 	BidderYieldlab,
 	BidderYieldmo,
 	BidderYieldone,
+	BidderZentotem,
 	BidderZeroClickFraud,
 	BidderZetaGlobalSsp,
 	BidderZmaticoo,
@@ -569,6 +570,7 @@ const (
 	BidderYieldlab          BidderName = "yieldlab"
 	BidderYieldmo           BidderName = "yieldmo"
 	BidderYieldone          BidderName = "yieldone"
+	BidderZentotem          BidderName = "zentotem"
 	BidderZeroClickFraud    BidderName = "zeroclickfraud"
 	BidderZetaGlobalSsp     BidderName = "zeta_global_ssp"
 	BidderZmaticoo          BidderName = "zmaticoo"
