8fe38fdeefdadfad5869f286488b5a4a6f2890f3
qt-io <octavian.cimpu@qt.io>
2024-08-01 10:01:54 +0300

    improved multi-imp test and used fake url in tests

--- adapters/qt/qt_test.go
+++ adapters/qt/qt_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestJsonSamples(t *testing.T) {
 	bidder, buildErr := Builder(openrtb_ext.BidderQT, config.Adapter{
-		Endpoint: "https://endpoint1.qt.io/pserver"}, config.Server{ExternalUrl: "http://hosturl.com", GvlID: 1, DataCenter: "2"})
+		Endpoint: "https://fake.test.io/pserver"}, config.Server{ExternalUrl: "http://hosturl.com", GvlID: 1, DataCenter: "2"})
 
 	if buildErr != nil {
 		t.Fatalf("Builder returned unexpected error %v", buildErr)
