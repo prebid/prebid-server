9de20c4c40449c1364564d2a9ef54a59035af4ea
Shriprasad Marathe <shriprasad.marathe@pubmatic.com>
2024-08-05 16:26:09 +0530

    Corrected ordering and refactoring as per review comments

--- exchange/non_bid_reason.go
+++ exchange/non_bid_reason.go
@@ -2,7 +2,7 @@ package exchange
 
 import (
 	"errors"
-	"strings"
+	"net"
 	"syscall"
 
 	"github.com/prebid/openrtb/v20/openrtb3"
@@ -14,42 +14,53 @@ import (
 // Reference:  https://github.com/InteractiveAdvertisingBureau/openrtb/blob/master/extensions/community_extensions/seat-non-bid.md#list-non-bid-status-codes
 
 const (
+	ErrorGeneral                           openrtb3.NoBidReason = 100 // Error - General
+	ErrorTimeout                           openrtb3.NoBidReason = 101 // Error - Timeout
+	ErrorBidderUnreachable                 openrtb3.NoBidReason = 103 // Error - Bidder Unreachable
 	ResponseRejectedGeneral                openrtb3.NoBidReason = 300
 	ResponseRejectedBelowFloor             openrtb3.NoBidReason = 301 // Response Rejected - Below Floor
 	ResponseRejectedCategoryMappingInvalid openrtb3.NoBidReason = 303 // Response Rejected - Category Mapping Invalid
 	ResponseRejectedBelowDealFloor         openrtb3.NoBidReason = 304 // Response Rejected - Bid was Below Deal Floor
 	ResponseRejectedCreativeSizeNotAllowed openrtb3.NoBidReason = 351 // Response Rejected - Invalid Creative (Size Not Allowed)
 	ResponseRejectedCreativeNotSecure      openrtb3.NoBidReason = 352 // Response Rejected - Invalid Creative (Not Secure)
-	ErrorTimeout                           openrtb3.NoBidReason = 101 // Error - Timeout
-	ErrorGeneral                           openrtb3.NoBidReason = 100 // Error - General
-	ErrorBidderUnreachable                 openrtb3.NoBidReason = 103 // Error - Bidder Unreachable
 )
 
-func errorToNonBidReason(errorCode int) openrtb3.NoBidReason {
-	switch errorCode {
+func errorToNonBidReason(err error) openrtb3.NoBidReason {
+	switch errortypes.ReadCode(err) {
 	case errortypes.TimeoutErrorCode:
 		return ErrorTimeout
+	default:
+		return ErrorGeneral
 	}
-	// return 0
-	return ErrorGeneral
 }
+
+// func errorToNonBidReason(errorCode int) openrtb3.NoBidReason {
+// 	switch errorCode {
+// 	case errortypes.TimeoutErrorCode:
+// 		return ErrorTimeout
+// 	}
+// 	return ErrorGeneral
+// }
 
 // httpInfoToNonBidReason determines NoBidReason code (NBR)
 // It will first try to resolve the NBR based on prebid's proprietary error code.
 // If proprietary error code not found then it will try to determine NBR using
 // system call level error code
 func httpInfoToNonBidReason(httpInfo *httpCallInfo) openrtb3.NoBidReason {
-	errorCode := errortypes.ReadCode(httpInfo.err)
-	nonBidReason := errorToNonBidReason(errorCode)
-	if nonBidReason == ErrorGeneral {
-		if isBidderUnreachableError(httpInfo) {
-			return ErrorBidderUnreachable
-		}
+	// errorCode := errortypes.ReadCode(httpInfo.err)
+	nonBidReason := errorToNonBidReason(httpInfo.err)
+	// nonBidReason := errorToNonBidReason(errorCode)
+	if nonBidReason != ErrorGeneral {
+		return nonBidReason
 	}
-	return nonBidReason
+	if isBidderUnreachableError(httpInfo) {
+		return ErrorBidderUnreachable
+	}
+	return ErrorGeneral
 }
 
 // isBidderUnreachableError checks if the error is due to connection refused or no such host
 func isBidderUnreachableError(httpInfo *httpCallInfo) bool {
-	return errors.Is(httpInfo.err, syscall.ECONNREFUSED) || strings.Contains(httpInfo.err.Error(), "no such host")
+	var dnsErr *net.DNSError
+	return errors.Is(httpInfo.err, syscall.ECONNREFUSED) || (errors.As(httpInfo.err, &dnsErr) && dnsErr.IsNotFound)
 }
