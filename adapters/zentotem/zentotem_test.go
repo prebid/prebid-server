2b0b9b108c971b4723d0db50d169b37e4ff7c4cb
Богатов Герман <bogatovgrmn@gmail.com>
2024-08-20 20:28:01 +0300

    zentotem: add adapter add adapter for zentotem

--- adapters/zentotem/zentotem_test.go
+++ adapters/zentotem/zentotem_test.go
@@ -1,9 +1,11 @@
 package zentotem
 
 import (
+	"github.com/prebid/openrtb/v20/openrtb2"
 	"github.com/prebid/prebid-server/v2/adapters/adapterstest"
 	"github.com/prebid/prebid-server/v2/config"
 	"github.com/prebid/prebid-server/v2/openrtb_ext"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -16,4 +18,55 @@ func TestJsonSamples(t *testing.T) {
 	}
 
 	adapterstest.RunJSONBidderTest(t, "zentotemtest", bidder)
+}
+
+func TestGetMediaTypeForBid(t *testing.T) {
+	tests := []struct {
+		name     string
+		imp      openrtb2.Imp
+		wantType openrtb_ext.BidType
+		wantErr  bool
+	}{
+		{
+			name: "get bid native type",
+			imp: openrtb2.Imp{
+				Native: &openrtb2.Native{
+					Request: "test",
+				},
+			},
+			wantType: openrtb_ext.BidTypeNative,
+			wantErr:  false,
+		},
+		{
+			name: "get bid banner type",
+			imp: openrtb2.Imp{
+				Banner: &openrtb2.Banner{
+					ID: "test",
+				},
+			},
+			wantType: openrtb_ext.BidTypeBanner,
+			wantErr:  false,
+		},
+		{
+			name: "get bid video type",
+			imp: openrtb2.Imp{
+				Video: &openrtb2.Video{
+					PodID: "test",
+				},
+			},
+			wantType: openrtb_ext.BidTypeVideo,
+			wantErr:  false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			bType, err := getMediaTypeForBid(tt.imp)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("getMediaTypeForBid error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			assert.Equal(t, bType, tt.wantType)
+		})
+	}
 }
