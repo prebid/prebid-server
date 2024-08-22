536f1183cc355a9683d5dfb1576512441b3f79c5
Богатов Герман <bogatovgrmn@gmail.com>
2024-08-22 15:10:49 +0300

    zentotem: fix after review fix after review

--- adapters/zentotem/zentotem.go
+++ adapters/zentotem/zentotem.go
@@ -6,9 +6,7 @@ import (
 	"github.com/prebid/openrtb/v20/openrtb2"
 	"github.com/prebid/prebid-server/v2/adapters"
 	"github.com/prebid/prebid-server/v2/config"
-	"github.com/prebid/prebid-server/v2/errortypes"
 	"github.com/prebid/prebid-server/v2/openrtb_ext"
-	"net/http"
 )
 
 type adapter struct {
@@ -24,13 +22,6 @@ func Builder(bidderName openrtb_ext.BidderName, config config.Adapter, server co
 }
 
 func (a *adapter) MakeRequests(request *openrtb2.BidRequest, requestInfo *adapters.ExtraRequestInfo) ([]*adapters.RequestData, []error) {
-	if request == nil {
-		return nil, []error{&errortypes.BadInput{Message: "empty request"}}
-	}
-	if len(request.Imp) == 0 {
-		return nil, []error{&errortypes.BadInput{Message: "empty request.Imp"}}
-	}
-
 	requests := make([]*adapters.RequestData, 0, len(request.Imp))
 	var errors []error
 
@@ -55,48 +46,27 @@ func (a *adapter) MakeRequests(request *openrtb2.BidRequest, requestInfo *adapte
 	return requests, errors
 }
 
-func getMediaTypeForBid(imp openrtb2.Imp) (openrtb_ext.BidType, error) {
-	if imp.Native != nil {
+func getMediaTypeForBid(bid openrtb2.Bid) (openrtb_ext.BidType, error) {
+	switch bid.MType {
+	case openrtb2.MarkupBanner:
+		return openrtb_ext.BidTypeBanner, nil
+	case openrtb2.MarkupVideo:
+		return openrtb_ext.BidTypeVideo, nil
+	case openrtb2.MarkupNative:
 		return openrtb_ext.BidTypeNative, nil
 	}
 
-	if imp.Banner != nil {
-		return openrtb_ext.BidTypeBanner, nil
-	}
-
-	if imp.Video != nil {
-		return openrtb_ext.BidTypeVideo, nil
-	}
-
-	return "", &errortypes.BadInput{
-		Message: fmt.Sprintf("Processing an invalid impression; cannot resolve impression type for imp #%s", imp.ID),
-	}
+	return "", fmt.Errorf("could not define media type for impression: %s", bid.ImpID)
 }
 
 func (a *adapter) MakeBids(request *openrtb2.BidRequest, requestData *adapters.RequestData, responseData *adapters.ResponseData) (*adapters.BidderResponse, []error) {
-	if responseData.StatusCode == http.StatusNoContent {
+	if adapters.IsResponseStatusCodeNoContent(responseData) {
 		return nil, nil
 	}
 
-	if responseData.StatusCode == http.StatusBadRequest {
-		err := &errortypes.BadInput{
-			Message: "Unexpected status code: 400. Bad request from publisher. Run with request.debug = 1 for more info.",
-		}
+	if err := adapters.CheckResponseStatusCodeForErrors(responseData); err != nil {
 		return nil, []error{err}
 	}
-
-	if responseData.StatusCode != http.StatusOK {
-		err := &errortypes.BadServerResponse{
-			Message: fmt.Sprintf("Unexpected status code: %d. Run with request.debug = 1 for more info.", responseData.StatusCode),
-		}
-		return nil, []error{err}
-	}
-
-	impMap := map[string]openrtb2.Imp{}
-	for _, imp := range request.Imp {
-		impMap[imp.ID] = imp
-	}
-
 	var response openrtb2.BidResponse
 	if err := json.Unmarshal(responseData.Body, &response); err != nil {
 		return nil, []error{err}
@@ -106,23 +76,15 @@ func (a *adapter) MakeBids(request *openrtb2.BidRequest, requestData *adapters.R
 	bidResponse.Currency = response.Cur
 	var errors []error
 	for _, seatBid := range response.SeatBid {
-		for i, bid := range seatBid.Bid {
-			imp, ok := impMap[bid.ImpID]
-			if !ok {
-				errors = append(errors, &errortypes.BadInput{
-					Message: fmt.Sprintf("Invalid bid imp ID #%s does not match any imp IDs from the original bid request", bid.ImpID),
-				})
-				continue
-			}
-
-			bidType, err := getMediaTypeForBid(imp)
+		for _, bid := range seatBid.Bid {
+			bidType, err := getMediaTypeForBid(bid)
 			if err != nil {
 				errors = append(errors, err)
 				continue
 			}
 
 			bidResponse.Bids = append(bidResponse.Bids, &adapters.TypedBid{
-				Bid:     &seatBid.Bid[i],
+				Bid:     &bid,
 				BidType: bidType,
 			})
 		}
--- adapters/zentotem/zentotem_test.go
+++ adapters/zentotem/zentotem_test.go
@@ -23,45 +23,44 @@ func TestJsonSamples(t *testing.T) {
 func TestGetMediaTypeForBid(t *testing.T) {
 	tests := []struct {
 		name     string
-		imp      openrtb2.Imp
+		bid      openrtb2.Bid
 		wantType openrtb_ext.BidType
 		wantErr  bool
 	}{
 		{
 			name: "get bid native type",
-			imp: openrtb2.Imp{
-				Native: &openrtb2.Native{
-					Request: "test",
-				},
+			bid: openrtb2.Bid{
+				MType: 4,
 			},
 			wantType: openrtb_ext.BidTypeNative,
 			wantErr:  false,
 		},
 		{
 			name: "get bid banner type",
-			imp: openrtb2.Imp{
-				Banner: &openrtb2.Banner{
-					ID: "test",
-				},
+			bid: openrtb2.Bid{
+				MType: 1,
 			},
 			wantType: openrtb_ext.BidTypeBanner,
 			wantErr:  false,
 		},
 		{
 			name: "get bid video type",
-			imp: openrtb2.Imp{
-				Video: &openrtb2.Video{
-					PodID: "test",
-				},
+			bid: openrtb2.Bid{
+				MType: 2,
 			},
 			wantType: openrtb_ext.BidTypeVideo,
 			wantErr:  false,
+		},
+		{
+			name:     "fail",
+			wantType: "",
+			wantErr:  true,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			bType, err := getMediaTypeForBid(tt.imp)
+			bType, err := getMediaTypeForBid(tt.bid)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("getMediaTypeForBid error = %v, wantErr %v", err, tt.wantErr)
 			}
