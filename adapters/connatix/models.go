395401d9fa1929876aa7942c5178117f21f37e03
Patrick Szeleczki <patrick.szeleczki@connatix.com>
2024-09-05 16:53:45 +0300

    add models for adapter

---
+++ adapters/connatix/models.go
@@ -0,0 +1,31 @@
+package connatix
+
+import (
+	"net/url"
+
+	"github.com/prebid/prebid-server/v2/openrtb_ext"
+)
+
+type adapter struct {
+	uri url.URL
+}
+
+type impExtIncoming struct {
+	Bidder openrtb_ext.ExtImpConnatix `json:"bidder"`
+}
+
+type impExt struct {
+	Connatix impExtConnatix `json:"connatix"`
+}
+
+type impExtConnatix struct {
+	PlacementId string `json:"placementId,omitempty"`
+}
+
+type bidExt struct {
+	Cnx bidCnxExt `json:"connatix,omitempty"`
+}
+
+type bidCnxExt struct {
+	MediaType string `json:"mediaType,omitempty"`
+}
