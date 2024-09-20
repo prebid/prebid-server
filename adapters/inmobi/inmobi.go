b8b21d0d77abed086151c6265bfc5fb57cc231f5
Brad Kaneyama <brad.kaneyama@gmail.com>
2024-09-20 08:37:38 -0700

    removing adapter support for audio mtype responses

--- adapters/inmobi/inmobi.go
+++ adapters/inmobi/inmobi.go
@@ -127,8 +127,6 @@ func getMediaTypeForImp(bid openrtb2.Bid) (openrtb_ext.BidType, error) {
 		return openrtb_ext.BidTypeBanner, nil
 	case openrtb2.MarkupVideo:
 		return openrtb_ext.BidTypeVideo, nil
-	case openrtb2.MarkupAudio:
-		return openrtb_ext.BidTypeAudio, nil
 	case openrtb2.MarkupNative:
 		return openrtb_ext.BidTypeNative, nil
 	default:
