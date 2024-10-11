c6e46ebc314d22c6df7af792ce6855528864154e
bsardo <1168933+bsardo@users.noreply.github.com>
2024-10-11 12:09:06 -0400

    Update agma TestShouldTrackEvent test to look for consent in 2.6 location

--- analytics/agma/agma_module_test.go
+++ analytics/agma/agma_module_test.go
@@ -1,7 +1,6 @@
 package agma
 
 import (
-	"encoding/json"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -212,7 +211,7 @@ func TestShouldTrackEvent(t *testing.T) {
 				},
 			},
 			User: &openrtb2.User{
-				Ext: json.RawMessage(`{"consent": "` + agmaConsent + `"}`),
+				Consent: agmaConsent,
 			},
 		},
 	})
@@ -245,7 +244,7 @@ func TestShouldTrackEvent(t *testing.T) {
 				},
 			},
 			User: &openrtb2.User{
-				Ext: json.RawMessage(`{"consent": "CP4LywcP4LywcLRAAAENCZCAAAIAAAIAAAAAIxQAQIwgAAAA.II7Nd_X__bX9n-_7_6ft0eY1f9_r37uQzDhfNs-8F3L_W_LwX32E7NF36tq4KmR4ku1bBIQNtHMnUDUmxaolVrzHsak2cpyNKJ_JkknsZe2dYGF9Pn9lD-YKZ7_5_9_f52T_9_9_-39z3_9f___dv_-__-vjf_599n_v9fV_78_Kf9______-____________8A"}`),
+				Consent: "CP4LywcP4LywcLRAAAENCZCAAAIAAAIAAAAAIxQAQIwgAAAA.II7Nd_X__bX9n-_7_6ft0eY1f9_r37uQzDhfNs-8F3L_W_LwX32E7NF36tq4KmR4ku1bBIQNtHMnUDUmxaolVrzHsak2cpyNKJ_JkknsZe2dYGF9Pn9lD-YKZ7_5_9_f52T_9_9_-39z3_9f___dv_-__-vjf_599n_v9fV_78_Kf9______-____________8A",
 			},
 		},
 	})
@@ -263,7 +262,7 @@ func TestShouldTrackEvent(t *testing.T) {
 				},
 			},
 			User: &openrtb2.User{
-				Ext: json.RawMessage(`{"consent": "CP4LywcP4LywcLRAAAENCZCAAIAAAAAAAAAAIxQAQIxAAAAA.II7Nd_X__bX9n-_7_6ft0eY1f9_r37uQzDhfNs-8F3L_W_LwX32E7NF36tq4KmR4ku1bBIQNtHMnUDUmxaolVrzHsak2cpyNKJ_JkknsZe2dYGF9Pn9lD-YKZ7_5_9_f52T_9_9_-39z3_9f___dv_-__-vjf_599n_v9fV_78_Kf9______-____________8A"}`),
+				Consent: "CP4LywcP4LywcLRAAAENCZCAAIAAAAAAAAAAIxQAQIxAAAAA.II7Nd_X__bX9n-_7_6ft0eY1f9_r37uQzDhfNs-8F3L_W_LwX32E7NF36tq4KmR4ku1bBIQNtHMnUDUmxaolVrzHsak2cpyNKJ_JkknsZe2dYGF9Pn9lD-YKZ7_5_9_f52T_9_9_-39z3_9f___dv_-__-vjf_599n_v9fV_78_Kf9______-____________8A",
 			},
 		},
 	})
@@ -281,7 +280,7 @@ func TestShouldTrackEvent(t *testing.T) {
 				},
 			},
 			User: &openrtb2.User{
-				Ext: json.RawMessage(`{"consent": "` + agmaConsent + `"}`),
+				Consent: agmaConsent,
 			},
 		},
 	})
@@ -296,7 +295,7 @@ func TestShouldTrackEvent(t *testing.T) {
 				ID: "track-me",
 			},
 			User: &openrtb2.User{
-				Ext: json.RawMessage(`{"consent": "` + agmaConsent + `"}`),
+				Consent: agmaConsent,
 			},
 		},
 	})
@@ -311,7 +310,7 @@ func TestShouldTrackEvent(t *testing.T) {
 				Bundle: "track-me",
 			},
 			User: &openrtb2.User{
-				Ext: json.RawMessage(`{"consent": "` + agmaConsent + `"}`),
+				Consent: agmaConsent,
 			},
 		},
 	})
