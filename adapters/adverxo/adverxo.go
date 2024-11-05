07b8d6b2a571a59e77b04afe9ab349e1ef94ac60
Adverxo <developer@adverxo.com>
2024-11-05 09:16:11 +0100

    Change packages import references to v3

--- adapters/adverxo/adverxo.go
+++ adapters/adverxo/adverxo.go
@@ -11,11 +11,11 @@ import (
 
 	"github.com/buger/jsonparser"
 	"github.com/prebid/openrtb/v20/openrtb2"
-	"github.com/prebid/prebid-server/v2/adapters"
-	"github.com/prebid/prebid-server/v2/config"
-	"github.com/prebid/prebid-server/v2/errortypes"
-	"github.com/prebid/prebid-server/v2/macros"
-	"github.com/prebid/prebid-server/v2/openrtb_ext"
+	"github.com/prebid/prebid-server/v3/adapters"
+	"github.com/prebid/prebid-server/v3/config"
+	"github.com/prebid/prebid-server/v3/errortypes"
+	"github.com/prebid/prebid-server/v3/macros"
+	"github.com/prebid/prebid-server/v3/openrtb_ext"
 )
 
 type adapter struct {
