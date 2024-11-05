f0649d6863f8607ea959fe32d1dc69813a2fb681
Przemysław Kaczmarek <pkaczmarek@prebid.org>
2024-11-05 11:39:23 +0000

    resolve conflicts

--- macros/macros.go
+++ macros/macros.go
@@ -18,6 +18,10 @@ type EndpointTemplateParams struct {
 	PageID      string
 	SupplyId    string
 	ImpID       string
+	SspId       string
+	SspID       string
+	SeatID      string
+	TokenID     string
 }
 
 // UserSyncPrivacy specifies privacy policy macros, represented as strings, for user sync urls.
