6b4e2d2ccb63c63ca0e44859b157435b9cfc846c
Romain N <rneuplanche@smartadserver.com>
2024-11-08 19:09:12 +0100

    [AUC-470] Equativ: Update bid type computation with Markup type

--- adapters/smartadserver/smartadserver_test.go
+++ adapters/smartadserver/smartadserver_test.go
@@ -3,9 +3,11 @@ package smartadserver
 import (
 	"testing"
 
+	"github.com/prebid/openrtb/v20/openrtb2"
 	"github.com/prebid/prebid-server/v3/adapters/adapterstest"
 	"github.com/prebid/prebid-server/v3/config"
 	"github.com/prebid/prebid-server/v3/openrtb_ext"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestJsonSamples(t *testing.T) {
@@ -17,4 +19,28 @@ func TestJsonSamples(t *testing.T) {
 	}
 
 	adapterstest.RunJSONBidderTest(t, "smartadservertest", bidder)
+}
+
+func TestGetBidTypeFromMarkupType_WhenBanner_ShouldReturnBanner(t *testing.T) {
+	mediaType := getBidTypeFromMarkupType(openrtb2.MarkupBanner)
+
+	assert.Equal(t, openrtb_ext.BidTypeBanner, mediaType)
+}
+
+func TestGetBidTypeFromMarkupType_WhenVideo_ShouldReturnVideo(t *testing.T) {
+	mediaType := getBidTypeFromMarkupType(openrtb2.MarkupVideo)
+
+	assert.Equal(t, openrtb_ext.BidTypeVideo, mediaType)
+}
+
+func TestGetBidTypeFromMarkupType_WhenAudio_ShouldReturnAudio(t *testing.T) {
+	mediaType := getBidTypeFromMarkupType(openrtb2.MarkupAudio)
+
+	assert.Equal(t, openrtb_ext.BidTypeAudio, mediaType)
+}
+
+func TestGetBidTypeFromMarkupType_WhenNative_ShouldReturnNative(t *testing.T) {
+	mediaType := getBidTypeFromMarkupType(openrtb2.MarkupNative)
+
+	assert.Equal(t, openrtb_ext.BidTypeNative, mediaType)
 }
