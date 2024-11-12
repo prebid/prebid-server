b4633806b0de6fba70727a44817a4c0b64d720f8
bsardo <1168933+bsardo@users.noreply.github.com>
2024-11-12 08:41:55 -0500

    Explicitly declare bidders as supporting ORTB 2.5 in test

--- exchange/utils_test.go
+++ exchange/utils_test.go
@@ -3557,7 +3557,18 @@ func TestCleanOpenRTBRequestsBuyerUID(t *testing.T) {
 				me:                &metrics.MetricsEngineMock{},
 				gdprPermsBuilder:  gdprPermissionsBuilder,
 				hostSChainNode:    nil,
-				bidderInfo:        config.BidderInfos{},
+				bidderInfo:        config.BidderInfos{
+					"appnexus": config.BidderInfo{
+						OpenRTB: &config.OpenRTBInfo{
+							Version: "2.5",
+						},
+					},
+					"pubmatic": config.BidderInfo{
+						OpenRTB: &config.OpenRTBInfo{
+							Version: "2.5",
+						},
+					},
+				},
 			}
 
 			results, _, errs := reqSplitter.cleanOpenRTBRequests(context.Background(), auctionReq, nil, gdpr.SignalNo, false, nil)
