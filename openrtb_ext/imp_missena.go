091ffa966ff6dffb317868171fe6683b0aafeb9d
Jean-Sébastien Ney <jeansebastien.ney@gmail.com>
2024-11-19 09:24:55 +0100

    add APIKey

--- openrtb_ext/imp_missena.go
+++ openrtb_ext/imp_missena.go
@@ -1,7 +1,7 @@
 package openrtb_ext
 
 type ExtImpMissena struct {
-	ApiKey    string   `json:"apiKey"`
+	APIKey    string   `json:"apiKey"`
 	Formats   []string `json:"formats"`
 	Placement string   `json:"placement"`
 	Sample    string   `json:"sample"`
