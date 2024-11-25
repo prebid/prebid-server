d03f94ad8aeccee907ae90ea30ce0ef15fa0152a
Romain N <rneuplanche@smartadserver.com>
2024-11-25 17:35:34 +0100

    [AUC-470] remove useless imports

--- adapters/smartadserver/smartadserver_test.go
+++ adapters/smartadserver/smartadserver_test.go
@@ -3,11 +3,9 @@ package smartadserver
 import (
 	"testing"
 
-	"github.com/prebid/openrtb/v20/openrtb2"
 	"github.com/prebid/prebid-server/v3/adapters/adapterstest"
 	"github.com/prebid/prebid-server/v3/config"
 	"github.com/prebid/prebid-server/v3/openrtb_ext"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestJsonSamples(t *testing.T) {
