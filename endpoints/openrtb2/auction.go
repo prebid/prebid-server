df8bde906e54e602c154d520092bd6602c85e475
Sheridan C Rawlins <scr@yahooinc.com>
2024-12-09 08:50:15 -0800

    Added protection on the parseRequest.

--- endpoints/openrtb2/auction.go
+++ endpoints/openrtb2/auction.go
@@ -164,10 +164,11 @@ func (deps *endpointDeps) Auction(w http.ResponseWriter, r *http.Request, _ http
 	// We can respect timeouts more accurately if we note the *real* start time, and use it
 	// to compute the auction timeout.
 	start := time.Now()
+	ctx := r.Context()
 
 	hookExecutor := hookexecution.NewHookExecutor(
 		deps.hookExecutionPlanBuilder, hookexecution.EndpointAuction, deps.metricsEngine,
-		hookexecution.WithContext(r.Context()),
+		hookexecution.WithContext(ctx),
 	)
 
 	ao := analytics.AuctionObject{
@@ -194,7 +195,16 @@ func (deps *endpointDeps) Auction(w http.ResponseWriter, r *http.Request, _ http
 	w.Header().Set("X-Prebid", version.BuildXPrebidHeader(version.Ver))
 	setBrowsingTopicsHeader(w, r)
 
-	req, impExtInfoMap, storedAuctionResponses, storedBidResponses, bidderImpReplaceImp, account, errL := deps.parseRequest(r, &labels, hookExecutor)
+	// tmax comes with the request, but when a tmax max is configured use that to prevent hanging reads.
+	parseRequestCtx := ctx
+	var parseRequestCancel context.CancelFunc
+	if deps.cfg.AuctionTimeouts.Max > 0 {
+		parseRequestTimeout := time.Duration(deps.cfg.AuctionTimeouts.Max) * time.Millisecond
+		parseRequestCtx, parseRequestCancel = context.WithDeadline(ctx, start.Add(parseRequestTimeout))
+		defer parseRequestCancel()
+	}
+	req, impExtInfoMap, storedAuctionResponses, storedBidResponses, bidderImpReplaceImp, account, errL := deps.parseRequest(
+		r.WithContext(parseRequestCtx), &labels, hookExecutor)
 	if errortypes.ContainsFatalError(errL) && writeError(errL, w, &labels) {
 		return
 	}
@@ -212,12 +222,13 @@ func (deps *endpointDeps) Auction(w http.ResponseWriter, r *http.Request, _ http
 	hookExecutor.SetActivityControl(activityControl)
 	hookExecutor.SetAccount(account)
 
-	ctx := r.Context()
 	timeout := deps.cfg.AuctionTimeouts.LimitAuctionTimeout(time.Duration(req.TMax) * time.Millisecond)
 	if timeout > 0 {
 		var cancel context.CancelFunc
 		ctx, cancel = context.WithDeadline(ctx, start.Add(timeout))
 		defer cancel()
+		hookexecution.WithContext(ctx)(hookExecutor)
+		r = r.WithContext(ctx)
 	}
 
 	// Read Usersyncs/Cookie
