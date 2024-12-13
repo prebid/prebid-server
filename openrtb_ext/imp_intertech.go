09f70731a2a750fd71cc218f7048fb659a0d0ff4
Przemysław Kaczmarek <pkaczmarek@prebid.org>
2024-12-13 14:08:24 +0000

    fix comments

--- openrtb_ext/imp_intertech.go
+++ openrtb_ext/imp_intertech.go
@@ -1,9 +1,6 @@
 package openrtb_ext
 
 type ExtImpIntertech struct {
-	PlacementID string `json:"placement_id"`
-	// Deprecated: in favor of `PlacementID`
 	PageID int64 `json:"page_id"`
-	// Deprecated: in favor of `PlacementID`
-	ImpID int64 `json:"imp_id"`
+	ImpID  int64 `json:"imp_id"`
 }
