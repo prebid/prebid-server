d233acba732af44aadcffd8e24bb59334dc07feb
Thomas Sormonte <t.sormonte@sparteo.com>
2024-12-24 11:25:19 +0100

    New Adapter: Sparteo

--- openrtb_ext/bidders.go
+++ openrtb_ext/bidders.go
@@ -222,6 +222,7 @@ var coreBidderNames []BidderName = []BidderName{
 	BidderSonobi,
 	BidderSovrn,
 	BidderSovrnXsp,
+	BidderSparteo,
 	BidderSspBC,
 	BidderStroeerCore,
 	BidderTaboola,
@@ -576,6 +577,7 @@ const (
 	BidderSonobi            BidderName = "sonobi"
 	BidderSovrn             BidderName = "sovrn"
 	BidderSovrnXsp          BidderName = "sovrnXsp"
+	BidderSparteo           BidderName = "sparteo"
 	BidderSspBC             BidderName = "sspBC"
 	BidderStroeerCore       BidderName = "stroeerCore"
 	BidderTaboola           BidderName = "taboola"
