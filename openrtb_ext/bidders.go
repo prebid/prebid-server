6ac29380b8d98374ae95c448f1283e0dfc5396fe
Oleksandr Balashov <a.m.balashov@gmail.com>
2025-02-12 19:49:48 +0200

    New Adapter: Loopme (#4023)

--- openrtb_ext/bidders.go
+++ openrtb_ext/bidders.go
@@ -153,6 +153,7 @@ var coreBidderNames []BidderName = []BidderName{
 	BidderLockerDome,
 	BidderLogan,
 	BidderLogicad,
+	BidderLoopme,
 	BidderLoyal,
 	BidderLunaMedia,
 	BidderMabidder,
@@ -504,6 +505,7 @@ const (
 	BidderLockerDome        BidderName = "lockerdome"
 	BidderLogan             BidderName = "logan"
 	BidderLogicad           BidderName = "logicad"
+	BidderLoopme            BidderName = "loopme"
 	BidderLoyal             BidderName = "loyal"
 	BidderLunaMedia         BidderName = "lunamedia"
 	BidderMabidder          BidderName = "mabidder"
