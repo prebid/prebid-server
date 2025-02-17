921044b4ffa3b62d49509b17fa19e95101215169
monica.bota@connatix.com <monica.bota@connatix.com>
2025-02-17 10:57:32 +0200

    Enhance endpoint with DC

--- adapters/connatix/connatix.go
+++ adapters/connatix/connatix.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
 	"strings"
 
 	"github.com/buger/jsonparser"
@@ -155,9 +156,37 @@ func splitRequests(imps []openrtb2.Imp, request *openrtb2.BidRequest, uri string
 			return nil, errs
 		}
 
+		endpoint, err := url.Parse(uri)
+		if err != nil {
+			errs = append(errs, err)
+			return nil, errs
+		}
+
+		if request.User != nil {
+			userID := strings.TrimSpace(request.User.BuyerUID)
+
+			if len(userID) > 0 {
+				queryParams := url.Values{}
+
+				if strings.HasPrefix(userID, "1-") {
+					queryParams.Add("dc", "us-east-2")
+				}
+
+				if strings.HasPrefix(userID, "2-") {
+					queryParams.Add("dc", "us-west-2")
+				}
+
+				if strings.HasPrefix(userID, "3-") {
+					queryParams.Add("dc", "eu-west-1")
+				}
+
+				endpoint.RawQuery = queryParams.Encode()
+			}
+		}
+
 		resArr = append(resArr, &adapters.RequestData{
 			Method:  "POST",
-			Uri:     uri,
+			Uri:     endpoint.String(),
 			Body:    reqJSON,
 			Headers: headers,
 			ImpIDs:  openrtb_ext.GetImpIDs(request.Imp),
