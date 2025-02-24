33fbff59495a068a4a470625c1e2aafcbfeeebe7
pixfuture-media <v.ioussoupov@pixfuture.com>
2025-02-24 21:39:21 +0000

    Made changes regarding GitHub reviews

--- adapters/pixfuture/pixfuture_test.go
+++ adapters/pixfuture/pixfuture_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/prebid/openrtb/v20/openrtb2"
 	"github.com/prebid/prebid-server/v3/adapters"
+	"github.com/prebid/prebid-server/v3/adapters/adapterstest"
 	"github.com/prebid/prebid-server/v3/config"
 	"github.com/prebid/prebid-server/v3/openrtb_ext"
 	"github.com/stretchr/testify/assert"
@@ -164,4 +165,16 @@ func int64Ptr(i int64) *int64 {
 
 func jsonRawExt(jsonStr string) json.RawMessage {
 	return json.RawMessage(jsonStr)
+}
+
+func TestJsonSamples(t *testing.T) {
+	bidder, buildErr := Builder(openrtb_ext.BidderPixfuture, config.Adapter{
+		Endpoint: "http://any.url"},
+		config.Server{ExternalUrl: "http://hosturl.com", GvlID: 1, DataCenter: "2"})
+
+	if buildErr != nil {
+		t.Fatalf("Builder returned unexpected error %v", buildErr)
+	}
+
+	adapterstest.RunJSONBidderTest(t, "pixfuturetest", bidder)
 }
