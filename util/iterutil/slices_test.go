6fcc22f3299f482a54a327da9175bc0a22751a2b
Sheridan C Rawlins <scr@yahooinc.com>
2025-02-25 19:45:12 -0800

    Add some iterator utilities to go with the v1.23.0 upgrade.

---
+++ util/iterutil/slices_test.go
@@ -0,0 +1,32 @@
+package iterators
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type Simple[T any] struct {
+	Value T
+}
+
+func TestSlicePointers(t *testing.T) {
+	s := []Simple[int]{{1}, {2}, {3}}
+	for i, v := range SlicePointers(s) {
+		v.Value = 99999
+		assert.EqualValues(t, 99999, s[i].Value)
+	}
+	for _, v := range s {
+		assert.EqualValues(t, 99999, v.Value)
+	}
+}
+
+func TestSlicePointerValues(t *testing.T) {
+	s := []Simple[int]{{1}, {2}, {3}}
+	for v := range SlicePointerValues(s) {
+		v.Value = 99999
+	}
+	for _, v := range s {
+		assert.EqualValues(t, 99999, v.Value)
+	}
+}
