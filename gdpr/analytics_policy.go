86da0165b366bf505943e4f98868c5e17e316ac0
bsardo <1168933+bsardo@users.noreply.github.com>
2025-03-20 11:01:44 -0400

    Update analytics policy getting GVL ID from map

--- gdpr/analytics_policy.go
+++ gdpr/analytics_policy.go
@@ -2,11 +2,9 @@ package gdpr
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/prebid/go-gdpr/api"
 	"github.com/prebid/go-gdpr/consentconstants"
-	"github.com/prebid/go-gdpr/vendorconsent"
 	tcf2 "github.com/prebid/go-gdpr/vendorconsent/tcf2"
 	"github.com/prebid/prebid-server/v3/config"
 	"github.com/prebid/prebid-server/v3/openrtb_ext"
@@ -14,7 +12,7 @@ import (
 
 // PrivacyPolicy indicates whether an analytics adapter is allowed to perform some activity
 type PrivacyPolicy interface {
-	Allow(ctx context.Context, name string, gvlID uint16) (bool, error)
+	Allow(ctx context.Context, name string) (bool, error)
 }
 
 // PrivacyPolicyBuilder is a builder function that generates an object that implements the PrivacyPolicy interface
@@ -22,7 +20,7 @@ type PrivacyPolicyBuilder func(TCF2ConfigReader, Signal, string) PrivacyPolicy
 
 // NewAnalyticsPolicyBuilder returns an PrivacyPolicyBuilder that has all global dependencies needed
 // to build a request-specific analytics privacy policy
-func NewAnalyticsPolicyBuilder(cfg config.GDPR, gvlVendorIDs map[openrtb_ext.BidderName]int16, vendorListFetcher VendorListFetcher) PrivacyPolicyBuilder {
+func NewAnalyticsPolicyBuilder(cfg config.GDPR, gvlVendorIDs map[openrtb_ext.BidderName]uint16, vendorListFetcher VendorListFetcher) PrivacyPolicyBuilder {
 	return func(tcf2Cfg TCF2ConfigReader, gdprSignal Signal, consent string) PrivacyPolicy {
 		purposeEnforcerBuilder := NewPurposeEnforcerBuilder(tcf2Cfg)
 
@@ -53,7 +51,7 @@ type analyticsPolicy struct {
 	// global
 	fetchVendorList        VendorListFetcher
 	purposeEnforcerBuilder PurposeEnforcerBuilder
-	vendorIDs              map[openrtb_ext.BidderName]int16
+	vendorIDs              map[openrtb_ext.BidderName]uint16
 	// request-specific
 	cfg        TCF2ConfigReader
 	consent    string
@@ -62,10 +60,6 @@ type analyticsPolicy struct {
 
 // Allow determines whether analytics are permitted for a given analytics module
 func (ap *analyticsPolicy) Allow(ctx context.Context, name string) (bool, error) {
-	//if vendorID, found = ap.vendorIDs[name]
-	// set the vendorID to 0 if not found
-
-
 	if ap.gdprSignal != SignalYes {
 		return true, nil
 	}
@@ -73,39 +67,23 @@ func (ap *analyticsPolicy) Allow(ctx context.Context, name string) (bool, error)
 		return ap.defaultPermissions(), nil
 	}
 
-	_, weakVendorEnforcement := ap.cfg.BasicEnforcementVendors()[name]
-	if vendorID == 0 && !weakVendorEnforcement {
-		return false, nil
-	}
-
-	parsedConsent, vendor, err := ap.parseVendor(ctx, vendorID, ap.consent)
+	pc, err := parseConsent(ap.consent)
 	if err != nil {
-		return ap.defaultPermissions(), err
+		return ap.defaultPermissions(), nil
 	}
 
-	// vendor will be nil if not a valid TCF2 consent string
-	if vendor == nil {
-		if weakVendorEnforcement && parsedConsent.Version() == 2 {
-			vendor = vendorTrue{}
-		} else {
-			return ap.defaultPermissions(), nil
-		}
-	}
+	// returns 0 if not found
+	vendorID := ap.vendorIDs[openrtb_ext.BidderName(name)]
 
-	if !ap.cfg.IsEnabled() {
-		return false, nil
-	}
-
-	consentMeta, ok := parsedConsent.(tcf2.ConsentMetadata)
-	if !ok {
-		err = fmt.Errorf("Unable to access TCF2 parsed consent")
-		return ap.defaultPermissions(), err
+	vendor, err := ap.getVendor(ctx, vendorID, *pc)
+	if err != nil {
+		return ap.defaultPermissions(), nil
 	}
 
 	enforcer := ap.purposeEnforcerBuilder(consentconstants.Purpose(7), name)
 
 	vendorInfo := VendorInfo{vendorID: vendorID, vendor: vendor}
-	return enforcer.LegalBasis(vendorInfo, name, consentMeta, Overrides{}), nil
+	return enforcer.LegalBasis(vendorInfo, name, pc.consentMeta, Overrides{}), nil
 }
 
 // defaultPermissions denies sending information to an analytics adapter when purpose 7 is
@@ -118,39 +96,13 @@ func (ap *analyticsPolicy) defaultPermissions() (allow bool) {
 	return false
 }
 
-// parseVendor parses the consent string and fetches the specified vendor's information from the GVL
-func (ap *analyticsPolicy) parseVendor(ctx context.Context, vendorID uint16, consent string) (parsedConsent api.VendorConsents, vendor api.Vendor, err error) {
-	parsedConsent, err = vendorconsent.ParseString(consent)
+// getVendor retrieves the GVL vendor information for a particular bidder
+func (ap *analyticsPolicy) getVendor(ctx context.Context, vendorID uint16, pc parsedConsent) (api.Vendor, error) {
+	vendorList, err := ap.fetchVendorList(ctx, pc.specVersion, pc.listVersion)
 	if err != nil {
-		err = &ErrorMalformedConsent{
-			Consent: consent,
-			Cause:   err,
-		}
-		return
+		return nil, err
 	}
-
-	version := parsedConsent.Version()
-	if version != 2 {
-		return
-	}
-
-	policyVersion := parsedConsent.TCFPolicyVersion()
-	specVersion, err := getSpecVersion(policyVersion)
-	if err != nil {
-		err = &ErrorMalformedConsent{
-			Consent: consent,
-			Cause:   err,
-		}
-		return
-	}
-
-	vendorList, err := ap.fetchVendorList(ctx, uint16(specVersion), parsedConsent.VendorListVersion())
-	if err != nil {
-		return
-	}
-
-	vendor = vendorList.Vendor(vendorID)
-	return
+	return vendorList.Vendor(vendorID), nil
 }
 
 // AllowAllAnalytics implements the PrivacyPolicy interface representing a policy that always
@@ -158,6 +110,6 @@ func (ap *analyticsPolicy) parseVendor(ctx context.Context, vendorID uint16, con
 type AllowAllAnalytics struct{}
 
 // Allow satisfies the PrivacyPolicy interface always returning true
-func (aaa *AllowAllAnalytics) Allow(ctx context.Context, name string, vendorID uint16) (bool, error) {
+func (aaa *AllowAllAnalytics) Allow(ctx context.Context, name string) (bool, error) {
 	return true, nil
 }
