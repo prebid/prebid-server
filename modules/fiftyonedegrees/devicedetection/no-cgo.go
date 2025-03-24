4efa55957ba76f4896b26ac0461bf8e058e805ce
Sheridan C Rawlins <scr@yahooinc.com>
2025-03-24 16:02:03 -0700

    include used packages.

--- modules/fiftyonedegrees/devicedetection/no-cgo.go
+++ modules/fiftyonedegrees/devicedetection/no-cgo.go
@@ -2,6 +2,12 @@
 
 package devicedetection
 
+import (
+	"encoding/json"
+
+	"github.com/prebid/prebid-server/v3/modules/moduledeps"
+)
+
 func Builder(_ json.RawMessage, _ moduledeps.ModuleDeps) (interface{}, error) {
 	panic("Do not enable the fiftyonedegrees module unless CGO is enabled")
 }
