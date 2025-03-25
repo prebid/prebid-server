4b8ba040420ac29f769c04c6aeeaa77930e5c93f
MadSense Ops <prebid@madsense.io>
2025-03-25 11:49:17 +0100

    pr comments

--- adapters/madsense/madsense_test.go
+++ adapters/madsense/madsense_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/prebid/prebid-server/v3/adapters/adapterstest"
 	"github.com/prebid/prebid-server/v3/config"
 	"github.com/prebid/prebid-server/v3/openrtb_ext"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
@@ -11,8 +12,6 @@ func TestJsonSamples(t *testing.T) {
 	bidder, buildErr := Builder(openrtb_ext.BidderMadSense, config.Adapter{
 		Endpoint: "https://test.localhost.com"}, config.Server{ExternalUrl: "http://hosturl.com", GvlID: 1, DataCenter: "2"})
 
-	if buildErr != nil {
-		t.Fatalf("Builder returned unexpected error %v", buildErr)
-	}
+	require.NoError(t, buildErr, "Builder returned unexpected error")
 	adapterstest.RunJSONBidderTest(t, "madsensetest", bidder)
 }
--- adapters/madsense/params_test.go
+++ adapters/madsense/params_test.go
@@ -3,6 +3,7 @@ package madsense
 import (
 	"encoding/json"
 	"github.com/prebid/prebid-server/v3/openrtb_ext"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
@@ -13,28 +14,22 @@ import (
 // TestValidParams makes sure that the madsense schema accepts all imp.ext fields which we intend to support.
 func TestValidParams(t *testing.T) {
 	validator, err := openrtb_ext.NewBidderParamsValidator("../../static/bidder-params")
-	if err != nil {
-		t.Fatalf("Failed to fetch the json-schemas. %v", err)
-	}
+	require.NoError(t, err, "Failed to fetch the json-schemas")
 
 	for _, validParam := range validParams {
-		if err := validator.Validate(openrtb_ext.BidderMadSense, json.RawMessage(validParam)); err != nil {
-			t.Errorf("Schema rejected madsense params: %s", validParam)
-		}
+		err := validator.Validate(openrtb_ext.BidderMadSense, json.RawMessage(validParam))
+		require.NoError(t, err, "Schema rejected madsense params: %s", validParam)
 	}
 }
 
 // TestInvalidParams makes sure that the madsense schema rejects all the imp.ext fields we don't support.
 func TestInvalidParams(t *testing.T) {
 	validator, err := openrtb_ext.NewBidderParamsValidator("../../static/bidder-params")
-	if err != nil {
-		t.Fatalf("Failed to fetch the json-schemas. %v", err)
-	}
+	require.NoError(t, err, "Failed to fetch the json-schemas")
 
 	for _, invalidParam := range invalidParams {
-		if err := validator.Validate(openrtb_ext.BidderMadSense, json.RawMessage(invalidParam)); err == nil {
-			t.Errorf("Schema allowed unexpected params: %s", invalidParam)
-		}
+		err := validator.Validate(openrtb_ext.BidderMadSense, json.RawMessage(invalidParam))
+		require.Error(t, err, "Schema allowed unexpected params: %s", invalidParam)
 	}
 }
 
