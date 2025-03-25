d43190ab8687634358542b9a1ffb993866de541b
Sheridan C Rawlins <scr@yahooinc.com>
2025-03-25 10:12:53 -0700

    Add a test for the Builder when CGO is not enabled.

--- modules/fiftyonedegrees/devicedetection/no-cgo.go
+++ modules/fiftyonedegrees/devicedetection/no-cgo.go
@@ -4,10 +4,13 @@ package devicedetection
 
 import (
 	"encoding/json"
+	"errors"
 
 	"github.com/prebid/prebid-server/v3/modules/moduledeps"
 )
 
+const errMsg = "fiftyonedegrees should not be enabled unless CGO is enabled"
+
 func Builder(_ json.RawMessage, _ moduledeps.ModuleDeps) (interface{}, error) {
-	panic("Do not enable the fiftyonedegrees module unless CGO is enabled")
+	return nil, errors.New(errMsg)
 }
