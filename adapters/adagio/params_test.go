c55b5659b80e090b768e217423471ddc7f639b69
Godefroi Roussel <groussel@adagio.io>
2025-04-30 15:17:11 +0200

    Adagio: Remove unnecessary comments

--- adapters/adagio/params_test.go
+++ adapters/adagio/params_test.go
@@ -7,11 +7,13 @@ import (
 	"github.com/prebid/prebid-server/v3/openrtb_ext"
 )
 
-// This file actually intends to test static/bidder-params/pubmatic.json
-//
-// These also validate the format of the external API: request.imp[i].ext.prebid.bidder.pubmatic
+var validParams = []string{
+	`{"organizationId":"1000","placement":"some-placement"}`,
+	`{"organizationId":"1000","placement":"some-placement","pagetype":"some-pagetype"}`,
+	`{"organizationId":"1000","placement":"some-placement","category":"some-category"}`,
+	`{"organizationId":"1000","placement":"some-placement","pagetype":"some-pagetype","category":"some-category"}`,
+}
 
-// TestValidParams makes sure that the adagio schema accepts all imp.ext fields which we intend to support.
 func TestValidParams(t *testing.T) {
 	validator, err := openrtb_ext.NewBidderParamsValidator("../../static/bidder-params")
 	if err != nil {
@@ -23,28 +25,6 @@ func TestValidParams(t *testing.T) {
 			t.Errorf("Schema rejected adagio params: %s \n Error: %s", validParam, err)
 		}
 	}
-}
-
-// TestInvalidParams makes sure that the pubmatic schema rejects all the imp.ext fields we don't support.
-func TestInvalidParams(t *testing.T) {
-	validator, err := openrtb_ext.NewBidderParamsValidator("../../static/bidder-params")
-	if err != nil {
-		t.Fatalf("Failed to fetch the json-schemas. %v", err)
-	}
-
-	for _, invalidParam := range invalidParams {
-		if err := validator.Validate(openrtb_ext.BidderAdagio, json.RawMessage(invalidParam)); err == nil {
-			t.Errorf("Schema allowed unexpected params: %s", invalidParam)
-		}
-	}
-}
-
-var validParams = []string{
-	`{"organizationId":"1000","placement":"some-placement"}`,
-	`{"organizationId":"1000","placement":"some-placement"}`,
-	`{"organizationId":"1000","placement":"some-placement","pagetype":"some-pagetype"}`,
-	`{"organizationId":"1000","placement":"some-placement","category":"some-category"}`,
-	`{"organizationId":"1000","placement":"some-placement","pagetype":"some-pagetype","category":"some-category"}`,
 }
 
 var invalidParams = []string{
@@ -59,7 +39,7 @@ var invalidParams = []string{
 	`{"placement":"some-placement"}`,
 	`{"category":"some-category"}`,
 	`{"pagetype":"some-pagetype"}`,
-	`{"organizationId":"1000"}`,
+	`{"organizationId":1000}`,
 	`{"organizationId":1000,"placement":"some-placement"}`,
 	`{"organizationId":"1000","placement":"this-is-a-very-very-long-placement"}`,
 	`{"organizationId":"1000","placement":123456}`,
@@ -67,4 +47,17 @@ var invalidParams = []string{
 	`{"organizationId":"1000","placement":"some-placement","pagetype":"this-is-a-very-very-long-pagetype"}`,
 	`{"organizationId":"1000","placement":"some-placement","category":123456}`,
 	`{"organizationId":"1000","placement":"some-placement","category":"this-is-a-very-very-long-category"}`,
+}
+
+func TestInvalidParams(t *testing.T) {
+	validator, err := openrtb_ext.NewBidderParamsValidator("../../static/bidder-params")
+	if err != nil {
+		t.Fatalf("Failed to fetch the json-schemas. %v", err)
+	}
+
+	for _, invalidParam := range invalidParams {
+		if err := validator.Validate(openrtb_ext.BidderAdagio, json.RawMessage(invalidParam)); err == nil {
+			t.Errorf("Schema allowed unexpected params: %s", invalidParam)
+		}
+	}
 }
--- openrtb_ext/imp_adagio.go
+++ openrtb_ext/imp_adagio.go
@@ -1,6 +1,5 @@
 package openrtb_ext
 
-// ExtImpAdagio defines the contract for bidrequest.imp[i].ext.prebid.bidder for Adagio
 type ExtImpAdagio struct {
 	OrganizationID string `json:"organizationId"`
 	Placement      string `json:"placement"`
