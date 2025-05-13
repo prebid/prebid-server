250f5bfa8f16f810fe2435aafde58b91337aceee
guscarreon <guscarreon@gmail.com>
2025-05-13 15:41:33 -0400

    accountID an alias for string

--- modules/prebid/rulesengine/cache.go
+++ modules/prebid/rulesengine/cache.go
@@ -5,12 +5,12 @@ import (
 	"sync/atomic"
 )
 
-type accountID string
+type accountID = string
 
 type cacher interface {
 	Get(string) *cacheEntry
 	Set(string, *cacheEntry)
-	Delete(id string)
+	Delete(id accountID)
 }
 
 type cache struct {
@@ -28,9 +28,9 @@ func NewCache() *cache {
 }
 
 // Get has been implemented to read from the cache without further synchronization
-func (c *cache) Get(id string) *cacheEntry {
+func (c *cache) Get(id accountID) *cacheEntry {
 	m1 := c.m.Load().(map[accountID]*cacheEntry)
-	if cachedObj, exists := m1[accountID(id)]; exists {
+	if cachedObj, exists := m1[id]; exists {
 		return cachedObj
 	}
 	return nil
@@ -38,31 +38,33 @@ func (c *cache) Get(id string) *cacheEntry {
 
 // Set stores the data parameter using the id parameter as key in a thread-safe manner. If data
 // is already found under the id key, this function swaps its value with the data parameter
-func (c *cache) Set(id string, data *cacheEntry) {
+func (c *cache) Set(id accountID, data *cacheEntry) {
+	if len(id) == 0 {
+		return
+	}
+
 	c.Lock()
 	defer c.Unlock()
 
 	m1 := c.m.Load().(map[accountID]*cacheEntry)
 	m2 := make(map[accountID]*cacheEntry)
 
-	// Copy map onto another map to make it thread-safe
+	// Copy all values that are not under key id (if id exists in this map)
 	for k, v := range m1 {
-		// if id exists in our cache, we'll substitute v for the data param at the end
-		if k == accountID(id) {
-			continue
+		if k != id {
+			m2[k] = v
 		}
-		m2[k] = v
 	}
 
 	// Set new value under account id even if it already exists
-	m2[accountID(id)] = data
+	m2[id] = data
 
 	c.m.Store(m2)
 	return
 }
 
 // Delete removes a cached object without further synchronization
-func (c *cache) Delete(id string) {
+func (c *cache) Delete(id accountID) {
 	if len(id) == 0 {
 		return
 	}
@@ -70,7 +72,7 @@ func (c *cache) Delete(id string) {
 	c.Lock()
 	defer c.Unlock()
 	m1 := c.m.Load().(map[accountID]*cacheEntry)
-	if _, exists := m1[accountID(id)]; !exists {
+	if _, exists := m1[id]; !exists {
 		return
 	}
 
@@ -78,10 +80,9 @@ func (c *cache) Delete(id string) {
 	m2 := make(map[accountID]*cacheEntry)
 	for k, v := range m1 {
 		// skip the element we want to delete
-		if k == accountID(id) {
-			continue
+		if k != id {
+			m2[k] = v
 		}
-		m2[k] = v
 	}
 
 	c.m.Store(m2)
