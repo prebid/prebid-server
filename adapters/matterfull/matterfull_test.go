93e1c5f886eccbda1d3e6adf4a5b1ae266b38047
Matterfull <prebid_support@bematterfull.com>
2025-05-19 12:54:51 +0300

    Fix: resolve test case failures

--- adapters/matterfull/matterfull_test.go
+++ adapters/matterfull/matterfull_test.go
@@ -11,7 +11,7 @@ import (
 
 func TestJsonSamples(t *testing.T) {
 	bidder, buildErr := Builder(openrtb_ext.BidderMatterfull, config.Adapter{
-		Endpoint: "http://us.matterfull.co/?pubid={{.PublisherID}}"}, config.Server{ExternalUrl: "http://hosturl.com", GvlID: 1, DataCenter: "2"})
+		Endpoint: "http://us.matterfull.co/?uqhash={{.PublisherID}}"}, config.Server{ExternalUrl: "http://hosturl.com", GvlID: 1, DataCenter: "2"})
 
 	if buildErr != nil {
 		t.Fatalf("Builder returned unexpected error %v", buildErr)
