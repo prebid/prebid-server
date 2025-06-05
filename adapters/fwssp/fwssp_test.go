e9795f323be7add08eb042f4f585cf87de0bf4ab
ghluo-fw <ghluo@freewheel.com>
2025-06-05 11:54:28 +0800

    New Adapter: fwssp (#4309)

---
+++ adapters/fwssp/fwssp_test.go
@@ -0,0 +1,20 @@
+package fwssp
+
+import (
+	"testing"
+
+	"github.com/prebid/prebid-server/v3/adapters/adapterstest"
+	"github.com/prebid/prebid-server/v3/config"
+	"github.com/prebid/prebid-server/v3/openrtb_ext"
+)
+
+func TestJsonSamples(t *testing.T) {
+	bidder, buildErr := Builder(openrtb_ext.BidderFWSSP, config.Adapter{
+		Endpoint: "https://testfreewheel.com"}, config.Server{ExternalUrl: "http://hosturl.com", GvlID: 1, DataCenter: "2"})
+
+	if buildErr != nil {
+		t.Fatalf("Builder returned unexpected error %v", buildErr)
+	}
+
+	adapterstest.RunJSONBidderTest(t, "fwssptest", bidder)
+}
