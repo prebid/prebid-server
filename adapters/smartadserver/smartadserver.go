63bb616678791974a7ca45f3a05d70d78a9d3295
nlesommer <nlesommer@equativ.com>
2025-06-16 11:48:45 +0200

    PIPS-3156 update comment

--- adapters/smartadserver/smartadserver.go
+++ adapters/smartadserver/smartadserver.go
@@ -85,7 +85,7 @@ func (a *SmartAdserverAdapter) MakeRequests(request *openrtb2.BidRequest, reqInf
 		smartRequest.Site.Publisher.ID = strconv.Itoa(smartadserverExt.NetworkID)
 	}
 
-	// we only create the request if it has correctly formatted impressions
+	// we only create the request if it has at least one correctly formatted impressions
 	if len(imps) != 0 {
 		smartRequest.Imp = imps
 
