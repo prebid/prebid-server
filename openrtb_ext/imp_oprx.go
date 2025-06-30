8bec429edada03df2518ec606add3345eff6b44c
Ivan Zivkovic <izivkovic@optimizerx.com>
2025-06-30 10:28:35 +0200

    fixed types in imp_oprx

--- openrtb_ext/imp_oprx.go
+++ openrtb_ext/imp_oprx.go
@@ -2,10 +2,10 @@ package openrtb_ext
 
 type ImpExtOprx struct {
 	Key         string `json:"key"`
-	PlacementID string `json:"placement_id"`
-	Width       string `json:"width"`
-	Height      string `json:"height"`
-	BidFloor    string `json:"bid_floor"`
+	PlacementID int `json:"placement_id"`
+	Width       int `json:"width"`
+	Height      int `json:"height"`
+	BidFloor    float64  `json:"bid_floor"`
 	Npi         string `json:"npi"`
 	Ndc         string `json:"ndc"`
 }
