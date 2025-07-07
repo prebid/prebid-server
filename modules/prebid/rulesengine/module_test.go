d10d600a6210124f0837a8a219ddb5c250343b0e
VeronikaSolovei9 <kalypsonika@gmail.com>
2025-07-07 13:54:55 -0400

    Added more unit tests

--- modules/prebid/rulesengine/module_test.go
+++ modules/prebid/rulesengine/module_test.go
@@ -45,7 +45,9 @@ func (mt mockTimeUtil) Now() time.Time {
 }
 
 var sampleJsonConfig json.RawMessage = json.RawMessage(`{"enabled": true, "ruleSets": []}`)
-var sampleJsonConfigWithUpdateFrequency json.RawMessage = json.RawMessage(`{"enabled": true, "updatetreefrequencyminutes": 1, "ruleSets": []}`)
+var incorrectJsonConfig json.RawMessage = json.RawMessage(`{"enabled": true, "updatetreefrequencyminutes": [[]], "ruleSets": []}`)
+var sampleJsonConfigWithUpdateFrequency1 json.RawMessage = json.RawMessage(`{"enabled": true, "updatetreefrequencyminutes": 1, "ruleSets": []}`)
+var sampleJsonConfigWithUpdateFrequency0 json.RawMessage = json.RawMessage(`{"enabled": true, "updatetreefrequencyminutes": 0, "ruleSets": []}`)
 
 func TestConfigChanged(t *testing.T) {
 
@@ -89,17 +91,18 @@ func TestRebuildTrees(t *testing.T) {
 		inCacheEntry   *cacheEntry
 		inJsonConfig   *json.RawMessage
 		expectedResult bool
+		expectErr      bool
 	}{
 		{
 			name: "non_expired_cache_entry_so_no_rebuild",
 			inCacheEntry: &cacheEntry{
 				timestamp: time.Date(2050, 1, 1, 0, 0, 0, 0, time.UTC),
 			},
-			inJsonConfig:   &sampleJsonConfigWithUpdateFrequency,
+			inJsonConfig:   &sampleJsonConfigWithUpdateFrequency1,
 			expectedResult: false,
 		},
 		{
-			name: "expired_entry_but_same_config_and_no_update_so_no_rebuild",
+			name: "expired_entry_but_same_config_and_default_no_update_so_no_rebuild",
 			inCacheEntry: &cacheEntry{
 				timestamp:    time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC),
 				hashedConfig: "e21c19982a618f9dd3286fc2eb08dad62a1e9ee81d51ffa94b267ab2e3813964",
@@ -108,21 +111,44 @@ func TestRebuildTrees(t *testing.T) {
 			expectedResult: false,
 		},
 		{
+			name: "expired_entry_but_same_config_and_zero_minutes_update_so_no_rebuild",
+			inCacheEntry: &cacheEntry{
+				timestamp:    time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC),
+				hashedConfig: "e21c19982a618f9dd3286fc2eb08dad62a1e9ee81d51ffa94b267ab2e3813964",
+			},
+			inJsonConfig:   &sampleJsonConfigWithUpdateFrequency0,
+			expectedResult: false,
+		},
+		{
 			name: "expired_entry_and_different_config_so_rebuild",
 			inCacheEntry: &cacheEntry{
 				timestamp:    time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC),
 				hashedConfig: "oldHash",
 			},
-			inJsonConfig:   &sampleJsonConfigWithUpdateFrequency,
+			inJsonConfig:   &sampleJsonConfigWithUpdateFrequency1,
 			expectedResult: true,
+		},
+		{
+			name: "incorrect_json_config_should_return_error",
+			inCacheEntry: &cacheEntry{
+				timestamp:    time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC),
+				hashedConfig: "oldHash",
+			},
+			inJsonConfig:   &incorrectJsonConfig,
+			expectedResult: false,
+			expectErr:      true,
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			res, err := rebuildTrees(tc.inCacheEntry, tc.inJsonConfig)
-			assert.NoError(t, err)
 			assert.Equal(t, tc.expectedResult, res)
+			if tc.expectErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
 		})
 	}
 }
