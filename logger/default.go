41fae3da87ef39f6c8a2f2d075f177eb508170be
Sheridan C Rawlins <scr@yahooinc.com>
2025-07-21 09:25:56 -0700

    Added StructuredLogger interface.

--- logger/default.go
+++ logger/default.go
@@ -1,12 +1,16 @@
 package logger
 
 import (
+	"context"
+
 	"github.com/golang/glog"
 )
 
 type GlogLogger struct {
 	depth int
 }
+
+var _ Logger = (*GlogLogger)(nil)
 
 func (logger *GlogLogger) Info(args ...any) {
 	glog.InfoDepth(logger.depth, args...)
@@ -42,6 +46,40 @@ func (logger *GlogLogger) Fatal(args ...any) {
 
 func (logger *GlogLogger) Fatalf(format string, args ...any) {
 	glog.FatalDepthf(logger.depth, format, args...)
+}
+
+func (logger *GlogLogger) Debugs(msg string, args ...any) {
+	// glog doesn't have Debug; just call info
+	logger.Infos(msg, args...)
+}
+
+func (logger *GlogLogger) DebugsContext(ctx context.Context, msg string, args ...any) {
+	// glog doesn't have Debug; just call info
+	logger.InfosContext(ctx, msg, args...)
+}
+
+func (logger *GlogLogger) Infos(msg string, args ...any) {
+	glog.Info(append([]any{msg}, args...)...)
+}
+
+func (logger *GlogLogger) InfosContext(ctx context.Context, msg string, args ...any) {
+	glog.InfoContext(ctx, append([]any{msg}, args...)...)
+}
+
+func (logger *GlogLogger) Warns(msg string, args ...any) {
+	glog.Warning(append([]any{msg}, args...)...)
+}
+
+func (logger *GlogLogger) WarnsContext(ctx context.Context, msg string, args ...any) {
+	glog.WarningContext(ctx, append([]any{msg}, args...)...)
+}
+
+func (logger *GlogLogger) Errors(msg string, args ...any) {
+	glog.Error(append([]any{msg}, args...)...)
+}
+
+func (logger *GlogLogger) ErrorsContext(ctx context.Context, msg string, args ...any) {
+	glog.ErrorContext(ctx, append([]any{msg}, args...)...)
 }
 
 func NewDefaultLogger(depth int) Logger {
--- logger/interface.go
+++ logger/interface.go
@@ -1,17 +1,38 @@
 package logger
 
-type Logger interface {
-	Info(args ...any)
-	Infof(format string, args ...any)
+import "context"
 
-	Warning(args ...any)
-	Warningf(format string, args ...any)
+type (
+	PrintfLogger interface {
+		Info(args ...any)
+		Infof(format string, args ...any)
 
-	Error(args ...any)
-	Errorf(format string, args ...any)
+		Warning(args ...any)
+		Warningf(format string, args ...any)
 
-	Exitf(format string, args ...any)
+		Error(args ...any)
+		Errorf(format string, args ...any)
 
-	Fatal(args ...any)
-	Fatalf(format string, args ...any)
-}
+		Exitf(format string, args ...any)
+
+		Fatal(args ...any)
+		Fatalf(format string, args ...any)
+	}
+	StructuredLogger interface {
+		Debugs(msg string, args ...any)
+		DebugsContext(ctx context.Context, msg string, args ...any)
+
+		Infos(msg string, args ...any)
+		InfosContext(ctx context.Context, msg string, args ...any)
+
+		Warns(msg string, args ...any)
+		WarnsContext(ctx context.Context, msg string, args ...any)
+
+		Errors(msg string, args ...any)
+		ErrorsContext(ctx context.Context, msg string, args ...any)
+	}
+	Logger interface {
+		PrintfLogger
+		StructuredLogger
+	}
+)
