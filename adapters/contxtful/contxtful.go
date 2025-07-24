0236690dc6ab109b595391e2cef43154611c1b1b
rufiange <sebastien@rufiange.com>
2025-07-24 15:26:03 -0400

    refactor: use http.MethodPost

--- adapters/contxtful/contxtful.go
+++ adapters/contxtful/contxtful.go
@@ -123,7 +123,7 @@ func (a *adapter) MakeRequests(request *openrtb2.BidRequest, reqInfo *adapters.E
 	}
 
 	requestData := &adapters.RequestData{
-		Method:  "POST",
+		Method:  http.MethodPost,
 		Uri:     endpoint,
 		Body:    requestJSON,
 		Headers: headers,
