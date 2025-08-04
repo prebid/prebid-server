1f0720c46e096575ca70163eb44de96097bf94b0
rbstdev <devgit204@xe.works>
2025-08-04 17:29:05 +0300

    added alias robustApps for xeworks adapter

--- openrtb_ext/bidders.go
+++ openrtb_ext/bidders.go
@@ -207,7 +207,6 @@ var coreBidderNames []BidderName = []BidderName{
 	BidderRevcontent,
 	BidderRichaudience,
 	BidderRise,
-	BidderRobustApps,
 	BidderRoulax,
 	BidderRTBHouse,
 	BidderRubicon,
@@ -571,7 +570,6 @@ const (
 	BidderRevcontent        BidderName = "revcontent"
 	BidderRichaudience      BidderName = "richaudience"
 	BidderRise              BidderName = "rise"
-	BidderRobustApps        BidderName = "robustApps"
 	BidderRoulax            BidderName = "roulax"
 	BidderRTBHouse          BidderName = "rtbhouse"
 	BidderRubicon           BidderName = "rubicon"
