13eeb29fd8f9c0d3f3c059bf8602f119b1e91143
Karim Mourra <karim@jwplayer.com>
2025-09-04 16:50:21 -0300

    adds test case for missing gpid

--- adapters/connatix/connatix.go
+++ adapters/connatix/connatix.go
@@ -227,7 +227,7 @@ func buildRequestImp(imp *openrtb2.Imp, ext impExtIncoming, displayManagerVer st
 			PlacementId:           ext.Bidder.PlacementId,
 			ViewabilityPercentage: ext.Bidder.ViewabilityPercentage,
 		},
-		Gpid: ext.Gpid,
+		GPID: ext.GPID,
 	}
 
 	var err error
--- adapters/connatix/models.go
+++ adapters/connatix/models.go
@@ -10,12 +10,12 @@ type adapter struct {
 
 type impExtIncoming struct {
 	Bidder openrtb_ext.ExtImpConnatix `json:"bidder"`
-	Gpid   string                     `json:"gpid"`
+	GPID   string                     `json:"gpid,omitempty"`
 }
 
 type impExt struct {
 	Connatix impExtConnatix `json:"connatix"`
-	Gpid     string         `json:"gpid"`
+	GPID     string         `json:"gpid"`
 }
 
 type impExtConnatix struct {
