b896d75e10c982a408d4c7747bb3a4d7cb587536
Filip Stamenkovic <filip.stamenkovic@showheroes-group.com>
2025-10-16 14:38:19 +0200

    remove comments and redundant tests

--- adapters/showheroes/showheroes_test.go
+++ adapters/showheroes/showheroes_test.go
@@ -2,21 +2,17 @@ package showheroes
 
 import (
 	"encoding/json"
-	"fmt"
-	"net/http"
 	"testing"
 
 	"github.com/prebid/openrtb/v20/openrtb2"
 	"github.com/prebid/prebid-server/v3/adapters"
 	"github.com/prebid/prebid-server/v3/adapters/adapterstest"
 	"github.com/prebid/prebid-server/v3/config"
-	"github.com/prebid/prebid-server/v3/errortypes"
 	"github.com/prebid/prebid-server/v3/openrtb_ext"
 	"github.com/prebid/prebid-server/v3/util/jsonutil"
 	"github.com/stretchr/testify/assert"
 )
 
-// Mock currency conversion
 type mockCurrencyConversion struct {
 	rate float64
 }
@@ -102,7 +98,7 @@ func TestShowheroesAdapter_MakeRequests(t *testing.T) {
 			name: "site_without_page",
 			args: args{
 				request: &openrtb2.BidRequest{
-					Site: &openrtb2.Site{}, // Missing Page URL
+					Site: &openrtb2.Site{},
 					Imp: []openrtb2.Imp{
 						{
 							Banner: &openrtb2.Banner{},
@@ -118,7 +114,7 @@ func TestShowheroesAdapter_MakeRequests(t *testing.T) {
 			name: "app_without_bundle",
 			args: args{
 				request: &openrtb2.BidRequest{
-					App: &openrtb2.App{}, // Missing Bundle ID
+					App: &openrtb2.App{},
 					Imp: []openrtb2.Imp{
 						{
 							Banner: &openrtb2.Banner{},
@@ -179,7 +175,7 @@ func TestShowheroesAdapter_MakeRequests(t *testing.T) {
 				},
 				reqInfo: &adapters.ExtraRequestInfo{
 					CurrencyConversions: mockCurrencyConversion{
-						rate: 0.85, // USD to EUR
+						rate: 0.85,
 					},
 				},
 			},
@@ -204,7 +200,6 @@ func TestShowheroesAdapter_MakeRequests(t *testing.T) {
 				err := json.Unmarshal(gotReqData[0].Body, &processedRequest)
 				assert.NoError(t, err)
 
-				// Check if imps have been properly processed
 				if len(processedRequest.Imp) > 0 {
 					if processedRequest.Imp[0].BidFloorCur != "" {
 						assert.Equal(t, "EUR", processedRequest.Imp[0].BidFloorCur)
@@ -224,146 +219,6 @@ func TestJsonSamples(t *testing.T) {
 	}
 
 	adapterstest.RunJSONBidderTest(t, "showheroestest", bidder)
-}
-
-func TestShowheroesAdapter_MakeBids(t *testing.T) {
-	type args struct {
-		internalRequest *openrtb2.BidRequest
-		externalRequest *adapters.RequestData
-		response        *adapters.ResponseData
-	}
-	tests := []struct {
-		name     string
-		args     args
-		wantErr  []error
-		wantResp *adapters.BidderResponse
-	}{
-		{
-			name: "happy_path_banner",
-			args: args{
-				response: &adapters.ResponseData{
-					StatusCode: http.StatusOK,
-					Body:       []byte(`{"id": "test-request-id", "seatbid":[{"seat": "showheroes", "bid":[{"mtype": 1, "id": "bid-id-1", "impid": "test-imp-id", "price": 0.50, "adid": "ad-id-1", "adm": "some-banner-ad", "adomain":["showheroes.com"], "crid": "creative-id-1", "h": 250, "w": 300, "dealid": "deal-id-1"}]}], "bidid": "bid-request-1", "cur": "EUR"}`),
-				},
-			},
-			wantErr: nil,
-			wantResp: &adapters.BidderResponse{
-				Bids: []*adapters.TypedBid{
-					{
-						Bid: &openrtb2.Bid{
-							ID:      "bid-id-1",
-							ImpID:   "test-imp-id",
-							Price:   0.50,
-							AdID:    "ad-id-1",
-							AdM:     "some-banner-ad",
-							ADomain: []string{"showheroes.com"},
-							CrID:    "creative-id-1",
-							H:       250,
-							W:       300,
-							DealID:  "deal-id-1",
-							MType:   openrtb2.MarkupBanner,
-						},
-						BidType: openrtb_ext.BidTypeBanner,
-					},
-				},
-				Currency: "EUR",
-			},
-		},
-		{
-			name: "happy_path_video",
-			args: args{
-				response: &adapters.ResponseData{
-					StatusCode: http.StatusOK,
-					Body:       []byte(`{"id": "test-request-id", "seatbid":[{"seat": "showheroes", "bid":[{"mtype": 2, "id": "bid-id-2", "impid": "test-imp-id", "price": 1.50, "adid": "ad-id-2", "adm": "some-video-ad", "adomain":["showheroes.com"], "crid": "creative-id-2", "h": 480, "w": 640}]}], "bidid": "bid-request-2", "cur": "USD"}`),
-				},
-			},
-			wantErr: nil,
-			wantResp: &adapters.BidderResponse{
-				Bids: []*adapters.TypedBid{
-					{
-						Bid: &openrtb2.Bid{
-							ID:      "bid-id-2",
-							ImpID:   "test-imp-id",
-							Price:   1.50,
-							AdID:    "ad-id-2",
-							AdM:     "some-video-ad",
-							ADomain: []string{"showheroes.com"},
-							CrID:    "creative-id-2",
-							H:       480,
-							W:       640,
-							MType:   openrtb2.MarkupVideo,
-						},
-						BidType: openrtb_ext.BidTypeVideo,
-					},
-				},
-				Currency: "USD",
-			},
-		},
-		{
-			name: "no_content_response",
-			args: args{
-				response: &adapters.ResponseData{
-					StatusCode: http.StatusNoContent,
-					Body:       nil,
-				},
-			},
-			wantErr:  nil,
-			wantResp: nil,
-		},
-		{
-			name: "invalid_status_code",
-			args: args{
-				response: &adapters.ResponseData{
-					StatusCode: http.StatusBadRequest,
-					Body:       []byte(`Bad Request`),
-				},
-			},
-			wantErr:  []error{fmt.Errorf("unexpected status code: 400")},
-			wantResp: nil,
-		},
-		{
-			name: "invalid_json_response",
-			args: args{
-				response: &adapters.ResponseData{
-					StatusCode: http.StatusOK,
-					Body:       []byte(`{"id": "test-request-id", "seatbid":[{"seat": "showheroes", "bid":[{]}`),
-				},
-			},
-			wantErr:  []error{&errortypes.FailedToUnmarshal{}},
-			wantResp: nil,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			a, buildErr := Builder(openrtb_ext.BidderShowheroes, config.Adapter{
-				Endpoint: "https://bid.showheroes.com/api/v1/bid",
-			}, config.Server{})
-			assert.Nil(t, buildErr)
-
-			gotResp, gotErr := a.MakeBids(tt.args.internalRequest, tt.args.externalRequest, tt.args.response)
-
-			if tt.wantErr == nil {
-				assert.Equal(t, tt.wantErr, gotErr)
-			} else if len(tt.wantErr) > 0 {
-				assert.NotNil(t, gotErr)
-				if len(gotErr) > 0 && len(tt.wantErr) > 0 {
-					assert.IsType(t, tt.wantErr[0], gotErr[0])
-				}
-			}
-
-			if tt.wantResp != nil {
-				assert.Equal(t, tt.wantResp.Currency, gotResp.Currency)
-				assert.Equal(t, len(tt.wantResp.Bids), len(gotResp.Bids))
-				if len(tt.wantResp.Bids) > 0 {
-					assert.Equal(t, tt.wantResp.Bids[0].BidType, gotResp.Bids[0].BidType)
-					assert.Equal(t, tt.wantResp.Bids[0].Bid.ID, gotResp.Bids[0].Bid.ID)
-					assert.Equal(t, tt.wantResp.Bids[0].Bid.MType, gotResp.Bids[0].Bid.MType)
-				}
-			} else if tt.wantResp == nil && tt.args.response.StatusCode == http.StatusNoContent {
-				assert.Nil(t, gotResp)
-			}
-		})
-	}
 }
 
 func TestPbsSourceExt(t *testing.T) {
--- openrtb_ext/imp_showheroes.go
+++ openrtb_ext/imp_showheroes.go
@@ -1,6 +1,5 @@
 package openrtb_ext
 
-// ExtImpShowheroes defines the contract for bidrequest.imp[i].ext.prebid.bidder.showheroes
 type ExtImpShowheroes struct {
 	UnitID string `json:"unitId"`
 }
