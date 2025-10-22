db6bc4e0c29fa90444e3019b3d8d52432927a16a
Jean BOUDET <jboudet+dev@protonmail.com>
2025-10-22 16:01:15 +0200

    feat: add imp_beop.go

---
+++ openrtb_ext/imp_beop.go
@@ -0,0 +1,7 @@
+package openrtb_ext
+
+type ExtImpBeop struct {
+	BeopPublisherID      string `json:"pid,omitempty"`
+	BeopNetworkID        string `json:"nid,omitempty"`
+	BeopNetworkPartnerID string `json:"nptnid,omitempty"`
+}
