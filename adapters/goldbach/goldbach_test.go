e297fc7b029589ed8f1ad268839d86a944c44ea4
Simon Aebli <simon.aebli@goldbach.com>
2025-10-28 19:42:35 +0100

    New Adapter: Goldbach (#4476)

---
+++ adapters/goldbach/goldbach_test.go
@@ -0,0 +1,28 @@
+package goldbach
+
+import (
+	"testing"
+
+	"github.com/prebid/prebid-server/v3/adapters/adapterstest"
+	"github.com/prebid/prebid-server/v3/config"
+	"github.com/prebid/prebid-server/v3/openrtb_ext"
+	"github.com/stretchr/testify/require"
+)
+
+func TestJsonSamples(t *testing.T) {
+	bidder, buildErr := Builder(
+		openrtb_ext.BidderGoldbach,
+		config.Adapter{
+			Endpoint: "https://gold.bach/prebid/adapter-endpoint",
+		},
+		config.Server{
+			ExternalUrl: "http://hosturl.com",
+			GvlID:       1,
+			DataCenter:  "2",
+		},
+	)
+
+	require.NoError(t, buildErr)
+
+	adapterstest.RunJSONBidderTest(t, "goldbachtest", bidder)
+}
