048d8098bb001eb534472b4c76474c96a2f01811
postindustria-code <oss@postindustria.com>
2025-10-30 11:30:19 +0200

    [Issue 4085] - Updated doc comments in logger interface

--- logger/interface.go
+++ logger/interface.go
@@ -1,18 +1,18 @@
 package logger
 
 type Logger interface {
-	// Debug level logging
+	// Debugf level logging
 	Debugf(msg string, args ...any)
 
-	// Info level logging
+	// Infof level logging
 	Infof(msg string, args ...any)
 
-	// Warn level logging
+	// Warnf level logging
 	Warnf(msg string, args ...any)
 
-	// Error level logging
+	// Errorf level logging
 	Errorf(msg string, args ...any)
 
-	// Fatal level logging
+	// Fatalf level logging
 	Fatalf(msg string, args ...any)
 }
