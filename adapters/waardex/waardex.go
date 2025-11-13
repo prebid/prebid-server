de150e546b003786419e3db6881d4501b5e2a87f
Yegor Serdiuk <yegorheiz@gmail.com>
2025-11-13 11:00:42 +0100

    Refactor Waardex MakeRequests to ignore unused reqInfo argument

--- adapters/waardex/waardex.go
+++ adapters/waardex/waardex.go
@@ -21,7 +21,7 @@ type waardexAdapter struct {
 }
 
 // MakeRequests prepares request information for prebid-server core
-func (adapter *waardexAdapter) MakeRequests(request *openrtb2.BidRequest, reqInfo *adapters.ExtraRequestInfo) ([]*adapters.RequestData, []error) {
+func (adapter *waardexAdapter) MakeRequests(request *openrtb2.BidRequest, _ *adapters.ExtraRequestInfo) ([]*adapters.RequestData, []error) {
 	errs := make([]error, 0, len(request.Imp))
 	if len(request.Imp) == 0 {
 		errs = append(errs, newBadInputError("No impression in the bid request"))
