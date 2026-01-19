9ae372efb5eb3f957720f5d72c3394ab4c361af7
Pavel Ptashyts <pavel.p@geomotiv.com>
2026-01-19 12:06:32 +0100

    Update Adoppler/ElementaTv add params_test.go

---
+++ adapters/elementaltv/params_test.go
@@ -0,0 +1,45 @@
+package elementaltv
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/prebid/prebid-server/v3/openrtb_ext"
+)
+
+func TestValidParams(t *testing.T) {
+	validator, err := openrtb_ext.NewBidderParamsValidator("../../static/bidder-params")
+	if err != nil {
+		t.Fatalf("Failed to fetch the json schema: %v", err)
+	}
+
+	for _, p := range validParams {
+		if err := validator.Validate(openrtb_ext.BidderFeedAd, json.RawMessage(p)); err != nil {
+			t.Errorf("Schema rejected valid params: %s", p)
+		}
+	}
+}
+
+func TestInvalidParams(t *testing.T) {
+	validator, err := openrtb_ext.NewBidderParamsValidator("../../static/bidder-params")
+	if err != nil {
+		t.Fatalf("Failed to fetch the json schema: %v", err)
+	}
+
+	for _, p := range invalidParams {
+		if err := validator.Validate(openrtb_ext.BidderFeedAd, json.RawMessage(p)); err == nil {
+			t.Errorf("Schema allowed invalid params: %s", p)
+		}
+	}
+}
+
+var validParams = []string{
+	`{"adunit":"123"}`,
+	`{"adunit":"SSP:123"}`,
+	`{"adunit":"5346"}`}
+
+var invalidParams = []string{
+	`{}`,
+	`{"adunit":""}`,
+	`{"adunitt":"123"}`,
+}
