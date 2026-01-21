ab7c2e8d5453197d66a1e8d258f03407fa99ad31
Yegor Serdiuk <yegorheiz@gmail.com>
2026-01-21 08:41:09 +0100

    Add unit tests for Waardex to validate bidder parameter schema

---
+++ adapters/waardex/params_test.go
@@ -0,0 +1,62 @@
+package waardex
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/prebid/prebid-server/v3/openrtb_ext"
+)
+
+func TestValidParams(t *testing.T) {
+	validator, err := openrtb_ext.NewBidderParamsValidator("../../static/bidder-params")
+	if err != nil {
+		t.Fatalf("Failed to fetch the json schema. %v", err)
+	}
+
+	for _, p := range validParams {
+		if err := validator.Validate(openrtb_ext.BidderWaardex, json.RawMessage(p)); err != nil {
+			t.Errorf("Schema rejected valid params: %s", p)
+		}
+	}
+}
+
+func TestInvalidParams(t *testing.T) {
+	validator, err := openrtb_ext.NewBidderParamsValidator("../../static/bidder-params")
+	if err != nil {
+		t.Fatalf("Failed to fetch the json schema. %v", err)
+	}
+
+	for _, p := range invalidParams {
+		if err := validator.Validate(openrtb_ext.BidderWaardex, json.RawMessage(p)); err == nil {
+			t.Errorf("Schema allowed invalid params: %s", p)
+		}
+	}
+}
+
+var validParams = []string{
+	`{
+		"zoneId": 1
+	}`,
+	`{
+		"zoneId": 12345
+	}`,
+}
+
+var invalidParams = []string{
+	``,
+	`null`,
+	`true`,
+	`5`,
+	`4.2`,
+	`[]`,
+	`{}`,
+	`{
+		"zoneId": 0
+	}`,
+	`{
+		"zoneId": -1
+	}`,
+	`{
+		"zoneId": "1"
+	}`,
+}
