2774221b625c6ac4daafdcfbf7087eaf1aa6f363
Pavel Ptashyts <49400901+pavel-ptashyts@users.noreply.github.com>
2026-01-30 11:27:28 +0100

    Update adapters/elementaltv/params_test.go
    Co-authored-by: linux019 <anode.dev@gmail.com>

--- adapters/elementaltv/params_test.go
+++ adapters/elementaltv/params_test.go
@@ -9,10 +9,7 @@ import (
 
 func TestValidParams(t *testing.T) {
 	validator, err := openrtb_ext.NewBidderParamsValidator("../../static/bidder-params")
-	if err != nil {
-		t.Fatalf("Failed to fetch the json schema: %v", err)
-	}
-
+    require.NoErrorf(t, err,  "Failed to fetch the json schema: %v", err)
 	for _, p := range validParams {
 		if err := validator.Validate(openrtb_ext.BidderElementalTV, json.RawMessage(p)); err != nil {
 			t.Errorf("Schema rejected valid params: %s", p)
