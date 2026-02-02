e68752d6a981a0c5e09f3a4d539d920bff2ca5bc
Tjorven Beckedorf <t.beckedorf@emetriq.com>
2026-02-02 09:09:17 +0100

    T-Advertising Blis Adapter: Add happy path test data

--- adapters/tadvertising_blis/tadvertising_blis_test.go
+++ adapters/tadvertising_blis/tadvertising_blis_test.go
@@ -1,1 +1,20 @@
 package tadvertising_blis
+
+import (
+	"testing"
+
+	"github.com/prebid/prebid-server/v3/adapters/adapterstest"
+	"github.com/prebid/prebid-server/v3/config"
+	"github.com/prebid/prebid-server/v3/openrtb_ext"
+	"github.com/stretchr/testify/require"
+)
+
+func TestJsonSamples(t *testing.T) {
+	bidder, buildErr := Builder(openrtb_ext.BidderTAdvertisingBlis, config.Adapter{
+		Endpoint: "https://example.endpoint/{{.PublisherID}}"},
+		config.Server{ExternalUrl: "http://example.server", GvlID: 1, DataCenter: "2"})
+
+	require.NoError(t, buildErr)
+
+	adapterstest.RunJSONBidderTest(t, "tadvertising_blistest", bidder)
+}
