324f6dc600eebd65a9f4e9aa52c28274ee7c63c5
Predrag.Milosevic <pedjoni@gmail.com>
2026-03-17 21:53:47 +0100

    switch to v4

--- openrtb_ext/imp_targetvideo.go
+++ openrtb_ext/imp_targetvideo.go
@@ -1,6 +1,6 @@
 package openrtb_ext
 
-import "github.com/prebid/prebid-server/v3/util/jsonutil"
+import "github.com/prebid/prebid-server/v4/util/jsonutil"
 
 // ExtImpTargetVideo defines the contract for bidrequest.imp[i].ext.prebid.bidder.targetVideo
 type ExtImpTargetVideo struct {
