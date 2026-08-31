4e1e406bd2a9cc0b1e1b2701580fd8abd0b81569
Dmytro Tananayskiy <dmitry.t@odeeo.io>
2026-08-31 14:02:30 +0200

    New Adapter: Odeeo

---
+++ openrtb_ext/imp_odeeo.go
@@ -0,0 +1,7 @@
+package openrtb_ext
+
+// ImpExtOdeeo defines the contract for bidrequest.imp[i].ext.prebid.bidder.odeeo
+type ImpExtOdeeo struct {
+	SP string `json:"sp"`
+	TK string `json:"tk"`
+}
